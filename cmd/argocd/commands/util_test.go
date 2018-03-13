b276d77c0dc0b3d889d9b02fbfe4807d0750ffac
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-03-13 13:54:10 -0700

    Don't ignore return values

--- cmd/argocd/commands/util_test.go
+++ cmd/argocd/commands/util_test.go
@@ -13,15 +13,15 @@ func TestReadLocalFile(t *testing.T) {
 	if err != nil {
 		t.Errorf("Could not write test data")
 	}
-	defer os.Remove(file.Name())
+	defer func() {
+		_ = os.Remove(file.Name())
+	}()
 
-	file.WriteString(sentinel)
-	file.Sync()
+	_, _ = file.WriteString(sentinel)
+	_ = file.Sync()
 
 	data := readLocalFile(file.Name())
 	if data != sentinel {
 		t.Errorf("Test data did not match! Expected \"%s\" and received \"%s\"", sentinel, data)
 	}
-
-	
 }
