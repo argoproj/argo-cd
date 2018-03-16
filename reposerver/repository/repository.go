253e65d70c9d94b0c3aa5143f8daee584475c92c
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-03-15 19:21:50 -0700

    Update reposerver subpackage

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -78,3 +78,48 @@ func (s *Service) GenerateManifest(c context.Context, q *ManifestRequest) (*Mani
 		Server:    env.Destination.Server,
 	}, nil
 }
+
+// GetEnvParams retrieves Ksonnet environment params in specified repo name and revision
+func (s *Service) GetEnvParams(c context.Context, q *EnvParamsRequest) (*EnvParamsResponse, error) {
+	appRepoPath := path.Join(os.TempDir(), strings.Replace(q.Repo.Repo, "/", "_", -1))
+	s.repoLock.Lock(appRepoPath)
+	defer s.repoLock.Unlock(appRepoPath)
+
+	err := s.gitClient.CloneOrFetch(q.Repo.Repo, q.Repo.Username, q.Repo.Password, q.Repo.SSHPrivateKey, appRepoPath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.gitClient.Checkout(appRepoPath, q.Revision)
+	if err != nil {
+		return nil, err
+	}
+	appPath := path.Join(appRepoPath, q.Path)
+	ksApp, err := ksutil.NewKsonnetApp(appPath)
+	if err != nil {
+		return nil, err
+	}
+	appSpec := ksApp.App()
+	env, err := appSpec.Environment(q.Environment)
+	if err != nil {
+		return nil, fmt.Errorf("environment '%s' does not exist in ksonnet app", q.Environment)
+	}
+
+	targetObjs, err := ksApp.Show(q.Environment)
+	if err != nil {
+		return nil, err
+	}
+	manifests := make([]string, len(targetObjs))
+	for i, target := range targetObjs {
+		manifestStr, err := json.Marshal(target.Object)
+		if err != nil {
+			return nil, err
+		}
+		manifests[i] = string(manifestStr)
+	}
+	return &ManifestResponse{
+		Manifests: manifests,
+		Namespace: env.Destination.Namespace,
+		Server:    env.Destination.Server,
+	}, nil
+}
