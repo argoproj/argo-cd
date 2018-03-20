adb8a2d2a09b004712880203f2fc0fd12b0ba0f8
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-03-19 17:01:11 -0700

    Update secret retrieval

--- util/secrets.go
+++ util/secrets.go
@@ -1,46 +1,60 @@
+package util
 
-import "github.com/argoproj/argo-cd/common"
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/argoproj/argo-cd/common"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func makeKubeclientset() (kubeclientset *kubernetes.Clientset) {
+	kubeclientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("REST config invalid: %s", err)
+	}
+	_, err = kubeclientset.ServerVersion()
+	if err != nil {
+		return fmt.Errorf("REST config invalid: %s", err)
+	}
+	return nil
+}
 
 // CreateSecret stores a new secret in Kubernetes.
-func (s *Server) CreateSecret(name, value, secretType string) (secret *apiv1.Secret, err error) {
+func CreateSecret(namespace, name string, value map[string]string, secretType string) (secret *apiv1.Secret, err error) {
 	newSecret := &apiv1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: secName,
+			Name: name,
 			Labels: map[string]string{
 				common.LabelKeySecretType: secretType,
 			},
 		},
 		StringData: value,
 	}
-	secret, err = s.kubeclientset.CoreV1().Secrets(s.ns).Create(newSecret)
-	if err != nil {
-		secret = nil
-	}
+	secret, err = makeKubeclientset().CoreV1().Secrets(namespace).Create(newSecret)
 	return
 }
 
 // ReadSecret retrieves a secret from Kubernetes.
-func (s *Server) ReadSecret(name string) (secret *apiv1.Secret, err error) {
-	secret, err = s.kubeclientset.CoreV1().Secrets(s.ns).Get(name, metav1.GetOptions{})
-	if err != nil {
-		secret = nil
-	}
+func ReadSecret(namespace, name string) (secret *apiv1.Secret, err error) {
+	secret, err = makeKubeclientset().CoreV1().Secrets(namespace).Get(name, metav1.GetOptions{})
 	return
 }
 
 // UpdateSecret updates an existing secret in Kubernetes.
-func (s *Server) UpdateSecret(name, value string) (secret *apiv1.Secret, err error) {
-	existingSecret = s.ReadSecret(name)
-	existingSecret.StringData = value
-	secret, err = s.kubeclientset.CoreV1().Secrets(s.ns).Update(existingSecret)
+func UpdateSecret(namespace, name string, value map[string]string) (secret *apiv1.Secret, err error) {
+	existingSecret, err := ReadSecret(namespace, name)
 	if err != nil {
-		secret = nil
+		existingSecret.StringData = value
+		secret, err = makeKubeclientset().CoreV1().Secrets(namespace).Update(existingSecret)
 	}
 	return
 }
 
 // DeleteSecret removes a secret from Kubernetes.
-func (s *Server) DeleteSecret(name string) (err error) {
-	err = s.kubeclientset.CoreV1().Secrets(s.ns).Delete(name, &metav1.DeleteOptions{})
+func DeleteSecret(namespace, name string) (err error) {
+	err = makeKubeclientset().CoreV1().Secrets(namespace).Delete(name, &metav1.DeleteOptions{})
 	return
 }
