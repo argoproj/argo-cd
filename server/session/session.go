b3545e7f0880b1ffdcf1da563b1cdd5cf456b5ba
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-04-10 09:14:15 -0700

    Factor out authentication/token gen code

--- server/session/session.go
+++ server/session/session.go
@@ -2,11 +2,9 @@ package session
 
 import (
 	"context"
-	"fmt"
 
 	appclientset "github.com/argoproj/argo-cd/pkg/client/clientset/versioned"
 	"github.com/argoproj/argo-cd/util/config"
-	"github.com/argoproj/argo-cd/util/password"
 	"github.com/argoproj/argo-cd/util/session"
 	"k8s.io/client-go/kubernetes"
 )
@@ -29,39 +27,11 @@ func NewServer(namespace string, kubeclientset kubernetes.Interface, appclientse
 	}
 }
 
-// invalidLoginMessage, for security purposes, doesn't say whether the username or password was invalid.  This does not mitigate the potential for timing attacks to determine which is which.
-const (
-	invalidLoginError  = "Invalid username or password"
-	blankPasswordError = "Blank passwords are not allowed"
-)
-
 // Create a a JWT for authentication.
 func (s *Server) Create(ctx context.Context, q *SessionRequest) (*SessionResponse, error) {
-	if q.Password == "" {
-		err := fmt.Errorf(blankPasswordError)
-		return nil, err
-	}
-
-	passwordHash, ok := s.serversettings.LocalUsers[q.Username]
-	if !ok {
-		// Username was not found in local user store.
-		// Ensure we still send password to hashing algorithm for comparison.
-		// This mitigates potential for timing attacks that benefit from short-circuiting,
-		// provided the hashing library/algorithm in use doesn't itself short-circuit.
-		passwordHash = ""
-	}
-
-	valid, _ := password.VerifyPassword(q.Password, passwordHash)
-	if !valid {
-		err := fmt.Errorf(invalidLoginError)
-		return nil, err
-	}
-
 	sessionManager := session.MakeSessionManager(s.serversettings.ServerSignature)
-	token, err := sessionManager.Create(q.Username)
-	if err != nil {
-		token = ""
-	}
+	token, err := sessionManager.LoginLocalUser(q.Username, q.Password, s.serversettings.LocalUsers)
+
 	return &SessionResponse{token}, err
 }
 
