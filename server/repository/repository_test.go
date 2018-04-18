eb0bd850ccd088e4743cc2d6467a3a34fb8a5d20
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-04-17 17:14:49 -0700

    Lowercase repo names before using in secret

---
+++ server/repository/repository_test.go
@@ -0,0 +1,18 @@
+package repository
+
+import "testing"
+
+func TestRepoURLToSecretName(t *testing.T) {
+	tables := map[string]string{
+		"git://git@github.com:argoproj/ARGO-cd.git": "repo-argo-cd-83273445",
+		"https://github.com/argoproj/ARGO-cd":       "repo-argo-cd-1890113693",
+		"https://github.com/argoproj/argo-cd":       "repo-argo-cd-42374749",
+		"ssh://git@github.com/argoproj/argo-cd.git": "repo-argo-cd-3569564120",
+	}
+
+	for k, v := range tables {
+		if sn := repoURLToSecretName(k); sn != v {
+			t.Errorf("Expected secret name %q for repo %q; instead, got %q", v, k, sn)
+		}
+	}
+}
