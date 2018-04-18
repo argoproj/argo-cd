d1ee6b3adb010aa80c3f2afa5e341e5d304d74af
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-04-18 14:24:43 -0700

    Rename IgnoreResourceDeletionError to Force

--- server/application/application.go
+++ server/application/application.go
@@ -90,19 +90,19 @@ func (s *Server) Delete(ctx context.Context, q *DeleteApplicationRequest) (*Appl
 	namespace := q.Namespace
 	if server == "" || namespace == "" {
 		server, namespace, err = s.getApplicationDestination(ctx, q.Name)
-		if err != nil && !apierr.IsNotFound(err) && !q.IgnoreResourceDeletionError {
+		if err != nil && !apierr.IsNotFound(err) && !q.Force {
 			return nil, err
 		}
 	}
 
 	if server != "" && namespace != "" {
 		clst, err := s.clusterService.Get(ctx, &cluster.ClusterQuery{Server: server})
-		if err != nil && !q.IgnoreResourceDeletionError {
+		if err != nil && !q.Force {
 			return nil, err
 		}
 		config := clst.RESTConfig()
 		err = kube.DeleteResourceWithLabel(config, namespace, common.LabelApplicationName, q.Name)
-		if err != nil && !q.IgnoreResourceDeletionError {
+		if err != nil && !q.Force {
 			return nil, err
 		}
 	}
