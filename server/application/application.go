e719035ea5ba3d08bc4118151989071befb127ac
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2018-04-18 14:27:37 -0700

    Allow ignoring recource deletion related errors while deleting application (#98)
    * Allow ignoring recource deletion related errors while deleting application
    
    * Rename IgnoreResourceDeletionError to Force

--- server/application/application.go
+++ server/application/application.go
@@ -90,19 +90,19 @@ func (s *Server) Delete(ctx context.Context, q *DeleteApplicationRequest) (*Appl
 	namespace := q.Namespace
 	if server == "" || namespace == "" {
 		server, namespace, err = s.getApplicationDestination(ctx, q.Name)
-		if err != nil && !apierr.IsNotFound(err) {
+		if err != nil && !apierr.IsNotFound(err) && !q.Force {
 			return nil, err
 		}
 	}
 
 	if server != "" && namespace != "" {
 		clst, err := s.clusterService.Get(ctx, &cluster.ClusterQuery{Server: server})
-		if err != nil {
+		if err != nil && !q.Force {
 			return nil, err
 		}
 		config := clst.RESTConfig()
 		err = kube.DeleteResourceWithLabel(config, namespace, common.LabelApplicationName, q.Name)
-		if err != nil {
+		if err != nil && !q.Force {
 			return nil, err
 		}
 	}
