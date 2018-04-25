5f9c8b862edbcba5d079621f0c4bba0e942add9b
Jesse Suen <Jesse_Suen@intuit.com>
2018-04-24 23:33:06 -0700

    Error properly when server address is unspecified (resolves #128)

--- pkg/apiclient/apiclient.go
+++ pkg/apiclient/apiclient.go
@@ -96,6 +96,10 @@ func NewClient(opts *ClientOptions) (ServerClient, error) {
 	if opts.ServerAddr != "" {
 		c.ServerAddr = opts.ServerAddr
 	}
+	// Make sure we got the server address and auth token from somewhere
+	if c.ServerAddr == "" {
+		return nil, errors.New("ArgoCD server address unspecified")
+	}
 	if parts := strings.Split(c.ServerAddr, ":"); len(parts) == 1 {
 		// If port is unspecified, assume the most likely port
 		c.ServerAddr += ":443"
@@ -121,11 +125,6 @@ func NewClient(opts *ClientOptions) (ServerClient, error) {
 	}
 	if opts.Insecure {
 		c.Insecure = true
-	}
-
-	// Make sure we got the server address and auth token from somewhere
-	if c.ServerAddr == "" {
-		return nil, errors.New("Argo CD server address unspecified")
 	}
 	return &c, nil
 }
