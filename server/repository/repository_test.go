65ef380233db4271ce5132bc03fd76093ff887a4
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-05-04 17:19:10 -0700

    Add additional repository tests, thanks @jessesuen

--- server/repository/repository_test.go
+++ server/repository/repository_test.go
@@ -7,6 +7,7 @@ func TestRepoURLToSecretName(t *testing.T) {
 		"git://git@github.com:argoproj/ARGO-cd.git": "repo-argo-cd-593837413",
 		"https://github.com/argoproj/ARGO-cd":       "repo-argo-cd-821842295",
 		"https://github.com/argoproj/argo-cd":       "repo-argo-cd-821842295",
+		"https://github.com/argoproj/argo-cd.git":   "repo-argo-cd-821842295",
 		"ssh://git@github.com/argoproj/argo-cd.git": "repo-argo-cd-1019298066",
 	}
 
