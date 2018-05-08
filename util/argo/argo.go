5541940da7c2e34ccf3a602952c621289496061b
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-05-08 13:34:46 -0700

    Rm ResolveServerNamespace function, thanks @jessesuen

--- util/argo/argo.go
+++ util/argo/argo.go
@@ -5,29 +5,12 @@ import (
 	"time"
 
 	"github.com/argoproj/argo-cd/common"
-	appv1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
 	argoappv1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/pkg/client/clientset/versioned/typed/application/v1alpha1"
-	"github.com/argoproj/argo-cd/reposerver/repository"
 	log "github.com/sirupsen/logrus"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 )
-
-// ResolveServerNamespace resolves server and namespace to use given an application spec,
-// and a manifest response. It looks to explicit server/namespace overridden in the app CRD spec
-// and falls back to the server/namespace defined in the ksonnet environment
-func ResolveServerNamespace(destination appv1.ApplicationDestination, manifestInfo *repository.ManifestResponse) (string, string) {
-	server := manifestInfo.Server
-	namespace := manifestInfo.Namespace
-	if destination.Server != "" {
-		server = destination.Server
-	}
-	if destination.Namespace != "" {
-		namespace = destination.Namespace
-	}
-	return server, namespace
-}
 
 // RefreshApp updates the refresh annotation of an application to coerce the controller to process it
 func RefreshApp(appIf v1alpha1.ApplicationInterface, name string) (*argoappv1.Application, error) {
