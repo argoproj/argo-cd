12598303d54bf288d49cb249747d40fad0d8305b
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-05-09 17:30:22 -0700

    Update script with suggestions from @jessesuen

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -2,79 +2,112 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"hash/fnv"
+	"os"
+	"strings"
 
 	argocdclient "github.com/argoproj/argo-cd/pkg/apiclient"
 	"github.com/argoproj/argo-cd/server/application"
 	"github.com/argoproj/argo-cd/server/repository"
 	"github.com/argoproj/argo-cd/util"
+	"github.com/argoproj/argo-cd/util/git"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-
-	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-func renameSecret(clientOpts argocdclient.ClientOptions, oldName, newName string) {
-	clientConfig := &restclient.Config{}
+// origRepoURLToSecretName hashes repo URL to the secret name using a formula.
+// Part of the original repo name is incorporated for debugging purposes
+func origRepoURLToSecretName(repo string) string {
+	repo = git.NormalizeGitURL(repo)
+	h := fnv.New32a()
+	_, _ = h.Write([]byte(repo))
+	parts := strings.Split(strings.TrimSuffix(repo, ".git"), "/")
+	return fmt.Sprintf("repo-%s-%v", strings.ToLower(parts[len(parts)-1]), h.Sum32())
+}
+
+// repoURLToSecretName hashes repo URL to the secret name using a formula.
+// Part of the original repo name is incorporated for debugging purposes
+func repoURLToSecretName(repo string) string {
+	repo = strings.ToLower(git.NormalizeGitURL(repo))
+	h := fnv.New32a()
+	_, _ = h.Write([]byte(repo))
+	parts := strings.Split(strings.TrimSuffix(repo, ".git"), "/")
+	return fmt.Sprintf("repo-%s-%v", parts[len(parts)-1], h.Sum32())
+}
+
+// RenameSecret renames a Kubernetes secret in a given namespace.
+func renameSecret(namespace, oldName, newName string) {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	overrides := clientcmd.ConfigOverrides{}
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &overrides)
+
+	fmt.Printf("*** Renaming secret %q to %q in namespace %q\n", oldName, newName, namespace)
 
 	config, err := clientConfig.ClientConfig()
 	if err != nil {
-		panic(err)
+		fmt.Println("Could not retrieve client config: ", err)
+		return
 	}
-
-	namespace := "default"
-	// namespace, _, err := clientConfig.Namespace()
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	kubeclientset := kubernetes.NewForConfigOrDie(config)
 	repoSecret, err := kubeclientset.CoreV1().Secrets(namespace).Get(oldName, metav1.GetOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Println("Could not retrieve old secret: ", err)
+		return
 	}
 
 	repoSecret.ObjectMeta.Name = newName
+	repoSecret.ObjectMeta.ResourceVersion = ""
 
 	repoSecret, err = kubeclientset.CoreV1().Secrets(namespace).Create(repoSecret)
 	if err != nil {
-		panic(err)
+		fmt.Println("Could not create new secret: ", err)
+		return
 	}
 
 	err = kubeclientset.CoreV1().Secrets(namespace).Delete(oldName, &metav1.DeleteOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Println("Could not remove old secret: ", err)
 	}
 }
 
-func renameSecrets(clientOpts argocdclient.ClientOptions) {
+// RenameRepositorySecrets ensures that repository secrets use the new naming format.
+func renameRepositorySecrets(clientOpts argocdclient.ClientOptions, namespace string) {
 	conn, repoIf := argocdclient.NewClientOrDie(&clientOpts).NewRepoClientOrDie()
 	defer util.Close(conn)
 	repos, err := repoIf.List(context.Background(), &repository.RepoQuery{})
 	if err != nil {
-		panic(err)
+		fmt.Println("An error occurred, so skipping secret renaming: ", err)
+		return
 	}
 
 	for _, repo := range repos.Items {
 		oldSecretName := origRepoURLToSecretName(repo.Repo)
 		newSecretName := repoURLToSecretName(repo.Repo)
-		renameSecret(clientOpts, oldSecretName, newSecretName)
+		renameSecret(namespace, oldSecretName, newSecretName)
 	}
 }
 
-func fillDestinations(clientOpts argocdclient.ClientOptions) {
+// PopulateAppDestinations ensures that apps have a Server and Namespace set explicitly.
+func populateAppDestinations(clientOpts argocdclient.ClientOptions) {
 	conn, appIf := argocdclient.NewClientOrDie(&clientOpts).NewApplicationClientOrDie()
 	defer util.Close(conn)
 	apps, err := appIf.List(context.Background(), &application.ApplicationQuery{})
 	if err != nil {
-		panic(err)
+		fmt.Println("An error occurred, so skipping destination population: ", err)
+		return
 	}
 
 	for _, app := range apps.Items {
+		fmt.Printf("*** Ensuring destination field is populated on app %q\n", app.ObjectMeta.Name)
 		if app.Spec.Destination.Server == "" {
-			app.Spec.Destination.Server = inputString("Server for %s: ", app.Name)
+			app.Spec.Destination.Server = app.Status.ComparisonResult.Server
 		}
 		if app.Spec.Destination.Namespace == "" {
-			app.Spec.Destination.Namespace = inputString("Namespace for %s: ", app.Name)
+			app.Spec.Destination.Namespace = app.Status.ComparisonResult.Namespace
 		}
 
 		_, err = appIf.UpdateSpec(context.Background(), &application.ApplicationSpecRequest{
@@ -82,7 +115,7 @@ func fillDestinations(clientOpts argocdclient.ClientOptions) {
 			Spec:    &app.Spec,
 		})
 		if err != nil {
-			panic(err)
+			fmt.Println("An error occurred (but continuing anyway): ", err)
 		}
 	}
 }
@@ -90,11 +123,10 @@ func fillDestinations(clientOpts argocdclient.ClientOptions) {
 func main() {
 	clientOpts := argocdclient.ClientOptions{
 		ConfigPath: "",
-		ServerAddr: "127.0.0.1:8080",
+		ServerAddr: os.Args[0],
 		Insecure:   false,
 		PlainText:  false,
 	}
-	renameSecret(clientOpts, "mysecret1", "mysecret2")
-	// renameSecrets(clientOpts)
-	// fillDestinations(clientOpts)
+	renameSecrets(clientOpts, namespace)
+	populateAppDestinations(clientOpts)
 }
