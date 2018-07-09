4c8f02e35d16d433f39fa2b659140a9bdc771555
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-07-09 13:40:43 -0700

    Use table instead of white boxes

--- src/app/applications/components/application-conditions/application-conditions.tsx
+++ src/app/applications/components/application-conditions/application-conditions.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 
 import * as models from '../../../shared/models';
+require('./application-conditions.scss');
 
 export const ApplicationConditions = ({conditions}: { conditions: models.ApplicationCondition[]}) => {
     return (
-        <div>
+        <div className='application-conditions'>
             <h4>Application conditions</h4>
             {conditions.length == 0 && (
                 <p>No conditions to report!</p>
             )}
-            {conditions.length > 0 && (
-            <div className='white-box'>
-                <div className='white-box__details'>
-                {conditions.map((condition, index) => (
-                    <div className='row white-box__details-row' key={index}>
+            {conditions.length > 0 && conditions.map((condition, index) => (
+            <div className='argo-table-list application-conditions__condition application-conditions__condition--warning' key={index}>
+                <div className='argo-table-list__row'>
+                    <div className='row'>
                         <div className='columns small-2'>
                             {condition.type}
                         </div>
@@ -21,9 +21,9 @@ export const ApplicationConditions = ({conditions}: { conditions: models.Applica
                             {condition.message}
                         </div>
                     </div>
-                ))}
                 </div>
-            </div>)}
+            </div>
+            ))}
         </div>
     );
 };
