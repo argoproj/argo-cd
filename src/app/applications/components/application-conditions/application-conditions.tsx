afe84768a52958ab59ff2410b2a14d35bca54da5
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-07-10 11:10:38 -0700

    Remove unnecessary left side border on app status panel

--- src/app/applications/components/application-conditions/application-conditions.tsx
+++ src/app/applications/components/application-conditions/application-conditions.tsx
@@ -9,21 +9,22 @@ export const ApplicationConditions = ({conditions}: { conditions: models.Applica
             <h4>Application conditions</h4>
             {conditions.length === 0 && (
                 <p>No conditions to report!</p>
-            )}
-            {conditions.length > 0 && conditions.map((condition, index) => (
-            <div className='argo-table-list application-conditions__condition application-conditions__condition--warning' key={index}>
-                <div className='argo-table-list__row'>
-                    <div className='row'>
-                        <div className='columns small-2'>
-                            {condition.type}
-                        </div>
-                        <div className='columns small-10'>
-                            {condition.message}
+            ) || (
+                <div className='argo-table-list'>
+                {conditions.map((condition, index) => (
+                    <div className='argo-table-list__row application-conditions__condition application-conditions__condition--warning'  key={index}>
+                        <div className='row'>
+                            <div className='columns small-2'>
+                                {condition.type}
+                            </div>
+                            <div className='columns small-10'>
+                                {condition.message}
+                            </div>
                         </div>
                     </div>
+                ))}
                 </div>
-            </div>
-            ))}
+            )}
         </div>
     );
 };
