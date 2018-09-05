c359a24017e79df9d8080b10caff2e67dc11be0b
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2018-09-06 00:37:27 +0300

    Issue 457 - Improve resource diff rendering (#36)

--- src/app/applications/components/application-resource-diff/application-resource-diff.tsx
+++ src/app/applications/components/application-resource-diff/application-resource-diff.tsx
@@ -1,4 +1,4 @@
-import { models } from 'argo-ui';
+import { Checkbox, models } from 'argo-ui';
 import * as React from 'react';
 
 const jsonDiffPatch = require('jsondiffpatch');
@@ -9,10 +9,55 @@ export interface ApplicationComponentDiffProps {
     targetState: models.TypeMeta & { metadata: models.ObjectMeta };
 }
 
-export const ApplicationResourceDiff = (props: ApplicationComponentDiffProps) => {
-    const delta = jsonDiffPatch.diff(props.targetState || {}, props.liveState || {});
-    const html = jsonDiffPatch.formatters.html.format(delta, props.targetState);
-    return (
-        <div className='application-component-diff' dangerouslySetInnerHTML={{__html: html}}/>
-    );
-};
+export class ApplicationResourceDiff extends React.Component<ApplicationComponentDiffProps, { hideDefaultedFields: boolean }> {
+    constructor(props: ApplicationComponentDiffProps) {
+        super(props);
+        this.state = { hideDefaultedFields: true };
+    }
+
+    public render() {
+        let liveState = this.props.liveState || {};
+        if (this.state.hideDefaultedFields) {
+            liveState = this.removeDefaultedFields(this.props.targetState || {}, liveState);
+        }
+        const delta = jsonDiffPatch.diff(this.props.targetState || {}, liveState) || {};
+        const html = jsonDiffPatch.formatters.html.format(delta, this.props.targetState);
+        return (
+            <div className='application-component-diff'>
+                <div className='application-component-diff__checkbox'>
+                    <Checkbox id='hideDefaultedFields' checked={this.state.hideDefaultedFields}
+                            onChange={() => this.setState({ hideDefaultedFields: !this.state.hideDefaultedFields })}/> <label htmlFor='hideDefaultedFields'>
+                        Hide default fields
+                    </label>
+                </div>
+                <div className='application-component-diff__manifest' dangerouslySetInnerHTML={{__html: html}}/>
+            </div>
+        );
+    }
+
+    private removeDefaultedFields(config: any, live: any): any {
+        if (config instanceof Array) {
+            const result = [];
+            for (let i = 0; i < live.length; i++) {
+                const v2 = live[i];
+                if (config.length > i) {
+                    result.push(this.removeDefaultedFields(config[i], v2));
+                } else {
+                    result.push(v2);
+                }
+            }
+            return result;
+        } else if (config instanceof Object) {
+            const result: any = {};
+            for (const k of Object.keys(config)) {
+                const v1 = config[k];
+                if (live.hasOwnProperty(k)) {
+                    const v2 = live[k];
+                    result[k] = this.removeDefaultedFields(v1, v2);
+                }
+            }
+            return result;
+        }
+        return live;
+    }
+}
