f8e392fcf4adbf78d401ebd980d57b0358661330
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-10-05 14:46:57 -0700

    Fix missing formatting option

--- cmd/argocd/commands/app.go
+++ cmd/argocd/commands/app.go
@@ -852,7 +852,7 @@ func NewApplicationSyncCommand(clientOpts *argocdclient.ClientOptions) *cobra.Co
 				for _, r := range *resources {
 					fields := strings.Split(r, resourceFieldDelimiter)
 					if len(fields) != resourceFieldCount {
-						log.Fatalf("Resource should have GROUP%sKIND%sNAME, but instead got: %s", resourceFieldDelimiter, resourceFieldDelimiter)
+						log.Fatalf("Resource should have GROUP%sKIND%sNAME, but instead got: %s", resourceFieldDelimiter, resourceFieldDelimiter, r)
 					}
 					rsrc := argoappv1.SyncOperationResource{
 						Group: fields[0],
