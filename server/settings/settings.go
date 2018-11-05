ad4bb01b71797ac629360d88ce4c9e9e76d8d998
Andrew Merenbach <andrew_merenbach@intuit.com>
2018-11-05 10:38:19 -0800

    Fix namespacing

--- server/settings/settings.go
+++ server/settings/settings.go
@@ -28,8 +28,8 @@ func (s *Server) Get(ctx context.Context, q *SettingsQuery) (*Settings, error) {
 
 	set := Settings{
 		URL: argoCDSettings.URL,
-		RBAC: []rbac.ListOfClaimActions{
-			rbac.ListOfClaimActions{
+		RBAC: []ListOfClaimActions{
+			ListOfClaimActions{
 				Key: rbac.ClaimsResourceApplications,
 				Values: []string{
 					rbac.ClaimsActionCreate,
@@ -39,7 +39,7 @@ func (s *Server) Get(ctx context.Context, q *SettingsQuery) (*Settings, error) {
 					rbac.ClaimsActionUpdate,
 				},
 			},
-			rbac.ListOfClaimActions{
+			ListOfClaimActions{
 				Key: rbac.ClaimsResourceClusters,
 				Values: []string{
 					rbac.ClaimsActionCreate,
@@ -48,7 +48,7 @@ func (s *Server) Get(ctx context.Context, q *SettingsQuery) (*Settings, error) {
 					rbac.ClaimsActionUpdate,
 				},
 			},
-			rbac.ListOfClaimActions{
+			ListOfClaimActions{
 				Key: rbac.ClaimsResourceProjects,
 				Values: []string{
 					rbac.ClaimsActionDelete,
@@ -56,7 +56,7 @@ func (s *Server) Get(ctx context.Context, q *SettingsQuery) (*Settings, error) {
 					rbac.ClaimsActionUpdate,
 				},
 			},
-			rbac.ListOfClaimActions{
+			ListOfClaimActions{
 				Key: rbac.ClaimsResourceRepositories,
 				Values: []string{
 					rbac.ClaimsActionCreate,
