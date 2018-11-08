d565a0a119a2c5da221c5e16952cc2b5460a1879
Jesse Suen <jessesuen@users.noreply.github.com>
2018-11-07 17:21:22 -0800

    Health check is not discerning apiVersion when assessing CRDs (issue #753) (#754)

--- util/health/health_test.go
+++ util/health/health_test.go
@@ -12,74 +12,45 @@ import (
 	"github.com/argoproj/argo-cd/util/kube"
 )
 
+func assertAppHealth(t *testing.T, yamlPath string, expectedStatus appv1.HealthStatusCode) {
+	yamlBytes, err := ioutil.ReadFile(yamlPath)
+	assert.Nil(t, err)
+	var obj unstructured.Unstructured
+	err = yaml.Unmarshal(yamlBytes, &obj)
+	assert.Nil(t, err)
+	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
+	assert.Nil(t, err)
+	assert.NotNil(t, health)
+	assert.Equal(t, expectedStatus, health.Status)
+}
+
 func TestDeploymentHealth(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("../kube/testdata/nginx.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusHealthy, health.Status)
-}
-
-func TestDeploymentProgressing(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("./testdata/progressing.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusProgressing, health.Status)
-}
-
-func TestDeploymentDegraded(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("./testdata/degraded.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusDegraded, health.Status)
+	assertAppHealth(t, "../kube/testdata/nginx.yaml", appv1.HealthStatusHealthy)
+	assertAppHealth(t, "./testdata/deployment-progressing.yaml", appv1.HealthStatusProgressing)
+	assertAppHealth(t, "./testdata/deployment-degraded.yaml", appv1.HealthStatusDegraded)
 }
 
 func TestStatefulSetHealth(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("./testdata/statefulset.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusHealthy, health.Status)
+	assertAppHealth(t, "./testdata/statefulset.yaml", appv1.HealthStatusHealthy)
 }
 
-func TestPvcHealthy(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("./testdata/pvc-bound.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusHealthy, health.Status)
+func TestPVCHealth(t *testing.T) {
+	assertAppHealth(t, "./testdata/pvc-bound.yaml", appv1.HealthStatusHealthy)
+	assertAppHealth(t, "./testdata/pvc-pending.yaml", appv1.HealthStatusProgressing)
 }
 
-func TestPvcPending(t *testing.T) {
-	yamlBytes, err := ioutil.ReadFile("./testdata/pvc-pending.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	health, err := GetAppHealth(kube.KubectlCmd{}, &obj)
-	assert.Nil(t, err)
-	assert.NotNil(t, health)
-	assert.Equal(t, appv1.HealthStatusProgressing, health.Status)
+func TestServiceHealth(t *testing.T) {
+	assertAppHealth(t, "./testdata/svc-clusterip.yaml", appv1.HealthStatusHealthy)
+	assertAppHealth(t, "./testdata/svc-loadbalancer.yaml", appv1.HealthStatusHealthy)
+	assertAppHealth(t, "./testdata/svc-loadbalancer-unassigned.yaml", appv1.HealthStatusProgressing)
+}
+
+func TestIngressHealth(t *testing.T) {
+	assertAppHealth(t, "./testdata/ingress.yaml", appv1.HealthStatusHealthy)
+	assertAppHealth(t, "./testdata/ingress-unassigned.yaml", appv1.HealthStatusProgressing)
+}
+
+func TestCRD(t *testing.T) {
+	// This ensures we do not try to compare only based on "Kind"
+	assertAppHealth(t, "./testdata/knative-service.yaml", appv1.HealthStatusHealthy)
 }
