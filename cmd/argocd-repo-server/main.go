f90a5c4bc1eb0538cb91a267b05b840837d9adbc
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-01-15 15:15:04 -0800

    Support repo server grpc methods throttling

--- cmd/argocd-repo-server/main.go
+++ cmd/argocd-repo-server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -33,6 +35,7 @@ func newCommand() *cobra.Command {
 	var (
 		logLevel               string
 		redisAddress           string
+		parallelismLimit       []string
 		tlsConfigCustomizerSrc func() (tls.ConfigCustomizer, error)
 	)
 	var command = cobra.Command{
@@ -44,7 +47,9 @@ func newCommand() *cobra.Command {
 			tlsConfigCustomizer, err := tlsConfigCustomizerSrc()
 			errors.CheckError(err)
 
-			server, err := reposerver.NewServer(git.NewFactory(), newCache(redisAddress), tlsConfigCustomizer)
+			parallelism, err := parseParallelismLimit(parallelismLimit)
+			errors.CheckError(err)
+			server, err := reposerver.NewServer(git.NewFactory(), newCache(redisAddress), tlsConfigCustomizer, parallelism)
 			errors.CheckError(err)
 			grpc := server.CreateGRPC()
 			listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -66,8 +71,26 @@ func newCommand() *cobra.Command {
 
 	command.Flags().StringVar(&logLevel, "loglevel", "info", "Set the logging level. One of: debug|info|warn|error")
 	command.Flags().StringVar(&redisAddress, "redis", "", "Redis server hostname and port (e.g. argocd-redis:6379). ")
+	command.Flags().StringArrayVar(&parallelismLimit,
+		"parallelism-limit", []string{}, "Sets parallelism limit for grpc method (e.g. /repository.RepositoryService/GenerateManifest=10). ")
 	tlsConfigCustomizerSrc = tls.AddTLSFlagsToCmd(&command)
 	return &command
+}
+
+func parseParallelismLimit(parallelismLimit []string) (map[string]int, error) {
+	parallelism := make(map[string]int)
+	for _, limit := range parallelismLimit {
+		parts := strings.Split(limit, "=")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("Expected parallelism-limit form is: grpc-method-name=number. Received: %s.", limit)
+		}
+		limitNum, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return nil, fmt.Errorf("Unable to convert limit specified in parallelism-limit=%s to number: %v", limit, err)
+		}
+		parallelism[parts[0]] = limitNum
+	}
+	return parallelism, nil
 }
 
 func newCache(redisAddress string) cache.Cache {
