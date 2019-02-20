b92e0a6d0f1a1b6160498c4baccce672a0a1e0fc
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-02-20 12:47:17 -0800

    UI fails to update application which has spec errors and not reconciled by app controller

--- src/app/applications/components/application-details/application-details.tsx
+++ src/app/applications/components/application-details/application-details.tsx
@@ -441,7 +441,8 @@ export class ApplicationDetails extends React.Component<RouteComponentProps<{ na
     private async updateApp(app: appModels.Application) {
         try {
             await services.applications.updateSpec(app.metadata.name, app.spec);
-            const [updatedApp, resources] = await Promise.all([services.applications.get(app.metadata.name), services.applications.resourceTree(app.metadata.name)]);
+            const [updatedApp, resources] = await Promise.all([
+                services.applications.get(app.metadata.name), services.applications.resourceTree(app.metadata.name).catch(() => [])]);
             this.loader.setData({...this.loader.getData() || {}, application: updatedApp, resources: resourcesFromSummaryInfo(app, resources)});
         } catch (e) {
             this.appContext.apis.notifications.show({
