0dea06b801784e97d68dc63fa507538c6a2040f6
Alex Collins <alex_collins@intuit.com>
2019-03-08 16:58:17 -0800

    Linting

--- test/e2e/fixture.go
+++ test/e2e/fixture.go
@@ -15,7 +15,7 @@ import (
 	"testing"
 	"time"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
