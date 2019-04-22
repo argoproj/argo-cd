ed46090ea272d808b3f376e6d153e8fc16eed022
Alex Collins <alex_collins@intuit.com>
2019-04-22 14:58:21 -0700

    test

--- util/settings/settings_test.go
+++ util/settings/settings_test.go
@@ -3,9 +3,8 @@ package settings
 import (
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
-
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
 )
 
 func TestArgoCDSettings_IsExcludedResource(t *testing.T) {
@@ -15,21 +14,55 @@ func TestArgoCDSettings_IsExcludedResource(t *testing.T) {
 	assert.False(t, settings.IsExcludedResource("rubbish.io", "", ""))
 }
 
-func TestUpdateSettingsFromConfigMapResourceExclusions(t *testing.T) {
-
-	settings := ArgoCDSettings{}
-	configMap := v1.ConfigMap{}
-	err := updateSettingsFromConfigMap(&settings, &configMap)
-
-	assert.NoError(t, err)
-	assert.Nil(t, settings.ResourceExclusions)
-
-	configMap.Data = map[string]string{
-		"resource.exclusions": "\n  - apiGroups: []\n    kinds: []\n    clusters: []\n",
+func Test_updateSettingsFromConfigMap(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		value   string
+		wantErr bool
+		get     func(settings ArgoCDSettings) interface{}
+		want    interface{}
+	}{
+		{
+			name:  "TestResourceExclusions",
+			key:   "resource.exclusions",
+			value: "\n  - apiGroups: []\n    kinds: []\n    clusters: []\n",
+			get: func(settings ArgoCDSettings) interface{} {
+				return settings.ResourceExclusions
+			},
+			want: []ExcludedResource{{APIGroups: []string{}, Kinds: []string{}, Clusters: []string{}}},
+		},
+		{
+			name:  "TestRepositories",
+			key:   "repositories",
+			value: "\n  - url: http://foo\n",
+			get: func(settings ArgoCDSettings) interface{} {
+				return settings.Repositories
+			},
+			want: []RepoCredentials{{URL: "http://foo"}},
+		},
+		{
+			name:  "TestRepositoryCredentials",
+			key:   "repository.credentials",
+			value: "\n  - url: http://foo\n",
+			get: func(settings ArgoCDSettings) interface{} {
+				return settings.RepositoryCredentials
+			},
+			want: []RepoCredentials{{URL: "http://foo"}},
+		},
 	}
-
-	err = updateSettingsFromConfigMap(&settings, &configMap)
-
-	assert.NoError(t, err)
-	assert.Equal(t, []ExcludedResource{{APIGroups: []string{}, Kinds: []string{}, Clusters: []string{}}}, settings.ResourceExclusions)
+	for _, tt := range tests {
+		settings := ArgoCDSettings{}
+		configMap := v1.ConfigMap{
+			Data: map[string]string{
+				tt.key: tt.value,
+			},
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			if err := updateSettingsFromConfigMap(&settings, &configMap); (err != nil) != tt.wantErr {
+				t.Errorf("updateSettingsFromConfigMap() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			assert.Equal(t, tt.want, tt.get(settings))
+		})
+	}
 }
