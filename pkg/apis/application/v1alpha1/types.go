ab0d6fec3b364ed2658f143a6bdcd5ca5a419614
Alex Collins <alex_collins@intuit.com>
2019-04-22 11:51:18 -0700

    Allow empty. Close #1504

--- pkg/apis/application/v1alpha1/types.go
+++ pkg/apis/application/v1alpha1/types.go
@@ -131,11 +131,11 @@ func (h *ApplicationSourceHelm) IsZero() bool {
 // ApplicationSourceKustomize holds kustomize specific options
 type ApplicationSourceKustomize struct {
 	// NamePrefix is a prefix appended to resources for kustomize apps
-	NamePrefix string `json:"namePrefix" protobuf:"bytes,1,opt,name=namePrefix"`
+	NamePrefix string `json:"namePrefix,omitempty" protobuf:"bytes,1,opt,name=namePrefix"`
 	// ImageTags are kustomize 1.0 image tag overrides
-	ImageTags []KustomizeImageTag `json:"imageTags" protobuf:"bytes,2,opt,name=imageTags"`
+	ImageTags []KustomizeImageTag `json:"imageTags,omitempty" protobuf:"bytes,2,opt,name=imageTags"`
 	// Images are kustomize 2.0 image overrides
-	Images []string `json:"images" protobuf:"bytes,3,opt,name=images"`
+	Images []string `json:"images,omitempty" protobuf:"bytes,3,opt,name=images"`
 }
 
 // KustomizeImageTag is a kustomize image tag
