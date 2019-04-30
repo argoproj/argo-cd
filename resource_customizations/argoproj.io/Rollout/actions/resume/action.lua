8b15d75de869abb2839ba14bc0cbf750557ba100
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-04-30 11:48:02 -0700

    Rollout health checks/actions should support v0.2 and v0.2+ versions

--- resource_customizations/argoproj.io/Rollout/actions/resume/action.lua
+++ resource_customizations/argoproj.io/Rollout/actions/resume/action.lua
@@ -1,2 +1,9 @@
-obj.spec.paused = false
+if obj.status.verifyingPreview ~= nil and obj.status.verifyingPreview then
+    obj.status.verifyingPreview = false
+end
+
+if obj.spec.paused ~= nil and obj.spec.paused then
+    obj.spec.paused = false
+end
+
 return obj
