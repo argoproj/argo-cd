ca1b4805c95d5f6e91eb9e570fb8c1a553dfd3b2
Danny Thomson <dthomson25@gmail.com>
2019-05-01 15:05:18 -0700

    Improve Rollout health.lua

--- resource_customizations/argoproj.io/Rollout/health.lua
+++ resource_customizations/argoproj.io/Rollout/health.lua
@@ -1,3 +1,55 @@
+function checkReplicasStatus(obj)
+  hs = {}
+  if obj.spec.replicas ~= nil and obj.status.updatedReplicas < obj.spec.replicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: More replicas need to be updated"
+    return hs
+  end
+  if obj.status.replicas > obj.status.updatedReplicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: old replicas are pending termination"
+    return hs
+  end
+  if obj.status.availableReplicas < obj.status.updatedReplicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: updated replicas are still becoming available"
+    return hs
+  end
+  if obj.spec.replicas ~= nil and obj.status.updatedReplicas < obj.spec.replicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: More replicas need to be updated"
+    return hs
+  end
+  if obj.status.replicas > obj.status.updatedReplicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: old replicas are pending termination"
+    return hs
+  end
+  if obj.status.availableReplicas < obj.status.updatedReplicas then
+    hs.status = "Progressing"
+    hs.message = "Waiting for roll out to finish: updated replicas are still becoming available"
+    return hs
+  end
+  return nil
+end
+
+function checkPaused(obj)
+  hs = {}
+  local paused = false
+  if obj.status.verifyingPreview ~= nil then
+    paused = obj.status.verifyingPreview
+  elseif obj.spec.paused ~= nil then
+    paused = obj.spec.paused
+  end
+
+  if paused then
+    hs.status = "Suspended"
+    hs.message = "Rollout is paused"
+    return hs
+  end
+  return nil
+end
+
 hs = {}
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
@@ -7,51 +59,76 @@ if obj.status ~= nil then
         hs.message = condition.message
         return hs
       end
+      if condition.type == "Progressing" and condition.reason == "ProgressDeadlineExceeded" then
+        hs.status = "Degraded"
+        hs.message = condition.message
+        return hs
+      end
     end
   end
   if obj.status.currentPodHash ~= nil then
-    if obj.spec.replicas ~= nil and obj.status.updatedReplicas < obj.spec.replicas then
-      hs.status = "Progressing"
-	  hs.message = "Waiting for roll out to finish: More replicas need to be updated"
-      return hs
-    end
-
-    local paused = false
-    if obj.status.verifyingPreview ~= nil then
-      paused = obj.status.verifyingPreview
-    elseif obj.spec.paused ~= nil then
-      paused = obj.spec.paused
-    end
-
-    if paused then
-      hs.status = "Suspended"
-      if obj.status.blueGreen.previewSelector ~= nil and obj.status.blueGreen.previewSelector == obj.status.currentPodHash then
-        hs.message = "The preview Service is serving traffic to the current pod spec"
-      elseif obj.status.blueGreen.activeSelector ~= nil and obj.status.blueGreen.activeSelector == obj.status.currentPodHash then
-        hs.message = "The active Service is serving traffic to the current pod spec"
+    if obj.spec.strategy.blueGreen ~= nil then
+      isPaused = checkPaused(obj)
+      if isPaused ~= nil then
+        return isPaused
       end
-      return hs
-    end
-
-    if obj.status.replicas > obj.status.updatedReplicas then
+      replicasHS = checkReplicasStatus(obj)
+      if replicasHS ~= nil then
+        return replicasHS
+      end
+      if obj.status.blueGreen.activeSelector ~= nil and obj.status.blueGreen.activeSelector == obj.status.currentPodHash then
+        hs.status = "Healthy"
+        hs.message = "The active Service is serving traffic to the current pod spec"
+        return hs
+      end
       hs.status = "Progressing"
-      hs.message = "Waiting for roll out to finish: old replicas are pending termination"
+      hs.message = "The current pod spec is not receiving traffic from the active service"
       return hs
     end
-    if obj.status.availableReplicas < obj.status.updatedReplicas then
-      hs.status = "Progressing"
-      hs.message = "Waiting for roll out to finish: updated replicas are still becoming available"
-      return hs
-    end
+    if obj.spec.strategy.canary ~= nil then
+      currentRSIsStable = obj.status.canary.stableRS == obj.status.currentPodHash
+      if obj.spec.strategy.canary.steps ~= nil then
+        stepCount = table.getn(obj.spec.strategy.canary.steps)
+        if obj.status.currentStepIndex ~= nil then
+          currentStepIndex = obj.status.currentStepIndex
+          isPaused = checkPaused(obj)
+          if isPaused ~= nil then
+            return isPaused
+          end
+      
+          if paused then
+            hs.status = "Suspended"
+            hs.message = "Rollout is paused"
+            return hs
+          end
+          if currentRSIsStable and stepCount == currentStepIndex then
+            replicasHS = checkReplicasStatus(obj)
+            if replicasHS ~= nil then
+              return replicasHS
+            end
+            hs.status = "Healthy"
+            hs.message = "The rollout has completed all steps"
+            return hs
+          end
+        end
+        hs.status = "Progressing"
+        hs.message = "Waiting for rollout to finish steps"
+        return hs
+      end
 
-    if obj.status.blueGreen.activeSelector ~= nil and obj.status.blueGreen.activeSelector == obj.status.currentPodHash then
-      hs.status = "Healthy"
-      hs.message = "The active Service is serving traffic to the current pod spec"
-      return hs
+      -- The detecting the health of the Canary deployment when there are no steps
+      replicasHS = checkReplicasStatus(obj)
+      if replicasHS ~= nil then
+        return replicasHS
+      end
+      if currentRSIsStable then
+        hs.status = "Healthy"
+        hs.message = "The rollout has completed canary deployment"
+        return hs
+      end
+      hs.status = "Progressing"
+      hs.message = "Waiting for rollout to finish canary deployment"
     end
-    hs.status = "Progressing"
-    hs.message = "The current pod spec is not receiving traffic from the active service"
-    return hs
   end
 end
 hs.status = "Progressing"
