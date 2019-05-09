036b8cf0a593dc605566a2e874be44fc66d19e91
Alex Collins <alex_collins@intuit.com>
2019-05-09 15:12:41 -0700

    fix

--- test/e2e/fixtures/app/actions.go
+++ test/e2e/fixtures/app/actions.go
@@ -15,7 +15,7 @@ type Actions struct {
 func (a *Actions) Create() *Actions {
 
 	if a.context.name == "" {
-		a.context.name = strings.Replace(a.context.path, "/", "-", 0)
+		a.context.name = strings.Replace(a.context.path, "/", "-", -1)
 	}
 
 	args := []string{
