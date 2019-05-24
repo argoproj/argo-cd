9b3ec877209fcb203b6aa8c805c7c53f04aabc9d
Alex Collins <alex_collins@intuit.com>
2019-05-24 15:55:32 -0700

    degraded

--- test/e2e/sync_waves_test.go
+++ test/e2e/sync_waves_test.go
@@ -15,21 +15,37 @@ func TestFixingDegradedApp(t *testing.T) {
 		PatchFile("pod-1.yaml", `[{"op": "replace", "path": "/spec/containers/0/image", "value": "rubbish"}]`).
 		Sync().
 		Then().
+		Expect(OperationPhaseIs(OperationFailed)).
+		Expect(SyncStatusIs(SyncStatusCodeOutOfSync)).
+		Expect(HealthIs(HealthStatusMissing)).
 		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeSynced)).
 		Expect(ResourceHealthIs("Pod", "pod-1", HealthStatusDegraded)).
 		Expect(ResourceSyncStatusIs("Pod", "pod-2", SyncStatusCodeOutOfSync)).
 		Expect(ResourceHealthIs("Pod", "pod-2", HealthStatusMissing)).
-		Expect(SyncStatusIs(SyncStatusCodeOutOfSync)).
-		Expect(HealthIs(HealthStatusMissing)).
 		When().
-		PatchFile("pod-1.yaml", `[{"op": "replace", "path": "/spec/containers/0/image", "value": "true"}]`).
+		PatchFile("pod-1.yaml", `[{"op": "replace", "path": "/spec/containers/0/image", "value": "nginx"}]`).
+		// need to force a refresh here
+		Refresh(RefreshTypeNormal).
+		Then().
+		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeOutOfSync)).
+		When().
 		Sync().
 		Then().
+		Expect(OperationPhaseIs(OperationFailed)).
+		Expect(SyncStatusIs(SyncStatusCodeOutOfSync)).
+		Expect(HealthIs(HealthStatusMissing)).
+		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeSynced)).
+		Expect(ResourceHealthIs("Pod", "pod-1", HealthStatusHealthy)).
+		Expect(ResourceSyncStatusIs("Pod", "pod-2", SyncStatusCodeOutOfSync)).
+		Expect(ResourceHealthIs("Pod", "pod-2", HealthStatusMissing)).
+		When().
+		Sync().
+		Then().
+		Expect(OperationPhaseIs(OperationSucceeded)).
+		Expect(SyncStatusIs(SyncStatusCodeSynced)).
+		Expect(HealthIs(HealthStatusHealthy)).
 		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeSynced)).
 		Expect(ResourceHealthIs("Pod", "pod-1", HealthStatusHealthy)).
 		Expect(ResourceSyncStatusIs("Pod", "pod-2", SyncStatusCodeSynced)).
-		Expect(ResourceHealthIs("Pod", "pod-2", HealthStatusHealthy)).
-		Expect(SyncStatusIs(SyncStatusCodeSynced)).
-		Expect(HealthIs(HealthStatusHealthy)).
-		Expect(OperationPhaseIs(OperationSucceeded))
+		Expect(ResourceHealthIs("Pod", "pod-2", HealthStatusHealthy))
 }
