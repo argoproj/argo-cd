b645589ed597edd17957f87371c909d5717f84de
Tom Wieczorek <t.wieczorek@yieldlab.de>
2019-06-04 16:54:56 +0200

    Add support for adding Kustomize commonLabels in Applications

--- test/e2e/app_management_test.go
+++ test/e2e/app_management_test.go
@@ -373,6 +373,49 @@ func TestKsonnetApp(t *testing.T) {
 		})
 }
 
+func TestKustomize2AppSource(t *testing.T) {
+
+	patchLabelMatchesFor := func(kind string) func(app *Application) {
+		return func(app *Application) {
+			name := "k2-patched-guestbook-ui"
+			labelValue, err := fixture.Run(
+				"", "kubectl", "-n="+fixture.DeploymentNamespace(),
+				"get", kind, name,
+				"-ojsonpath={.metadata.labels.patched-by}")
+			assert.NoError(t, err)
+			assert.Equal(t, "argo-cd", labelValue, "wrong value of 'patched-by' label of %s %s", kind, name)
+		}
+	}
+
+	Given(t).
+		Path(guestbookPath).
+		NamePrefix("k2-").
+		When().
+		Create().
+		Refresh(RefreshTypeHard).
+		PatchApp(`[
+			{
+				"op": "replace",
+				"path": "/spec/source/kustomize/namePrefix",
+				"value": "k2-patched-"
+			},
+			{
+				"op": "add",
+				"path": "/spec/source/kustomize/commonLabels",
+				"value": {
+					"patched-by": "argo-cd"
+				}
+			}
+		]`).
+		Then().
+		Expect(Success("")).
+		When().
+		Sync().
+		Then().
+		And(patchLabelMatchesFor("Service")).
+		And(patchLabelMatchesFor("Deployment"))
+}
+
 const actionsConfig = `discovery.lua: return { sample = {} }
 definitions:
 - name: sample
