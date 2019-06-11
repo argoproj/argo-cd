677c3842cf850f0d8e3dac760e042d76901acfb4
Alex Collins <alex_collins@intuit.com>
2019-06-11 16:00:02 -0700

    ""strconv"" to repos.go

--- test/e2e/fixture/repos/repos.go
+++ test/e2e/fixture/repos/repos.go
@@ -2,7 +2,6 @@ package repos
 
 import (
 	"io/ioutil"
-	"strconv"
 
 	"github.com/argoproj/argo-cd/errors"
 	"github.com/argoproj/argo-cd/test/e2e/fixture"
@@ -15,7 +14,11 @@ func AddSSHRepo() string {
 	r := test_repos.SSHTestRepo
 	errors.FailOnErr(tmpFile.WriteString(r.SSHPrivateKey))
 	errors.CheckError(tmpFile.Close())
-	errors.FailOnErr(fixture.RunCli("repo", "add", r.URL, "--ssh-private-key-path", tmpFile.Name(), "--insecure-ignore-host-key", strconv.FormatBool(r.InsecureIgnoreHostKey)))
+	args :=[]string{"repo", "add", r.URL, "--ssh-private-key-path", tmpFile.Name()}
+	if r.InsecureIgnoreHostKey {
+		args = append(args , "--insecure-ignore-host-key")
+	}
+	errors.FailOnErr(fixture.RunCli(args...))
 	return r.URL
 }
 
