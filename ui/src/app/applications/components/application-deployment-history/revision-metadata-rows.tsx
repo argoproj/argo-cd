ebcd94f2dcefc066cecc0f94c57872902a50f80e
Alex Collins <alex_collins@intuit.com>
2019-06-21 15:44:01 -0700

    "import {DataLoader} from 'argo-ui';" to revision-metadata-rows.tsx

--- ui/src/app/applications/components/application-deployment-history/revision-metadata-rows.tsx
+++ ui/src/app/applications/components/application-deployment-history/revision-metadata-rows.tsx
@@ -1,62 +1,36 @@
+import {DataLoader} from 'argo-ui';
 import * as React from 'react';
 import {Timestamp} from '../../../shared/components/timestamp';
-import * as models from '../../../shared/models';
+import {RevisionMetadata} from '../../../shared/models';
 import {services} from '../../../shared/services';
 
-interface Props {
+export const RevisionMetadataRows = (props: {
     applicationName: string;
     revision: string;
-}
-
-interface State {
-    revisionMetadata?: models.RevisionMetadata;
-    error?: Error;
-}
-
-export class RevisionMetadataRows extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {};
-    }
-
-    public componentDidMount() {
-        services.applications.revisionMetadata(this.props.applicationName, this.props.revision)
-            .then((value) => this.setState({revisionMetadata: value}))
-            .catch((e) => this.setState({error: e}));
-    }
-
-    public render() {
-        const revisionMetadata = this.state.revisionMetadata;
-        if (revisionMetadata) {
-            const author = revisionMetadata.author;
-            const date = revisionMetadata.date;
-            const tags = revisionMetadata.tags;
-            const message = revisionMetadata.message;
-            return (
-                <div>
-                    <div className='row'>
-                        <div className='columns small-3'>Authored by</div>
-                        <div className='columns small-9'>
-                            {author}<br/>
-                            <Timestamp date={date}/>
-                        </div>
-                    </div>
-                    {tags && (
-                        <div className='row'>
-                            <div className='columns small-3'>Tagged</div>
-                            <div className='columns small-9'>{tags.join(', ')}</div>
-                        </div>
-                    )}
-                    <div className='row'>
-                        <div className='columns small-3'/>
-                        <div className='columns small-9'>{message}</div>
+}) => {
+    return (
+        <DataLoader input={props}
+                    load={(input) => services.applications.revisionMetadata(input.applicationName, input.revision || 'HEAD')}
+        >{(m: RevisionMetadata) => (
+            <div>
+                <div className='row'>
+                    <div className='columns small-3'>Authored by</div>
+                    <div className='columns small-9'>
+                        {m.author}<br/>
+                        <Timestamp date={m.date}/>
                     </div>
                 </div>
-            );
-        }
-        if (this.state.error) {
-            return <div>{this.state.error.message}</div>;
-        }
-        return <div>Loading...</div>;
-    }
-}
+                {m.tags && (
+                    <div className='row'>
+                        <div className='columns small-3'>Tagged</div>
+                        <div className='columns small-9'>{m.tags.join(', ')}</div>
+                    </div>
+                )}
+                <div className='row'>
+                    <div className='columns small-3'/>
+                    <div className='columns small-9'>{m.message}</div>
+                </div>
+            </div>
+        )}</DataLoader>
+    );
+};
--- ui/src/app/applications/components/application-status-panel/revision-metadata-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/revision-metadata-panel.tsx
@@ -1,60 +1,30 @@
-import {Tooltip} from 'argo-ui';
+import {DataLoader, Tooltip} from 'argo-ui';
 import * as React from 'react';
 import {Timestamp} from '../../../shared/components/timestamp';
-import * as models from '../../../shared/models';
+import {RevisionMetadata} from '../../../shared/models';
 import {services} from '../../../shared/services';
 
-interface Props {
+export const RevisionMetadataPanel = (props: {
     applicationName: string;
     revision: string;
-}
-
-interface State {
-    revisionMetadata?: models.RevisionMetadata;
-    error?: Error;
-}
-
-export class RevisionMetadataPanel extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {};
-    }
-
-    public componentDidMount() {
-        services.applications.revisionMetadata(this.props.applicationName, this.props.revision || 'HEAD')
-            .then((value) => this.setState({revisionMetadata: value}))
-            .catch((e) => this.setState({error: e}));
-    }
-
-    public render() {
-        const revisionMetadata = this.state.revisionMetadata;
-        if (revisionMetadata) {
-            const author = revisionMetadata.author;
-            const date = revisionMetadata.date;
-            const tags = revisionMetadata.tags;
-            const message = revisionMetadata.message;
-            const tip = (
+}) => {
+    return (
+        <DataLoader input={props}
+                    load={(input) => services.applications.revisionMetadata(input.applicationName, input.revision || 'HEAD')}
+        >{(m: RevisionMetadata) => (
+            <Tooltip content={(
                 <span>
-                    <span>Authored by {author} <Timestamp date={date}/></span><br/>
-                    {tags && (<span>Tags: {tags}<br/></span>)}
-                    <span>{message}</span>
-                </span>
-            );
-
-            return (
-                <Tooltip content={tip} placement='bottom' allowHTML={true}>
-                    <div className='application-status-panel__item-name'>
-                        Authored by {author}<br/>
-                        {tags && <span>Tagged {tags.join(', ')}<br/></span>}
-                        {message}
-                    </div>
-                </Tooltip>
-            );
-        }
-        if (this.state.error) {
-            return <div className='application-status-panel__item-name'>{this.state.error.message}</div>;
-        }
-        return <div className='application-status-panel__item-name'>Loading....</div>;
-
-    }
-}
+            <span>Authored by {m.author} <Timestamp date={m.date}/></span><br/>
+                    {m.tags && (<span>Tags: {m.tags}<br/></span>)}
+                    <span>{m.message}</span>
+        </span>
+            )} placement='bottom' allowHTML={true}>
+                <div className='application-status-panel__item-name'>
+                    Authored by {m.author}<br/>
+                    {m.tags && <span>Tagged {m.tags.join(', ')}<br/></span>}
+                    {m.message}
+                </div>
+            </Tooltip>
+        )}</DataLoader>
+    );
+};
