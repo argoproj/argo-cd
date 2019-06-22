e9c10f5fb06c2b1c7c1781e5067e23b6cea86616
Edwin P Jacques <edwin@shamarsoft.com>
2019-06-21 22:15:49 -0400

    improve readability, and user experience for proj_role_add/remove-policy

--- cmd/argocd/commands/completion.go
+++ cmd/argocd/commands/completion.go
@@ -124,10 +124,18 @@ __argocd_proj_role(){
 
 __argocd_custom_func() {
 	case ${last_command} in
-		argocd_app_delete | argocd_app_diff | argocd_app_edit | \
-		argocd_app_get | argocd_app_history | argocd_app_manifests | \
-		argocd_app_patch-resource | argocd_app_set | argocd_app_sync | \
-		argocd_app_terminate-op | argocd_app_unset | argocd_app_wait | \
+		argocd_app_delete | \
+		argocd_app_diff | \
+		argocd_app_edit | \
+		argocd_app_get | \
+		argocd_app_history | \
+		argocd_app_manifests | \
+		argocd_app_patch-resource | \
+		argocd_app_set | \
+		argocd_app_sync | \
+		argocd_app_terminate-op | \
+		argocd_app_unset | \
+		argocd_app_wait | \
 		argocd_app_create)
 			__argocd_list_apps
 			return
@@ -136,30 +144,44 @@ __argocd_custom_func() {
 			__argocd_app_rollback
 			return
 			;;
-		argocd_cluster_get | argocd_cluster_rm | argocd_login | \
+		argocd_cluster_get | \
+		argocd_cluster_rm | \
+		argocd_login | \
 	    argocd_cluster_add)
 			__argocd_list_servers
 			return
 			;;
-		argocd_repo_rm | argocd_repo_add)
+		argocd_repo_rm | \
+		argocd_repo_add)
 			__argocd_list_repos
 			return
 			;;
-		argocd_proj_add-destination | argocd_proj_remove-destination)
+		argocd_proj_add-destination | \
+		argocd_proj_remove-destination)
 			__argocd_proj_server_namespace
 			return
 			;;
-		argocd_proj_add-source | argocd_proj_remove-source | \
-		argocd_proj_allow-cluster-resource | argocd_proj_allow-namespace-resource | \
-		argocd_proj_deny-cluster-resource | argocd_proj_deny-namespace-resource | \
-		argocd_proj_delete | argocd_proj_edit | argocd_proj_get | argocd_proj_set | \
-	    argocd_proj_role_list | argocd_proj_role_add-policy)
+		argocd_proj_add-source | \
+		argocd_proj_remove-source | \
+		argocd_proj_allow-cluster-resource | \
+		argocd_proj_allow-namespace-resource | \
+		argocd_proj_deny-cluster-resource | \
+		argocd_proj_deny-namespace-resource | \
+		argocd_proj_delete | \
+		argocd_proj_edit | \
+		argocd_proj_get | \
+		argocd_proj_set | \
+	    argocd_proj_role_list)
 			__argocd_list_projects
 			return
 			;;
-		argocd_proj_role_remove-policy | argocd_proj_role_create | \
-		argocd_proj_role_delete | argocd_proj_role_get | \
-		argocd_proj_role_create-token | argocd_proj_role_delete-token)
+		argocd_proj_role_remove-policy | \
+		argocd_proj_role_add-policy | \
+		argocd_proj_role_create | \
+		argocd_proj_role_delete | \
+		argocd_proj_role_get | \
+		argocd_proj_role_create-token | \
+		argocd_proj_role_delete-token)
 			__argocd_proj_role
 			return
 			;;
