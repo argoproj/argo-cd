f641e9b71bb50a820ab1cffdebd7c410a84c0967
naynasiddharth <nayna.siddharth@gmail.com>
2019-06-24 13:13:42 -0700

    Added badges if incorrect query or application name

--- server/badge/badge.go
+++ server/badge/badge.go
@@ -19,20 +19,28 @@ type Handler struct {
 	appClientset versioned.Interface
 }
 
+//Sample url: http://localhost:8080/api/badge?name=123
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	keys, ok := r.URL.Query()["name"]
+
+	keys, ok := r.URL.Query()["job"]
 
 	if !ok || len(keys[0]) < 1 {
-		w.Write([]byte("URL Parameter 'name=(enter application name here)' is missing"))
+		svgOne := svg.New(w)
+		svgOne.Start(2000, 2000)
+		svgOne.Roundrect(0, 0, 120, 25, 2, 3, "fill:rgb(200,321,233);stroke:black;stroke-width:0.7")
+		svgOne.Text(4, 18, "Param 'job=' missing", "font-family:times;font-size:12")
+		svgOne.End()
 		return
 	}
 
 	key := keys[0]
-
-	// Sample url: http://localhost:8080/api/badge?name=123
 	app, err := h.appClientset.ArgoprojV1alpha1().Applications(h.namespace).Get(key, v1.GetOptions{})
 	if err != nil {
-		w.Write([]byte("Sorry, the application whose health and sync status you were looking for does not exist, or was mistyped."))
+		svgTwo := svg.New(w)
+		svgTwo.Start(2000, 2000)
+		svgTwo.Roundrect(0, 0, 114, 25, 2, 3, "fill:rgb(255,43,0);opacity:0.7;stroke:black;stroke-width:0.7")
+		svgTwo.Text(3, 18, "Application not found", "font-family:times;font-size:12")
+		svgTwo.End()
 		return
 	}
 
@@ -42,45 +50,49 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(200)
 	}
-	s := svg.New(w)
-	s.Start(2000, 2000)
 
 	health := app.Status.Health.Status
 	status := app.Status.Sync.Status
-	syncStart := len(health)*6 + 1
+	syncTextStart := len(health) * 6
+
+	svgThree := svg.New(w)
+	svgThree.Start(2000, 2000)
 
 	switch {
 	case health == "Healthy":
-		s.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(127,255,131);stroke:black;stroke-width:1")
-		s.Text(3, 18, "Healthy", "font-size:12;fill:black")
+		svgThree.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(127,255,131);stroke:black;stroke-width:0.7")
+		svgThree.Text(3, 18, "Healthy", "font-family:times;font-size:12;fill:black")
 	case health == "Progressing":
-		s.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,251,92);stroke:black;stroke-width:1")
-		s.Text(3, 18, "Progressing", "font:Helvetica;font-size:12;fill:black")
+		svgThree.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,251,92);stroke:black;stroke-width:0.7")
+		svgThree.Text(3, 18, "Progressing", "font-family:times;font-size:12;fill:black")
 	case health == "Suspended":
-		s.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,145,0);stroke:black;stroke-width:1")
-		s.Text(2, 18, "Suspended", "font-size:12;fill:black")
+		svgThree.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,145,0);stroke:black;stroke-width:0.7")
+		svgThree.Text(2, 18, "Suspended", "font-family:times;font-size:12;fill:black")
 	case health == "Degraded":
-		s.Roundrect(0, 0, len(health)*7-3, 25, 2, 2, "fill:rgb(109,202,205);stroke:black;stroke-width:1")
-		s.Text(2, 18, "Degraded", "font-size:12;fill:black")
-		syncStart = len(health)*7 - 3
+		svgThree.Roundrect(0, 0, len(health)*7-3, 25, 2, 2, "fill:rgb(109,202,205);stroke:black;stroke-width:0.7")
+		svgThree.Text(2, 18, "Degraded", "font-family:times;font-size:12;fill:black")
+		syncTextStart = len(health)*7 - 3
 	case health == "Missing":
-		s.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,36,36);stroke:black;stroke-width:1")
-		s.Text(2, 18, "Missing", "font-size:12;fll:black")
+		svgThree.Roundrect(0, 0, len(health)*6, 25, 2, 2, "fill:rgb(255,36,36);stroke:black;stroke-width:0.7")
+		svgThree.Text(2, 18, "Missing", "font-family:times;font-size:12;fll:black")
 	default:
-		s.Roundrect(0, 0, len("Unknown")*7+2, 25, 2, 2, "fill:rgb(178,102,255);stroke:black;stroke-width:1")
-		s.Text(2, 18, "Unknown", "font-size:12;fill:black")
-		syncStart = len("Unknown")*7 + 2
+		svgThree.Roundrect(0, 0, len("Unknown")*7+2, 25, 2, 2, "fill:rgb(178,102,255);stroke:black;stroke-width:0.7")
+		svgThree.Text(2, 18, "Unknown", "font-family:times;font-size:12;fill:black")
+		syncTextStart = len("Unknown")*7 + 2
 	}
 	switch {
 	case status == "Synced":
-		s.Roundrect(syncStart, 0, len("Synced")*7, 25, 2, 2, "fill:rgb(0,204,0);stroke:black;stroke-width:1")
-		s.Text(syncStart+3, 18, "Synced", "font-size:12;fill:black")
+		svgThree.Roundrect(syncTextStart, 0, len("Synced")*7, 25, 2, 2, "fill:rgb(0,204,0);stroke:black;stroke-width:0.7")
+		svgThree.Text(syncTextStart+3, 18, "Synced", "font-family:times;font-size:12;fill:black")
 	case status == "OutOfSync":
-		s.Roundrect(syncStart, 0, len("Out of Sync")*6, 25, 2, 2, "fill:rgb(255,57,57);stroke:black;stroke-width:1")
-		s.Text(syncStart+3, 18, "Out Of Sync", "font-size:12;fill:black")
+		svgThree.Roundrect(syncTextStart, 0, len("Out of Sync")*6, 25, 2, 2, "fill:rgb(255,57,57);stroke:black;stroke-width:0.7")
+		svgThree.Text(syncTextStart+3, 18, "Out Of Sync", "font-family:times;font-size:12;fill:black")
 	default:
-		s.Roundrect(syncStart, 0, len("Unknown")*8, 25, 2, 2, "fill:rgb(209,155,177);stroke:black;stroke-width:1")
-		s.Text(syncStart+4, 18, "Unknown", "font-size:12;fill:black")
+		svgThree.Roundrect(syncTextStart, 0, len("Unknown")*8, 25, 2, 2, "fill:rgb(209,155,177);stroke:black;stroke-width:0.7")
+		svgThree.Text(syncTextStart+4, 18, "Unknown", "font-familye:times;font-size:12;fill:black")
 	}
-	s.End()
+
+	svgThree.End()
 }
+
+//
