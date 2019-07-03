7d86b51246503f9a082359efffb5649bf8b046f3
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-07-02 19:10:53 -0700

    Issue #1620 - Support anonymous argocd access (#1864)

--- util/settings/settings.go
+++ util/settings/settings.go
@@ -62,6 +62,8 @@ type ArgoCDSettings struct {
 	WebhookGogsSecret string `json:"webhookGogsSecret,omitempty"`
 	// Secrets holds all secrets in argocd-secret as a map[string]string
 	Secrets map[string]string `json:"secrets,omitempty"`
+	// Indicates if anonymous user is enabled or not
+	AnonymousUserEnabled bool
 }
 
 type GoogleAnalytics struct {
@@ -145,6 +147,8 @@ const (
 	resourceInclusionsKey = "resource.inclusions"
 	// configManagementPluginsKey is the key to the list of config management plugins
 	configManagementPluginsKey = "configManagementPlugins"
+	// anonymousUserEnabledKey is the key which enables or disables anonymous user
+	anonymousUserEnabledKey = "users.anonymous.enabled"
 )
 
 // SettingsManager holds config info for a new manager with which to access Kubernetes ConfigMaps.
@@ -448,6 +452,7 @@ func updateSettingsFromConfigMap(settings *ArgoCDSettings, argoCDCM *apiv1.Confi
 	settings.OIDCConfigRAW = argoCDCM.Data[settingsOIDCConfigKey]
 	settings.URL = argoCDCM.Data[settingURLKey]
 	settings.StatusBadgeEnabled = argoCDCM.Data[statusBadgeEnabledKey] == "true"
+	settings.AnonymousUserEnabled = argoCDCM.Data[anonymousUserEnabledKey] == "true"
 	return nil
 }
 
