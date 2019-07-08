90d352d8e336cbee147e2951867857886d503be0
Alex Collins <alex_collins@intuit.com>
2019-07-08 13:11:20 -0700

    "// return a key suitable for use for locking this ..." to client.go

--- util/depot/client.go
+++ util/depot/client.go
@@ -15,15 +15,20 @@ type RevisionMetadata struct {
 type Client interface {
 	// test to see we can connect to the repo, returning an error if we cannot
 	Test() error
-	// return a unique key for the repo server to use for locking
+	// return a key suitable for use for locking this object
 	LockKey() string
 	// clean-up any working directories, connect to repo
 	Init() error
 	// fetch data
 	Fetch() error
+	// checkout a specific directory, the revision maybe empty - in that case assume the latest version
 	Checkout(path, revision string) error
+	// convert an ambiguous revision (e.g. "master" or "HEAD") into a specific revision
 	LsRemote(path, revision string) (string, error)
+	// list files matching the path
 	LsFiles(path string) ([]string, error)
+	// return the revision for the checked out code
 	CommitSHA() (string, error)
+	// return the revision meta-data for the checked out code
 	RevisionMetadata(revision string) (*RevisionMetadata, error)
 }
