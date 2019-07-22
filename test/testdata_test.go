6031d7f830be48ab3f1a2434137d5ed17dd6e961
Alex Collins <alexec@users.noreply.github.com>
2019-07-22 14:52:38 -0700

    Do not ignore Argo hooks when there is a Helm hook. Closes #1952 (#1973)

---
+++ test/testdata_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnnotate(t *testing.T) {
+	obj := Annotate(NewPod(), "foo", "bar")
+	assert.Equal(t, "bar", obj.GetAnnotations()["foo"])
+
+	obj = Annotate(Annotate(NewPod(), "foo", "bar"), "baz", "qux")
+	assert.Equal(t, "bar", obj.GetAnnotations()["foo"])
+	assert.Equal(t, "qux", obj.GetAnnotations()["baz"])
+}
