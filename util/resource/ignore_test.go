d237a080afdae1629721fa278cfd895878a800c3
Alex Collins <alex_collins@intuit.com>
2019-07-22 12:28:20 -0700

    Do not ignore Argo hooks when there is a Helm hook. Closes #1952

--- util/resource/ignore_test.go
+++ util/resource/ignore_test.go
@@ -3,29 +3,28 @@ package resource
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+	"github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/test"
 )
 
 func TestIgnore(t *testing.T) {
-	type args struct {
-		obj *unstructured.Unstructured
-	}
 	tests := []struct {
 		name string
-		args args
+		obj  *unstructured.Unstructured
 		want bool
 	}{
-		{"DoNotIgnoreEmpty", args{test.NewPod()}, false},
-		{"IgnoreHelmHook", args{test.NewHelmHook("pre-install")}, true},
-		{"DoNotIgnoreHelmCrdInstall", args{test.NewHelmHook("crd-install")}, false},
+		{"IgnoreHelmHook", test.HelmHook(test.NewPod(), "pre-install"), true},
+		{"DoNotIgnoreEmpty", test.NewPod(), false},
+		{"DoNotIgnoreHook", test.Hook(test.NewPod(), v1alpha1.HookTypeSync), false},
+		{"DoNotIgnoreHelmCrdInstall", test.HelmHook(test.NewPod(), "crd-install"), false},
+		{"DoNotIgnoreHelmPlusArgoHook", test.HelmHook(test.Hook(test.NewPod(), v1alpha1.HookTypeSync), "pre-install"), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Ignore(tt.args.obj); got != tt.want {
-				t.Errorf("Ignore() = %v, want %v", got, tt.want)
-			}
+			assert.Equal(t, tt.want, Ignore(tt.obj))
 		})
 	}
 }
