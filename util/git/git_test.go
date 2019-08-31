02d82a760e8dcee75bf63df32e7a687b74675a9e
Oscar Gomez <50819676+termux-io@users.noreply.github.com>
2019-08-31 16:38:26 -0700

    Revert "Temporary disable Git LFS test to unblock release (#2172)"
    This reverts commit cdeff93e3a7d970f50d8eacf2f3fbaddd0566a60.

--- util/git/git_test.go
+++ util/git/git_test.go
@@ -209,9 +209,7 @@ func TestLsRemote(t *testing.T) {
 // Running this test requires git-lfs to be installed on your machine.
 func TestLFSClient(t *testing.T) {
 
-	// temporary disable LFS test
-	// TODO(alexmt): dockerize tests in and enabled it
-	t.Skip()
+	test.CIOnly(t)
 
 	tempDir, err := ioutil.TempDir("", "git-client-lfs-test-")
 	assert.NoError(t, err)
