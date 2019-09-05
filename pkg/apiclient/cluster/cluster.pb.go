b37be09d6bdfe8c9c8d0401c66fce53ab6521971
Alex Collins <alexec@users.noreply.github.com>
2019-09-05 15:29:30 -0700

    codegen (#2254)

--- pkg/apiclient/cluster/cluster.pb.go
+++ pkg/apiclient/cluster/cluster.pb.go
@@ -9,18 +9,27 @@ package cluster // import "github.com/argoproj/argo-cd/pkg/apiclient/cluster"
 	Cluster Service API performs CRUD actions against cluster resources
 */
 
-import proto "github.com/gogo/protobuf/proto"
-import fmt "fmt"
-import math "math"
-import v1alpha1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
-import _ "github.com/gogo/protobuf/gogoproto"
-import _ "google.golang.org/genproto/googleapis/api/annotations"
-import _ "k8s.io/api/core/v1"
+import (
+	fmt "fmt"
 
-import context "golang.org/x/net/context"
-import grpc "google.golang.org/grpc"
+	proto "github.com/gogo/protobuf/proto"
 
-import io "io"
+	math "math"
+
+	v1alpha1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
+
+	_ "github.com/gogo/protobuf/gogoproto"
+
+	_ "google.golang.org/genproto/googleapis/api/annotations"
+
+	_ "k8s.io/api/core/v1"
+
+	context "golang.org/x/net/context"
+
+	grpc "google.golang.org/grpc"
+
+	io "io"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
