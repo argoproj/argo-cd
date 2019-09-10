1fb8a17557538d730c0d632000bb85310612705f
Alex Collins <alex_collins@intuit.com>
2019-09-10 13:54:33 -0700

    "func TestClusterGet(t *testing.T) {" to cluster_test.go

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -15,3 +15,17 @@ func TestClusterList(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf(`SERVER                          NAME  VERSION  STATUS      MESSAGE
 https://kubernetes.default.svc        %v     Successful  `, GetVersions().ServerVersion), output)
 }
+
+func TestClusterGet(t *testing.T) {
+	output := FailOnErr(RunCli("cluster", "get", "https://kubernetes.default.svc")).(string)
+	assert.Equal(t, fmt.Sprintf(`config:
+  tlsClientConfig:
+    insecure: false
+connectionState:
+  attemptedAt: null
+  message: ""
+  status: Successful
+name: ""
+server: https://kubernetes.default.svc
+serverVersion: "%v"`, GetVersions().ServerVersion), output)
+}
