3195f085b43e0962155fc7efad87c05446402fca
Alex Collins <alex_collins@intuit.com>
2019-10-07 14:42:59 -0700

    changes

--- hack/install-go-tools.sh
+++ hack/install-go-tools.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eux
 
-GO111MODULE=on go get github.com/emicklei/go-restful@v2.7.0
 GO111MODULE=on go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.19.1
 GO111MODULE=on go get github.com/jstemmer/go-junit-report@v0.0.0-20190106144839-af01ea7f8024
