675026c1e7ce8fb45fadc6b3e1cec89948ea7abb
Alex Collins <alex_collins@intuit.com>
2019-10-07 20:26:29 -0700

    changes

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -10,7 +10,7 @@ go get k8s.io/kube-openapi@v0.0.0-20190502190224-411b2483e503
 PROJECT_ROOT=$(cd $(dirname "$0")/.. ; pwd)
 VERSION="v1alpha1"
 
-go run $GOPATH//pkg/mod/k8s.io/kube-openapi@v0.0.0-20190918143330-0270cf2f1c1d/cmd/openapi-gen/openapi-gen.go \
+go run $GOPATH/pkg/mod/k8s.io/kube-openapi@v0.0.0-20190918143330-0270cf2f1c1d/cmd/openapi-gen/openapi-gen.go \
   --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
   --input-dirs github.com/argoproj/argo-cd/pkg/apis/application/${VERSION} \
   --output-package github.com/argoproj/argo-cd/pkg/apis/application/${VERSION} \
