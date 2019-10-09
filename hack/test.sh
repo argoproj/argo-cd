f37abe11fa450965b9a3fc82f526bd7ad02b4c84
Alex Collins <alex_collins@intuit.com>
2019-10-08 19:27:41 -0700

    changes on lint to hack/test.sh,

--- hack/test.sh
+++ hack/test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -eux -o pipefail
 
-mkdir -p test-results
-
 report() {
   set -eux -o pipefail
 
-  go-junit-report --package-name com.github.argoproj.argo_cd < test-results/test.out > test-results/junit.xml
-  xsltproc junit-noframes.xsl test-results/junit.xml > test-results/test.html
+  go-junit-report --package-name com.github.argoproj.argo_cd < test.out > junit.xml
+  xsltproc junit-noframes.xsl junit.xml > test.html
 }
 
 trap 'report' EXIT
 
-go test -v -covermode=count -coverprofile=coverage.out $* 2>&1 | tee test-results/test.out
+go test -v -covermode=count -coverprofile=coverage.out $* 2>&1 | tee test.out
