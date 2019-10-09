c8537aea54543a9db88e775c91b9634f236473ca
Alex Collins <alex_collins@intuit.com>
2019-10-08 17:21:48 -0700

    changes on lint to hack/installers/install-kubectl.sh,

--- hack/installers/install-kubectl.sh
+++ hack/installers/install-kubectl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux -o pipefail
 
-[ -e $BIN/kubectl ] || curl -sLf -o /tmp/dl/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
+[ -e $BIN/kubectl ] || curl -sLf -o $BIN/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
 chmod +x $BIN/kubectl
