ed703717c659de5d028fc538d961f5eac5f5ec9d
Alex Collins <alex_collins@intuit.com>
2019-10-08 18:18:00 -0700

    changes on lint to .circleci/config.yml,hack/installers/install-jq.sh,

--- hack/installers/install-jq.sh
+++ hack/installers/install-jq.sh
@@ -2,6 +2,6 @@
 set -eux -o pipefail
 
 [ -e $DOWNLOADS/jq ] || curl -sLf --retry 3 -o $DOWNLOADS/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-sudo cp /tmp/jq $BIN/jq
+sudo cp $DOWNLOADS/jq $BIN/jq
 sudo chmod +x $BIN/jq
 jq --version
