c4a85d05f9f19350c89acbb18f03cffb36481902
Alex Collins <alex_collins@intuit.com>
2019-10-08 18:06:41 -0700

    changes on lint to hack/installers/install-jq.sh,hack/installers/install-protoc.sh,hack/installers/install-swagger.sh,

--- hack/installers/install-protoc.sh
+++ hack/installers/install-protoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 5 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
 sudo unzip $DOWNLOADS/protoc.zip bin/protoc -d /usr/local/
 sudo chmod +x /usr/local/bin/protoc
 sudo unzip $DOWNLOADS/protoc.zip include/* -d /usr/local/
