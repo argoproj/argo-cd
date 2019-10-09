d0374bfed35c7e48c6849e560a881e8b3ab6933c
Alex Collins <alex_collins@intuit.com>
2019-10-08 18:35:12 -0700

    changes on lint to .circleci/config.yml,.gitignore,hack/test.sh,

--- hack/test.sh
+++ hack/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eux -o pipefail
 
+mkdir -p tests-results
+
 report() {
   set -eux -o pipefail
 
-  go-junit-report --package-name com.github.argoproj.argo_cd < test.out > junit.xml
-  xsltproc junit-noframes.xsl junit.xml > test.html
+  go-junit-report --package-name com.github.argoproj.argo_cd < tests-results/test.out > tests-results/junit.xml
+  xsltproc junit-noframes.xsl tests-results/junit.xml > tests-results/test.html
 }
 
 trap 'report' EXIT
 
-go test -v -covermode=count -coverprofile=coverage.out $* 2>&1 | tee test.out
+go test -v -covermode=count -coverprofile=coverage.out $* 2>&1 | tee tests-results/test.out
