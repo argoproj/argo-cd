62cc46491ca00e6d6b7295483f026f07580b0a9c
Alex Collins <alex_collins@intuit.com>
2019-10-08 18:04:45 -0700

    changes on lint to .circleci/config.yml,hack/installers/install-protoc.sh,hack/installers/install-swagger.sh,

--- hack/installers/install-protoc.sh
+++ hack/installers/install-protoc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
-curl -sLf -C - -o /tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
-unzip /tmp/protoc.zip bin/protoc -d /usr/local/
+[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 5 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+sudo unzip $DOWNLOADS/protoc.zip bin/protoc -d /usr/local/
 sudo chmod +x /usr/local/bin/protoc
-sudo unzip /tmp/protoc.zip include/* -d /usr/local/
+sudo unzip $DOWNLOADS/protoc.zip include/* -d /usr/local/
 protoc --version
