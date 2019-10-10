f8fc2ef7fed715896dcbc50a90d3342021845832
Alex Collins <alex.e.c@gmail.com>
2019-10-09 22:56:30 -0700

    k3

--- hack/installers/install-kustomize-linux.sh
+++ hack/installers/install-kustomize-linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-export VER=2.0.3
-[ -e $DOWNLOADS/kustomize_${VER} ] || curl -sLf --retry 3 -o $DOWNLOADS/kustomize_${VER} https://github.com/kubernetes-sigs/kustomize/releases/download/v${VER}/kustomize_${VER}_linux_amd64
-cp $DOWNLOADS/kustomize_${VER} $BIN/kustomize
+[ -e $DOWNLOADS/kustomize ] || curl -sLf --retry 3 -o $DOWNLOADS/kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v3.1.0/kustomize_3.1.0_linux_amd64
+cp $DOWNLOADS/kustomize $BIN/kustomize
 chmod +x $BIN/kustomize
 kustomize version
