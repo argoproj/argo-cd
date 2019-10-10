fd55e42baffc6995a49378fd4ccf9a141a67f917
Alex Collins <alex_collins@intuit.com>
2019-10-09 19:33:16 -0700

    changes on tweak to .circleci/config.yml,hack/installers/install-swagger-linux.sh,

--- hack/installers/install-swagger-linux.sh
+++ hack/installers/install-swagger-linux.sh
@@ -2,6 +2,6 @@
 set -eux -o pipefail
 
 [ -e $DOWNLOADS/swagger ] || curl -sLf --retry 3 -o $DOWNLOADS/swagger https://github.com/go-swagger/go-swagger/releases/download/v0.19.0/swagger_linux_amd64
-cp $DOWNLOADS/swagger /usr/local/bin/swagger
-chmod +x /usr/local/bin/swagger
+cp $DOWNLOADS/swagger $BIN/swagger
+chmod +x $BIN/swagger
 swagger version
