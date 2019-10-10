3f8ad20a74a703a040aaeab8bf42dcf4ba991b1e
Alex Collins <alex_collins@intuit.com>
2019-10-09 19:51:11 -0700

    changes on tweak to hack/installers/install-kustomize-linux.sh,

--- hack/installers/install-kustomize-linux.sh
+++ hack/installers/install-kustomize-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-export VER=3.1.0
+export VER=2.0.3
 [ -e $DOWNLOADS/kustomize_${VER} ] || curl -sLf --retry 3 -o $DOWNLOADS/kustomize_${VER} https://github.com/kubernetes-sigs/kustomize/releases/download/v${VER}/kustomize_${VER}_linux_amd64
 cp $DOWNLOADS/kustomize_${VER} $BIN/kustomize
 chmod +x $BIN/kustomize
