95b3a03193a25b1fe14ed778bf7f27400440b338
Alex Collins <alex_collins@intuit.com>
2019-10-28 13:52:38 -0700

    changes on no-prune to

--- controller/sync.go
+++ controller/sync.go
@@ -558,7 +558,7 @@ func (sc *syncContext) applyObject(targetObj *unstructured.Unstructured, dryRun 
 func (sc *syncContext) pruneObject(liveObj *unstructured.Unstructured, prune, dryRun bool) (v1alpha1.ResultCode, string) {
 	if !prune {
 		return v1alpha1.ResultCodePruneSkipped, "ignored (requires pruning)"
-	} else if resource.NoPrune(liveObj) {
+	} else if resource.HasAnnotationOption(liveObj, common.AnnotationSyncOptions, "Prune=false") {
 		return v1alpha1.ResultCodePruneSkipped, "ignored (no prune)"
 	} else {
 		if dryRun {
