43d9d8a477d03ffaa7b9ea9b0193edbea5843b75
Gustav Paul <gpaul@mesosphere.io>
2019-10-28 15:08:09 +0200

    tools.go: pin dependency on tool github.com/gogo/protobuf/protoc-gen-gogofast

--- tools.go
+++ tools.go
@@ -6,5 +6,5 @@ import (
 	_ "github.com/gobuffalo/packr/packr"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo"
+	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 )
-
