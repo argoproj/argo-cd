4431814227d774624c438896bdc656236e647ded
Alex Collins <alex_collins@intuit.com>
2019-10-31 17:26:40 -0700

    changes on no-validate to assets/swagger.json,manifests/crds/application-crd.yaml,manifests/ha/install.yaml,manifests/ha/namespace-install.yaml,manifests/install.yaml,manifests/namespace-install.yaml,pkg/apis/application/v1alpha1/generated.pb.go,pkg/apis/application/v1alpha1/generated.proto,pkg/apis/application/v1alpha1/openapi_generated.go,

--- pkg/apis/application/v1alpha1/openapi_generated.go
+++ pkg/apis/application/v1alpha1/openapi_generated.go
@@ -2883,7 +2883,7 @@ func schema_pkg_apis_application_v1alpha1_SyncOperation(ref common.ReferenceCall
 					},
 					"syncOptions": {
 						SchemaProps: spec.SchemaProps{
-							Description: "SyncOptions provide per-sync sync-options",
+							Description: "SyncOptions provide per-sync sync-options, e.g. Validate=false",
 							Type:        []string{"array"},
 							Items: &spec.SchemaOrArray{
 								Schema: &spec.Schema{
