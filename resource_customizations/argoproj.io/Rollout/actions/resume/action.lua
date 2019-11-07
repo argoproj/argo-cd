7982a1996685f8c6260635037442c90760b65a60
dthomson25 <dthomson25@gmail.com>
2019-11-07 15:08:32 -0800

    Add AnalysisRun and Experiment HealthCheck (#2579)

--- resource_customizations/argoproj.io/Rollout/actions/resume/action.lua
+++ resource_customizations/argoproj.io/Rollout/actions/resume/action.lua
@@ -1,5 +1,5 @@
-if obj.status.verifyingPreview ~= nil and obj.status.verifyingPreview then
-    obj.status.verifyingPreview = false
+if obj.status.pauseConditions ~= nil and table.getn(obj.status.pauseConditions) > 0 then
+    obj.status.pauseConditions = nil
 end
 
 if obj.spec.paused ~= nil and obj.spec.paused then
