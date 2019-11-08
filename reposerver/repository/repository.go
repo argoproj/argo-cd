786a94b03bab5f5177d4038e0209744bd4760d0c
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-11-07 18:28:13 -0800

    Issue #2559 - Add gauge Prometheus metric which represents the number of pending manifest requests. (#2658)

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -132,6 +132,9 @@ func (s *Service) runRepoOperation(
 		return nil
 	}
 
+	s.metricsServer.IncPendingRepoRequest(repo.Repo)
+	defer s.metricsServer.DecPendingRepoRequest(repo.Repo)
+
 	if settings.sem != nil {
 		err = settings.sem.Acquire(c, 1)
 		if err != nil {
