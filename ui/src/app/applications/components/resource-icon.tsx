6d65d01757bbc6dce6f40319a295eb7416cadef2
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2020-01-14 14:13:00 -0800

    fix: fix rendering CRD acronym (#2978)

--- ui/src/app/applications/components/resource-icon.tsx
+++ ui/src/app/applications/components/resource-icon.tsx
@@ -11,22 +11,20 @@ export const ResourceIcon = ({kind}: {kind: string}) => {
     }
     const initials = kind.replace(/[a-z]/g, '');
     const n = initials.length;
+    const style: React.CSSProperties = {
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        padding: '2px 4px',
+        width: '32px',
+        height: '32px',
+        borderRadius: '50%',
+        backgroundColor: '#8FA4B1',
+        textAlign: 'center',
+        lineHeight: '30px'
+    };
     return (
-        <div style={{display: 'inline-block', padding: '2px 4px', width: '40px', height: '32px'}}>
-            <i title={kind} className='icon fa fa-circle' />
-            <div
-                style={{
-                    position: 'absolute',
-                    left: '10px',
-                    top: `${n <= 2 ? 10 : 14}px`,
-                    width: '40px',
-                    margin: 'auto',
-                    color: 'white',
-                    textAlign: 'center',
-                    fontSize: `${n <= 2 ? 1 : 0.6}em`
-                }}>
-                {initials}
-            </div>
+        <div style={style}>
+            <span style={{color: 'white', fontSize: `${n <= 2 ? 1 : 0.6}em`}}>{initials}</span>
         </div>
     );
 };
