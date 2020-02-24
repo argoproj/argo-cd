fc6be541fad7007814c91c943e76b19597be67a5
Shelby Moore <shelbylmoore@gmail.com>
2020-02-24 15:43:05 -0800

    fix: regenerate manifests, revert removal of failover cache client

--- util/cache/cache.go
+++ util/cache/cache.go
@@ -23,14 +23,27 @@ func NewCache(client CacheClient) *Cache {
 // AddCacheFlagsToCmd adds flags which control caching to the specified command
 func AddCacheFlagsToCmd(cmd *cobra.Command) func() (*Cache, error) {
 	redisAddress := ""
+	sentinelAddresses := make([]string, 0)
+	sentinelMaster := ""
 	redisDB := 0
 	var defaultCacheExpiration time.Duration
 
 	cmd.Flags().StringVar(&redisAddress, "redis", "", "Redis server hostname and port (e.g. argocd-redis:6379). ")
 	cmd.Flags().IntVar(&redisDB, "redisdb", 0, "Redis database.")
+	cmd.Flags().StringArrayVar(&sentinelAddresses, "sentinel", []string{}, "Redis sentinel hostname and port (e.g. argocd-redis-ha-announce-0:6379). ")
+	cmd.Flags().StringVar(&sentinelMaster, "sentinelmaster", "master", "Redis sentinel master group name.")
 	cmd.Flags().DurationVar(&defaultCacheExpiration, "default-cache-expiration", 24*time.Hour, "Cache expiration default")
 	return func() (*Cache, error) {
 		password := os.Getenv(envRedisPassword)
+		if len(sentinelAddresses) > 0 {
+			client := redis.NewFailoverClient(&redis.FailoverOptions{
+				MasterName:    sentinelMaster,
+				SentinelAddrs: sentinelAddresses,
+				DB:            redisDB,
+				Password:      password,
+			})
+			return NewCache(NewRedisCache(client, defaultCacheExpiration)), nil
+		}
 
 		if redisAddress == "" {
 			redisAddress = common.DefaultRedisAddr
