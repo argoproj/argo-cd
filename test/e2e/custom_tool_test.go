e8beb2258e10289a166c46013adbc0f39a391067
Zhang <may_zhang@intuit.com>
2020-03-30 17:28:20 -0700

    Using assert.EqualValues for assertion.

--- test/e2e/custom_tool_test.go
+++ test/e2e/custom_tool_test.go
@@ -155,25 +155,6 @@ func TestCustomToolWithEnv(t *testing.T) {
 			outputSlice := strings.Split(output, ",")
 			sort.Strings(outputSlice)
 
-			assert.True(t, testEq(expectedApiVersionSlice, outputSlice))
+			assert.EqualValues(t, expectedApiVersionSlice, outputSlice)
 		})
-}
-
-func testEq(a, b []string) bool {
-	// If one is nil, the other must also be nil.
-	if (a == nil) != (b == nil) {
-		return false
-	}
-
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
 }
