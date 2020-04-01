b2dcfb81363aab957616da9732a37aabc19ce2fe
Zhang <may_zhang@intuit.com>
2020-03-31 19:50:14 -0700

    Fixing codegen error

--- pkg/apiclient/repository/repository.pb.go
+++ pkg/apiclient/repository/repository.pb.go
@@ -10,6 +10,10 @@ package repository
 import (
 	context "context"
 	fmt "fmt"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+
 	v1alpha1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
 	apiclient "github.com/argoproj/argo-cd/reposerver/apiclient"
 	_ "github.com/gogo/protobuf/gogoproto"
@@ -18,10 +22,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	io "io"
 	_ "k8s.io/api/core/v1"
-	math "math"
-	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -587,7 +588,9 @@ func init() {
 	proto.RegisterType((*RepoUpdateRequest)(nil), "repository.RepoUpdateRequest")
 }
 
-func init() { proto.RegisterFile("server/repository/repository.proto", fileDescriptor_8d38260443475705) }
+func init() {
+	proto.RegisterFile("server/repository/repository.proto", fileDescriptor_8d38260443475705)
+}
 
 var fileDescriptor_8d38260443475705 = []byte{
 	// 935 bytes of a gzipped FileDescriptorProto
