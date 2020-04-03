55bc1444102074dd09bf413d5827d7b3d6bf3011
May Zhang <may_zhang@intuit.com>
2020-04-03 13:41:17 -0700

    fix: Helm v3 CRD are not deployed (#3345)
    * Fixing could not find plugin issue when app sync and app diff
    
    * Fixing codegen error
    
    * Revert "Fixing codegen error"
    
    This reverts commit b2dcfb81
    
    * Fixing codegen error
    
    * If user is logged in, settings API would return ConfigManagementPlugins
    
    * For helm3, add flag --include-crds when calling helm template to support helm3 crd
    
    * Fixing typo.
    
    * Added further assertion of ResourceSyncStatusIs for CRD resources.

--- util/helm/cmd.go
+++ util/helm/cmd.go
@@ -221,6 +221,9 @@ func (c *Cmd) template(chartPath string, opts *TemplateOpts) (string, error) {
 	for _, v := range opts.APIVersions {
 		args = append(args, "--api-versions", v)
 	}
+	if c.HelmVer.additionalTemplateArgs != nil {
+		args = append(args, c.HelmVer.additionalTemplateArgs...)
+	}
 
 	return c.run(args...)
 }
