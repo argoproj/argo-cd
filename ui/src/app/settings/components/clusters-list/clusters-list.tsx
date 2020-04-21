d0aec6f520702ee81c42515bffdb580ded1f4043
Alexander Matyushentsev <amatyushentsev@gmail.com>
2020-04-21 09:45:12 -0700

    fix: javascript error on accounts list page

--- ui/src/app/settings/components/clusters-list/clusters-list.tsx
+++ ui/src/app/settings/components/clusters-list/clusters-list.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {clusterName, ConnectionStateIcon, DataLoader, Page} from '../../../shared/components';
+import {clusterName, ConnectionStateIcon, DataLoader, EmptyState, Page} from '../../../shared/components';
 
 import * as models from '../../../shared/models';
 import {services} from '../../../shared/services';
@@ -10,7 +10,7 @@ export const ClustersList = () => (
             <div className='argo-container'>
                 <DataLoader load={() => services.clusters.list().then(clusters => clusters.sort((first, second) => first.name.localeCompare(second.name)))}>
                     {(clusters: models.Cluster[]) =>
-                        clusters.length > 0 && (
+                        (clusters.length > 0 && (
                             <div className='argo-table-list'>
                                 <div className='argo-table-list__head'>
                                     <div className='row'>
@@ -35,6 +35,11 @@ export const ClustersList = () => (
                                     </div>
                                 ))}
                             </div>
+                        )) || (
+                            <EmptyState icon='argo-icon-hosts'>
+                                <h4>No clusters connected</h4>
+                                <h5>Connect more clusters using argocd CLI</h5>
+                            </EmptyState>
                         )
                     }
                 </DataLoader>
