bc398ddc21f90824ccee62c756bed77abb97c6fa
rachelwang20 <62673041+rachelwang20@users.noreply.github.com>
2020-05-08 12:59:41 -0700

    Update application-deployment-history.tsx

--- ui/src/app/applications/components/application-deployment-history/application-deployment-history.tsx
+++ ui/src/app/applications/components/application-deployment-history/application-deployment-history.tsx
@@ -33,11 +33,14 @@ export const ApplicationDeploymentHistory = ({
                 <div className='row application-deployment-history__item' key={info.deployedAt} onClick={() => selectDeployment(index)}>
                     <div className='columns small-3'>
                         <div>
-                            <i className='fa fa-clock' /> Deployed At:<br/>
+                            <i className='fa fa-clock' /> Deployed At:
+                            <br/>
                             <Timestamp date={info.deployedAt} />
                         </div>
                         <div>
-                            <br/>Active for:<br/>
+                            <br/>
+                            Active for:
+                            <br/>
                             <Duration durationMs={info.durationMs} />
                         </div>
                     </div>
