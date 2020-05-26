b18a357f8611bff8edbde4842c36bfafca8c31e8
Josh Soref <jsoref@users.noreply.github.com>
2020-05-25 22:48:28 -0400

    spelling: command

--- hack/git-ask-pass.sh
+++ hack/git-ask-pass.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This script is used as the commaned supplied to GIT_ASKPASS as a way to supply username/password
+# This script is used as the command supplied to GIT_ASKPASS as a way to supply username/password
 # credentials to git, without having to use git credentials helpers, or having on-disk config.
 case "$1" in
 Username*) echo "${GIT_USERNAME}" ;;
