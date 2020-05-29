8db2bc0b5dd8a015f96c04dbf727e5d8a312daba
Aaron Globerman <agloberman@ea.com>
2020-05-28 21:50:42 -0700

    gofmt test updates

--- cmd/argocd/commands/cluster_test.go
+++ cmd/argocd/commands/cluster_test.go
@@ -34,30 +34,30 @@ func Test_printClusterTable(t *testing.T) {
 }
 
 func Test_newCluster(t *testing.T) {
-	clusterWithData := newCluster("test-cluster", []string{"test-namespace"},&rest.Config{
+	clusterWithData := newCluster("test-cluster", []string{"test-namespace"}, &rest.Config{
 		TLSClientConfig: rest.TLSClientConfig{
-			Insecure: false,
+			Insecure:   false,
 			ServerName: "test-endpoint.example.com",
-			CAData: []byte("test-ca-data"),
-			CertData: []byte("test-cert-data"),
-			KeyData: []byte("test-key-data"),
+			CAData:     []byte("test-ca-data"),
+			CertData:   []byte("test-cert-data"),
+			KeyData:    []byte("test-key-data"),
 		},
 		Host: "test-endpoint.example.com",
 	},
-	"test-bearer-token",
-	&v1alpha1.AWSAuthConfig{})
+		"test-bearer-token",
+		&v1alpha1.AWSAuthConfig{})
 
 	assert.Equal(t, "test-cert-data", string(clusterWithData.Config.CertData))
 	assert.Equal(t, "test-key-data", string(clusterWithData.Config.KeyData))
 	assert.Equal(t, "", clusterWithData.Config.BearerToken)
 
-	clusterWithFiles := newCluster("test-cluster", []string{"test-namespace"},&rest.Config{
+	clusterWithFiles := newCluster("test-cluster", []string{"test-namespace"}, &rest.Config{
 		TLSClientConfig: rest.TLSClientConfig{
-			Insecure: false,
+			Insecure:   false,
 			ServerName: "test-endpoint.example.com",
-			CAData: []byte("test-ca-data"),
-			CertFile: "./testdata/test.cert.pem",
-			KeyFile: "./testdata/test.key.pem",
+			CAData:     []byte("test-ca-data"),
+			CertFile:   "./testdata/test.cert.pem",
+			KeyFile:    "./testdata/test.key.pem",
 		},
 		Host: "test-endpoint.example.com",
 	},
@@ -68,11 +68,11 @@ func Test_newCluster(t *testing.T) {
 	assert.True(t, strings.Contains(string(clusterWithFiles.Config.KeyData), "test-key-data"))
 	assert.Equal(t, "", clusterWithFiles.Config.BearerToken)
 
-	clusterWithBearerToken := newCluster("test-cluster", []string{"test-namespace"},&rest.Config{
+	clusterWithBearerToken := newCluster("test-cluster", []string{"test-namespace"}, &rest.Config{
 		TLSClientConfig: rest.TLSClientConfig{
-			Insecure: false,
+			Insecure:   false,
 			ServerName: "test-endpoint.example.com",
-			CAData: []byte("test-ca-data"),
+			CAData:     []byte("test-ca-data"),
 		},
 		Host: "test-endpoint.example.com",
 	},
@@ -81,5 +81,3 @@ func Test_newCluster(t *testing.T) {
 
 	assert.Equal(t, "test-bearer-token", clusterWithBearerToken.Config.BearerToken)
 }
-
-
