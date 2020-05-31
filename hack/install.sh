00f99edf1a3ec234bccd7d7ad369647a24aea3d4
Alin Balutoiu <KeopsAlin@Gmail.com>
2020-05-31 19:31:29 +0200

    feat: Add build support for ARM images (#3554)
    Signed-off-by: Alin Balutoiu <alinbalutoiu@gmail.com>

--- hack/install.sh
+++ hack/install.sh
@@ -6,6 +6,17 @@ export BIN=${BIN:-/usr/local/bin}
 
 mkdir -p $DOWNLOADS
 
+ARCHITECTURE=""
+case $(uname -m) in
+    x86_64)                     ARCHITECTURE="amd64" ;;
+    arm|armv7l|armv8l|aarch64)  dpkg --print-architecture | grep -q "arm64" && ARCHITECTURE="arm64" || ARCHITECTURE="arm" ;;
+esac
+
+if [ -z "$ARCHITECTURE" ]; then
+      echo "Could not detect the architecture of the system"
+      exit 1
+fi
+
 for product in $*; do
-  "$(dirname $0)/installers/install-${product}.sh"
+  ARCHITECTURE=$ARCHITECTURE "$(dirname $0)/installers/install-${product}.sh"
 done
