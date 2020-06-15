60957a89d652551c360cefbf8218cb1978d4a4da
jannfis <jann@mistrust.net>
2020-06-15 17:24:56 +0200

    Support GPG verification of git annotated tags as well

--- hack/git-verify-wrapper.sh
+++ hack/git-verify-wrapper.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
-# Simple wrapper around gpg verify-commit so we can capture stderr output
-COMMITS="$*"
-IFS=''
-OUTPUT=$(git verify-commit $COMMITS 2>&1)
-RET=$?
+# Wrapper script to perform GPG signature validation on git commit SHAs and
+# annotated tags.
+#
+# We capture stderr to stdout, so we can have the output in the logs. Also,
+# we ignore error codes that are emitted if signature verification failed.
+#
+if test "$1" = ""; then
+	echo "Wrong usage of git-verify-wrapper.sh" >&2
+	exit 1
+fi
+
+REVISION="$1"
+
+# Figure out we have an annotated tag or a commit SHA
+if git describe "${REVISION}" >/dev/null 2>&1; then
+	IFS=''
+	OUTPUT=$(git verify-tag "$REVISION" 2>&1)
+	RET=$?
+else
+	IFS=''
+	OUTPUT=$(git verify-commit "$REVISION" 2>&1)
+	RET=$?
+fi
+
 case "$RET" in
 0)
 	echo $OUTPUT
