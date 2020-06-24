28942ca09cbb8f6631dd79bf96965701deec8a30
Balazs Zachar <balazs.zachar@ctbto.org>
2020-06-24 19:58:36 +0200

    add version and app labels for deployments and pods plus rename service ports to make argocd manifest Istio compatible

--- hack/update-manifests.sh
+++ hack/update-manifests.sh
@@ -13,13 +13,14 @@ cd ${SRCROOT}/manifests/ha/base/redis-ha && ./generate.sh
 
 IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-argoproj}"
 IMAGE_TAG="${IMAGE_TAG:-}"
+ARGOCD_VERSION_LABEL=v$(cat $SRCROOT/VERSION)
 
 # if the tag has not been declared, and we are on a release branch, use the VERSION file.
 if [ "$IMAGE_TAG" = "" ]; then
   branch=$(git rev-parse --abbrev-ref HEAD)
   if [[ $branch = release-* ]]; then
     pwd
-    IMAGE_TAG=v$(cat $SRCROOT/VERSION)
+    IMAGE_TAG=${ARGOCD_VERSION_LABEL}
   fi
 fi
 # otherwise, use latest
@@ -27,7 +28,7 @@ if [ "$IMAGE_TAG" = "" ]; then
   IMAGE_TAG=latest
 fi
 
-cd ${SRCROOT}/manifests/base && $KUSTOMIZE edit set image argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG}
+cd ${SRCROOT}/manifests/base && $KUSTOMIZE edit set image argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG} && sed "s/ARGOCD_VERSION_TO_BE_REPLACED/${ARGOCD_VERSION_LABEL}/g" version_label_patches.tmpl > version_label_patches.yaml
 cd ${SRCROOT}/manifests/ha/base && $KUSTOMIZE edit set image argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG}
 
 echo "${AUTOGENMSG}" > "${SRCROOT}/manifests/install.yaml"
