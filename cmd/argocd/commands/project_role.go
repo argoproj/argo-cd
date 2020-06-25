cb99924002dded318c55767fcfaea56bd4d68174
Zhang <may_zhang@intuit.com>
2020-06-24 17:17:30 -0700

    check for nil

--- cmd/argocd/commands/project_role.go
+++ cmd/argocd/commands/project_role.go
@@ -332,12 +332,14 @@ func NewProjectRoleGetCommand(clientOpts *argocdclient.ClientOptions) *cobra.Com
 			// TODO(jessesuen): print groups
 			w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 			fmt.Fprintf(w, "ID\tISSUED-AT\tEXPIRES-AT\n")
-			for _, token := range proj.Status.JWTTokensByRole[roleName].Items {
-				expiresAt := "<none>"
-				if token.ExpiresAt > 0 {
-					expiresAt = humanizeTimestamp(token.ExpiresAt)
+			if proj.Status.JWTTokensByRole != nil {
+				for _, token := range proj.Status.JWTTokensByRole[roleName].Items {
+					expiresAt := "<none>"
+					if token.ExpiresAt > 0 {
+						expiresAt = humanizeTimestamp(token.ExpiresAt)
+					}
+					fmt.Fprintf(w, "%d\t%s\t%s\n", token.IssuedAt, humanizeTimestamp(token.IssuedAt), expiresAt)
 				}
-				fmt.Fprintf(w, "%d\t%s\t%s\n", token.IssuedAt, humanizeTimestamp(token.IssuedAt), expiresAt)
 			}
 			_ = w.Flush()
 		},
