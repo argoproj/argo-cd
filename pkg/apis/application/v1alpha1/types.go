b3c118d4c0b0a8c742667f5a2a04dfb17fac3fe7
Darshan Chaudhary <deathbullet@gmail.com>
2020-07-03 11:58:36 +0530

    feat: ignore status globally (#3754)
    feat: ignore status globally (#3754)

--- pkg/apis/application/v1alpha1/types.go
+++ pkg/apis/application/v1alpha1/types.go
@@ -17,12 +17,12 @@ import (
 
 	"github.com/argoproj/gitops-engine/pkg/health"
 	synccommon "github.com/argoproj/gitops-engine/pkg/sync/common"
+	"github.com/ghodss/yaml"
 	"github.com/google/go-cmp/cmp"
 	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	yaml "gopkg.in/yaml.v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -1060,12 +1060,43 @@ type KnownTypeField struct {
 	Type  string `json:"type,omitempty" protobuf:"bytes,2,opt,name=type"`
 }
 
+type OverrideIgnoreDiff struct {
+	JSONPointers []string `json:"jsonPointers" protobuf:"bytes,1,rep,name=jSONPointers"`
+}
+
+type rawResourceOverride struct {
+	HealthLua         string           `json:"health.lua,omitempty"`
+	Actions           string           `json:"actions,omitempty"`
+	IgnoreDifferences string           `json:"ignoreDifferences,omitempty"`
+	KnownTypeFields   []KnownTypeField `json:"knownTypeFields,omitempty"`
+}
+
 // ResourceOverride holds configuration to customize resource diffing and health assessment
 type ResourceOverride struct {
-	HealthLua         string           `json:"health.lua,omitempty" protobuf:"bytes,1,opt,name=healthLua"`
-	Actions           string           `json:"actions,omitempty" protobuf:"bytes,3,opt,name=actions"`
-	IgnoreDifferences string           `json:"ignoreDifferences,omitempty" protobuf:"bytes,2,opt,name=ignoreDifferences"`
-	KnownTypeFields   []KnownTypeField `json:"knownTypeFields,omitempty" protobuf:"bytes,4,opt,name=knownTypeFields"`
+	HealthLua         string             `protobuf:"bytes,1,opt,name=healthLua"`
+	Actions           string             `protobuf:"bytes,3,opt,name=actions"`
+	IgnoreDifferences OverrideIgnoreDiff `protobuf:"bytes,2,opt,name=ignoreDifferences"`
+	KnownTypeFields   []KnownTypeField   `protobuf:"bytes,4,opt,name=knownTypeFields"`
+}
+
+func (s *ResourceOverride) UnmarshalJSON(data []byte) error {
+	raw := &rawResourceOverride{}
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+	s.KnownTypeFields = raw.KnownTypeFields
+	s.HealthLua = raw.HealthLua
+	s.Actions = raw.Actions
+	return yaml.Unmarshal([]byte(raw.IgnoreDifferences), &s.IgnoreDifferences)
+}
+
+func (s ResourceOverride) MarshalJSON() ([]byte, error) {
+	ignoreDifferencesData, err := yaml.Marshal(s.IgnoreDifferences)
+	if err != nil {
+		return nil, err
+	}
+	raw := &rawResourceOverride{s.HealthLua, s.Actions, string(ignoreDifferencesData), s.KnownTypeFields}
+	return json.Marshal(raw)
 }
 
 func (o *ResourceOverride) GetActions() (ResourceActions, error) {
--- util/argo/normalizers/diff_normalizer_test.go
+++ util/argo/normalizers/diff_normalizer_test.go
@@ -55,7 +55,7 @@ func TestNormalizeNoMatchedGroupKinds(t *testing.T) {
 func TestNormalizeMatchedResourceOverrides(t *testing.T) {
 	normalizer, err := NewIgnoreNormalizer([]v1alpha1.ResourceIgnoreDifferences{}, map[string]v1alpha1.ResourceOverride{
 		"apps/Deployment": {
-			IgnoreDifferences: `jsonPointers: ["/spec/template/spec/containers"]`,
+			IgnoreDifferences: v1alpha1.OverrideIgnoreDiff{JSONPointers: []string{"/spec/template/spec/containers"}},
 		},
 	})
 
@@ -95,10 +95,10 @@ spec:
 func TestNormalizeMissingJsonPointer(t *testing.T) {
 	normalizer, err := NewIgnoreNormalizer([]v1alpha1.ResourceIgnoreDifferences{}, map[string]v1alpha1.ResourceOverride{
 		"apps/Deployment": {
-			IgnoreDifferences: `jsonPointers: ["/garbage"]`,
+			IgnoreDifferences: v1alpha1.OverrideIgnoreDiff{JSONPointers: []string{"/garbage"}},
 		},
 		"apiextensions.k8s.io/CustomResourceDefinition": {
-			IgnoreDifferences: `jsonPointers: ["/spec/additionalPrinterColumns/0/priority"]`,
+			IgnoreDifferences: v1alpha1.OverrideIgnoreDiff{JSONPointers: []string{"/spec/additionalPrinterColumns/0/priority"}},
 		},
 	})
 	assert.NoError(t, err)
@@ -114,4 +114,26 @@ func TestNormalizeMissingJsonPointer(t *testing.T) {
 
 	err = normalizer.Normalize(&crd)
 	assert.NoError(t, err)
+}
+
+func TestNormalizeGlobMatch(t *testing.T) {
+	normalizer, err := NewIgnoreNormalizer([]v1alpha1.ResourceIgnoreDifferences{}, map[string]v1alpha1.ResourceOverride{
+		"*/*": {
+			IgnoreDifferences: v1alpha1.OverrideIgnoreDiff{JSONPointers: []string{"/spec/template/spec/containers"}},
+		},
+	})
+
+	assert.Nil(t, err)
+
+	deployment := test.NewDeployment()
+
+	_, has, err := unstructured.NestedSlice(deployment.Object, "spec", "template", "spec", "containers")
+	assert.Nil(t, err)
+	assert.True(t, has)
+
+	err = normalizer.Normalize(deployment)
+	assert.Nil(t, err)
+	_, has, err = unstructured.NestedSlice(deployment.Object, "spec", "template", "spec", "containers")
+	assert.Nil(t, err)
+	assert.False(t, has)
 }
