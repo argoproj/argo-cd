4789bcc28c67d31453bba93e49866c5db28ee217
Alin Balutoiu <alinbalutoiu@gmail.com>
2020-07-09 18:55:49 +0200

    fix: Update dependencies built from source for ARM (#3909)
    Signed-off-by: Alin Balutoiu <alinbalutoiu@gmail.com>

--- hack/installers/install-ksonnet-linux.sh
+++ hack/installers/install-ksonnet-linux.sh
@@ -7,10 +7,11 @@ KSONNET_VERSION=${ksonnet_version}
 case $ARCHITECTURE in
   arm|arm64)
     set +o pipefail
+    # Clone the repository in $GOPATH/src/github.com/ksonnet/ksonnet
     go get -u github.com/ksonnet/ksonnet || true
     set -o pipefail
     cd $GOPATH/src/github.com/ksonnet/ksonnet && git checkout tags/v$KSONNET_VERSION
-    cd $GOPATH/src/github.com/ksonnet/ksonnet && make install
+    cd $GOPATH/src/github.com/ksonnet/ksonnet && CGO_ENABLED=0 GO_LDFLAGS="-s" make install
     mv $GOPATH/bin/ks $BIN/ks
     ;;
   *)
