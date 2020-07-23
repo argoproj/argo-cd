4ffa655cab006dc5d659b6f99b8af1f4d61cbb06
darshanime <deathbullet@gmail.com>
2020-07-23 13:24:47 +0530

    fix: skipped resources should not impact sync status
    Signed-off-by: darshanime <deathbullet@gmail.com>

--- controller/state.go
+++ controller/state.go
@@ -463,7 +463,7 @@ func (m *appStateManager) CompareAppState(app *v1alpha1.Application, project *ap
 		} else {
 			diffResult = diff.DiffResult{Modified: false, NormalizedLive: []byte("{}"), PredictedLive: []byte("{}")}
 		}
-		if resState.Hook || ignore.Ignore(obj) {
+		if resState.Hook || ignore.Ignore(obj) || hookutil.Skip(obj) {
 			// For resource hooks, don't store sync status, and do not affect overall sync status
 		} else if diffResult.Modified || targetObj == nil || liveObj == nil {
 			// Set resource state to OutOfSync since one of the following is true:
