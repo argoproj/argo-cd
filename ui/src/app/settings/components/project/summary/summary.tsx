6b51db839227c5dadd29438750957600150b72d0
Remington Breeze <remington@breeze.software>
2020-07-30 16:46:34 -0700

    Reformat save/reset buttons for description and update on save

--- ui/src/app/settings/components/project/summary/summary.tsx
+++ ui/src/app/settings/components/project/summary/summary.tsx
@@ -21,6 +21,10 @@ export class ProjectSummary extends React.Component<SummaryProps, SummaryState> 
         this.state = {name: props.proj.metadata.name, description: props.proj.spec.description, proj: props.proj};
     }
 
+    get descriptionChanged(): boolean {
+        return this.state.description !== this.props.proj.spec.description;
+    }
+
     public render() {
         return (
             <div className='project-summary'>
@@ -28,22 +32,35 @@ export class ProjectSummary extends React.Component<SummaryProps, SummaryState> 
                     <div className='project-summary__label'>PROJECT</div>
                     <div className='project-summary__title'>{this.state.name}</div>
                     <div className='project-summary__description'>
-                        <div className='project-summary__col'>
-                            {this.state.description !== this.props.proj.spec.description ? (
-                                <button
-                                    className='project__button project__button-save'
-                                    onClick={async () => {
-                                        const update = {...this.state.proj};
-                                        update.spec.description = this.state.description;
-                                        await services.projects.updateLean(this.state.name, update);
-                                    }}>
-                                    SAVE
-                                </button>
-                            ) : (
+                        <div className='project-summary__description--row'>
+                            <div className='project-summary__col'>
                                 <i className='fa fa-pencil-alt' />
-                            )}
+                            </div>
+                            <input value={this.state.description} onChange={e => this.setState({description: e.target.value})} />
                         </div>
-                        <input value={this.state.description} onChange={e => this.setState({description: e.target.value})} />
+                        <div className='project-summary__description--row'>
+                            {this.descriptionChanged ? (
+                                <div className='project-summary__description--actions'>
+                                    <button
+                                        className='project__button project__button-save'
+                                        onClick={async () => {
+                                            const update = {...this.state.proj};
+                                            update.spec.description = this.state.description;
+                                            const res = await services.projects.updateLean(this.state.name, update);
+                                            this.setState({proj: res});
+                                        }}>
+                                        SAVE
+                                    </button>
+                                    <button
+                                        className='project__button project__button-cancel'
+                                        onClick={async () => {
+                                            this.setState({description: this.props.proj.spec.description});
+                                        }}>
+                                        REVERT
+                                    </button>
+                                </div>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
                 <div className='project-summary__section'>
