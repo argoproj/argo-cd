845b90854b4ac928e13bdd2cd0148d89cbf6f628
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2020-09-02 12:51:24 -0700

    fix: improve applications list page client side performance

--- ui/src/app/settings/components/project-role-policies-edit/project-role-policies-edit.tsx
+++ ui/src/app/settings/components/project-role-policies-edit/project-role-policies-edit.tsx
@@ -20,7 +20,7 @@ function generatePolicy(project: string, role: string, action?: string, object?:
 const actions = ['get', 'create', 'update', 'delete', 'sync', 'override'];
 
 export const ProjectRolePoliciesEdit = (props: ProjectRolePoliciesProps) => (
-    <DataLoader load={() => services.applications.list([props.projName], {fields: ['items.metadata.name']})}>
+    <DataLoader load={() => services.applications.list([props.projName], {fields: ['items.metadata.name']}).then(list => list.items)}>
         {applications => (
             <React.Fragment>
                 <h4>Policy Rules</h4>
