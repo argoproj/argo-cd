c159426c36c517f621e20696650bbea019428eb1
William Tam <email.wtam@gmail.com>
2020-10-19 20:17:52 -0400

    Set HOME  env var to a fixed value so  that generated docs do not depend on the running user's home directory

--- tools/cmd-docs/main.go
+++ tools/cmd-docs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/argoproj/argo-cd/cmd/argocd/commands"
 
@@ -9,6 +10,9 @@ import (
 )
 
 func main() {
+	// set HOME env var so that default values involve user's home directory do not depend on the running user.
+	os.Setenv("HOME", "/home/user")
+
 	err := doc.GenMarkdownTree(commands.NewCommand(), "./docs/user-guide/commands")
 	if err != nil {
 		log.Fatal(err)
