7a4a48731c243197bf32668d24056ebd3b11c19e
Mark Pim <j.mark.pim@gmail.com>
2020-10-29 12:18:15 +0000

    More lint fix

--- ui/src/app/settings/components/cluster-details/cluster-details.tsx
+++ ui/src/app/settings/components/cluster-details/cluster-details.tsx
@@ -68,7 +68,8 @@ export const ClusterDetails = (props: RouteComponentProps<{server: string}>) => 
                                 },
                                 {
                                     title: 'CREDENTIALS TYPE',
-                                    view: (cluster.config.awsAuthConfig && `IAM AUTH (cluster name: ${cluster.config.awsAuthConfig.clusterName})`) ||
+                                    view:
+                                        (cluster.config.awsAuthConfig && `IAM AUTH (cluster name: ${cluster.config.awsAuthConfig.clusterName})`) ||
                                         (cluster.config.execProviderConfig && `External provider (command: ${cluster.config.execProviderConfig.command})`) ||
                                         'Token/Basic Auth'
                                 },
