e54ac307eac9e5054ab9be4e6465e0f23d9b8926
Dewan Ahmed <dewan.ishtiaque@hotmail.com>
2020-11-04 09:32:13 -0500

    Adding comment to describe the test
    Signed-off-by: Dewan Ahmed <dewan.ishtiaque@hotmail.com>
    
    Date:      Wed Nov 4 09:32:13 2020 -0500

--- test/smoke/app_create_test.go
+++ test/smoke/app_create_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/argoproj/argo-cd/util/errors"
 )
 
+// TestCreateApp tests "argocd app create" command from a black-box perspective
 func TestCreateApp(t *testing.T) {
 	expected := "application 'guestbook' created\n"
 	output, err := RunCmd("argocd", "app", "create", "guestbook", "--repo", "https://github.com/argoproj/argocd-example-apps.git", "--path", "guestbook", "--dest-server", "https://kubernetes.default.svc", "--dest-namespace", "default")
