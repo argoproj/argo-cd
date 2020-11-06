d8c34e716f5c703ead31f41e893e57a4eb9cb365
Jesse Suen <jessesuen@users.noreply.github.com>
2020-11-06 15:16:32 -0800

    feat: add new rollout actions (abort, retry, promote-full, terminate). improve health check (#4777)
    Signed-off-by: Jesse Suen <jesse_suen@intuit.com>
    Signed-off-by: Dewan Ahmed <dewan.ishtiaque@hotmail.com>

--- resource_customizations/argoproj.io/Rollout/actions/discovery.lua
+++ resource_customizations/argoproj.io/Rollout/actions/discovery.lua
@@ -1,18 +1,18 @@
 actions = {}
-actions["resume"] = {["disabled"] = false}
 actions["restart"] = {["disabled"] = false}
 
 local paused = false
-
 if obj.status ~= nil and obj.status.pauseConditions ~= nil then
     paused = table.getn(obj.status.pauseConditions) > 0
 elseif obj.spec.paused ~= nil then
     paused = obj.spec.paused
 end
-if paused then
-    actions["resume"]["disabled"] = false
-else
-    actions["resume"]["disabled"] = true
-end
+actions["resume"] = {["disabled"] = not(paused)}
+
+fullyPromoted = obj.status.currentPodHash == obj.status.stableRS
+actions["abort"] = {["disabled"] = fullyPromoted or obj.status.abort}
+actions["retry"] = {["disabled"] = fullyPromoted or not(obj.status.abort)}
+
+actions["promote-full"] = {["disabled"] = fullyPromoted or obj.spec.strategy.blueGreen ~= nil}
 
 return actions
