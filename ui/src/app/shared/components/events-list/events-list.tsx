ada456a848385c9e1e28ad0825222b8dc2b74d85
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2020-11-11 08:57:08 -0800

    fix: add FIRST OCCURRED, LAST OCCURRED to events table (#4812)
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Signed-off-by: mickkael <19755421+mickkael@users.noreply.github.com>

--- ui/src/app/shared/components/events-list/events-list.tsx
+++ ui/src/app/shared/components/events-list/events-list.tsx
@@ -1,32 +1,39 @@
+import * as moment from 'moment';
 import * as React from 'react';
 
 import * as models from '../../models';
 
 require('./events-list.scss');
 
-export const EventsList = (props: {events: models.Event[]}) => (
-    <div className='events-list'>
-        {(props.events.length === 0 && <p>No events available</p>) || (
-            <div className='argo-table-list'>
-                <div className='argo-table-list__head'>
-                    <div className='row'>
-                        <div className='columns small-2'>REASON</div>
-                        <div className='columns small-6'>MESSAGE</div>
-                        <div className='columns small-2'>COUNT</div>
-                        <div className='columns small-2'>EVENT TIMESTAMP</div>
-                    </div>
-                </div>
-                {props.events.map(event => (
-                    <div className={`argo-table-list__row events-list__event events-list__event--${event.type}`} key={event.metadata.uid}>
+export const EventsList = (props: {events: models.Event[]}) => {
+    const events = props.events.sort((first, second) => moment(second.firstTimestamp).diff(first.lastTimestamp));
+
+    return (
+        <div className='events-list'>
+            {(events.length === 0 && <p>No events available</p>) || (
+                <div className='argo-table-list'>
+                    <div className='argo-table-list__head'>
                         <div className='row'>
-                            <div className='columns small-2'>{event.reason}</div>
-                            <div className='columns small-6'>{event.message}</div>
-                            <div className='columns small-2'>{event.count}</div>
-                            <div className='columns small-2'>{event.firstTimestamp}</div>
+                            <div className='columns small-2'>REASON</div>
+                            <div className='columns small-5'>MESSAGE</div>
+                            <div className='columns small-1'>COUNT</div>
+                            <div className='columns small-2'>FIRST OCCURRED</div>
+                            <div className='columns small-2'>LAST OCCURRED</div>
                         </div>
                     </div>
-                ))}
-            </div>
-        )}
-    </div>
-);
+                    {events.map(event => (
+                        <div className={`argo-table-list__row events-list__event events-list__event--${event.type}`} key={event.metadata.uid}>
+                            <div className='row'>
+                                <div className='columns small-2'>{event.reason}</div>
+                                <div className='columns small-5'>{event.message}</div>
+                                <div className='columns small-1'>{event.count}</div>
+                                <div className='columns small-2'>{event.firstTimestamp}</div>
+                                <div className='columns small-2'>{event.lastTimestamp}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
