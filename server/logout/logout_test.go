fcd2f96094b9f5d73742ea1b75e14750657ef13f
jaideepr97 <jaideep.r97@gmail.com>
2020-11-13 10:09:43 -0500

    fixed broken test
    Signed-off-by: jaideepr97 <jaideep.r97@gmail.com>

--- server/logout/logout_test.go
+++ server/logout/logout_test.go
@@ -28,9 +28,10 @@ var (
 	baseLogoutURLwithToken               = "http://localhost:4000/logout?id_token_hint={{token}}"
 	baseLogoutURLwithRedirectURL         = "http://localhost:4000/logout?post_logout_redirect_uri={{logoutRedirectURL}}"
 	baseLogoutURLwithTokenAndRedirectURL = "http://localhost:4000/logout?id_token_hint={{token}}&post_logout_redirect_uri={{logoutRedirectURL}}"
-	token                                = "sample-token"
+	invalidToken                         = "sample-token"
+	validToken                           = "eyJraWQiOiJYQi1MM3ZFdHhYWXJLcmRSQnVEV0NwdnZsSnk3SEJVb2d5N253M1U1Z1ZZIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMHVqNnM1NDVyNU5peVNLcjVkNSIsIm5hbWUiOiJqZCByIiwiZW1haWwiOiJqYWlkZWVwMTdydWx6QGdtYWlsLmNvbSIsInZlciI6MSwiaXNzIjoiaHR0cHM6Ly9kZXYtNTY5NTA5OC5va3RhLmNvbSIsImF1ZCI6IjBvYWowM2FmSEtqN3laWXJwNWQ1IiwiaWF0IjoxNjA0NjExMDg5LCJleHAiOjE2MDQ2MTQ2ODksImp0aSI6IklELjZEVlM2enluandLeFJuNkhNUkJIaXBJMDdmNUxQRzFHc054OTVScENlYmciLCJhbXIiOlsicHdkIl0sImlkcCI6IjAwb2lnaGZmdkpRTDYzWjhoNWQ1IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiamFpZGVlcDE3cnVsekBnbWFpbC5jb20iLCJhdXRoX3RpbWUiOjE2MDQ2MTEwODgsImF0X2hhc2giOiJKWjcwWUhsM3k5eWhpaXZaaU9OQTVRIn0.TauJ_MyIT3EtbkXfRvEdSh4H7YS6ezwO4mJyv5A1_ml9HkKsxWUht09U9T-VFJyqweOim_2fyRMsc6VCtAla9kCsNUHvHU1uEDrMageWePfIxrM0yQ2Fys2cbSl2dZGeTlX2I-xt9_EhZszLdKccdyhaBL1JYMTc0ajTWNmFN9azn-WJKkAwDe-3EUMYm9hfYSLkrMqqsExPNCQlc0LMgcWPzj3gQwCYj1MvMO3F3U99i5FglIHjw99sC7StMEHOfKzuCwIuceNfqLXHZ0GpbDgDfYpfn4JksCfXsWJd2niWGeOULAgl1-vk1WUt3K5qKFCz0HGGPfLMjqkdcGTQ_A"
 	logoutRedirectURL                    = "http://localhost:4000/login"
-	constructedLogoutURL                 = "http://localhost:4000/logout?id_token_hint=" + token + "&post_logout_redirect_uri=" + logoutRedirectURL
+	constructedLogoutURL                 = "http://localhost:4000/logout?id_token_hint=" + validToken + "&post_logout_redirect_uri=" + logoutRedirectURL
 )
 
 func TestConstructLogoutURL(t *testing.T) {
@@ -45,7 +46,7 @@ func TestConstructLogoutURL(t *testing.T) {
 		{
 			name:                 "Case: No additional parameters passed to logout URL",
 			logoutURL:            baseLogoutURL,
-			token:                token,
+			token:                validToken,
 			logoutRedirectURL:    logoutRedirectURL,
 			constructedLogoutURL: baseLogoutURL,
 		},
@@ -53,15 +54,15 @@ func TestConstructLogoutURL(t *testing.T) {
 
 			name:                 "Case: ID token passed to logout URL",
 			logoutURL:            baseLogoutURLwithToken,
-			token:                token,
+			token:                validToken,
 			logoutRedirectURL:    logoutRedirectURL,
-			constructedLogoutURL: "http://localhost:4000/logout?id_token_hint=" + token,
+			constructedLogoutURL: "http://localhost:4000/logout?id_token_hint=" + validToken,
 		},
 		{
 
 			name:                 "Case: Redirect URL passed to logout URL",
 			logoutURL:            baseLogoutURLwithRedirectURL,
-			token:                token,
+			token:                validToken,
 			logoutRedirectURL:    logoutRedirectURL,
 			constructedLogoutURL: "http://localhost:4000/logout?post_logout_redirect_uri=" + logoutRedirectURL,
 		},
@@ -69,9 +70,9 @@ func TestConstructLogoutURL(t *testing.T) {
 
 			name:                 "Case: ID token and redirect URL passed to logout URL",
 			logoutURL:            baseLogoutURLwithTokenAndRedirectURL,
-			token:                token,
+			token:                validToken,
 			logoutRedirectURL:    logoutRedirectURL,
-			constructedLogoutURL: "http://localhost:4000/logout?id_token_hint=" + token + "&post_logout_redirect_uri=" + logoutRedirectURL,
+			constructedLogoutURL: "http://localhost:4000/logout?id_token_hint=" + validToken + "&post_logout_redirect_uri=" + logoutRedirectURL,
 		},
 	}
 	for _, tt := range tests {
@@ -144,16 +145,23 @@ func TestHandlerConstructLogoutURL(t *testing.T) {
 	validHandler := NewHandler(appclientset.NewSimpleClientset(), settingsManager, "default")
 	handlerWithInvalidSettingsManager := NewHandler(appclientset.NewSimpleClientset(), settingsManagerWithoutValidConfigMap, "default")
 
-	header := make(map[string][]string)
-	header["Cookie"] = []string{"argocd.token=" + token}
+	validHeader := make(map[string][]string)
+	validHeader["Cookie"] = []string{"argocd.token=" + validToken}
+
+	invalidHeader := make(map[string][]string)
+	invalidHeader["Cookie"] = []string{"argocd.token=" + invalidToken}
 
 	validRequest, err := http.NewRequest("GET", "http://localhost:4000/api/logout", nil)
 	assert.NoError(t, err)
 	// Setting cookie in request
-	validRequest.Header = header
+	validRequest.Header = validHeader
 
 	invalidRequest, err := http.NewRequest("GET", "http://localhost:4000/api/logout", nil)
 	assert.NoError(t, err)
+
+	RequestWithInvalidToken, err := http.NewRequest("GET", "http://localhost:4000/api/logout", nil)
+	assert.NoError(t, err)
+	RequestWithInvalidToken.Header = invalidHeader
 
 	tests := []struct {
 		name              string
@@ -184,6 +192,14 @@ func TestHandlerConstructLogoutURL(t *testing.T) {
 			name:              "Case: valid oid config available but cookie not present in http request to handler",
 			handler:           validHandler,
 			request:           invalidRequest,
+			responseRecorder:  httptest.NewRecorder(),
+			expectedLogoutURL: constructedLogoutURL,
+			wantErr:           true,
+		},
+		{
+			name:              "Case: valid oid config available but token present in cookie doesnt not satisfy required format",
+			handler:           validHandler,
+			request:           RequestWithInvalidToken,
 			responseRecorder:  httptest.NewRecorder(),
 			expectedLogoutURL: constructedLogoutURL,
 			wantErr:           true,
