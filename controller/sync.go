fc4c1fd861d85b35249368d71dce9ae4d16be301
Jesse Suen <jessesuen@users.noreply.github.com>
2020-11-13 01:18:15 -0800

    fix: sync hooks using generateName was using fixed timestamp suffix (#4787)
    Signed-off-by: Jesse Suen <Jesse_Suen@intuit.com>
    Signed-off-by: mickkael <19755421+mickkael@users.noreply.github.com>

--- controller/sync.go
+++ controller/sync.go
@@ -154,7 +154,7 @@ func (m *appStateManager) SyncAppState(app *v1alpha1.Application, state *v1alpha
 			return nil
 		}),
 		sync.WithOperationSettings(syncOp.DryRun, syncOp.Prune, syncOp.SyncStrategy.Force(), syncOp.IsApplyStrategy() || len(syncOp.Resources) > 0),
-		sync.WithInitialState(state.Phase, state.Message, initialResourcesRes),
+		sync.WithInitialState(state.Phase, state.Message, initialResourcesRes, state.StartedAt),
 		sync.WithResourcesFilter(func(key kube.ResourceKey, target *unstructured.Unstructured, live *unstructured.Unstructured) bool {
 			return len(syncOp.Resources) == 0 || argo.ContainsSyncResource(key.Name, key.Namespace, schema.GroupVersionKind{Kind: key.Kind, Group: key.Group}, syncOp.Resources)
 		}),
