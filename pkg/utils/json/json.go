0bc2f8c395f67123156d4ce6b667bf730618307f
May Zhang <may_zhang@intuit.com>
2020-11-23 11:38:46 -0800

    fix: panic: interface conversion: interface {} is string (#189)
    * panic: interface conversion: interface {} is string
    
    Signed-off-by: May Zhang <may_zhang@intuit.com>

--- pkg/utils/json/json.go
+++ pkg/utils/json/json.go
@@ -4,12 +4,23 @@ package json
 func removeFields(config, live interface{}) interface{} {
 	switch c := config.(type) {
 	case map[string]interface{}:
-		return RemoveMapFields(c, live.(map[string]interface{}))
+		l, ok := live.(map[string]interface{})
+		if ok {
+			return RemoveMapFields(c, l)
+		} else {
+			return live
+		}
 	case []interface{}:
-		return RemoveListFields(c, live.([]interface{}))
+		l, ok := live.([]interface{})
+		if ok {
+			return RemoveListFields(c, l)
+		} else {
+			return live
+		}
 	default:
 		return live
 	}
+
 }
 
 // RemoveMapFields remove all non-existent fields in the live that don't exist in the config
