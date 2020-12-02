810352370c0072be00f83cce6d397b134966bfe8
Remington Breeze <remington@breeze.software>
2020-12-02 13:26:34 -0800

    begin pagination of pod logs
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/pod-logs-viewer/pod-logs-viewer.tsx
+++ ui/src/app/applications/components/pod-logs-viewer/pod-logs-viewer.tsx
@@ -1,8 +1,10 @@
-import {LogsViewer} from 'argo-ui';
+import {DataLoader} from 'argo-ui';
 import * as React from 'react';
 
 import * as models from '../../../shared/models';
 import {services} from '../../../shared/services';
+
+const lines: string[] = [];
 
 export const PodsLogsViewer = (props: {applicationName: string; pod: models.ResourceNode & any; containerIndex: number}) => {
     const containers = (props.pod.spec.initContainers || []).concat(props.pod.spec.containers || []);
@@ -13,18 +15,30 @@ export const PodsLogsViewer = (props: {applicationName: string; pod: models.Reso
     const containerStatuses = ((props.pod.status && props.pod.status.containerStatuses) || []).concat((props.pod.status && props.pod.status.initContainerStatuses) || []);
     const containerStatus = containerStatuses.find((status: any) => status.name === container.name);
     const isRunning = !!(containerStatus && containerStatus.state && containerStatus && containerStatus.state.running);
+    const maxLines = 10;
     return (
-        <div style={{height: '100%'}}>
-            <LogsViewer
+        <pre style={{height: '100%', lineHeight: '1em'}}>
+            <DataLoader load={() => services.applications.getContainerLogs(props.applicationName, props.pod.metadata.namespace, props.pod.metadata.name, container.name, maxLines)}>
+                {(log) => {
+                    if (isRunning) {
+                        if (lines.length >= maxLines) {
+                            lines.shift();
+                        }
+                        lines.push(log.content);
+                    }
+                    return lines.map((l) => <p>{l}</p>);
+                }}
+            </DataLoader>
+            {/* <LogsViewer
                 source={{
                     key: `${props.pod.metadata.name}:${container.name}`,
                     loadLogs: () =>
                         services.applications
                             .getContainerLogs(props.applicationName, props.pod.metadata.namespace, props.pod.metadata.name, container.name)
-                            .map(item => item.content + '\n'),
-                    shouldRepeat: () => isRunning
+                            .map((item) => item.content + '\n'),
+                    shouldRepeat: () => isRunning,
                 }}
-            />
-        </div>
+            /> */}
+        </pre>
     );
 };
