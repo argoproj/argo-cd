911eef607cc5f2fb6233ad18c95511a75f08af64
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-01-20 09:12:49 -0800

    fix: directory source include/exclude should match relative file path (#5277)
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Signed-off-by: mickkael <19755421+mickkael@users.noreply.github.com>

--- test/e2e/app_management_test.go
+++ test/e2e/app_management_test.go
@@ -1432,29 +1432,6 @@ spec:
 			assert.Equal(t, []HelmParameter{{Name: "foo", Value: "foo"}}, app.Spec.Source.Helm.Parameters)
 		})
 }
-func TestAppCreationWithExclude(t *testing.T) {
-	Given(t).
-		Path("app-exclusions").
-		When().
-		Create("--directory-exclude", "**/.*", "--directory-recurse").
-		Sync().
-		Then().
-		Expect(OperationPhaseIs(OperationSucceeded)).
-		Expect(SyncStatusIs(SyncStatusCodeSynced)).
-		Expect(HealthIs(health.HealthStatusHealthy))
-}
-
-func TestAppCreationWithInclude(t *testing.T) {
-	Given(t).
-		Path("app-inclusions").
-		When().
-		Create("--directory-include", "**/.*", "--directory-recurse").
-		Sync().
-		Then().
-		Expect(OperationPhaseIs(OperationSucceeded)).
-		Expect(SyncStatusIs(SyncStatusCodeSynced)).
-		Expect(HealthIs(health.HealthStatusHealthy))
-}
 
 // Ensure actions work when using a resource action that modifies status and/or spec
 func TestCRDStatusSubresourceAction(t *testing.T) {
