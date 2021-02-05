d84270db66fad10eae1176a0902668bef0357fe1
Remington Breeze <remington@breeze.software>
2021-02-04 17:13:54 -0800

    fix(ui): Add header and browser title to fullscreen logs viewer
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/application-fullscreen-logs/application-fullscreen-logs.tsx
+++ ui/src/app/applications/components/application-fullscreen-logs/application-fullscreen-logs.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import Helmet from 'react-helmet';
 import {RouteComponentProps} from 'react-router-dom';
 import {PodLogsProps, PodsLogsViewer} from '../pod-logs-viewer/pod-logs-viewer';
 import './application-fullscreen-logs.scss';
 
 export const ApplicationFullscreenLogs = (props: RouteComponentProps<PodLogsProps>) => {
+    const title = `${props.match.params.podName}/${props.match.params.containerName}`;
     return (
         <div className='application-fullscreen-logs'>
+            <Helmet title={`Argo CD - ${title}`} />
+            <h4 style={{fontSize: '18px', textAlign: 'center'}}>{title}</h4>
             <PodsLogsViewer {...props.match.params} fullscreen={true} />
         </div>
     );
