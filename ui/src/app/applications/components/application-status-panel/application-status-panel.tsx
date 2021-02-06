c5e3584c630d371cdb17df437ee728eabe1a81a0
Remington Breeze <remington@breeze.software>
2021-02-05 17:29:13 -0800

    Begin updating status panel
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
@@ -17,6 +17,13 @@ interface Props {
     showMetadataInfo?: (revision: string, info: models.RevisionMetadata) => any;
 }
 
+const sectionLabel = (title: string, helpContent?: string) => (
+    <label>
+        {title}
+        {helpContent && <HelpIcon title={helpContent} />}
+    </label>
+);
+
 export const ApplicationStatusPanel = ({application, showOperation, showConditions, showMetadataInfo}: Props) => {
     const today = new Date();
 
@@ -26,6 +33,24 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
         const deployDate = new Date(history[history.length - 1].deployedAt);
         daysSinceLastSynchronized = Math.round(Math.abs((today.getTime() - deployDate.getTime()) / (24 * 60 * 60 * 1000)));
     }
+
+    application.status.conditions = [
+        {
+            type: 'Error',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+            lastTransitionTime: 'Wed Jan 20 2021 14:52:45 GMT-0800'
+        },
+        {
+            type: 'Warning',
+            message: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+            lastTransitionTime: 'Wed Jan 20 2021 14:52:45 GMT-0800'
+        },
+        {
+            type: 'info',
+            message: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+            lastTransitionTime: 'Wed Jan 20 2021 14:52:45 GMT-0800'
+        }
+    ];
     const cntByCategory = (application.status.conditions || []).reduce(
         (map, next) => map.set(getConditionCategory(next), (map.get(getConditionCategory(next)) || 0) + 1),
         new Map<string, number>()
@@ -35,53 +60,68 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
         showOperation = null;
     }
 
+    const infos = cntByCategory.get('info');
+    const warnings = cntByCategory.get('warning');
+    const errors = cntByCategory.get('error');
+
     return (
         <div className='application-status-panel row'>
-            <div className='application-status-panel__item columns small-2'>
+            <div className='application-status-panel__item'>
+                {sectionLabel('APP HEALTH', 'The health status of your app')}
                 <div className='application-status-panel__item-value'>
                     <HealthStatusIcon state={application.status.health} />
                     &nbsp;
                     {application.status.health.status}
-                    <HelpIcon title='The health status of your app' />
                 </div>
                 <div className='application-status-panel__item-name'>{application.status.health.message}</div>
             </div>
-            <div className='application-status-panel__item columns small-2' style={{position: 'relative'}}>
-                <div className='application-status-panel__item-value'>
-                    <ComparisonStatusIcon status={application.status.sync.status} label={true} />
-                    <HelpIcon title='Whether or not the version of your app is up to date with your repo. You may wish to sync your app if it is out-of-sync.' />
+            <div className='application-status-panel__item'>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    {sectionLabel(
+                        'CURRENT SYNC STATUS',
+                        'Whether or not the version of your app is up to date with your repo. You may wish to sync your app if it is out-of-sync.'
+                    )}
+                    <button
+                        className='argo-button argo-button--base argo-button--sm application-status-panel__more-button'
+                        onClick={showMetadataInfo && (info => showMetadataInfo(application.status.sync.revision, info))}>
+                        MORE
+                    </button>
                 </div>
-                <div className='application-status-panel__item-name'>{syncStatusMessage(application)}</div>
+                <div className='application-status-panel__item-value'>
+                    <div>
+                        <ComparisonStatusIcon status={application.status.sync.status} label={true} />
+                    </div>
+                    <div className='application-status-panel__item-value__revision'>{syncStatusMessage(application)}</div>
+                </div>
+                <div className='application-status-panel__item-name'></div>
                 <div className='application-status-panel__item-name'>
                     {application.status && application.status.sync && application.status.sync.revision && (
                         <RevisionMetadataPanel
                             appName={application.metadata.name}
                             type={application.spec.source.chart && 'helm'}
                             revision={application.status.sync.revision}
-                            showInfo={showMetadataInfo && (info => showMetadataInfo(application.status.sync.revision, info))}
+                            showInfo={}
                         />
                     )}
                 </div>
             </div>
             {appOperationState && (
-                <div className='application-status-panel__item columns small-4 '>
+                <div className='application-status-panel__item'>
+                    {sectionLabel(
+                        'LAST SYNC',
+                        'Whether or not your last app sync was successful. It has been ' + daysSinceLastSynchronized + ' days since last sync. Click for the status of that sync.'
+                    )}
                     <div className={`application-status-panel__item-value application-status-panel__item-value--${appOperationState.phase}`}>
                         <a onClick={() => showOperation && showOperation()}>
-                            <OperationState app={application} />
-                            <HelpIcon
-                                title={
-                                    'Whether or not your last app sync was successful. It has been ' +
-                                    daysSinceLastSynchronized +
-                                    ' days since last sync. Click for the status of that sync.'
-                                }
-                            />
+                            <OperationState app={application} />{' '}
                         </a>
+                        {appOperationState.syncResult && appOperationState.syncResult.revision && (
+                            <div className='application-status-panel__item-value__revision'>
+                                To <Revision repoUrl={application.spec.source.repoURL} revision={appOperationState.syncResult.revision} />
+                            </div>
+                        )}
                     </div>
-                    {appOperationState.syncResult && appOperationState.syncResult.revision && (
-                        <div className='application-status-panel__item-name'>
-                            To <Revision repoUrl={application.spec.source.repoURL} revision={appOperationState.syncResult.revision} />
-                        </div>
-                    )}
+
                     <div className='application-status-panel__item-name'>
                         {appOperationState.phase} <Timestamp date={appOperationState.finishedAt || appOperationState.startedAt} />
                     </div>
@@ -96,11 +136,24 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
                 </div>
             )}
             {application.status.conditions && (
-                <div className={`application-status-panel__item columns small-2`}>
-                    <div className='application-status-panel__item-value' onClick={() => showConditions && showConditions()}>
-                        {cntByCategory.get('info') && <a className='info'>{cntByCategory.get('info')} Info</a>}
-                        {cntByCategory.get('warning') && <a className='warning'>{cntByCategory.get('warning')} Warnings</a>}
-                        {cntByCategory.get('error') && <a className='error'>{cntByCategory.get('error')} Errors</a>}
+                <div className={`application-status-panel__item`}>
+                    {sectionLabel('APP CONDITIONS')}
+                    <div className='application-status-panel__item-value application-status-panel__conditions' onClick={() => showConditions && showConditions()}>
+                        {infos && (
+                            <a className='info'>
+                                <i className='fa fa-info-circle' /> {infos} Info
+                            </a>
+                        )}
+                        {warnings && (
+                            <a className='warning'>
+                                <i className='fa fa-exclamation-triangle' /> {warnings} Warning{warnings !== 1 && 's'}
+                            </a>
+                        )}
+                        {errors && (
+                            <a className='error'>
+                                <i className='fa fa-exclamation-circle' /> {errors} Error{errors !== 1 && 's'}
+                            </a>
+                        )}
                     </div>
                 </div>
             )}
@@ -112,7 +165,7 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
                 }}>
                 {(data: models.ApplicationSyncWindowState) => (
                     <React.Fragment>
-                        <div className='application-status-panel__item columns small-2' style={{position: 'relative'}}>
+                        <div className='application-status-panel__item columns' style={{position: 'relative'}}>
                             <div className='application-status-panel__item-value'>
                                 {data.assignedWindows && (
                                     <React.Fragment>
