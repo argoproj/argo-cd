ad1dd80bd4e12a46231f8d71974ebbba37126ef3
Remington Breeze <remington@breeze.software>
2021-02-16 10:34:29 -0800

    fix(ui): improve spacing of app status panel
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
@@ -64,6 +64,9 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
     const warnings = cntByCategory.get('warning');
     const errors = cntByCategory.get('error');
 
+    application.status.health.message =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
     return (
         <div className='application-status-panel row'>
             <div className='application-status-panel__item'>
@@ -73,7 +76,7 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
                     &nbsp;
                     {application.status.health.status}
                 </div>
-                <div className='application-status-panel__item-name'>{application.status.health.message}</div>
+                {application.status.health.message && <div className='application-status-panel__item-name'>{application.status.health.message}</div>}
             </div>
             <div className='application-status-panel__item'>
                 <React.Fragment>
@@ -102,7 +105,7 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
                     <React.Fragment>
                         {sectionHeader(
                             {
-                                title: 'LAST SYNC STATUS',
+                                title: 'LAST SYNC RESULT',
                                 helpContent:
                                     'Whether or not your last app sync was successful. It has been ' +
                                     daysSinceLastSynchronized +
