5d9309be946a293a9647021f54608cd7608f0034
Remington Breeze <remington@breeze.software>
2021-02-16 10:36:30 -0800

    Remove lorem ipsum text
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
@@ -64,9 +64,6 @@ export const ApplicationStatusPanel = ({application, showOperation, showConditio
     const warnings = cntByCategory.get('warning');
     const errors = cntByCategory.get('error');
 
-    application.status.health.message =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
     return (
         <div className='application-status-panel row'>
             <div className='application-status-panel__item'>
