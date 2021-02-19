dd3839c3a0f5d3bd8f4a0f265623d9e3412051d4
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-02-19 14:34:11 -0800

    fix: 'argocd app wait --suspended' stuck if operation is in progress (#5511)
    * fix: 'argocd app wait --suspended' stuck if operation is in progress
    
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- test/e2e/sync_waves_test.go
+++ test/e2e/sync_waves_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	. "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
+	. "github.com/argoproj/argo-cd/test/e2e/fixture"
 	. "github.com/argoproj/argo-cd/test/e2e/fixture/app"
 
 	"github.com/argoproj/gitops-engine/pkg/health"
@@ -16,23 +17,30 @@ func TestFixingDegradedApp(t *testing.T) {
 		When().
 		IgnoreErrors().
 		Create().
-		PatchFile("pod-1.yaml", `[{"op": "replace", "path": "/spec/containers/0/image", "value": "rubbish"}]`).
+		And(func() {
+			SetResourceOverrides(map[string]ResourceOverride{
+				"ConfigMap": {
+					HealthLua: `return { status = obj.metadata.annotations and obj.metadata.annotations['health'] or 'Degraded' }`,
+				},
+			})
+		}).
 		Sync().
 		Then().
 		Expect(OperationPhaseIs(OperationFailed)).
 		Expect(SyncStatusIs(SyncStatusCodeOutOfSync)).
 		Expect(HealthIs(health.HealthStatusMissing)).
 		Expect(ResourceResultNumbering(1)).
-		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeSynced)).
-		Expect(ResourceHealthIs("Pod", "pod-1", health.HealthStatusDegraded)).
-		Expect(ResourceSyncStatusIs("Pod", "pod-2", SyncStatusCodeOutOfSync)).
-		Expect(ResourceHealthIs("Pod", "pod-2", health.HealthStatusMissing)).
+		Expect(ResourceSyncStatusIs("ConfigMap", "cm-1", SyncStatusCodeSynced)).
+		Expect(ResourceHealthIs("ConfigMap", "cm-1", health.HealthStatusDegraded)).
+		Expect(ResourceSyncStatusIs("ConfigMap", "cm-2", SyncStatusCodeOutOfSync)).
+		Expect(ResourceHealthIs("ConfigMap", "cm-2", health.HealthStatusMissing)).
 		When().
-		PatchFile("pod-1.yaml", `[{"op": "replace", "path": "/spec/containers/0/image", "value": "nginx:1.17.4-alpine"}]`).
+		PatchFile("cm-1.yaml", `[{"op": "replace", "path": "/metadata/annotations/health", "value": "Healthy"}]`).
+		PatchFile("cm-2.yaml", `[{"op": "replace", "path": "/metadata/annotations/health", "value": "Healthy"}]`).
 		// need to force a refresh here
 		Refresh(RefreshTypeNormal).
 		Then().
-		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeOutOfSync)).
+		Expect(ResourceSyncStatusIs("ConfigMap", "cm-1", SyncStatusCodeOutOfSync)).
 		When().
 		Sync().
 		Then().
@@ -40,10 +48,10 @@ func TestFixingDegradedApp(t *testing.T) {
 		Expect(SyncStatusIs(SyncStatusCodeSynced)).
 		Expect(HealthIs(health.HealthStatusHealthy)).
 		Expect(ResourceResultNumbering(2)).
-		Expect(ResourceSyncStatusIs("Pod", "pod-1", SyncStatusCodeSynced)).
-		Expect(ResourceHealthIs("Pod", "pod-1", health.HealthStatusHealthy)).
-		Expect(ResourceSyncStatusIs("Pod", "pod-2", SyncStatusCodeSynced)).
-		Expect(ResourceHealthIs("Pod", "pod-2", health.HealthStatusHealthy))
+		Expect(ResourceSyncStatusIs("ConfigMap", "cm-1", SyncStatusCodeSynced)).
+		Expect(ResourceHealthIs("ConfigMap", "cm-1", health.HealthStatusHealthy)).
+		Expect(ResourceSyncStatusIs("ConfigMap", "cm-2", SyncStatusCodeSynced)).
+		Expect(ResourceHealthIs("ConfigMap", "cm-2", health.HealthStatusHealthy))
 }
 
 func TestOneProgressingDeploymentIsSucceededAndSynced(t *testing.T) {
