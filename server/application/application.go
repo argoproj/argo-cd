d6c5c72eb4503746fa1d89a9a642805d8f6cf51a
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-02-25 19:24:40 -0800

    fix: API server should not print resource body when resource update fails (#5617)
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- server/application/application.go
+++ server/application/application.go
@@ -899,6 +899,10 @@ func (s *Server) PatchResource(ctx context.Context, q *application.ApplicationRe
 
 	manifest, err := s.kubectl.PatchResource(ctx, config, res.GroupKindVersion(), res.Name, res.Namespace, types.PatchType(q.PatchType), []byte(q.Patch))
 	if err != nil {
+		// don't expose real error for secrets since it might contain secret data
+		if res.Kind == kube.SecretKind && res.Group == "" {
+			return nil, fmt.Errorf("failed to patch Secret %s/%s", res.Namespace, res.Name)
+		}
 		return nil, err
 	}
 	manifest, err = replaceSecretValues(manifest)
