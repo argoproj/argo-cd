cd147263c877850a838102291dfc6957ecbba347
Jonah Back <jonah@jonahback.com>
2021-03-12 23:10:29 -0800

    fix: don't assume x86 when downloading protoc
    Signed-off-by: Jonah Back <jonah@jonahback.com>

--- hack/installers/install-protoc-linux.sh
+++ hack/installers/install-protoc-linux.sh
@@ -3,7 +3,15 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-x86_64.zip
+case $ARCHITECTURE in
+  arm|arm64)
+    [ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-aarch_64.zip
+    ;;
+  *)
+    [ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-x86_64.zip
+    ;;
+esac
+
 unzip $DOWNLOADS/protoc.zip bin/protoc -d /usr/local/
 chmod +x /usr/local/bin/protoc
 unzip $DOWNLOADS/protoc.zip include/* -d /usr/local/
