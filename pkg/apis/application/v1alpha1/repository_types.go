586eb91d4d539be06865ce245abea88b7779508c
Shoubhik Bose <shbose@redhat.com>
2021-03-29 02:21:57 -0400

    chore: make lint happy
    Signed-off-by: Shoubhik Bose <shbose@redhat.com>

--- pkg/apis/application/v1alpha1/repository_types.go
+++ pkg/apis/application/v1alpha1/repository_types.go
@@ -7,6 +7,7 @@ import (
 	"github.com/argoproj/argo-cd/util/cert"
 	"github.com/argoproj/argo-cd/util/git"
 	"github.com/argoproj/argo-cd/util/helm"
+
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
