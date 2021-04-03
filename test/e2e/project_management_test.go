ad76747ca46d88de400359fbdca5462545b4914a
Shoubhik Bose <shbose@redhat.com>
2021-04-02 23:00:37 -0400

    fix: missed conflict
    Signed-off-by: Shoubhik Bose <shbose@redhat.com>

--- test/e2e/project_management_test.go
+++ test/e2e/project_management_test.go
@@ -15,16 +15,9 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/utils/pointer"
 
-<<<<<<< HEAD
-	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	"github.com/argoproj/argo-cd/test/e2e/fixture"
-	"github.com/argoproj/argo-cd/util/argo"
-=======
-	"github.com/argoproj/argo-cd/v2/common"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v2/test/e2e/fixture"
 	"github.com/argoproj/argo-cd/v2/util/argo"
->>>>>>> de522bf43bd53b9f81c13095e0288c529032eb27
 )
 
 func assertProjHasEvent(t *testing.T, a *v1alpha1.AppProject, message string, reason string) {
