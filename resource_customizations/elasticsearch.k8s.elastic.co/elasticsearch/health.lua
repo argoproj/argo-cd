eb542054a83ca746a8ffbfc33f1eb0b6e9cc3a9a
Sebastian Greco <pup_seba@yahoo.com>
2021-04-05 20:30:38 +0200

    Add additional nil value check.
    Signed-off-by: Sebastian Greco <pup_seba@yahoo.com>

--- resource_customizations/elasticsearch.k8s.elastic.co/elasticsearch/health.lua
+++ resource_customizations/elasticsearch.k8s.elastic.co/elasticsearch/health.lua
@@ -1,31 +1,33 @@
 hs = {}
 if obj.status ~= nil then
-  if obj.status.phase == "Ready" then
-    if obj.status.health == "green" then
-      hs.status = "Healthy"
-      hs.message = "Elasticsearch Cluster status is Green"
+  if obj.status.phase ~= nil and obj.status.health ~= nil then
+    if obj.status.phase == "Ready" then
+      if obj.status.health == "green" then
+        hs.status = "Healthy"
+        hs.message = "Elasticsearch Cluster status is Green"
+        return hs
+      elseif obj.status.health == "yellow" then
+        hs.status = "Degraded"
+        hs.message = "Elasticsearch Cluster status is Yellow"
+        return hs
+      elseif obj.status.health == "red" then
+        hs.status = "Degraded"
+        hs.message = "Elasticsearch Cluster status is Red"
+        return hs
+      end
+    elseif obj.status.phase == "ApplyingChanges" then
+      hs.status = "Progressing" 
+      hs.message = "Elasticsearch phase is ApplyingChanges"
       return hs
-    elseif obj.status.health == "yellow" then
-      hs.status = "Degraded"
-      hs.message = "Elasticsearch Cluster status is Yellow"
+    elseif obj.status.phase == "MigratingData" then
+      hs.status = "Progressing"
+      hs.message = "Elasticsearch phase is MigratingData"
       return hs
-    elseif obj.status.health == "red" then
+    elseif obj.status.phase == "Invalid" then
       hs.status = "Degraded"
-      hs.message = "Elasticsearch Cluster status is Red"
+      hs.message = "Elasticsearch phase is Invalid"
       return hs
     end
-  elseif obj.status.phase == "ApplyingChanges" then
-    hs.status = "Progressing" 
-    hs.message = "Elasticsearch phase is ApplyingChanges"
-    return hs
-  elseif obj.status.phase == "MigratingData" then
-    hs.status = "Progressing"
-    hs.message = "Elasticsearch phase is MigratingData"
-    return hs
-  elseif obj.status.phase == "Invalid" then
-    hs.status = "Degraded"
-    hs.message = "Elasticsearch phase is Invalid"
-    return hs
   end
 end
 
