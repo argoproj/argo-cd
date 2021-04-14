8d0f2d7cd5b084d7d6d11787dca2b536b6e1b57e
Dmitrii Kashin <freehck@freehck.com>
2021-04-14 09:18:10 +0300

    CR-4058: cleanup for linting

--- util/gpg/gpg_test.go
+++ util/gpg/gpg_test.go
@@ -87,27 +87,6 @@ func Test_GPG_InitializeGnuPG(t *testing.T) {
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "does not point to a directory")
 
-// THIS CODE WORKS ONLY FOR NON-ROOT BUILDS, SO DISABLE IT
-//	// Unaccessible GNUPGHOME
-//	p = initTempDir()
-//	defer os.RemoveAll(p)
-//	fp := fmt.Sprintf("%s/gpg", p)
-//	err = os.Mkdir(fp, 0000)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//	os.Setenv(common.EnvGnuPGHome, fp)
-//	err = InitializeGnuPG()
-//	assert.Error(t, err)
-//	// Restore permissions so path can be deleted
-//	err = os.Chmod(fp, 0700)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-
 	// GNUPGHOME with too wide permissions
 	// We do not expect an error here, because of openshift's random UIDs that
 	// forced us to use an emptyDir mount (#4127)
