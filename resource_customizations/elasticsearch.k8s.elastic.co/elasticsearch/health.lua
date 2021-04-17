85a4db82be2d40e25937308a8d4c3bd8639ee4e8
Sebastian Greco <pup_seba@yahoo.com>
2021-04-17 12:36:47 +0200

    Improve messages and add new healthcheck
    Signed-off-by: Sebastian Greco <pup_seba@yahoo.com>

--- resource_customizations/elasticsearch.k8s.elastic.co/elasticsearch/health.lua
+++ resource_customizations/elasticsearch.k8s.elastic.co/elasticsearch/health.lua
@@ -1,32 +1,44 @@
 hs = {}
 if obj.status ~= nil then
-  if obj.status.phase ~= nil and obj.status.health ~= nil then
-    if obj.status.phase == "Ready" then
-      if obj.status.health == "green" then
-        hs.status = "Healthy"
-        hs.message = "Elasticsearch Cluster status is Green"
-        return hs
-      elseif obj.status.health == "yellow" then
-        hs.status = "Degraded"
-        hs.message = "Elasticsearch Cluster status is Yellow"
-        return hs
-      elseif obj.status.health == "red" then
-        hs.status = "Degraded"
-        hs.message = "Elasticsearch Cluster status is Red"
-        return hs
-      end
-    elseif obj.status.phase == "ApplyingChanges" then
-      hs.status = "Progressing" 
-      hs.message = "Elasticsearch phase is ApplyingChanges"
-      return hs
-    elseif obj.status.phase == "MigratingData" then
+  if obj.status.availableNodes ~= nil then
+    local sum = 0
+    for _,node in pairs(obj.spec.nodeSets) do
+      sum = sum + node.count
+    end
+    if obj.status.availableNodes < sum then
       hs.status = "Progressing"
-      hs.message = "Elasticsearch phase is MigratingData"
+      hs.message = "The desired amount of nodes is " .. sum .. " and the availableNodes are " .. obj.status.availableNodes
       return hs
-    elseif obj.status.phase == "Invalid" then
-      hs.status = "Degraded"
-      hs.message = "Elasticsearch phase is Invalid"
-      return hs
+    elseif obj.status.availableNodes == sum then
+      if obj.status.phase ~= nil and obj.status.health ~= nil then
+        if obj.status.phase == "Ready" then
+          if obj.status.health == "green" then
+            hs.status = "Healthy"
+            hs.message = "Elasticsearch Cluster status is Green"
+            return hs
+          elseif obj.status.health == "yellow" then
+            hs.status = "Degraded"
+            hs.message = "Elasticsearch Cluster status is Yellow. Check the status of your ES indices, shards and replicas"
+            return hs
+          elseif obj.status.health == "red" then
+            hs.status = "Degraded"
+            hs.message = "Elasticsearch Cluster status is Red. Check the status of your ES indices, shards and replicas"
+            return hs
+          end
+        elseif obj.status.phase == "ApplyingChanges" then
+          hs.status = "Progressing"
+          hs.message = "Elasticsearch phase is ApplyingChanges"
+          return hs
+        elseif obj.status.phase == "MigratingData" then
+          hs.status = "Progressing"
+          hs.message = "Elasticsearch phase is MigratingData"
+          return hs
+        elseif obj.status.phase == "Invalid" then
+          hs.status = "Degraded"
+          hs.message = "Elasticsearch phase is Invalid"
+          return hs
+        end
+      end
     end
   end
 end
