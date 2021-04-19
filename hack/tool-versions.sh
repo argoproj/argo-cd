7aef4b9ab16af583f24539478394a168ce7d7417
May Zhang <may_zhang@intuit.com>
2021-04-19 11:56:30 -0700

    fix: trigger build
    Signed-off-by: May Zhang <may_zhang@intuit.com>

--- hack/tool-versions.sh
+++ hack/tool-versions.sh
@@ -8,8 +8,6 @@ ksonnet_version=0.13.1
 kubectl_version=1.17.8
 kubectx_version=0.6.3
 kustomize4_version=4.1.2
-kustomize3_version=3.9.4
 packr_version=1.21.9
 protoc_version=3.7.1
 swagger_version=0.19.0
-
