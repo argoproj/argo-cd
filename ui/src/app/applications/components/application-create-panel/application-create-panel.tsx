49454f0d8b1c2232e5a4934cc080ab436d97236d
Remington Breeze <remington@breeze.software>
2021-04-21 16:53:35 +0000

    revert create panel changes
    Signed-off-by: GitHub <noreply@github.com>

--- ui/src/app/applications/components/application-create-panel/application-create-panel.tsx
+++ ui/src/app/applications/components/application-create-panel/application-create-panel.tsx
@@ -8,7 +8,7 @@ import {services} from '../../../shared/services';
 import {ApplicationParameters} from '../application-parameters/application-parameters';
 import {ApplicationSyncOptionsField} from '../application-sync-options/application-sync-options';
 import {RevisionFormField} from '../revision-form-field/revision-form-field';
-import {useDebounce} from '../utils';
+// import {useDebounce} from '../utils';
 
 const jsonMergePatch = require('json-merge-patch');
 
@@ -105,7 +105,7 @@ export const ApplicationCreatePanel = (props: {
     const [yamlMode, setYamlMode] = React.useState(false);
     const [explicitPathType, setExplicitPathType] = React.useState<{path: string; type: models.AppSourceType}>(null);
     const [destFormat, setDestFormat] = React.useState('URL');
-    const [repoURL, setIntermediateRepoURL] = useDebounce<string>('', 500);
+    // const [repoURL, setIntermediateRepoURL] = useDebounce<string>('', 500);
 
     function normalizeTypeFields(formApi: FormApi, type: models.AppSourceType) {
         const app = formApi.getFormState().values;
@@ -129,6 +129,7 @@ export const ApplicationCreatePanel = (props: {
                     ]).then(([projects, clusters, reposInfo]) => ({projects, clusters, reposInfo}))
                 }>
                 {({projects, clusters, reposInfo}) => {
+                    const repos = reposInfo.map(info => info.repo).sort();
                     const app = deepMerge(DEFAULT_APP, props.app || {});
                     const repoInfo = reposInfo.find(info => info.repo === app.spec.source.repoURL);
                     if (repoInfo) {
@@ -232,15 +233,14 @@ export const ApplicationCreatePanel = (props: {
                                                 <p>SOURCE</p>
                                                 <div className='row argo-form-row'>
                                                     <div className='columns small-10'>
-                                                        <input
-                                                            className='argo-field'
-                                                            placeholder='Repository URL'
-                                                            value={repoURL.intermediate}
-                                                            onChange={e => {
-                                                                setIntermediateRepoURL(e.target.value);
-                                                                api.setValue('spec.source.repoURL', repoURL.value);
-                                                            }}
-                                                        />
+                                                        <FormField
+                                                             formApi={api}
+                                                             label='Repository URL'
+                                                             qeId='application-create-field-repository-url'
+                                                             field='spec.source.repoURL'
+                                                             component={AutocompleteField}
+                                                             componentProps={{items: repos}}
+                                                         />
                                                     </div>
                                                     <div className='columns small-2'>
                                                         <div style={{paddingTop: '1.5em'}}>
@@ -277,7 +277,7 @@ export const ApplicationCreatePanel = (props: {
                                                         <RevisionFormField formApi={api} helpIconTop={'2.5em'} repoURL={app.spec.source.repoURL} />
                                                         <div className='argo-form-row'>
                                                             <DataLoader
-                                                                input={{repoURL: repoURL.value, revision: app.spec.source.targetRevision}}
+                                                                input={{repoURL: app.spec.source.repoURL, revision: app.spec.source.targetRevision}}
                                                                 load={async src =>
                                                                     (src.repoURL &&
                                                                         services.repos
