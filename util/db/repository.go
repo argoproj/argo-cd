2f0ef2c22df3387161aba64b37f8ee20bda430a9
Jan Gräfen <223234+jangraefen@users.noreply.github.com>
2021-05-02 22:25:52 +0200

    Add missing constructor variable
    Signed-off-by: Jan Gräfen <223234+jangraefen@users.noreply.github.com>

--- util/db/repository.go
+++ util/db/repository.go
@@ -48,58 +48,58 @@ type repositoryBackend interface {
 }
 
 func (db *db) CreateRepository(ctx context.Context, r *appsv1.Repository) (*appsv1.Repository, error) {
-	return (&settingRepositoryBackend{}).CreateRepository(ctx, r)
+	return (&settingRepositoryBackend{db: db}).CreateRepository(ctx, r)
 }
 
 func (db *db) GetRepository(ctx context.Context, repoURL string) (*appsv1.Repository, error) {
-	return (&settingRepositoryBackend{}).GetRepository(ctx, repoURL)
+	return (&settingRepositoryBackend{db: db}).GetRepository(ctx, repoURL)
 }
 
 func (db *db) ListRepositories(ctx context.Context) ([]*appsv1.Repository, error) {
-	return (&settingRepositoryBackend{}).ListRepositories(ctx, nil)
+	return (&settingRepositoryBackend{db: db}).ListRepositories(ctx, nil)
 }
 
 func (db *db) listRepositories(ctx context.Context, repoType *string) ([]*appsv1.Repository, error) {
-	return (&settingRepositoryBackend{}).ListRepositories(ctx, repoType)
+	return (&settingRepositoryBackend{db: db}).ListRepositories(ctx, repoType)
 }
 
 func (db *db) UpdateRepository(ctx context.Context, r *appsv1.Repository) (*appsv1.Repository, error) {
-	return (&settingRepositoryBackend{}).UpdateRepository(ctx, r)
+	return (&settingRepositoryBackend{db: db}).UpdateRepository(ctx, r)
 }
 
 func (db *db) DeleteRepository(ctx context.Context, repoURL string) error {
-	return (&settingRepositoryBackend{}).DeleteRepository(ctx, repoURL)
+	return (&settingRepositoryBackend{db: db}).DeleteRepository(ctx, repoURL)
 }
 
 // ListRepositoryCredentials returns a list of URLs that contain repo credential sets
 func (db *db) ListRepositoryCredentials(ctx context.Context) ([]string, error) {
-	return (&settingRepositoryBackend{}).ListRepoCreds(ctx)
+	return (&settingRepositoryBackend{db: db}).ListRepoCreds(ctx)
 }
 
 // GetRepositoryCredentials retrieves a repository credential set
 func (db *db) GetRepositoryCredentials(ctx context.Context, repoURL string) (*appsv1.RepoCreds, error) {
-	return (&settingRepositoryBackend{}).GetRepoCreds(ctx, repoURL)
+	return (&settingRepositoryBackend{db: db}).GetRepoCreds(ctx, repoURL)
 }
 
 // GetAllHelmRepositoryCredentials retrieves all repository credentials
 func (db *db) GetAllHelmRepositoryCredentials(ctx context.Context) ([]*appsv1.RepoCreds, error) {
-	return (&settingRepositoryBackend{}).GetAllHelmRepoCreds(ctx)
+	return (&settingRepositoryBackend{db: db}).GetAllHelmRepoCreds(ctx)
 }
 
 // CreateRepositoryCredentials creates a repository credential set
 func (db *db) CreateRepositoryCredentials(ctx context.Context, r *appsv1.RepoCreds) (*appsv1.RepoCreds, error) {
-	return (&settingRepositoryBackend{}).CreateRepoCreds(ctx, r)
+	return (&settingRepositoryBackend{db: db}).CreateRepoCreds(ctx, r)
 }
 
 // UpdateRepositoryCredentials updates a repository credential set
 func (db *db) UpdateRepositoryCredentials(ctx context.Context, r *appsv1.RepoCreds) (*appsv1.RepoCreds, error) {
-	return (&settingRepositoryBackend{}).UpdateRepoCreds(ctx, r)
+	return (&settingRepositoryBackend{db: db}).UpdateRepoCreds(ctx, r)
 }
 
 // DeleteRepositoryCredentials deletes a repository credential set from config, and
 // also all the secrets which actually contained the credentials.
 func (db *db) DeleteRepositoryCredentials(ctx context.Context, name string) error {
-	return (&settingRepositoryBackend{}).DeleteRepoCreds(ctx, name)
+	return (&settingRepositoryBackend{db: db}).DeleteRepoCreds(ctx, name)
 }
 
 // getRepositoryCredentialIndex returns the index of the best matching repository credential
