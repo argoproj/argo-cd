f3c495feb5fea914f17caa80fbe323698f115930
Balazs Zachar <zachar.balazs@gmail.com>
2021-06-29 21:04:11 +0200

    feat: extend TridentOrchestrator check with degraded state (based on official documentation)
    Signed-off-by: Balazs Zachar <zachar.balazs@gmail.com>

--- resource_customizations/trident.netapp.io/TridentOrchestrator/health.lua
+++ resource_customizations/trident.netapp.io/TridentOrchestrator/health.lua
@@ -5,6 +5,11 @@ if obj.status ~= nil then
     hs.message = obj.status.message
     return hs
   end
+  if obj.status.status == "Failed" or obj.status.status == "Error" then
+    hs.status = "Degraded"
+    hs.message = obj.status.message
+    return hs
+  end
 end
 hs.status = "Progressing"
 hs.message = "Waiting for trident installation"
