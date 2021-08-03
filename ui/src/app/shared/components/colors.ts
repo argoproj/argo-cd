4fef2114fe804936ddfe2280253ad954ab549787
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-08-03 16:05:08 -0700

    feat: Improve Replace sync option description in UI (#6899)
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- ui/src/app/shared/components/colors.ts
+++ ui/src/app/shared/components/colors.ts
@@ -1,9 +1,9 @@
-const ARGO_SUCCESS_COLOR = '#18BE94';
-const ARGO_WARNING_COLOR = '#f4c030';
-const ARGO_FAILED_COLOR = '#E96D76';
-const ARGO_RUNNING_COLOR = '#0DADEA';
-const ARGO_GRAY4_COLOR = '#CCD6DD';
-const ARGO_TERMINATING_COLOR = '#DE303D';
+export const ARGO_SUCCESS_COLOR = '#18BE94';
+export const ARGO_WARNING_COLOR = '#f4c030';
+export const ARGO_FAILED_COLOR = '#E96D76';
+export const ARGO_RUNNING_COLOR = '#0DADEA';
+export const ARGO_GRAY4_COLOR = '#CCD6DD';
+export const ARGO_TERMINATING_COLOR = '#DE303D';
 
 export const COLORS = {
     connection_status: {
