2480cdf8ef205f18ca27eb799afecd0942b99419
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2021-08-10 17:10:24 -0700

    fix: applications/resources filter improvement and bug fixes (#6931)
    * fix: applications/resources filter improvement and bug fixes
    
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Signed-off-by: benmanifold <benmanifold@gmail.com>

--- ui/src/app/applications/components/applications-list/applications-tiles.tsx
+++ ui/src/app/applications/components/applications-list/applications-tiles.tsx
@@ -217,7 +217,7 @@ export const ApplicationTiles = ({applications, syncApplication, refreshApplicat
                                                         refreshApplication(app.metadata.name);
                                                     }}>
                                                     <i className={classNames('fa fa-redo', {'status-icon--spin': AppUtils.isAppRefreshing(app)})} />{' '}
-                                                    <span className='show-for-xlarge'>Refresh</span>
+                                                    <span className='show-for-xxlarge'>Refresh</span>
                                                 </a>
                                                 &nbsp;
                                                 <a
@@ -227,7 +227,7 @@ export const ApplicationTiles = ({applications, syncApplication, refreshApplicat
                                                         e.stopPropagation();
                                                         deleteApplication(app.metadata.name);
                                                     }}>
-                                                    <i className='fa fa-times-circle' /> Delete
+                                                    <i className='fa fa-times-circle' /> <span className='show-for-xxlarge'>Delete</span>
                                                 </a>
                                             </div>
                                         </div>
