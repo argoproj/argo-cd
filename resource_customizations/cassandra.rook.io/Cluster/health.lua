110f377ffee3356c5ac55e2774adce7238ebaeee
benmanifold <benmanifold@gmail.com>
2021-08-16 10:26:06 -0400

    Fixing indents
    Signed-off-by: benmanifold <benmanifold@gmail.com>

--- resource_customizations/cassandra.rook.io/Cluster/health.lua
+++ resource_customizations/cassandra.rook.io/Cluster/health.lua
@@ -1,22 +1,23 @@
 hs = {}
 if obj.status ~= nil then
-if obj.status.racks ~= nil then
+  if obj.status.racks ~= nil then
     all_racks_good = true
     for key, value in pairs(obj.status.racks) do
-    if all_racks_good and value.members ~= nil and value.readyMembers ~= nil and value.members ~= value.readyMembers then
+      if all_racks_good and value.members ~= nil and value.readyMembers ~= nil and value.members ~= value.readyMembers then
         all_racks_good = false
         break
-    end
+      end
     end
     if all_racks_good then
-    hs.status = "Healthy"
+      hs.status = "Healthy"
     else
-    hs.status = "Progressing"
-    hs.message = "Waiting for Cassandra Cluster"
+      hs.status = "Progressing"
+      hs.message = "Waiting for Cassandra Cluster"
     end
     return hs
+  end
 end
-end
+
 hs.status = "Progressing"
 hs.message = "Waiting for Cassandra Cluster"
 return hs
