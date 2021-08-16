4d33179276abe3e26020f0897ee977fc3bd90652
pashavictorovich <pavel@codefresh.io>
2021-08-16 16:57:49 +0300

    rerun tests
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- server/repository/repository_test.go
+++ server/repository/repository_test.go
@@ -9,7 +9,6 @@ import (
 func Test_createRBACObject(t *testing.T) {
 	object := createRBACObject("test-prj", "test-repo")
 	assert.Equal(t, "test-prj/test-repo", object)
-
 	objectWithoutPrj := createRBACObject("", "test-repo")
 	assert.Equal(t, "test-repo", objectWithoutPrj)
 }
