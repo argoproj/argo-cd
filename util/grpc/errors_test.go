90d888096885095cb2d813265e2e865e7fba7ca2
olegz-codefresh <olegz@codefresh.io>
2021-08-17 12:45:27 +0300

    https://github.com/argoproj/argo-cd/issues/6424
    Signed-off-by: olegz-codefresh <olegz@codefresh.io>

---
+++ util/grpc/errors_test.go
@@ -0,0 +1,35 @@
+package grpc
+
+import (
+	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_gitErrToGRPC(t *testing.T) {
+	var ok bool
+	assert.Equal(t, gitErrToGRPC(nil), nil)
+
+	defaultErrorMsg := "default error"
+	defaultError := gitErrToGRPC(errors.New(defaultErrorMsg))
+	_, ok = defaultError.(interface{ GRPCStatus() *status.Status })
+	assert.Equal(t, ok, false)
+	assert.Equal(t, defaultError.Error(), defaultErrorMsg)
+
+	grpcErrorMsg := "grpc error"
+	grpcError := gitErrToGRPC(status.Errorf(codes.Unknown, grpcErrorMsg))
+	se, ok := grpcError.(interface{ GRPCStatus() *status.Status })
+	assert.Equal(t, ok, true)
+	assert.Equal(t, se.GRPCStatus().Code(), codes.Unknown)
+	assert.Equal(t, se.GRPCStatus().Message(), grpcErrorMsg)
+
+	notFoundMsg := "repository not found"
+	notFound := gitErrToGRPC(status.Errorf(codes.NotFound, notFoundMsg))
+	se1, ok := notFound.(interface{ GRPCStatus() *status.Status })
+	assert.Equal(t, ok, true)
+	assert.Equal(t, se1.GRPCStatus().Code(), codes.NotFound)
+	assert.Equal(t, se1.GRPCStatus().Message(), notFoundMsg)
+}
