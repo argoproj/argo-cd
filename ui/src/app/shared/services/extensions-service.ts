c683dc86e96c3c1c36e1c13be325192960400e45
Remington Breeze <remington@breeze.software>
2021-08-24 11:23:56 -0700

    fix(ui): Add State to props passed to Extensions (#7045)
    Signed-off-by: Remington Breeze <remington@breeze.software>
    Co-authored-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Signed-off-by: viktorplakida <plakyda1@gmail.com>

--- ui/src/app/shared/services/extensions-service.ts
+++ ui/src/app/shared/services/extensions-service.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {ApplicationTree, ResourceNode} from '../models';
+import {ApplicationTree, State} from '../models';
 
 const extensions: {[key: string]: Extension} = {};
 const cache = new Map<string, Promise<Extension>>();
@@ -9,7 +9,7 @@ export interface Extension {
 }
 
 export interface ExtensionComponentProps {
-    node: ResourceNode;
+    resource: State;
     tree: ApplicationTree;
 }
 
