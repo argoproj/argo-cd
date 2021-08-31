fa913ceafec0bd3895d5911e51d9b315fe09ce57
pashavictorovich <pavel@codefresh.io>
2021-08-31 13:12:49 +0300

    improve clear state and add cluster tests
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -52,16 +52,10 @@ func TestClusterGet(t *testing.T) {
 	defer RecordTestRun(t)
 	output := FailOnErr(RunCli("cluster", "get", "https://kubernetes.default.svc")).(string)
 
-	assert.Contains(t, output, fmt.Sprintf(`
-name: test-cluster-add
-project: %s
-server: https://kubernetes.default.svc
-serverVersion: %v`, ProjectName, GetVersions().ServerVersion))
-
 	assert.Contains(t, output, "name: test-cluster-add")
 	assert.Contains(t, output, "server: https://kubernetes.default.svc")
 	assert.Contains(t, output, fmt.Sprintf(`project: %s`, ProjectName))
-	assert.Contains(t, output, fmt.Sprintf(`serverVersion: %v`, GetVersions().ServerVersion))
+	assert.Contains(t, output, fmt.Sprintf(`serverVersion: "%v"`, GetVersions().ServerVersion))
 
 	assert.Contains(t, output, `config:
   tlsClientConfig:
