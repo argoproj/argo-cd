a9f009cb10765d6871cbb8e17c5873f5bc0a4a95
pasha-codefresh <pavel@codefresh.io>
2021-08-31 20:32:54 +0300

    feat: e2e for clusters (#7118)
    feat: e2e for clusters (#7118)
    
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -6,16 +6,45 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	. "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	. "github.com/argoproj/argo-cd/v2/test/e2e/fixture"
+	clusterFixture "github.com/argoproj/argo-cd/v2/test/e2e/fixture/cluster"
 	. "github.com/argoproj/argo-cd/v2/util/errors"
 )
 
 func TestClusterList(t *testing.T) {
 	SkipIfAlreadyRun(t)
 	defer RecordTestRun(t)
-	output := FailOnErr(RunCli("cluster", "list")).(string)
-	assert.Equal(t, fmt.Sprintf(`SERVER                          NAME        VERSION  STATUS      MESSAGE
-https://kubernetes.default.svc  in-cluster  %v     Successful  `, GetVersions().ServerVersion), output)
+
+	clusterFixture.
+		Given(t).
+		Project(ProjectName).
+		When().
+		List().
+		Then().
+		AndCLIOutput(func(output string, err error) {
+			assert.Equal(t, fmt.Sprintf(`SERVER                          NAME        VERSION  STATUS      MESSAGE  PROJECT
+https://kubernetes.default.svc  in-cluster  %v     Successful           `, GetVersions().ServerVersion), output)
+		})
+}
+
+func TestClusterAdd(t *testing.T) {
+	SkipIfAlreadyRun(t)
+	defer RecordTestRun(t)
+
+	clusterFixture.
+		Given(t).
+		Project(ProjectName).
+		Upsert(true).
+		Server(KubernetesInternalAPIServerAddr).
+		When().
+		Create().
+		List().
+		Then().
+		AndCLIOutput(func(output string, err error) {
+			assert.Equal(t, fmt.Sprintf(`SERVER                          NAME              VERSION  STATUS      MESSAGE  PROJECT
+https://kubernetes.default.svc  test-cluster-add  %v     Successful           %s`, GetVersions().ServerVersion, ProjectName), output)
+		})
 }
 
 func TestClusterGet(t *testing.T) {
@@ -23,10 +52,10 @@ func TestClusterGet(t *testing.T) {
 	defer RecordTestRun(t)
 	output := FailOnErr(RunCli("cluster", "get", "https://kubernetes.default.svc")).(string)
 
-	assert.Contains(t, output, fmt.Sprintf(`
-name: in-cluster
-server: https://kubernetes.default.svc
-serverVersion: "%v"`, GetVersions().ServerVersion))
+	assert.Contains(t, output, "name: test-cluster-add")
+	assert.Contains(t, output, "server: https://kubernetes.default.svc")
+	assert.Contains(t, output, fmt.Sprintf(`project: %s`, ProjectName))
+	assert.Contains(t, output, fmt.Sprintf(`serverVersion: "%v"`, GetVersions().ServerVersion))
 
 	assert.Contains(t, output, `config:
   tlsClientConfig:
