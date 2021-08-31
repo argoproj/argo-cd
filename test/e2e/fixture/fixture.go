3149bb7d986fb61d63af9ff6f3a1bb2f7a2b0ee0
pasha-codefresh <pavel@codefresh.io>
2021-08-31 20:32:54 +0300

    feat: e2e for clusters (#7118)
    feat: e2e for clusters (#7118)
    
    Signed-off-by: pashavictorovich <pavel@codefresh.io>
    Signed-off-by: viktorplakida <plakyda1@gmail.com>

--- test/e2e/fixture/fixture.go
+++ test/e2e/fixture/fixture.go
@@ -51,6 +51,8 @@ const (
 	submoduleParentDir = "submoduleParent.git"
 
 	GuestbookPath = "guestbook"
+
+	ProjectName = "argo-project"
 )
 
 const (
@@ -437,6 +439,9 @@ func EnsureCleanState(t *testing.T) {
 	// kubectl delete secrets -l argocd.argoproj.io/secret-type=repo-creds
 	CheckError(KubeClientset.CoreV1().Secrets(TestNamespace()).DeleteCollection(context.Background(),
 		v1.DeleteOptions{PropagationPolicy: &policy}, v1.ListOptions{LabelSelector: common.LabelKeySecretType + "=" + common.LabelValueSecretTypeRepoCreds}))
+	// kubectl delete secrets -l argocd.argoproj.io/secret-type=cluster
+	CheckError(KubeClientset.CoreV1().Secrets(TestNamespace()).DeleteCollection(context.Background(),
+		v1.DeleteOptions{PropagationPolicy: &policy}, v1.ListOptions{LabelSelector: common.LabelKeySecretType + "=" + common.LabelValueSecretTypeCluster}))
 	// kubectl delete secrets -l e2e.argoproj.io=true
 	CheckError(KubeClientset.CoreV1().Secrets(TestNamespace()).DeleteCollection(context.Background(),
 		v1.DeleteOptions{PropagationPolicy: &policy}, v1.ListOptions{LabelSelector: testingLabel + "=true"}))
