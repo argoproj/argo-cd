22c1318eede43f6bd434e1df774c475f2e363fb6
ishitasequeira <isequeir@redhat.com>
2021-09-03 09:26:19 -0400

    Address PR comments
    Signed-off-by: ishitasequeira <isequeir@redhat.com>

--- resource_customizations/apps.openshift.io/DeploymentConfig/health.lua
+++ resource_customizations/apps.openshift.io/DeploymentConfig/health.lua
@@ -3,20 +3,17 @@ if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     numTrue = 0
     for i, condition in pairs(obj.status.conditions) do
-      if condition.type == "Available" and condition.status == "True" then
+      if (condition.type == "Available" or condition.type == "Progressing") and condition.status == "True" then
         numTrue = numTrue + 1
-      elseif condition.type == "Progressing" and condition.status == "True" then
-        numTrue = numTrue + 1
-        health_check.message = condition.message
-      elseif condition.type == "Progressing" and condition.status == "Unknown" then
-        health_check.message = condition.message
       end
     end
     if numTrue == 2 then
       health_check.status = "Healthy"
+      health_check.message = "replication controller successfully rolled out"
       return health_check
     elseif numTrue == 1 then
       health_check.status = "Progressing"
+      health_check.message = "replication controller is waiting for pods to run"
       return health_check
     else
       health_check.status = "Degraded"
@@ -25,3 +22,6 @@ if obj.status ~= nil then
     end
   end
 end
+health_check.status = "Progressing"
+health_check.message = "replication controller is waiting for pods to run"
+return health_check
