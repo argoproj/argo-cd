47c74659ef8a7710cf9de26f0d77b33b33d4a5a3
joaomo <joao.oneill@gmail.com>
2021-09-15 13:34:17 +0100

    Update health.lua
    To avoid getting the `<string>:6: attempt to index a non-table object(string) with key 'find' stack traceback: <string>:6: in main chunk [G]: ?` error everytime an external secret is in error we can cast the status to string and use  string functions

--- resource_customizations/kubernetes-client.io/ExternalSecret/health.lua
+++ resource_customizations/kubernetes-client.io/ExternalSecret/health.lua
@@ -3,9 +3,9 @@ if obj.status ~= nil then
   if obj.status.status == "SUCCESS" then
     health_status.status = "Healthy"
     health_status.message = "Fetched ExternalSecret."
-  elseif obj.status.status:find('^ERROR') ~= nil then
+  elseif tostring(obj.status.status).find('^ERROR') ~= nil then
     health_status.status = "Degraded"
-    health_status.message = obj.status.status:gsub("ERROR, ", "")
+    health_status.message = tostring(obj.status.status).gsub("ERROR, ", "")
   else
     health_status.status = "Progressing"
     health_status.message = "Waiting for ExternalSecret."
