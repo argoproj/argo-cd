a9d7aa469d13eb9c438a1a604d32fecc3073770d
pashavictorovich <pavel@codefresh.io>
2021-09-23 19:45:01 +0300

    add tests
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- util/argo/resource_tracking_test.go
+++ util/argo/resource_tracking_test.go
@@ -3,13 +3,52 @@ package argo
 import (
 	"io/ioutil"
 	"testing"
+	"time"
+
+	"github.com/argoproj/argo-cd/v2/test"
+
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/argoproj/argo-cd/v2/common"
+
+	appsv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	appsfake "github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned/fake"
+	appinformers "github.com/argoproj/argo-cd/v2/pkg/client/informers/externalversions/application/v1alpha1"
 )
+
+const fakeApp = `
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: test-app
+  namespace: default
+spec:
+  source:
+    path: some/path
+    repoURL: https://github.com/argoproj/argocd-example-apps.git
+    targetRevision: HEAD
+    ksonnet:
+      environment: default
+  destination:
+    namespace: ` + test.FakeDestNamespace + `
+    server: https://cluster-api.com
+`
+
+func createTestApp(testApp string, opts ...func(app *appsv1.Application)) *appsv1.Application {
+	var app appsv1.Application
+	err := yaml.Unmarshal([]byte(testApp), &app)
+	if err != nil {
+		panic(err)
+	}
+	for i := range opts {
+		opts[i](&app)
+	}
+	return &app
+}
 
 func TestSetAppInstanceLabel(t *testing.T) {
 	yamlBytes, err := ioutil.ReadFile("testdata/svc.yaml")
@@ -57,4 +96,13 @@ func TestSetAppInstanceAnnotationNotFound(t *testing.T) {
 
 	app := resourceTracking.GetAppName(&obj, common.LabelKeyAppInstance, TrackingMethodAnnotation)
 	assert.Equal(t, "", app)
+}
+
+func TestGetTrackingMethodFromApplicationInformer(t *testing.T) {
+	appclientset := appsfake.NewSimpleClientset()
+	appInformer := appinformers.NewApplicationInformer(appclientset, "", time.Minute, cache.Indexers{})
+
+	appName, err := GetTrackingMethodFromApplicationInformer(appInformer, "", "test")
+	assert.NoError(t, err)
+
 }
