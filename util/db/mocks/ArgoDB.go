f4fd836a1b06b47fa62e6cd67bfc7789a3856f74
Simon Ninon <simon.ninon@gmail.com>
2021-10-30 01:11:19 -0700

    fix: return a `codes.NotFound` error when trying to get a non-existent repository (#7548)
    * fix: return a codes.NotFound error when trying to get a non-existent repository
    
    Signed-off-by: Simon Ninon <simon.ninon@gmail.com>
    
    * move s.db.RepositoryExists call after the permission check
    
    Signed-off-by: Simon Ninon <simon.ninon@gmail.com>
    
    * update ArgoDB mock and add unit tests
    
    Signed-off-by: Simon Ninon <simon.ninon@gmail.com>

--- util/db/mocks/ArgoDB.go
+++ util/db/mocks/ArgoDB.go
@@ -265,6 +265,29 @@ func (_m *ArgoDB) GetRepository(ctx context.Context, url string) (*v1alpha1.Repo
 	return r0, r1
 }
 
+// RepositoryExists provides a mock function with given fields: ctx, url
+func (_m *ArgoDB) RepositoryExists(ctx context.Context, url string) (bool, error) {
+	ret := _m.Called(ctx, url)
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func(context.Context, string) bool); ok {
+		r0 = rf(ctx, url)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(bool)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, url)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetRepositoryCredentials provides a mock function with given fields: ctx, name
 func (_m *ArgoDB) GetRepositoryCredentials(ctx context.Context, name string) (*v1alpha1.RepoCreds, error) {
 	ret := _m.Called(ctx, name)
