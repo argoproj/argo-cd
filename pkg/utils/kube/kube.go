c7bab2eeca221d0a32b47df49af517f6d3ac1c7c
pasha-codefresh <pavel@codefresh.io>
2021-11-04 00:01:10 +0200

    feat: add support split yaml that return actual yamls (#346)
    * add support split yaml that return actual yamls
    
    Signed-off-by: pashavictorovich <pavel@codefresh.io>
    
    * change description
    
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- pkg/utils/kube/kube.go
+++ pkg/utils/kube/kube.go
@@ -284,12 +284,30 @@ func newAuthInfo(restConfig *rest.Config) *clientcmdapi.AuthInfo {
 // SplitYAML splits a YAML file into unstructured objects. Returns list of all unstructured objects
 // found in the yaml. If an error occurs, returns objects that have been parsed so far too.
 func SplitYAML(yamlData []byte) ([]*unstructured.Unstructured, error) {
+	var objs []*unstructured.Unstructured
+	ymls, err := SplitYAMLToString(yamlData)
+	if err != nil {
+		return nil, err
+	}
+	for _, yml := range ymls {
+		u := &unstructured.Unstructured{}
+		if err := yaml.Unmarshal([]byte(yml), u); err != nil {
+			return objs, fmt.Errorf("failed to unmarshal manifest: %v", err)
+		}
+		objs = append(objs, u)
+	}
+	return objs, nil
+}
+
+// SplitYAMLToString splits a YAML file into strings. Returns list of yamls
+// found in the yaml. If an error occurs, returns objects that have been parsed so far too.
+func SplitYAMLToString(yamlData []byte) ([]string, error) {
 	// Similar way to what kubectl does
 	// https://github.com/kubernetes/cli-runtime/blob/master/pkg/resource/visitor.go#L573-L600
 	// Ideally k8s.io/cli-runtime/pkg/resource.Builder should be used instead of this method.
 	// E.g. Builder does list unpacking and flattening and this code does not.
 	d := kubeyaml.NewYAMLOrJSONDecoder(bytes.NewReader(yamlData), 4096)
-	var objs []*unstructured.Unstructured
+	var objs []string
 	for {
 		ext := runtime.RawExtension{}
 		if err := d.Decode(&ext); err != nil {
@@ -302,11 +320,7 @@ func SplitYAML(yamlData []byte) ([]*unstructured.Unstructured, error) {
 		if len(ext.Raw) == 0 || bytes.Equal(ext.Raw, []byte("null")) {
 			continue
 		}
-		u := &unstructured.Unstructured{}
-		if err := yaml.Unmarshal(ext.Raw, u); err != nil {
-			return objs, fmt.Errorf("failed to unmarshal manifest: %v", err)
-		}
-		objs = append(objs, u)
+		objs = append(objs, string(ext.Raw))
 	}
 	return objs, nil
 }
