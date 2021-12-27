24a216c598dd4cc13ef1c9d2d6be288d09aa6b22
pashavictorovich <pavel@codefresh.io>
2021-12-27 13:56:24 +0200

    empty commit
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- hack/gen-resources/cmd/commands/root.go
+++ hack/gen-resources/cmd/commands/root.go
@@ -25,7 +25,6 @@ func initConfig() {
 // NewCommand returns a new instance of an argocd command
 func NewCommand() *cobra.Command {
 	var generateOpts generator.GenerateOpts
-
 	var command = &cobra.Command{
 		Use:   cliName,
 		Short: "Generator for argocd resources",
