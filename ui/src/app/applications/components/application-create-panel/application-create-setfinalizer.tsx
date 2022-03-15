fecd47fe1b3b43114dc93949131b3e03d7f78952
rishabh625 <rishabhmishra625@gmail.com>
2022-03-15 11:15:42 +0530

    Made finalizer to be appended into array instead of replacing
    Signed-off-by: rishabh625 <rishabhmishra625@gmail.com>

--- ui/src/app/applications/components/application-create-panel/application-create-setfinalizer.tsx
+++ ui/src/app/applications/components/application-create-panel/application-create-setfinalizer.tsx
@@ -10,18 +10,26 @@ export const SetFinalizerOnApplication = ReactForm.FormField((props: {fieldApi: 
     const setval = getValue() || [];
     const index = setval.findIndex((item: string) => item === finalizerval);
     const isChecked = index < 0 ? false : true;
-    const val = [finalizerval] || [];
     return (
         <div className='small-12 large-6' style={{borderBottom: '0'}}>
             <React.Fragment>
                 <Checkbox
                     id='set-finalizer'
                     checked={isChecked}
-                    onChange={() => {
-                        if (!isChecked) {
-                            setValue(val);
+                    onChange={(state: boolean) => {
+                        if (!state) {
+                            const v = getValue() || [];
+                            const index2 = v.findIndex((item: string) => item === finalizerval);
+                            if (index2 >= 0) {
+                                const u = v.slice();
+                                u.splice(index2, 1);
+                                setValue(u);
+                            }
                         } else {
-                            setValue([]);
+                            const v = getValue() || [];
+                            const c = v.slice();
+                            c.push(finalizerval);
+                            setValue(c);
                         }
                     }}
                 />
