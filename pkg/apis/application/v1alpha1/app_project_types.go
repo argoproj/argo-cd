af03b291d4b7e9d3ce9a6580ae9c8141af0e05cf
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2022-03-22 10:57:30 -0700

    Merge pull request from GHSA-2f5v-8r3f-8pww
    * fix: application resource APIs must enforce project restrictions
    
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    
    * Fix unit tests
    
    Signed-off-by: jannfis <jann@mistrust.net>
    
    Co-authored-by: jannfis <jann@mistrust.net>

--- pkg/apis/application/v1alpha1/app_project_types.go
+++ pkg/apis/application/v1alpha1/app_project_types.go
@@ -313,11 +313,15 @@ func (proj AppProject) IsGroupKindPermitted(gk schema.GroupKind, namespaced bool
 
 // IsLiveResourcePermitted returns whether a live resource found in the cluster is permitted by an AppProject
 func (proj AppProject) IsLiveResourcePermitted(un *unstructured.Unstructured, server string, name string) bool {
-	if !proj.IsGroupKindPermitted(un.GroupVersionKind().GroupKind(), un.GetNamespace() != "") {
+	return proj.IsResourcePermitted(un.GroupVersionKind().GroupKind(), un.GetNamespace(), ApplicationDestination{Server: server, Name: name})
+}
+
+func (proj AppProject) IsResourcePermitted(groupKind schema.GroupKind, namespace string, dest ApplicationDestination) bool {
+	if !proj.IsGroupKindPermitted(groupKind, namespace != "") {
 		return false
 	}
-	if un.GetNamespace() != "" {
-		return proj.IsDestinationPermitted(ApplicationDestination{Server: server, Namespace: un.GetNamespace(), Name: name})
+	if namespace != "" {
+		return proj.IsDestinationPermitted(ApplicationDestination{Server: dest.Server, Name: dest.Name, Namespace: namespace})
 	}
 	return true
 }
