e0d8633951720a7b194f443b9e910f4f868e5406
Michael Crenshaw <michael@crenshaw.dev>
2022-04-05 16:50:00 -0400

    chore: simplify check and add tests
    Signed-off-by: Michael Crenshaw <michael@crenshaw.dev>

--- server/server.go
+++ server/server.go
@@ -11,6 +11,7 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"path"
 	"reflect"
 	"regexp"
 	go_runtime "runtime"
@@ -910,18 +911,19 @@ func (server *ArgoCDServer) newStaticAssetsHandler() func(http.ResponseWriter, *
 			}
 			http.ServeContent(w, r, "index.html", modTime, io.NewByteReadSeeker(data))
 		} else {
-			//If file is matches the following regex, it's more or less guaranteed to be an application bundle that
-			//has a unique name based off of the hashing of the file contents. Since the filename will change when
-			//the content changes, we can direct clients to cache this for a very long amount of time.
-			pathSegments := strings.Split(r.URL.Path, "/")
-			lastPathSegment := pathSegments[len(pathSegments)-1]
-			match, _ := regexp.MatchString("^([^\\.]+).([0-9a-f]){20}.js", lastPathSegment)
-			if match {
+			if isMainJsBundle(r.URL) {
 				w.Header().Set("Cache-Control", "public, max-age=31536000, immutable")
 			}
 			http.FileServer(server.staticAssets).ServeHTTP(w, r)
 		}
 	}
+}
+
+var mainJsBundleRegex = regexp.MustCompile(`^main\.[0-9a-f]{20}\.js$`)
+
+func isMainJsBundle(url *url.URL) bool {
+	filename := path.Base(url.Path)
+	return mainJsBundleRegex.Match([]byte(filename))
 }
 
 type registerFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error
--- server/server_test.go
+++ server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 	"time"
@@ -672,4 +673,52 @@ func TestOIDCConfigChangeDetection_NoChange(t *testing.T) {
 
 	//Then
 	assert.Equal(t, result, false, "no error since no config change")
+}
+
+func TestIsMainJsBundle(t *testing.T) {
+	testCases := []struct{
+		name           string
+		url            string
+		isMainJsBundle bool
+	}{
+		{
+			name: "localhost with valid main bundle",
+			url: "https://localhost:8080/main.e4188e5adc97bbfc00c3.js",
+			isMainJsBundle: true,
+		},
+		{
+			name: "localhost and deep path with valid main bundle",
+			url: "https://localhost:8080/some/argo-cd-instance/main.e4188e5adc97bbfc00c3.js",
+			isMainJsBundle: true,
+		},
+		{
+			name: "font file",
+			url: "https://localhost:8080/assets/fonts/google-fonts/Heebo-Bols.woff2",
+			isMainJsBundle: false,
+		},
+		{
+			name: "no dot after main",
+			url: "https://localhost:8080/main/e4188e5adc97bbfc00c3.js",
+			isMainJsBundle: false,
+		},
+		{
+			name: "wrong extension character",
+			url: "https://localhost:8080/main.e4188e5adc97bbfc00c3/js",
+			isMainJsBundle: false,
+		},
+		{
+			name: "wrong hash length",
+			url: "https://localhost:8080/main.e4188e5adc97bbfc00c3abcdefg.js",
+			isMainJsBundle: false,
+		},
+	}
+	for _, testCase := range testCases {
+		testCaseCopy := testCase
+		t.Run(testCaseCopy.name, func(t *testing.T) {
+			t.Parallel()
+			testUrl, _ := url.Parse(testCaseCopy.url)
+			isMainJsBundle := isMainJsBundle(testUrl)
+			assert.Equal(t, testCaseCopy.isMainJsBundle, isMainJsBundle)
+		})
+	}
 }
