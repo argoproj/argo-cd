5c7ae5a413299ccc0d6dffdbe652846cc0792e9e
jkulkarn <jay.p.kulkarni@blackrock.com>
2022-04-12 10:48:03 -0700

    Added import for applicationSet in matrix.go
    Signed-off-by: jkulkarn <jay.p.kulkarni@blackrock.com>

--- applicationset/generators/matrix.go
+++ applicationset/generators/matrix.go
@@ -2,11 +2,8 @@ package generators
 
 import (
 	"fmt"
-	"github.com/argoproj/argo-cd/v2/applicationset/utils"
-	argoprojiov1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/applicationset/v1alpha1"
 	"time"
-)
--
+
 	"github.com/argoproj/argo-cd/v2/applicationset/utils"
 	argoprojiov1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/applicationset/v1alpha1"
 )
