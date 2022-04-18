79d3398cedc7b26e12048028d788b3c2073d3c4c
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2022-04-18 10:47:13 -0400

    chore: update grpc runtime library (#8929)
    * chore: update grpc POC
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * fix: register gogo codec (#1)
    
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    
    * Remove gogo annotations from application.proto
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix unit tests
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix e2e test
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix lint
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix lint
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix unit-test
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix LogEntry.Last required field not populated
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix LogEntry required fields
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix get log content
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix app actions list
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix ApplicationPodLogsQuery
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    * Fix RunResourceAction
    
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>
    
    Co-authored-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Signed-off-by: wojtekidd <wojtek.cichon@protonmail.com>

--- server/server_norace_test.go
+++ server/server_norace_test.go
@@ -65,12 +65,6 @@ func TestUserAgent(t *testing.T) {
 			userAgent: fmt.Sprintf("%s/%s-rc1", common.ArgoCDUserAgentName, currentVersion),
 		},
 		{
-			// Reject legacy client
-			// NOTE: after we update the grpc-go client past 1.15.0, this test will break and should be deleted
-			userAgent: " ", // need a space here since the apiclient will set the default user-agent if empty
-			errorMsg:  "unsatisfied client version constraint",
-		},
-		{
 			// Permit custom clients
 			userAgent: "foo/1.2.3",
 		},
