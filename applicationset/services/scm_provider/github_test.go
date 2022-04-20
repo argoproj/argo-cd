93f468941b3fcebcd441dd6ab49b7c4f291b382e
rishabh625 <rishabhmishra625@gmail.com>
2022-04-20 19:09:08 +0530

    test: Added Test for Get all branches github to increase codecov.
    Signed-off-by: rishabh625 <rishabhmishra625@gmail.com>

--- applicationset/services/scm_provider/github_test.go
+++ applicationset/services/scm_provider/github_test.go
@@ -130,4 +130,14 @@ func TestGithubGetBranches(t *testing.T) {
 	} else {
 		assert.Equal(t, repos[0].Branch, "master")
 	}
+	// Get all branches
+	host.allBranches = true
+	repos, err = host.GetBranches(context.Background(), repo)
+	if err != nil {
+		checkRateLimit(t, err)
+		assert.NoError(t, err)
+	} else {
+		// considering master and one release branch to always exist.
+		assert.Greater(t, len(repos), 1)
+	}
 }
