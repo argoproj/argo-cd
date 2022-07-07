e2242eadf5e266c6934a060cebf4bd1d82117e29
jannfis <jann@mistrust.net>
2022-07-07 17:37:36 +0000

    Remove dead code
    Signed-off-by: jannfis <jann@mistrust.net>

--- pkg/client/listers/application/v1alpha1/expansion_generated.go
+++ pkg/client/listers/application/v1alpha1/expansion_generated.go
@@ -9,3 +9,11 @@ type AppProjectListerExpansion interface{}
 // AppProjectNamespaceListerExpansion allows custom methods to be added to
 // AppProjectNamespaceLister.
 type AppProjectNamespaceListerExpansion interface{}
+
+// ApplicationListerExpansion allows custom methods to be added to
+// ApplicationLister.
+type ApplicationListerExpansion interface{}
+
+// ApplicationNamespaceListerExpansion allows custom methods to be added to
+// ApplicationNamespaceLister.
+type ApplicationNamespaceListerExpansion interface{}
