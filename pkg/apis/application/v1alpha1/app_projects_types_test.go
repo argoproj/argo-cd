e7a742f92c36ffced66654806aa14c81ad4e065f
CI <michael@crenshaw.dev>
2022-07-16 14:41:57 -0400

    more tests
    Signed-off-by: CI <michael@crenshaw.dev>

--- pkg/apis/application/v1alpha1/app_projects_types_test.go
+++ pkg/apis/application/v1alpha1/app_projects_types_test.go
@@ -114,7 +114,7 @@ func Test_checkParents(t *testing.T) {
 
 func Test_IsGroupKindPermitted(t *testing.T) {
 	t.Run("parent projects block resources when child does not", func(t *testing.T) {
-		projects := map[string]AppProject{
+		projects := map[string]*AppProject{
 			"root": {
 				ObjectMeta: v1.ObjectMeta{
 					Name: "root",
@@ -148,27 +148,45 @@ func Test_IsGroupKindPermitted(t *testing.T) {
 
 		getProject := func(name string) (*AppProject, error) {
 			// No restrictions in the child app.
-			project := projects[name]
-			return &project, nil
+			project, ok := projects[name]
+			if !ok {
+				return nil, fmt.Errorf("failed to get project %q", name)
+			}
+			return project, nil
 		}
 
-		isPermitted, err := projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "ConfigMap"}, true, getProject)
-		assert.NoError(t, err)
-		assert.False(t, isPermitted)
+		t.Run("ConfigMap should be blocked by project 'b'", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "ConfigMap"}, true, getProject)
+			assert.NoError(t, err)
+			assert.False(t, isPermitted)
+		})
 
-		isPermitted, err = projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "ServiceAccount"}, true, getProject)
-		assert.NoError(t, err)
-		assert.False(t, isPermitted)
+		t.Run("ServiceAccount should be blocked by project 'c'", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "ServiceAccount"}, true, getProject)
+			assert.NoError(t, err)
+			assert.False(t, isPermitted)
+		})
 
-		isPermitted, err = projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "Pod"}, true, getProject)
-		assert.NoError(t, err)
-		assert.True(t, isPermitted)
+		t.Run("Pod is not blocked", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "Pod"}, true, getProject)
+			assert.NoError(t, err)
+			assert.True(t, isPermitted)
+		})
+
+		rootProj, _ := projects["root"]
+		rootProj.Spec.ParentProject = "does-not-exist"
+
+		t.Run("error caused by non-existent project in chain", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsGroupKindPermitted(schema.GroupKind{Group: "v1", Kind: "Pod"}, true, getProject)
+			assert.Error(t, err)
+			assert.False(t, isPermitted)
+		})
 	})
 }
 
 func Test_IsDestinationPermitted(t *testing.T) {
 	t.Run("parent projects block destination when child does not", func(t *testing.T) {
-		projects := map[string]AppProject{
+		projects := map[string]*AppProject{
 			"root": {
 				ObjectMeta: v1.ObjectMeta{
 					Name: "root",
@@ -205,23 +223,39 @@ func Test_IsDestinationPermitted(t *testing.T) {
 
 		getProject := func(name string) (*AppProject, error) {
 			// No restrictions in the child app.
-			project := projects[name]
-			return &project, nil
+			project, ok := projects[name]
+			if !ok {
+				return nil, fmt.Errorf("failed to get project %q", name)
+			}
+			return project, nil
 		}
 
-		isPermitted, err := projects["root"].IsDestinationPermitted(ApplicationDestination{Name: "dev-use2-cluster", Namespace: "dev-use2-namespace"}, getProject)
-		assert.NoError(t, err)
-		assert.False(t, isPermitted)
+		t.Run("use2 cluster/ns is blocked by project 'b'", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsDestinationPermitted(ApplicationDestination{Name: "dev-use2-cluster", Namespace: "dev-use2-namespace"}, getProject)
+			assert.NoError(t, err)
+			assert.False(t, isPermitted)
+		})
 
-		isPermitted, err = projects["root"].IsDestinationPermitted(ApplicationDestination{Name: "dev-usw2-cluster", Namespace: "dev-usw2-namespace"}, getProject)
-		assert.NoError(t, err)
-		assert.True(t, isPermitted)
+		t.Run("usw2 is allowed by the whole chain", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsDestinationPermitted(ApplicationDestination{Name: "dev-usw2-cluster", Namespace: "dev-usw2-namespace"}, getProject)
+			assert.NoError(t, err)
+			assert.True(t, isPermitted)
+		})
+
+		rootProj, _ := projects["root"]
+		rootProj.Spec.ParentProject = "does-not-exist"
+
+		t.Run("error because parent project does not exist", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsDestinationPermitted(ApplicationDestination{Name: "dev-usw2-cluster", Namespace: "dev-usw2-namespace"}, getProject)
+			assert.Error(t, err)
+			assert.False(t, isPermitted)
+		})
 	})
 }
 
 func Test_IsSourcePermitted(t *testing.T) {
 	t.Run("parent projects block source when child does not", func(t *testing.T) {
-		projects := map[string]AppProject{
+		projects := map[string]*AppProject{
 			"root": {
 				ObjectMeta: v1.ObjectMeta{
 					Name: "root",
@@ -258,17 +292,137 @@ func Test_IsSourcePermitted(t *testing.T) {
 
 		getProject := func(name string) (*AppProject, error) {
 			// No restrictions in the child app.
-			project := projects[name]
-			return &project, nil
+			project, ok := projects[name]
+			if !ok {
+				return nil, fmt.Errorf("failed to get project %q", name)
+			}
+			return project, nil
 		}
 
-		isPermitted, err := projects["root"].IsSourcePermitted(ApplicationSource{RepoURL: "https://github.company.com/dev-org/app1-deployment.git"}, getProject)
-		assert.NoError(t, err)
-		assert.True(t, isPermitted)
+		t.Run("repo URL allowed by full chain", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsSourcePermitted(ApplicationSource{RepoURL: "https://github.company.com/dev-org/app1-deployment.git"}, getProject)
+			assert.NoError(t, err)
+			assert.True(t, isPermitted)
+		})
 
-		isPermitted, err = projects["root"].IsSourcePermitted(ApplicationSource{RepoURL: "https://github.company.com/other-org/app1-deployment.git"}, getProject)
-		assert.NoError(t, err)
-		assert.False(t, isPermitted)
+		t.Run("other-org blocked by chain", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsSourcePermitted(ApplicationSource{RepoURL: "https://github.company.com/other-org/app1-deployment.git"}, getProject)
+			assert.NoError(t, err)
+			assert.False(t, isPermitted)
+		})
+
+		rootProj, _ := projects["root"]
+		rootProj.Spec.ParentProject = "does-not-exist"
+
+		t.Run("error because the parent project does not exist", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsSourcePermitted(ApplicationSource{RepoURL: "https://github.company.com/other-org/app1-deployment.git"}, getProject)
+			assert.Error(t, err)
+			assert.False(t, isPermitted)
+		})
 	})
 }
 
+func Test_IsResourcePermitted(t *testing.T) {
+	t.Run("parent projects block source when child does not", func(t *testing.T) {
+		projects := map[string]*AppProject{
+			"root": {
+				ObjectMeta: v1.ObjectMeta{
+					Name: "root",
+				},
+				Spec: AppProjectSpec{
+					ParentProject: "b",
+					Destinations: []ApplicationDestination{
+						{Name: "*", Namespace: "*"},
+					},
+					ClusterResourceWhitelist: []v1.GroupKind{
+						{Group: "*", Kind: "*"},
+					},
+				},
+			},
+			"b": {
+				ObjectMeta: v1.ObjectMeta{
+					Name: "b",
+				},
+				Spec: AppProjectSpec{
+					ParentProject: "c",
+					Destinations: []ApplicationDestination{
+						{Name: "*", Namespace: "*"},
+					},
+					NamespaceResourceBlacklist: []v1.GroupKind{
+						{Group: "v1", Kind: "ConfigMap"},
+					},
+					ClusterResourceWhitelist: []v1.GroupKind{
+						{Group: "v1", Kind: "Pod"}, // not actually cluster-scoped, but it works for the test
+					},
+				},
+			},
+			"c": {
+				ObjectMeta: v1.ObjectMeta{
+					Name: "c",
+				},
+				Spec: AppProjectSpec{
+					Destinations: []ApplicationDestination{
+						{Name: "dev-usw2-*", Namespace: "dev-usw2-*"},
+					},
+					ClusterResourceWhitelist: []v1.GroupKind{
+						{Group: "*", Kind: "*"},
+					},
+				},
+			},
+		}
+
+		getProject := func(name string) (*AppProject, error) {
+			// No restrictions in the child app.
+			project, ok := projects[name]
+			if !ok {
+				return nil, fmt.Errorf("failed to get project %q", name)
+			}
+			return project, nil
+		}
+
+		allowedGroupKind := schema.GroupKind{Group: "v1", Kind: "Pod"}
+		disallowedGroupKind := schema.GroupKind{Group: "v1", Kind: "ConfigMap"}
+		allowedNamespace := "dev-usw2-namespace"
+		disallowedNamespace := "dev-use2-namespace"
+		allowedDestination := ApplicationDestination{Name: "dev-usw2-cluster"}
+		disallowedDestination := ApplicationDestination{Name: "dev-use2-cluster"}
+
+		for _, groupKind := range []schema.GroupKind{allowedGroupKind, disallowedGroupKind} {
+			for _, namespace := range []string{allowedNamespace, disallowedNamespace} {
+				for _, destination := range []ApplicationDestination{allowedDestination, disallowedDestination} {
+					groupKindAllowed := groupKind == allowedGroupKind
+					namespaceAllowed := namespace == allowedNamespace
+					destinationAllowed := destination == allowedDestination
+
+
+					destWithNamespace := ApplicationDestination{Name: destination.Name, Namespace: namespace}
+
+					t.Run(fmt.Sprintf("GroupKind %v allowed: %v, namespace %s allowed: %v, destination %v allowed: %v", groupKind, groupKindAllowed, namespace, namespaceAllowed, destination, destinationAllowed), func(t *testing.T) {
+						isPermitted, err := projects["root"].IsResourcePermitted(groupKind, namespace, destWithNamespace, getProject)
+						resourceAllowed := groupKindAllowed && namespaceAllowed && destinationAllowed
+						assert.NoError(t, err)
+						assert.Equal(t, resourceAllowed, isPermitted)
+					})
+
+					t.Run(fmt.Sprintf("GroupKind %v allowed: %v, destination %v allowed: %v", groupKind, groupKindAllowed, destination, destinationAllowed), func(t *testing.T) {
+						// test for cluster-scoped resource
+						isPermitted, err := projects["root"].IsResourcePermitted(groupKind, "", destination, getProject)
+						// Namespace isn't checked for cluster resources, and destination server is checked at the Application level instead of the resource level.
+						clusterResourceAllowed := groupKindAllowed
+						assert.NoError(t, err)
+						assert.Equal(t, clusterResourceAllowed, isPermitted)
+					})
+				}
+			}
+		}
+
+		rootProj, _ := projects["root"]
+		rootProj.Spec.ParentProject = "does-not-exist"
+
+		t.Run("error getting parent project", func(t *testing.T) {
+			isPermitted, err := projects["root"].IsResourcePermitted(allowedGroupKind, allowedNamespace, allowedDestination, getProject)
+			assert.Error(t, err)
+			assert.False(t, isPermitted)
+		})
+	})
+}
