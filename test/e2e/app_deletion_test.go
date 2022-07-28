d9f92be9a2c01b3380287b448528992d11459e59
Daniel Helfand <helfand.4@gmail.com>
2022-07-28 14:00:09 -0500

    fix: assert on Contains instead of Equal
    Signed-off-by: Daniel Helfand <helfand.4@gmail.com>

--- test/e2e/app_deletion_test.go
+++ test/e2e/app_deletion_test.go
@@ -58,7 +58,7 @@ func TestDeletingAppByLabel(t *testing.T) {
 		// delete is unsuccessful since no selector match
 		AndCLIOutput(
 			func(output string, err error) {
-				assert.Equal(t, "no apps match selector foo=baz", err.Error())
+				assert.Contains(t, "no apps match selector foo=baz", err.Error())
 			},
 		).
 		When().
