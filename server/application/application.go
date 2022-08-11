e0112a4ca96f750203d106f4f5c316f7eee58de4
David Monks <david.monks@octoenergy.com>
2022-08-11 16:22:23 +0100

    Expand `RunResourceAction` to support the root App
    Prior to this change, similar to the `ListResourceActions` function
    updated in the previous commit, the `RunResourceAction` function only
    supports running actions on resources managed by the Application, rather
    than the Application itself.
    
    This change re-uses in the `RunResourceAction` function the new
    behaviour introduced in the previous commit to make the Application a
    valid target for resource actions.
    
    Signed-off-by: David Monks <david.monks@octoenergy.com>

--- server/application/application.go
+++ server/application/application.go
@@ -1988,7 +1988,7 @@ func (s *Server) logResourceEvent(res *appv1.ResourceNode, ctx context.Context, 
 }
 
 func (s *Server) ListResourceActions(ctx context.Context, q *application.ApplicationResourceRequest) (*application.ResourceActionsListResponse, error) {
-	obj, err := s.getUnstructuredLiveResourceOrApp(ctx, rbacpolicy.ActionGet, q)
+	obj, _, _, _, err := s.getUnstructuredLiveResourceOrApp(ctx, rbacpolicy.ActionGet, q)
 	if err != nil {
 		return nil, err
 	}
@@ -2009,35 +2009,30 @@ func (s *Server) ListResourceActions(ctx context.Context, q *application.Applica
 	return &application.ResourceActionsListResponse{Actions: actionsPtr}, nil
 }
 
-func (s *Server) getUnstructuredLiveResourceOrApp(ctx context.Context, rbacRequest string, q *application.ApplicationResourceRequest) (obj *unstructured.Unstructured, err error) {
-	var (
-		app       *appv1.Application
-		config    *rest.Config
-	)
+func (s *Server) getUnstructuredLiveResourceOrApp(ctx context.Context, rbacRequest string, q *application.ApplicationResourceRequest) (obj *unstructured.Unstructured, res *appv1.ResourceNode, app *appv1.Application, config *rest.Config, err error) {
 	if q.GetKind() == "Application" && q.GetGroup() == "argoproj.io" && q.GetName() == q.GetResourceName() {
 		namespace := s.appNamespaceOrDefault(q.GetAppNamespace())
 		app, err = s.appLister.Applications(namespace).Get(q.GetName())
 		if err != nil {
-			return nil, fmt.Errorf("error getting app by name: %w", err)
+			return nil, nil, nil, nil, fmt.Errorf("error getting app by name: %w", err)
 		}
 		if err = s.enf.EnforceErr(ctx.Value("claims"), rbacpolicy.ResourceApplications, rbacRequest, app.RBACName(s.ns)); err != nil {
-			return nil, err
+			return nil, nil, nil, nil, err
 		}
 		config, err = s.getApplicationClusterConfig(ctx, app)
 		if err != nil {
-			return nil, fmt.Errorf("error getting application cluster config: %w", err)
+			return nil, nil, nil, nil, fmt.Errorf("error getting application cluster config: %w", err)
 		}
 		obj, err = kube.ToUnstructured(app)
 	} else {
-		var res *appv1.ResourceNode
 		res, config, app, err = s.getAppLiveResource(ctx, rbacRequest, q)
 		if err != nil {
-			return nil, fmt.Errorf("error getting app live resource: %w", err)
+			return nil, nil, nil, nil, fmt.Errorf("error getting app live resource: %w", err)
 		}
 		obj, err = s.kubectl.GetResource(ctx, config, res.GroupKindVersion(), res.Name, res.Namespace)
 	}
 	if err != nil {
-		return nil, fmt.Errorf("error getting resource: %w", err)
+		return nil, nil, nil, nil, fmt.Errorf("error getting resource: %w", err)
 	}
 	return
 }
@@ -2073,13 +2068,9 @@ func (s *Server) RunResourceAction(ctx context.Context, q *application.ResourceA
 		Group:        q.Group,
 	}
 	actionRequest := fmt.Sprintf("%s/%s/%s/%s", rbacpolicy.ActionAction, q.GetGroup(), q.GetKind(), q.GetAction())
-	res, config, a, err := s.getAppLiveResource(ctx, actionRequest, resourceRequest)
+	liveObj, res, a, config, err := s.getUnstructuredLiveResourceOrApp(ctx, actionRequest, resourceRequest)
 	if err != nil {
-		return nil, fmt.Errorf("error getting app live resource: %w", err)
-	}
-	liveObj, err := s.kubectl.GetResource(ctx, config, res.GroupKindVersion(), res.Name, res.Namespace)
-	if err != nil {
-		return nil, fmt.Errorf("error getting resource: %w", err)
+		return nil, err
 	}
 
 	resourceOverrides, err := s.settingsMgr.GetResourceOverrides()
@@ -2150,8 +2141,12 @@ func (s *Server) RunResourceAction(ctx context.Context, q *application.ResourceA
 		}
 	}
 
-	s.logAppEvent(a, ctx, argo.EventReasonResourceActionRan, fmt.Sprintf("ran action %s on resource %s/%s/%s", q.GetAction(), res.Group, res.Kind, res.Name))
-	s.logResourceEvent(res, ctx, argo.EventReasonResourceActionRan, fmt.Sprintf("ran action %s", q.GetAction()))
+	if res == nil {
+		s.logAppEvent(a, ctx, argo.EventReasonResourceActionRan, fmt.Sprintf("ran action %s", q.GetAction()))
+	} else {
+		s.logAppEvent(a, ctx, argo.EventReasonResourceActionRan, fmt.Sprintf("ran action %s on resource %s/%s/%s", q.GetAction(), res.Group, res.Kind, res.Name))
+		s.logResourceEvent(res, ctx, argo.EventReasonResourceActionRan, fmt.Sprintf("ran action %s", q.GetAction()))
+	}
 	return &application.ApplicationResponse{}, nil
 }
 
