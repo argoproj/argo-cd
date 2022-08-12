8e27d88a1e33457278f23769c3dd95bea3d63f17
CI <michael@crenshaw.dev>
2022-08-12 11:45:40 -0400

    fix: no single quotes around resource names in delete prompts (#10008)
    Signed-off-by: CI <michael@crenshaw.dev>

--- ui/src/app/applications/components/utils.tsx
+++ ui/src/app/applications/components/utils.tsx
@@ -65,7 +65,7 @@ export async function deleteApplication(appName: string, appNamespace: string, a
         'Delete application',
         api => (
             <div>
-                <p>Are you sure you want to delete the application '{appName}'?</p>
+                <p>Are you sure you want to delete the application <code>{appName}</code>?</p>
                 <div className='argo-form-row'>
                     <FormField
                         label={`Please type '${appName}' to confirm the deletion of the resource`}
@@ -293,7 +293,7 @@ export const deletePodAction = async (pod: appModels.Pod, appContext: AppContext
         'Delete pod',
         () => (
             <div>
-                <p>Are you sure you want to delete Pod '{pod.name}'?</p>
+                <p>Are you sure you want to delete Pod <code>{pod.name}</code>?</p>
                 <div className='argo-form-row' style={{paddingLeft: '30px'}}>
                     <CheckboxField id='force-delete-checkbox' field='force'>
                         <label htmlFor='force-delete-checkbox'>Force delete</label>
@@ -330,7 +330,7 @@ export const deletePopup = async (ctx: ContextApis, resource: ResourceTreeNode, 
         api => (
             <div>
                 <p>
-                    Are you sure you want to delete {resource.kind} '{resource.name}'?
+                    Are you sure you want to delete {resource.kind} <code>{resource.name}</code>?
                 </p>
                 {isManaged ? (
                     <div className='argo-form-row'>
