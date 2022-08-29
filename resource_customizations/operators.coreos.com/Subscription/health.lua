216b10e6d8c2d1b5d56413908f943d45cdef8443
Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
2022-08-29 08:20:51 -0400

    fix: fix subscription health check (#10450)
    Signed-off-by: ishitasequeira <ishiseq29@gmail.com>
    
    Signed-off-by: ishitasequeira <ishiseq29@gmail.com>
    
    Signed-off-by: ishitasequeira <ishiseq29@gmail.com>

--- resource_customizations/operators.coreos.com/Subscription/health.lua
+++ resource_customizations/operators.coreos.com/Subscription/health.lua
@@ -8,7 +8,9 @@ if obj.status ~= nil then
       msg = msg .. i .. ": " .. condition.type .. " | " .. condition.status .. "\n"
       if condition.type == "InstallPlanPending" and condition.status == "True" then
         numPending = numPending + 1
-      elseif (condition.type == "CatalogSourcesUnhealthy" or condition.type == "InstallPlanMissing" or condition.type == "InstallPlanFailed" or condition.type == "ResolutionFailed") and condition.status == "True" then
+      elseif (condition.type == "InstallPlanMissing" and condition.reason ~= "ReferencedInstallPlanNotFound") then
+        numDegraded = numDegraded + 1
+      elseif (condition.type == "CatalogSourcesUnhealthy" or condition.type == "InstallPlanFailed" or condition.type == "ResolutionFailed") and condition.status == "True" then
         numDegraded = numDegraded + 1
       end
     end
