a057ae16072711566623e7b0f1f2b5e0cae29d14
Jessie Teng <101035990+JessieTeng89@users.noreply.github.com>
2022-09-01 22:35:19 +0800

    fix: Remove quotes from delete dialog #10008 (#10471)
    Signed-off-by: Teng, Jessie <yilin.teng@fmr.com>

--- ui/src/app/applications/components/utils.tsx
+++ ui/src/app/applications/components/utils.tsx
@@ -65,7 +65,9 @@ export async function deleteApplication(appName: string, appNamespace: string, a
         'Delete application',
         api => (
             <div>
-                <p>Are you sure you want to delete the application '{appName}'?</p>
+                <p>
+                    Are you sure you want to delete the application <kbd>{appName}</kbd>?
+                </p>
                 <div className='argo-form-row'>
                     <FormField
                         label={`Please type '${appName}' to confirm the deletion of the resource`}
@@ -293,7 +295,9 @@ export const deletePodAction = async (pod: appModels.Pod, appContext: AppContext
         'Delete pod',
         () => (
             <div>
-                <p>Are you sure you want to delete Pod '{pod.name}'?</p>
+                <p>
+                    Are you sure you want to delete Pod <kbd>{pod.name}</kbd>?
+                </p>
                 <div className='argo-form-row' style={{paddingLeft: '30px'}}>
                     <CheckboxField id='force-delete-checkbox' field='force'>
                         <label htmlFor='force-delete-checkbox'>Force delete</label>
@@ -330,7 +334,7 @@ export const deletePopup = async (ctx: ContextApis, resource: ResourceTreeNode, 
         api => (
             <div>
                 <p>
-                    Are you sure you want to delete {resource.kind} '{resource.name}'?
+                    Are you sure you want to delete {resource.kind} <kbd>{resource.name}</kbd>?
                 </p>
                 {isManaged ? (
                     <div className='argo-form-row'>
