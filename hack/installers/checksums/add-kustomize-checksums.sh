80475cb80d69562dd344e0ea043805ca71557e27
Michael Crenshaw <michael@crenshaw.dev>
2022-09-15 10:08:10 -0400

    remove v from kustomize version
    Signed-off-by: Michael Crenshaw <michael@crenshaw.dev>

--- hack/installers/checksums/add-kustomize-checksums.sh
+++ hack/installers/checksums/add-kustomize-checksums.sh
@@ -8,8 +8,8 @@ wget "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2
 
 while IFS="" read -r line || [ -n "$line" ]
 do
-  filename=$(echo "$line" | awk -F ' ' '{print $2}')
-  test "${line#*windows}" == "$line" && echo "$line" > "$filename.sha256"
+  filename=$(echo "$line" | awk -F ' ' '{print $2}' | sed "s#v$1#$1#")
+  test "${line#*windows}" == "$line" && echo "$line" | sed "s#v$1#$1#" > "$filename.sha256"
 done < checksums.txt
 
 rm checksums.txt
