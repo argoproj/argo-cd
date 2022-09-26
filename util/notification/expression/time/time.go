ad42c90f6519ca0ab67ae38a6b95bee5dac66073
George MacRorie <me@georgemac.com>
2022-09-26 13:48:22 +0100

    feat(util/notification): add duration and timestamp constants from stdlib time package
    Signed-off-by: George MacRorie <me@georgemac.com>

--- util/notification/expression/time/time.go
+++ util/notification/expression/time/time.go
@@ -4,10 +4,37 @@ import (
 	"time"
 )
 
+var now = time.Now
+
 func NewExprs() map[string]interface{} {
 	return map[string]interface{}{
+		// Functions
 		"Parse": parse,
 		"Now":   now,
+		// Durations
+		"Nanosecond":  time.Nanosecond,
+		"Microsecond": time.Microsecond,
+		"Millisecond": time.Millisecond,
+		"Second":      time.Second,
+		"Minute":      time.Minute,
+		"Hour":        time.Hour,
+		// Timestamps
+		"Layout":      time.Layout,
+		"ANSIC":       time.ANSIC,
+		"UnixDate":    time.UnixDate,
+		"RubyDate":    time.RubyDate,
+		"RFC822":      time.RFC822,
+		"RFC822Z":     time.RFC822Z,
+		"RFC850":      time.RFC850,
+		"RFC1123":     time.RFC1123,
+		"RFC1123Z":    time.RFC1123Z,
+		"RFC3339":     time.RFC3339,
+		"RFC3339Nano": time.RFC3339Nano,
+		"Kitchen":     time.Kitchen,
+		"Stamp":       time.Stamp,
+		"StampMilli":  time.StampMilli,
+		"StampMicro":  time.StampMicro,
+		"StampNano":   time.StampNano,
 	}
 }
 
@@ -17,8 +44,4 @@ func parse(timestamp string) time.Time {
 		panic(err)
 	}
 	return res
-}
-
-func now() time.Time {
-	return time.Now()
 }
