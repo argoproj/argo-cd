510abde55be1f79d15cfc92f11a15fa23c4386a4
emirot <emirot.nolan@gmail.com>
2022-09-28 23:13:22 -0700

    tests: readAppset
    Signed-off-by: emirot <emirot.nolan@gmail.com>

---
+++ cmd/util/applicationset_test.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"testing"
+
+	argoprojiov1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+var appSet string = `apiVersion: argoproj.io/v1alpha1
+kind: ApplicationSet
+metadata:
+  name: guestbook
+spec:
+  generators:
+  - list:
+      elements:
+      - cluster: engineering-dev
+        url: https://1.2.3.4
+  template:
+    metadata:
+      name: '{{cluster}}-guestbook'
+    spec:
+      source:
+        repoURL: https://github.com/infra-team/cluster-deployments.git
+        targetRevision: HEAD
+        path: guestbook/{{cluster}}
+      destination:
+        server: '{{url}}'
+        namespace: guestbook
+`
+
+func TestReadAppSet(t *testing.T) {
+	appsets := []*argoprojiov1alpha1.ApplicationSet{}
+	readAppset([]byte(appSet), &appsets)
+	assert.Equal(t, len(appsets), 1)
+}
+
+func TestReadBadAppSet(t *testing.T) {
+	appsets := []*argoprojiov1alpha1.ApplicationSet{}
+	err := readAppset([]byte(`badyaml`), &appsets)
+	assert.Error(t, err)
+}
