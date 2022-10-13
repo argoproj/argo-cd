bcb4438ef4debc09d268b4ab5e9dc32dbc267ef4
nsahai8 <ns8gupta@gmail.com>
2022-10-13 15:33:45 +0530

    Update reposerver/repository/lock.go
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Signed-off-by: nsahai8 <ns8gupta@gmail.com>
    Signed-off-by: Niharika <niharika_sahai@intuit.com>

--- reposerver/repository/lock.go
+++ reposerver/repository/lock.go
@@ -43,7 +43,7 @@ func (r *repositoryLock) Lock(path string, revision string, allowConcurrent bool
 			state.cond.Broadcast()
 		}
 		if err != nil {
-			return fmt.Errorf("init closer failed: %w", err)
+	        return fmt.Errorf("init closer failed: %w", err)
 		}
 		return nil
 	})
