49d7d747ebb540318b70fab92f76bd67757561d2
Smriti Prakash <smriti_prakash@intuit.com>
2022-10-18 18:55:34 +0530

    fix: addressed review comments
    Signed-off-by: Smriti Prakash <smriti_prakash@intuit.com>

--- controller/clusterinfoupdater.go
+++ controller/clusterinfoupdater.go
@@ -93,7 +93,7 @@ func (c *clusterInfoUpdater) updateClusters() {
 func (c *clusterInfoUpdater) updateClusterInfo(cluster appv1.Cluster, info *cache.ClusterInfo) error {
 	apps, err := c.appLister.List(labels.Everything())
 	if err != nil {
-		return fmt.Errorf("error while fetching the apps list  %w", err)
+		return fmt.Errorf("error while fetching the apps list: %w", err)
 	}
 	var appCount int64
 	for _, a := range apps {
