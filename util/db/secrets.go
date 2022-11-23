696b51fe13d3a140d348292f00c71779298c7d15
zillani <shaikzillani@gmail.com>
2022-11-23 19:51:22 +0530

    fix flow
    Signed-off-by: zillani <shaikzillani@gmail.com>

--- util/db/secrets.go
+++ util/db/secrets.go
@@ -157,17 +157,19 @@ func URIToSecretName(uriType, uri string) (string, error) {
 	}
 	h := fnv.New32a()
 	_, _ = h.Write([]byte(uri))
-	var host string
+	host := strings.ToLower(strings.Split(parsedURI.Host, ":")[0])
 	parsedHost := strings.Split(parsedURI.Host, ":")
-	//If IPv6 with port
+
+	//If IPv6 address with port
 	if len(parsedHost) >= 8 {
+		replacer := strings.NewReplacer(":", "", "[", "", "]", "")
 		if len(parsedHost) > 8 {
 			host = strings.TrimSuffix(parsedURI.Host, parsedHost[8])
+			host = replacer.Replace(host)
+		} else {
+			host = replacer.Replace(parsedURI.Host)
 		}
-		replacer := strings.NewReplacer(":", "", "[", "", "]", "")
-		host = replacer.Replace(parsedURI.Host)
-	} else {
-		host = strings.ToLower(strings.Split(parsedURI.Host, ":")[0])
 	}
 	return fmt.Sprintf("%s-%s-%v", uriType, host, h.Sum32()), nil
 }
+
