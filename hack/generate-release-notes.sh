d34e5356bc4afde545934397ac221322fed288a4
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2022-11-29 09:31:19 -0500

    fix duplication
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- hack/generate-release-notes.sh
+++ hack/generate-release-notes.sh
@@ -76,30 +76,6 @@ more_log=$(git log --pretty="format:%s %ae" "$new_ref..$old_ref")
 
 new_commits=$(diff --new-line-format="" --unchanged-line-format="" <(echo "$less_log") <(echo "$more_log") | grep -v "Merge pull request from GHSA")
 new_commits_no_email=$(echo "$new_commits" | strip_last_word)
-
-contributors_num=$(echo "$new_commits" | only_last_word | sort -u | nonempty_line_count)
-
-new_commits_num=$(echo "$new_commits" | nonempty_line_count)
-features_num=$(echo "$new_commits_no_email" | grep '^feat' | nonempty_line_count)
-fixes_num=$(echo "$new_commits_no_email" | grep '^fix' | nonempty_line_count)
-
-previous_contributors=$(git log --pretty="format:%an %ae" "$old_ref" | sort -uf)
-all_contributors=$(git log --pretty="format:%an %ae" "$new_ref" | sort -uf)
-new_contributors=$(diff --new-line-format="" --unchanged-line-format="" <(echo "$all_contributors") <(echo "$previous_contributors"))
-new_contributors_num=$(echo "$new_contributors" | only_last_word | nonempty_line_count)  # Count contributors by email
-new_contributors_names=$(echo "$new_contributors" | strip_last_word | to_list_items)
-
-new_contributors_message=""
-if [ "$new_contributors_num" -gt 0 ]; then
-  new_contributors_message=" ($new_contributors_num of them new)"
-fi
-
-# Adapted from https://stackoverflow.com/a/67029088/684776
-less_log=$(git log --pretty="format:%s %ae" --cherry-pick --left-only --no-merges "$new_ref...$old_ref")
-more_log=$(git log --pretty="format:%s %ae" "$new_ref..$old_ref")
-
-new_commits=$(diff --new-line-format="" --unchanged-line-format="" <(echo "$less_log") <(echo "$more_log") | grep -v "Merge pull request from GHSA")
-new_commits_no_email=$(echo "$new_commits" | strip_last_word)
 features=$(echo "$new_commits_no_email" | grep '^feat' | to_list_items)
 fixes=$(echo "$new_commits_no_email" | grep '^fix' | to_list_items)
 docs=$(echo "$new_commits_no_email" | grep '^docs' | to_list_items)
