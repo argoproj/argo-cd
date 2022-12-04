8ad9df1aef299c9726ed8f5cb15c131ac788c505
Alex Eftimie <alex.eftimie@getyourguide.com>
2022-12-04 09:06:25 +0100

    Update helm_test.go
    Signed-off-by: Alex Eftimie <alex.eftimie@getyourguide.com>

--- test/e2e/helm_test.go
+++ test/e2e/helm_test.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/argoproj/gitops-engine/pkg/health"
@@ -201,7 +202,7 @@ func TestHelmValuesLiteralFileLocal(t *testing.T) {
 			if err != nil {
 				panic(err)
 			}
-			assert.Equal(t, string(data), app.Spec.GetSource().Helm.ValuesString())
+			assert.Equal(t, strings.TrimSuffix(string(data), "\n"), app.Spec.GetSource().Helm.ValuesString())
 		}).
 		When().
 		AppUnSet("--values-literal").
