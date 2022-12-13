005ad9faefa83466afc6448f7a5b8941b63cee63
Remington Breeze <remington@breeze.software>
2022-12-12 22:04:03 -0800

    add noopener
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/shared/components/deep-links.tsx
+++ ui/src/app/shared/components/deep-links.tsx
@@ -7,7 +7,7 @@ export const DeepLinks = (props: {links: LinkInfo[]}) => {
         <div style={{margin: '10px 0'}}>
             {(links || []).map((link: LinkInfo) => (
                 <div key={link.title} style={{display: 'flex', alignItems: 'center', height: '35px'}}>
-                    <a href={link.url} target='_blank' style={{display: 'flex', alignItems: 'center', marginRight: '7px'}}>
+                    <a href={link.url} target='_blank' style={{display: 'flex', alignItems: 'center', marginRight: '7px'}} rel='noopener'>
                         <i className='fa fa-external-link-alt' style={{marginRight: '5px'}} />
                         <div>{link.title}</div>
                     </a>
