99b222ce3982d36505b30fb30b441678f1a65616
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2022-12-13 08:01:06 +0530

    feat: show cmpv2 plugins in create app wizard (#11615)
    * feat: show cmpv2 plugins in create app wizard
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>
    
    * fix: fixed doc formatting and sidecar plugin info
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>
    
    * feat: updated plugin info
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- server/server.go
+++ server/server.go
@@ -779,7 +779,7 @@ func newArgoCDServiceSet(a *ArgoCDServer) *ArgoCDServiceSet {
 	applicationSetService := applicationset.NewServer(a.db, a.KubeClientset, a.enf, a.Cache, a.AppClientset, a.appLister, a.appsetInformer, a.appsetLister, a.projLister, a.settingsMgr, a.Namespace, projectLock)
 	projectService := project.NewServer(a.Namespace, a.KubeClientset, a.AppClientset, a.enf, projectLock, a.sessionMgr, a.policyEnforcer, a.projInformer, a.settingsMgr, a.db)
 	appsInAnyNamespaceEnabled := len(a.ArgoCDServerOpts.ApplicationNamespaces) > 0
-	settingsService := settings.NewServer(a.settingsMgr, a, a.DisableAuth, appsInAnyNamespaceEnabled)
+	settingsService := settings.NewServer(a.settingsMgr, a.RepoClientset, a, a.DisableAuth, appsInAnyNamespaceEnabled)
 	accountService := account.NewServer(a.sessionMgr, a.settingsMgr, a.enf)
 
 	notificationService := notification.NewServer(a.apiFactory)
