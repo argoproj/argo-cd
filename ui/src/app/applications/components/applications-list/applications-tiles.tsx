711e544581b64f69703501e2b233bb9e4cac5750
asingh <11219262+ashutosh16@users.noreply.github.com>
2022-12-13 06:35:04 -0800

    fix: appname in searchbar (#11493)
    * fix: appname in searchbar
    
    Signed-off-by: ashutosh16 <11219262+ashutosh16@users.noreply.github.com>
    
    * fix: appname in searchbar
    
    Signed-off-by: ashutosh16 <11219262+ashutosh16@users.noreply.github.com>
    
    Signed-off-by: ashutosh16 <11219262+ashutosh16@users.noreply.github.com>

--- ui/src/app/applications/components/applications-list/applications-tiles.tsx
+++ ui/src/app/applications/components/applications-list/applications-tiles.tsx
@@ -53,7 +53,7 @@ export const ApplicationTiles = ({applications, syncApplication, refreshApplicat
     const appRef = {ref: React.useRef(null), set: false};
     const appContainerRef = React.useRef(null);
     const appsPerRow = useItemsPerContainer(appRef.ref, appContainerRef);
-    const authSettingsCtx = React.useContext(AuthSettingsCtx);
+    const useAuthSettingsCtx = React.useContext(AuthSettingsCtx);
 
     const {useKeybinding} = React.useContext(KeybindingContext);
 
@@ -131,7 +131,7 @@ export const ApplicationTiles = ({applications, syncApplication, refreshApplicat
                                                             <i className={'icon argo-icon-' + (app.spec.source.chart != null ? 'helm' : 'git')} />
                                                             <Tooltip content={AppUtils.appInstanceName(app)}>
                                                                 <span className='applications-list__title'>
-                                                                    {AppUtils.appQualifiedName(app, authSettingsCtx?.appsInAnyNamespaceEnabled)}
+                                                                    {AppUtils.appQualifiedName(app, useAuthSettingsCtx?.appsInAnyNamespaceEnabled)}
                                                                 </span>
                                                             </Tooltip>
                                                         </div>
