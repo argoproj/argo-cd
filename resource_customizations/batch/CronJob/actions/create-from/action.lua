8e1d9eb67dab542d4e16d1f34a9f2db2f994d48e
reggie <reginakagan@gmail.com>
2022-12-23 16:10:11 +0200

    Dummy action in
    Signed-off-by: reggie <reginakagan@gmail.com>

--- resource_customizations/batch/CronJob/actions/create-from/action.lua
+++ resource_customizations/batch/CronJob/actions/create-from/action.lua
@@ -1,2 +1,9 @@
+local io = require("io")
+
+local file = io.open("job-manifest-template.yaml", "r");
+assert(file);
+local data = file:read("*a"); -- Read everything
+file:close();
+
 obj.metadata.annotations["kuku"] = "muku"
 return obj
