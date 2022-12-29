ba17c86ee55579fe6af4fdaa6a12e0ae3a379b3d
Sergey-Kizimov <sergey.kizimov@hiya.com>
2022-12-29 09:33:02 -0800

    Expose OPA warnings to ArgoCD UI
    Signed-off-by: Sergey-Kizimov <sergey.kizimov@hiya.com>

--- ui/src/app/applications/components/utils.tsx
+++ ui/src/app/applications/components/utils.tsx
@@ -206,6 +206,10 @@ export const OperationPhaseIcon = ({app}: {app: appModels.Application}) => {
             className = 'fa fa-check-circle';
             color = COLORS.operation.success;
             break;
+        case appModels.OperationPhases.Warning:
+            className = 'fa fa-check-circle';
+            color = COLORS.operation.warning;
+            break;
         case appModels.OperationPhases.Error:
             className = 'fa fa-times-circle';
             color = COLORS.operation.error;
@@ -764,6 +768,10 @@ export const ResourceResultIcon = ({resource}: {resource: appModels.ResourceResu
             case appModels.ResultCodes.PruneSkipped:
                 icon = 'fa-heart';
                 break;
+            case appModels.ResultCodes.SyncedWithWarning:
+                color = COLORS.sync_result.SyncedWithWarning;
+                icon = 'fa-heart-broken';
+                break;
         }
         let title: string = resource.message;
         if (resource.message) {
@@ -793,6 +801,10 @@ export const ResourceResultIcon = ({resource}: {resource: appModels.ResourceResu
             case appModels.OperationPhases.Terminating:
                 color = COLORS.operation.terminating;
                 className = 'fa fa-circle-notch fa-spin';
+                break;
+            case appModels.OperationPhases.Warning:
+                color = COLORS.operation.warning;
+                className = 'fa fa-heart-broken';
                 break;
         }
         let title: string = resource.message;
@@ -867,6 +879,8 @@ const getOperationStateTitle = (app: appModels.Application) => {
                     return 'Sync OK';
                 case 'Terminating':
                     return 'Terminated';
+                case 'Warning':
+                    return 'Sync warning';
             }
     }
     return 'Unknown';
@@ -877,7 +891,7 @@ export const OperationState = ({app, quiet}: {app: appModels.Application; quiet?
     if (appOperationState === undefined) {
         return <React.Fragment />;
     }
-    if (quiet && [appModels.OperationPhases.Running, appModels.OperationPhases.Failed, appModels.OperationPhases.Error].indexOf(appOperationState.phase) === -1) {
+    if (quiet && [appModels.OperationPhases.Running, appModels.OperationPhases.Failed, appModels.OperationPhases.Error, appModels.OperationPhases.Warning].indexOf(appOperationState.phase) === -1) {
         return <React.Fragment />;
     }
 
