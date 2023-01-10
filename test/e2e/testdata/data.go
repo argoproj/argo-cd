e9a21025937ef8db1860825f850b658e253d29b9
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2023-01-10 12:19:16 -0500

    Revert "fix: ssa e2e tests failing after updating to kubectl 1.26 (#11753)"
    This reverts commit a10a54782a564355c6d978a754e2ab28ad7cabe6.
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- test/e2e/testdata/data.go
+++ test/e2e/testdata/data.go
@@ -5,7 +5,4 @@ import _ "embed"
 var (
 	//go:embed ssa-revision-history/deployment.yaml
 	SSARevisionHistoryDeployment string
-
-	//go:embed guestbook/guestbook-ui-deployment.yaml
-	GuestbookDeployment string
 )
