0ee36ec95c95aab7c4ef7ac8ba31967e5562847d
Remington Breeze <remington@breeze.software>
2023-01-19 12:00:15 -0800

    consistent font sizes
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/applications/components/applications-list/applications-filter.tsx
+++ ui/src/app/applications/components/applications-list/applications-filter.tsx
@@ -233,7 +233,7 @@ const FavoriteFilter = (props: AppFilterProps) => {
     return (
         <div
             className={`filter filter__item ${props.pref.showFavorites ? 'filter__item--selected' : ''}`}
-            style={{margin: '0.5em 0'}}
+            style={{margin: '0.5em 0', marginTop: '-1em'}}
             onClick={() => onChange(!props.pref.showFavorites)}>
             <Checkbox
                 value={!!props.pref.showFavorites}
