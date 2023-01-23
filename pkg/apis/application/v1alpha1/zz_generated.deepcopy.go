074110a0ae4b84e78fc5c2fd79cfb13234a927ec
pasha-codefresh <pavel@codefresh.io>
2023-01-23 11:44:29 +0200

    fix codegen (#199)
    Co-authored-by: Pasha Kostohrys <pashakostohrys@MacBook-Pro-Pasha.local>

--- pkg/apis/application/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/application/v1alpha1/zz_generated.deepcopy.go
@@ -1794,6 +1794,20 @@ func (in *ResourceNode) DeepCopyInto(out *ResourceNode) {
 		in, out := &in.CreatedAt, &out.CreatedAt
 		*out = (*in).DeepCopy()
 	}
+	if in.Labels != nil {
+		in, out := &in.Labels, &out.Labels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if in.Annotations != nil {
+		in, out := &in.Annotations, &out.Annotations
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
 	return
 }
 
