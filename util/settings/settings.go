80dd8c529f2ce2c6928babe365e2e769a6cbfd8a
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2023-01-27 14:09:34 -0500

    feat(security): require the `aud` claim from OIDC providers by default
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- util/settings/settings.go
+++ util/settings/settings.go
@@ -1758,7 +1758,7 @@ func (a *ArgoCDSettings) SkipAudienceCheckWhenTokenHasNoAudience() bool {
 		if config.SkipAudienceCheckWhenTokenHasNoAudience != nil {
 			return *config.SkipAudienceCheckWhenTokenHasNoAudience
 		}
-		return true
+		return false
 	}
 	// When using the bundled Dex, the audience check is required. Dex will always send JWTs with an audience.
 	return false
