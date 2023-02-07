234d4f60b9a2143f188c8d674dc8ab843cb9840d
shin04 <daikon0413@gmail.com>
2023-02-07 20:35:28 +0900

    fix /setting only show help field if chatURL is not empty
    Signed-off-by: shin04 <daikon0413@gmail.com>

--- util/settings/settings.go
+++ util/settings/settings.go
@@ -1137,8 +1137,12 @@ func (mgr *SettingsManager) GetHelp() (*Help, error) {
 	if !ok {
 		chatText = "Chat now!"
 	}
+	chatURL, ok := argoCDCM.Data[helpChatURL]
+	if !ok {
+		chatText = ""
+	}
 	return &Help{
-		ChatURL:    argoCDCM.Data[helpChatURL],
+		ChatURL:    chatURL,
 		ChatText:   chatText,
 		BinaryURLs: getDownloadBinaryUrlsFromConfigMap(argoCDCM),
 	}, nil
