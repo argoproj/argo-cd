ed70eac8b7bd6b2f276502398fdbccccab5d189a
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2023-02-14 22:23:51 +0530

    feat: add sync delete option (#507)
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- pkg/sync/common/types.go
+++ pkg/sync/common/types.go
@@ -29,6 +29,8 @@ const (
 	SyncOptionReplace = "Replace=true"
 	// Sync option that enables use of --server-side flag instead of client-side
 	SyncOptionServerSideApply = "ServerSideApply=true"
+	// Sync option that disables resource deletion
+	SyncOptionDisableDeletion = "Delete=false"
 )
 
 type PermissionValidator func(un *unstructured.Unstructured, res *metav1.APIResource) error
