ca852a70eacce90a925fa93450e6c807982255f4
Alex Collins <alex_collins@intuit.com>
2023-02-22 15:27:02 -0800

    fix: remove "ERROR" button

--- ui/src/app/applications/components/pod-logs-viewer/log-message-filter.tsx
+++ ui/src/app/applications/components/pod-logs-viewer/log-message-filter.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {Tooltip} from 'argo-ui';
 import {Spacer} from '../../../shared/components/spacer';
-import {ToggleButton} from '../../../shared/components/toggle-button';
 
 // Filter is a component that renders a filter input for log lines.
 export const LogMessageFilter = ({filterText, setFilterText}: {filterText: string; setFilterText: (value: string) => void}) => (
@@ -11,11 +10,5 @@ export const LogMessageFilter = ({filterText, setFilterText}: {filterText: strin
         <Tooltip content='Filter log lines by text. Prefix with `!` to invert, e.g. `!foo` will find lines without `foo` in them'>
             <input className='argo-field' value={filterText} onChange={e => setFilterText(e.target.value)} />
         </Tooltip>
-        <ToggleButton
-            icon='exclamation-triangle'
-            toggled={filterText === 'ERROR'}
-            onToggle={() => setFilterText(filterText !== 'ERROR' ? 'ERROR' : '')}
-            title='Only show log messages with "ERROR" in them'
-        />
     </>
 );
