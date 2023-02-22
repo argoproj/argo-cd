88e74fef0e4fa5ebd4b7bebb8c8f931ced373c0b
Alex Collins <alexec@users.noreply.github.com>
2023-02-22 17:21:30 +0000

    feat: refactor
    Signed-off-by: Alex Collins <alexec@users.noreply.github.com>

--- .devcontainer/post-start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-set -eux
-kit up
--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 set -eux
 
+# install kubernetes
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d cluster get k3s-default || k3d cluster create --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 
+# install kit
 curl -q https://raw.githubusercontent.com/alexec/kit/main/install.sh | sh
 
-kit deps build ui-deps
+# build is slow first time, so pre-build
+kit build
