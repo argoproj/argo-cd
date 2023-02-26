e34d40231fb18f9cfe703466f647ddcb05dc6ab5
juunini <juuni.ni.i@gmail.com>
2023-02-26 16:52:27 +0900

    feat: add `react-i18next`
    Signed-off-by: juunini <juuni.ni.i@gmail.com>

--- ui/src/app/i18n.ts
+++ ui/src/app/i18n.ts
@@ -1,13 +1,18 @@
 import i18next from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
 import en from './locales/en';
 import ko from './locales/ko';
 
-i18next.init({
+i18next.use(initReactI18next).init({
     lng: navigator.language,
     fallbackLng: 'en',
     debug: process.env.NODE_ENV !== 'production',
     resources: {
         en: {translation: en},
         ko: {translation: ko}
+    },
+    interpolation: {
+        escapeValue: false
     }
 });
