2e725ede271477cbe1f2024158383729d806f7b1
Alex Collins <alex_collins@intuit.com>
2023-03-19 10:39:18 -0700

    fix: copy from workflows
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- .devcontainer/pre-build.sh
+++ .devcontainer/pre-build.sh
@@ -6,8 +6,14 @@ wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d cluster get k3s-default || k3d cluster create --wait
 k3d kubeconfig merge --kubeconfig-merge-default
 
+# install kubectl
+curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl cluster-info
+
 # install kit
 curl -q https://raw.githubusercontent.com/kitproj/kit/main/install.sh | sh
 
-# build is slow first time, so pre-build
-kit build
+# download dependencies and do first-pass compile
+CI=1 kit pre-up
