7a0bf8588e93a7ca57ffad4512906c8d1edd6667
Laurentiu Soica <laurentiu@soica.ro>
2023-03-22 18:55:35 +0200

    feat: Accept ElementsYaml for list generator to get dynamic content from a git file (#12428) (#12490)
    * fix: use field-wise templating for child matrix generators (#11661)
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * test shouldn't use go template
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * feat: extend List generator with ElementsJsonBase64
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: proper field name and crd update
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: indentation
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: remove b64 encoding. Based on #12287
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: generated with codegen
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: reset some of the generated files
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: elementsyaml to cover both yaml and json
    
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    
    * fix: regenerate code
    
    Signed-off-by: laurentiusoica <laurentiu@soica.ro>
    
    * Regenerate code
    
    Signed-off-by: laurentiusoica <laurentiu@soica.ro>
    
    * fix: update ApplicationSet docs
    
    Signed-off-by: laurentiusoica <laurentiu@soica.ro>
    
    * fix: elementsyaml to elementsYaml to be more consistent with other fields
    
    Signed-off-by: laurentiusoica <laurentiu@soica.ro>
    
    ---------
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Signed-off-by: Laurentiu Soica <laurentiu@soica.ro>
    Signed-off-by: laurentiusoica <laurentiu@soica.ro>
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- pkg/apis/application/v1alpha1/applicationset_types.go
+++ pkg/apis/application/v1alpha1/applicationset_types.go
@@ -182,8 +182,9 @@ func (g ApplicationSetTerminalGenerators) toApplicationSetNestedGenerators() []A
 
 // ListGenerator include items info
 type ListGenerator struct {
-	Elements []apiextensionsv1.JSON `json:"elements" protobuf:"bytes,1,name=elements"`
-	Template ApplicationSetTemplate `json:"template,omitempty" protobuf:"bytes,2,name=template"`
+	Elements     []apiextensionsv1.JSON `json:"elements" protobuf:"bytes,1,name=elements"`
+	ElementsYaml string                 `json:"elementsYaml,omitempty" protobuf:"bytes,2,opt,name=elementsYaml"`
+	Template     ApplicationSetTemplate `json:"template,omitempty" protobuf:"bytes,3,name=template"`
 }
 
 // MatrixGenerator generates the cartesian product of two sets of parameters. The parameters are defined by two nested
