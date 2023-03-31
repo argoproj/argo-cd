483f95c74fba0b7c36af8a87676bb49db7ede55b
Constantin <41841989+Lucostus@users.noreply.github.com>
2023-03-31 12:13:06 +0200

    changed i to _ for idiomatic lua
    Signed-off-by: Constantin <41841989+Lucostus@users.noreply.github.com>

--- resource_customizations/argoproj.io/EventBus/health.lua
+++ resource_customizations/argoproj.io/EventBus/health.lua
@@ -2,7 +2,7 @@ hs={ status = "Progressing", message = "Waiting for initialization" }
 
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
-    for i, condition in ipairs(obj.status.conditions) do
+    for _, condition in ipairs(obj.status.conditions) do
       if condition.type == "Deployed" and condition.status == "False" then
         hs.status = "Degraded"
         hs.message = condition.message or condition.reason
