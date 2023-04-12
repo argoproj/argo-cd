f7b248fc0b550c4cef83c50ffbe7cab95d551a15
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2023-04-12 19:15:17 +0530

    feat: removed legacy app tracking label
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- util/kube/kube_test.go
+++ util/kube/kube_test.go
@@ -83,56 +83,6 @@ func TestSetLabels(t *testing.T) {
 	}
 }
 
-func TestSetLegacyLabels(t *testing.T) {
-	for _, yamlStr := range []string{depWithoutSelector, depWithSelector} {
-		var obj unstructured.Unstructured
-		err := yaml.Unmarshal([]byte(yamlStr), &obj)
-		assert.Nil(t, err)
-
-		err = SetAppInstanceLabel(&obj, common.LabelKeyLegacyApplicationName, "my-app")
-		assert.Nil(t, err)
-
-		manifestBytes, err := json.MarshalIndent(obj.Object, "", "  ")
-		assert.Nil(t, err)
-		log.Println(string(manifestBytes))
-
-		var depV1Beta1 extv1beta1.Deployment
-		err = json.Unmarshal(manifestBytes, &depV1Beta1)
-		assert.Nil(t, err)
-		assert.Equal(t, 1, len(depV1Beta1.Spec.Selector.MatchLabels))
-		assert.Equal(t, "nginx", depV1Beta1.Spec.Selector.MatchLabels["app"])
-		assert.Equal(t, 2, len(depV1Beta1.Spec.Template.Labels))
-		assert.Equal(t, "nginx", depV1Beta1.Spec.Template.Labels["app"])
-		assert.Equal(t, "my-app", depV1Beta1.Spec.Template.Labels[common.LabelKeyLegacyApplicationName])
-	}
-}
-
-func TestSetLegacyJobLabel(t *testing.T) {
-	yamlBytes, err := os.ReadFile("testdata/job.yaml")
-	assert.Nil(t, err)
-	var obj unstructured.Unstructured
-	err = yaml.Unmarshal(yamlBytes, &obj)
-	assert.Nil(t, err)
-	err = SetAppInstanceLabel(&obj, common.LabelKeyLegacyApplicationName, "my-app")
-	assert.Nil(t, err)
-
-	manifestBytes, err := json.MarshalIndent(obj.Object, "", "  ")
-	assert.Nil(t, err)
-	log.Println(string(manifestBytes))
-
-	job := unstructured.Unstructured{}
-	err = json.Unmarshal(manifestBytes, &job)
-	assert.Nil(t, err)
-
-	labels := job.GetLabels()
-	assert.Equal(t, "my-app", labels[common.LabelKeyLegacyApplicationName])
-
-	templateLabels, ok, err := unstructured.NestedMap(job.UnstructuredContent(), "spec", "template", "metadata", "labels")
-	assert.True(t, ok)
-	assert.Nil(t, err)
-	assert.Equal(t, "my-app", templateLabels[common.LabelKeyLegacyApplicationName])
-}
-
 func TestSetSvcLabel(t *testing.T) {
 	yamlBytes, err := os.ReadFile("testdata/svc.yaml")
 	assert.Nil(t, err)
