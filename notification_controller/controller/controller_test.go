66e6bb3a7a565a75af16fa830b0f302d76e4b450
zhaque44 <haque.zubair@gmail.com>
2023-04-19 23:40:32 -0500

    Adding unit tests for the Init function: for the notification controller
    Signed-off-by: zhaque44 <haque.zubair@gmail.com>

---
+++ notification_controller/controller/controller_test.go
@@ -0,0 +1,78 @@
+package controller
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic/fake"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestInit(t *testing.T) {
+	scheme := runtime.NewScheme()
+	err := v1alpha1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Error registering the resource: %v", err)
+	}
+	dynamicClient := fake.NewSimpleDynamicClient(scheme)
+	k8sClient := k8sfake.NewSimpleClientset()
+	appLabelSelector := "app=test"
+
+	nc := NewController(
+		k8sClient,
+		dynamicClient,
+		nil,
+		"default",
+		appLabelSelector,
+		nil,
+		"my-secret",
+		"my-configmap",
+	)
+
+	assert.NotNil(t, nc)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = nc.Init(ctx)
+
+	assert.NoError(t, err)
+}
+
+func TestInitTimeout(t *testing.T) {
+	scheme := runtime.NewScheme()
+	err := v1alpha1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Error registering the resource: %v", err)
+	}
+	dynamicClient := fake.NewSimpleDynamicClient(scheme)
+	k8sClient := k8sfake.NewSimpleClientset()
+	appLabelSelector := "app=test"
+
+	nc := NewController(
+		k8sClient,
+		dynamicClient,
+		nil,
+		"default",
+		appLabelSelector,
+		nil,
+		"my-secret",
+		"my-configmap",
+	)
+
+	assert.NotNil(t, nc)
+
+	// Use a very short context timeout to simulate a timeout during cache synchronization
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
+	defer cancel()
+
+	err = nc.Init(ctx)
+
+	// Expect an error since we simulate a timeout for cache synchronization
+	assert.Error(t, err)
+	assert.Equal(t, "Timed out waiting for caches to sync", err.Error())
+}
