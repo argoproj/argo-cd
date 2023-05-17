9d4e875e537ed78414add7f19505e16e84a77a3f
voidspooks <cameronbtesterman@gmail.com>
2023-05-17 13:46:10 -0500

    ensure kubectl gets installed

--- hack/installers/install-kubectl-linux.sh
+++ hack/installers/install-kubectl-linux.sh
@@ -9,4 +9,7 @@ export TARGET_FILE=kubectl_${ARCHITECTURE}_${kubectl_version}
 #[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/$ARCHITECTURE/kubectl
 #$(dirname $0)/compare-chksum.sh
 curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
-sudo install -m 0755 ${DOWNLOADS}/${TARGET_FILE} $BIN/kubectl
+chmod +x kubectl
+cp kubectl $BIN/kubectl
+#sudo install -m 0755 ${DOWNLOADS}/${TARGET_FILE} $BIN/kubectl
+kubectl version
