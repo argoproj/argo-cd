78e37d05f209a3f19cbe8706948f08c3e8da826b
voidspooks <cameronbtesterman@gmail.com>
2023-05-17 13:50:07 -0500

    download kubectl bin and save it as kubectl; lets keep things simple

--- hack/installers/install-kubectl-linux.sh
+++ hack/installers/install-kubectl-linux.sh
@@ -8,7 +8,9 @@ export TARGET_FILE=kubectl_${ARCHITECTURE}_${kubectl_version}
 # NOTE: keep the version synced with https://storage.googleapis.com/kubernetes-release/release/stable.txt
 #[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/$ARCHITECTURE/kubectl
 #$(dirname $0)/compare-chksum.sh
-curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
+#curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
+mkdir -p /tmp/kubectl && cd /tmp/kubectl
+curl -LO https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
 chmod +x kubectl
 cp kubectl $BIN/kubectl
 #sudo install -m 0755 ${DOWNLOADS}/${TARGET_FILE} $BIN/kubectl
