08794c4fd9267a29a9913ffea2ba51b8ca3bdb0a
voidspooks <cameronbtesterman@gmail.com>
2023-05-23 12:01:11 -0500

    set the kubectl version explicitly in the install-kubectl-linux script

--- hack/installers/install-kubectl-linux.sh
+++ hack/installers/install-kubectl-linux.sh
@@ -5,13 +5,8 @@ set -eux -o pipefail
 
 export TARGET_FILE=kubectl_${ARCHITECTURE}_${kubectl_version}
 
-# NOTE: keep the version synced with https://storage.googleapis.com/kubernetes-release/release/stable.txt
-#[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/$ARCHITECTURE/kubectl
-#$(dirname $0)/compare-chksum.sh
-#curl -sLf --retry 3 -o ${DOWNLOADS}/${TARGET_FILE} https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
 mkdir -p /tmp/kubectl && cd /tmp/kubectl
-curl -LO https://dl.k8s.io/release/v${kubectl_version}/bin/linux/${ARCHITECTURE}/kubectl
+curl -LO https://github.com/betterup/kubernetes/releases/download/v1.25.10-1%2Bdee9f04a2c7a62-dirty/kubectl
 chmod +x kubectl
 cp kubectl $BIN/kubectl
-#sudo install -m 0755 ${DOWNLOADS}/${TARGET_FILE} $BIN/kubectl
 kubectl version --client
