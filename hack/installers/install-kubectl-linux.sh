2587aa26493a830c73f4f3f176591dae095147bc
voidspooks <cameronbtesterman@gmail.com>
2023-05-23 13:32:49 -0500

    update kubectl to version that has updated golang-jwt package

--- hack/installers/install-kubectl-linux.sh
+++ hack/installers/install-kubectl-linux.sh
@@ -3,10 +3,8 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-export TARGET_FILE=kubectl_${ARCHITECTURE}_${kubectl_version}
-
 mkdir -p /tmp/kubectl && cd /tmp/kubectl
-curl -LO https://github.com/betterup/kubernetes/releases/download/v1.25.10-1%2Bdee9f04a2c7a62-dirty/kubectl
+curl -LO https://github.com/betterup/kubernetes/releases/download/v${kubectl_version}/kubectl
 chmod +x kubectl
 cp kubectl $BIN/kubectl
 kubectl version --client
