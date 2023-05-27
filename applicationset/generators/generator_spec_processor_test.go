e0bae9f1c09b7cae86fa080dbe642adf0fb705e5
Blake Pettersson <blake.pettersson@gmail.com>
2023-05-27 18:52:33 +0200

    fix(appset): allow cluster urls to be matched (#13715)
    * fix: allow cluster urls to be matched
    
    Related to #13646, and after discussion with @crenshaw-dev, it turns
    out that matching on cluster urls is not possible. This is due to the
    fact that the implementation of `LabelSelectorAsSelector` from
    `k8s.io/apimachinery` validates that a label value is no longer than 63
    characters, and validates that it's alphanumeric. In order to work
    around that, we'll create our own implementation of
    `LabelSelectorAsSelector`.
    
    This implementation has been copied verbatim, with the difference that
    in `isValidLabelValue`, we first check if the label value is a valid
    url. If it is not, we proceed with the label checks as with the
    original implementation.
    
    Apart from that, the only other differences are making as much as
    possible to be package-private; the intent is to only make `Matches`
    and `LabelSelectorAsSelector` available from outside the package.
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>
    
    * chore: drop all label value restrictions
    
    We want to be more flexible in what we accept in post-selectors, mainly
    that we want to allow other values than only server urls. For this, we
    will drop all restrictions that a typical "label value" would typically
    have.
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>
    
    ---------
    
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- applicationset/generators/generator_spec_processor_test.go
+++ applicationset/generators/generator_spec_processor_test.go
@@ -177,6 +177,75 @@ func TestMatchValuesGoTemplate(t *testing.T) {
 	}
 }
 
+func TestTransForm(t *testing.T) {
+	testCases := []struct {
+		name     string
+		selector *metav1.LabelSelector
+		expected []map[string]interface{}
+	}{
+		{
+			name: "server filter",
+			selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"server": "https://production-01.example.com"},
+			},
+			expected: []map[string]interface{}{{
+				"metadata.annotations.foo.argoproj.io":           "production",
+				"metadata.labels.argocd.argoproj.io/secret-type": "cluster",
+				"metadata.labels.environment":                    "production",
+				"metadata.labels.org":                            "bar",
+				"name":                                           "production_01/west",
+				"nameNormalized":                                 "production-01-west",
+				"server":                                         "https://production-01.example.com",
+			}},
+		},
+		{
+			name: "server filter with long url",
+			selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"server": "https://some-really-long-url-that-will-exceed-63-characters.com"},
+			},
+			expected: []map[string]interface{}{{
+				"metadata.annotations.foo.argoproj.io":           "production",
+				"metadata.labels.argocd.argoproj.io/secret-type": "cluster",
+				"metadata.labels.environment":                    "production",
+				"metadata.labels.org":                            "bar",
+				"name":                                           "some-really-long-server-url",
+				"nameNormalized":                                 "some-really-long-server-url",
+				"server":                                         "https://some-really-long-url-that-will-exceed-63-characters.com",
+			}},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testGenerators := map[string]Generator{
+				"Clusters": getMockClusterGenerator(),
+			}
+
+			applicationSetInfo := argov1alpha1.ApplicationSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "set",
+				},
+				Spec: argov1alpha1.ApplicationSetSpec{},
+			}
+
+			results, err := Transform(
+				argov1alpha1.ApplicationSetGenerator{
+					Selector: testCase.selector,
+					Clusters: &argov1alpha1.ClusterGenerator{
+						Selector: metav1.LabelSelector{},
+						Template: argov1alpha1.ApplicationSetTemplate{},
+						Values:   nil,
+					}},
+				testGenerators,
+				emptyTemplate(),
+				&applicationSetInfo, nil)
+
+			assert.NoError(t, err)
+			assert.ElementsMatch(t, testCase.expected, results[0].Params)
+		})
+	}
+}
+
 func emptyTemplate() argov1alpha1.ApplicationSetTemplate {
 	return argov1alpha1.ApplicationSetTemplate{
 		Spec: argov1alpha1.ApplicationSpec{
@@ -235,8 +304,35 @@ func getMockClusterGenerator() Generator {
 			},
 			Type: corev1.SecretType("Opaque"),
 		},
+		&corev1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "some-really-long-server-url",
+				Namespace: "namespace",
+				Labels: map[string]string{
+					"argocd.argoproj.io/secret-type": "cluster",
+					"environment":                    "production",
+					"org":                            "bar",
+				},
+				Annotations: map[string]string{
+					"foo.argoproj.io": "production",
+				},
+			},
+			Data: map[string][]byte{
+				"config": []byte("{}"),
+				"name":   []byte("some-really-long-server-url"),
+				"server": []byte("https://some-really-long-url-that-will-exceed-63-characters.com"),
+			},
+			Type: corev1.SecretType("Opaque"),
+		},
 	}
 	runtimeClusters := []runtime.Object{}
+	for _, clientCluster := range clusters {
+		runtimeClusters = append(runtimeClusters, clientCluster)
+	}
 	appClientset := kubefake.NewSimpleClientset(runtimeClusters...)
 
 	fakeClient := fake.NewClientBuilder().WithObjects(clusters...).Build()
