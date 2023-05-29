7192eabaf4715f73eeb2e132df95d3a15762cd09
Mmadu Manasseh <mmadumanasseh@gmail.com>
2023-05-29 17:17:50 +0100

    feat(appset): Add argocd.argoproj.io/application-set-name to rendered appset applications (#13456)
    * Add app.kubernetes.io/appset-instance to rendered applications
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * initialize labels map if nil
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * fix tests
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    * Updated label ->argocd.argoproj.io/application-set-name
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>
    
    ---------
    
    Signed-off-by: Mmadu Manasseh <mmadumanasseh@gmail.com>

--- test/e2e/clusterdecisiongenerator_e2e_test.go
+++ test/e2e/clusterdecisiongenerator_e2e_test.go
@@ -27,6 +27,9 @@ func TestSimpleClusterDecisionResourceGenerator(t *testing.T) {
 			Name:       "cluster1-guestbook",
 			Namespace:  fixture.TestNamespace(),
 			Finalizers: []string{"resources-finalizer.argocd.argoproj.io"},
+			Labels: map[string]string{
+				LabelKeyAppSetInstance: "simple-cluster-generator",
+			},
 		},
 		Spec: argov1alpha1.ApplicationSpec{
 			Project: "default",
@@ -106,7 +109,10 @@ func TestSimpleClusterDecisionResourceGenerator(t *testing.T) {
 		And(func() {
 			expectedAppNewMetadata = expectedAppNewNamespace.DeepCopy()
 			expectedAppNewMetadata.ObjectMeta.Annotations = map[string]string{"annotation-key": "annotation-value"}
-			expectedAppNewMetadata.ObjectMeta.Labels = map[string]string{"label-key": "label-value"}
+			expectedAppNewMetadata.ObjectMeta.Labels = map[string]string{
+				LabelKeyAppSetInstance: "simple-cluster-generator",
+				"label-key":            "label-value",
+			}
 		}).
 		Update(func(appset *v1alpha1.ApplicationSet) {
 			appset.Spec.Template.Annotations = map[string]string{"annotation-key": "annotation-value"}
@@ -129,6 +135,9 @@ func TestSimpleClusterDecisionResourceGeneratorAddingCluster(t *testing.T) {
 			Name:       "{{name}}-guestbook",
 			Namespace:  fixture.TestNamespace(),
 			Finalizers: []string{"resources-finalizer.argocd.argoproj.io"},
+			Labels: map[string]string{
+				LabelKeyAppSetInstance: "simple-cluster-generator",
+			},
 		},
 		Spec: argov1alpha1.ApplicationSpec{
 			Project: "default",
@@ -224,6 +233,9 @@ func TestSimpleClusterDecisionResourceGeneratorDeletingClusterSecret(t *testing.
 			Name:       "{{name}}-guestbook",
 			Namespace:  fixture.TestNamespace(),
 			Finalizers: []string{"resources-finalizer.argocd.argoproj.io"},
+			Labels: map[string]string{
+				LabelKeyAppSetInstance: "simple-cluster-generator",
+			},
 		},
 		Spec: argov1alpha1.ApplicationSpec{
 			Project: "default",
@@ -321,6 +333,9 @@ func TestSimpleClusterDecisionResourceGeneratorDeletingClusterFromResource(t *te
 			Name:       "{{name}}-guestbook",
 			Namespace:  fixture.TestNamespace(),
 			Finalizers: []string{"resources-finalizer.argocd.argoproj.io"},
+			Labels: map[string]string{
+				LabelKeyAppSetInstance: "simple-cluster-generator",
+			},
 		},
 		Spec: argov1alpha1.ApplicationSpec{
 			Project: "default",
