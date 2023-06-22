0c2eaca4e12c096822ced280726e135834021eaf
Alexandre Gaudreault <alexandre.gaudreault@logmein.com>
2023-06-22 14:09:28 -0400

    add unit test for ignored logs
    Signed-off-by: Alexandre Gaudreault <alexandre.gaudreault@logmein.com>

--- util/argo/normalizers/diff_normalizer.go
+++ util/argo/normalizers/diff_normalizer.go
@@ -180,13 +180,9 @@ func (n *ignoreNormalizer) Normalize(un *unstructured.Unstructured) error {
 	for _, patch := range matched {
 		patchedDocData, err := patch.Apply(docData)
 		if err != nil {
-			if strings.Contains(err.Error(), "Unable to remove nonexistent key") {
-				continue
+			if shouldLogError(err) {
+				log.Debugf("Failed to apply normalization: %v", err)
 			}
-			if strings.Contains(err.Error(), "remove operation does not apply: doc is missing path") {
-				continue
-			}
-			log.Debugf("Failed to apply normalization: %v", err)
 			continue
 		}
 		docData = patchedDocData
@@ -197,4 +193,14 @@ func (n *ignoreNormalizer) Normalize(un *unstructured.Unstructured) error {
 		return err
 	}
 	return nil
+}
+
+func shouldLogError(e error) bool {
+	if strings.Contains(e.Error(), "Unable to remove nonexistent key") {
+		return false
+	}
+	if strings.Contains(e.Error(), "remove operation does not apply: doc is missing path") {
+		return false
+	}
+	return true
 }
