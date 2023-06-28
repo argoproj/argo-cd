2f0f7f068115025d1aba89dee16a7d875c78b9ff
satoru <satorulogic@gmail.com>
2023-06-28 15:44:54 +0800

    chore: just use strings.TrimSuffix
    It's implemented in the same way in the standard library.
    
    Signed-off-by: satoru <satorulogic@gmail.com>

--- util/git/git_test.go
+++ util/git/git_test.go
@@ -45,18 +45,6 @@ func TestEnsurePrefix(t *testing.T) {
 	}
 }
 
-func TestRemoveSuffix(t *testing.T) {
-	data := [][]string{
-		{"hello.git", ".git", "hello"},
-		{"hello", ".git", "hello"},
-		{".git", ".git", ""},
-	}
-	for _, table := range data {
-		result := removeSuffix(table[0], table[1])
-		assert.Equal(t, table[2], result)
-	}
-}
-
 func TestIsSSHURL(t *testing.T) {
 	data := map[string]bool{
 		"git://github.com/argoproj/test.git":     false,
