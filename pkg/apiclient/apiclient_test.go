6abd5c6a17101f25b910f859abebd52f7ad48bdf
bogay <pojay11523@gmail.com>
2023-06-30 02:00:28 +0800

    fix(grpcproxy): parse headers contain colon
    Signed-off-by: bogay <pojay11523@gmail.com>

--- pkg/apiclient/apiclient_test.go
+++ pkg/apiclient/apiclient_test.go
@@ -14,3 +14,11 @@ func Test_parseHeaders(t *testing.T) {
 	assert.Equal(t, headers.Get("foo1"), "bar1")
 	assert.Equal(t, headers.Get("foo2"), "bar2:bar2")
 }
+
+func Test_parseGRPCHeaders(t *testing.T) {
+	headerStrings := []string{"origin: https://foo.bar", "content-length: 123"}
+	headers, err := parseGRPCHeaders(headerStrings)
+	assert.NoError(t, err)
+	assert.Equal(t, headers.Get("origin"), []string{" https://foo.bar"})
+	assert.Equal(t, headers.Get("content-length"), []string{" 123"})
+}
