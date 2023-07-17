983b085cd0c3c3e6d94164521394b230775c272c
Yuan Tang <terrytangyuan@gmail.com>
2023-07-17 12:54:49 -0400

    chore: Print in-cluster svr addr disabled warning during ArgoDB initialization (#14539)
    * chore: Print in-cluster svr addr disabled warning during ArgoDB initialization
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    * fix: undo a change
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    * chore: move to a function
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    * chore: rename
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    ---------
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- util/db/cluster.go
+++ util/db/cluster.go
@@ -77,8 +77,6 @@ func (db *db) ListClusters(ctx context.Context) (*appv1.ClusterList, error) {
 			if inClusterEnabled {
 				hasInClusterCredentials = true
 				clusterList.Items = append(clusterList.Items, *cluster)
-			} else {
-				log.Warnf("failed to add cluster %q to cluster list: in-cluster server address is disabled in Argo CD settings", cluster.Name)
 			}
 		} else {
 			clusterList.Items = append(clusterList.Items, *cluster)
