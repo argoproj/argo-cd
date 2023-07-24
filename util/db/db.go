07bb664769cdd4f33bf2c43413e4f2bd0a6d2cff
Yuan Tang <terrytangyuan@gmail.com>
2023-07-24 13:49:51 -0400

    chore: Print in-cluster svr addr disabled warning when server starts (#14553)
    * chore: Print in-cluster svr addr disabled warning when server starts
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    * fix: mock
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    
    * no interface change
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- util/db/db.go
+++ util/db/db.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/argoproj/argo-cd/v2/common"
 	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v2/util/settings"
 )
@@ -95,35 +93,10 @@ type db struct {
 
 // NewDB returns a new instance of the argo database
 func NewDB(namespace string, settingsMgr *settings.SettingsManager, kubeclientset kubernetes.Interface) ArgoDB {
-	dbInstance := db{
+	return &db{
 		settingsMgr:   settingsMgr,
 		ns:            namespace,
 		kubeclientset: kubeclientset,
-	}
-	dbInstance.logInClusterWarning()
-	return &dbInstance
-}
-
-func (db *db) logInClusterWarning() {
-	clusterSecrets, err := db.listSecretsByType(common.LabelValueSecretTypeCluster)
-	if err != nil {
-		log.WithError(err).Errorln("could not list secrets by type")
-	}
-	dbSettings, err := db.settingsMgr.GetSettings()
-	if err != nil {
-		log.WithError(err).Errorln("could not get DB settings")
-	}
-	for _, clusterSecret := range clusterSecrets {
-		cluster, err := secretToCluster(clusterSecret)
-		if err != nil {
-			log.Errorf("could not unmarshal cluster secret %s", clusterSecret.Name)
-			continue
-		}
-		if cluster.Server == appv1.KubernetesInternalAPIServerAddr {
-			if !dbSettings.InClusterEnabled {
-				log.Warnf("cluster %q uses in-cluster server address but it's disabled in Argo CD settings", cluster.Name)
-			}
-		}
 	}
 }
 
