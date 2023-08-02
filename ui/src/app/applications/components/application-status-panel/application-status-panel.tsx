78b8a572d8a5a530240cc7e781182bc67f9a4dc4
ashinsabu3 <ashin.sabu@harness.io>
2023-08-02 19:10:42 +0530

    fix: Fixes health icon positioning #14708
    Signed-off-by: ashinsabu3 <ashin.sabu@harness.io>

--- ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
@@ -72,7 +72,7 @@ export const ApplicationStatusPanel = ({application, showDiff, showOperation, sh
         <div className='application-status-panel row'>
             <div className='application-status-panel__item'>
                 <div style={{marginBottom: '1em'}}>{sectionLabel({title: 'APP HEALTH', helpContent: 'The health status of your app'})}</div>
-                <div className='application-status-panel__item-value'>
+                <div style={{alignItems: 'center'}} className='application-status-panel__item-value'>
                     <HealthStatusIcon state={application.status.health} />
                     &nbsp;
                     {application.status.health.status}
