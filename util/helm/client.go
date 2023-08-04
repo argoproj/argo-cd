b3eaa61c91098f63ec58eff79aefa783f8010918
xashr <103113861+xashr@users.noreply.github.com>
2023-08-04 20:17:11 +0000

    fix: Change underscore (_) back to plus (+) to get valid SemVer when when reading tags from OCI registry (#14537)
    * fix: Change underscore (_) back to plus (+) to get valid SemVer when reading tags from OCI registry
    
    Signed-off-by: xashr <saschasynaos@gmail.com>
    
    * Add test coverage for SemVer tags in TestGetTagsFromUrl
    
    Signed-off-by: xashr <saschasynaos@gmail.com>
    
    ---------
    
    Signed-off-by: xashr <saschasynaos@gmail.com>

--- util/helm/client.go
+++ util/helm/client.go
@@ -425,8 +425,13 @@ func (c *nativeHelmChart) GetTags(chart string, noCache bool) (*TagsList, error)
 		}
 
 		ctx := context.Background()
-		err = repo.Tags(ctx, "", func(tagResult []string) error {
-			tags.Tags = append(tags.Tags, tagResult...)
+		err = repo.Tags(ctx, "", func(tagsResult []string) error {
+			for _, tag := range tagsResult {
+				// By convention: Change underscore (_) back to plus (+) to get valid SemVer
+				convertedTag := strings.ReplaceAll(tag, "_", "+")
+				tags.Tags = append(tags.Tags, convertedTag)
+			}
+
 			return nil
 		})
 
