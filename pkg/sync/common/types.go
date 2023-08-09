ed7c77a9290bd4ee55107f23e12c0fc71ca10be9
pasha-codefresh <pavel@codefresh.io>
2023-08-09 16:45:34 +0300

    feat: Apply out of sync option only (#533)
    Signed-off-by: pashakostohrys <pavel@codefresh.io>

--- pkg/sync/common/types.go
+++ pkg/sync/common/types.go
@@ -31,6 +31,8 @@ const (
 	SyncOptionServerSideApply = "ServerSideApply=true"
 	// Sync option that disables resource deletion
 	SyncOptionDisableDeletion = "Delete=false"
+	// Sync option that sync only out of sync resources
+	SyncOptionApplyOutOfSyncOnly = "ApplyOutOfSyncOnly=true"
 )
 
 type PermissionValidator func(un *unstructured.Unstructured, res *metav1.APIResource) error
