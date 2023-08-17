1ec1b8b09ea91f464bd04e4c20d057f8be193ba1
voidspooks <cameronbtesterman@gmail.com>
2023-08-17 13:37:52 -0500

    bump helm to forked v3.12.4

--- hack/installers/install-helm-linux.sh
+++ hack/installers/install-helm-linux.sh
@@ -3,14 +3,14 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-export TARGET_FILE=helm-v${helm3_version}-linux-${ARCHITECTURE}.tar.gz
+# export TARGET_FILE=helm-v${helm3_version}-linux-${ARCHITECTURE}.tar.gz
 
-[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o $DOWNLOADS/${TARGET_FILE} https://get.helm.sh/helm-v${helm3_version}-linux-$ARCHITECTURE.tar.gz
-$(dirname $0)/compare-chksum.sh
-mkdir -p /tmp/helm && tar -C /tmp/helm -xf $DOWNLOADS/${TARGET_FILE}
-sudo install -m 0755 /tmp/helm/linux-$ARCHITECTURE/helm $BIN/helm
-# mkdir -p /tmp/helm && cd /tmp/helm
-# curl -LO https://github.com/voidspooks/helm/releases/download/v3.12.1/helm
-# chmod +x helm
-# cp helm $BIN/helm
-# helm version --client
+# [ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o $DOWNLOADS/${TARGET_FILE} https://get.helm.sh/helm-v${helm3_version}-linux-$ARCHITECTURE.tar.gz
+# $(dirname $0)/compare-chksum.sh
+# mkdir -p /tmp/helm && tar -C /tmp/helm -xf $DOWNLOADS/${TARGET_FILE}
+# sudo install -m 0755 /tmp/helm/linux-$ARCHITECTURE/helm $BIN/helm
+mkdir -p /tmp/helm && cd /tmp/helm
+curl -LO https://github.com/voidspooks/helm/releases/download/v3.12.4/helm
+chmod +x helm
+cp helm $BIN/helm
+helm version --client
