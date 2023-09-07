655de0ac234484fef9a24f98b7c126eaf633811d
pasha-codefresh <pavel@codefresh.io>
2023-09-07 17:12:15 +0300

    Merge pull request from GHSA-g687-f2gx-6wm8
    * feat: use untar with limiter
    
    Signed-off-by: pashakostohrys <pavel@codefresh.io>
    
    * feat: use untar with limiter
    
    Signed-off-by: pashakostohrys <pavel@codefresh.io>
    
    ---------
    
    Signed-off-by: pashakostohrys <pavel@codefresh.io>
    Signed-off-by: Jimmy Neville <jimmyeneville@gmail.com>

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -106,6 +106,8 @@ type RepoServerInitConstants struct {
 	AllowOutOfBoundsSymlinks                     bool
 	StreamedManifestMaxExtractedSize             int64
 	StreamedManifestMaxTarSize                   int64
+	HelmManifestMaxExtractedSize                 int64
+	DisableHelmManifestMaxExtractedSize          bool
 }
 
 // NewService returns a new instance of the Manifest service
@@ -345,7 +347,7 @@ func (s *Service) runRepoOperation(
 		if source.Helm != nil {
 			helmPassCredentials = source.Helm.PassCredentials
 		}
-		chartPath, closer, err := helmClient.ExtractChart(source.Chart, revision, helmPassCredentials)
+		chartPath, closer, err := helmClient.ExtractChart(source.Chart, revision, helmPassCredentials, s.initConstants.HelmManifestMaxExtractedSize, s.initConstants.DisableHelmManifestMaxExtractedSize)
 		if err != nil {
 			return err
 		}
@@ -2266,7 +2268,7 @@ func (s *Service) GetRevisionChartDetails(ctx context.Context, q *apiclient.Repo
 	if err != nil {
 		return nil, fmt.Errorf("helm client error: %v", err)
 	}
-	chartPath, closer, err := helmClient.ExtractChart(q.Name, revision, false)
+	chartPath, closer, err := helmClient.ExtractChart(q.Name, revision, false, s.initConstants.HelmManifestMaxExtractedSize, s.initConstants.DisableHelmManifestMaxExtractedSize)
 	if err != nil {
 		return nil, fmt.Errorf("error extracting chart: %v", err)
 	}
