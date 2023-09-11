d8c63b9229b2303d64c0f69a7551c5fbab0cc068
Ashutosh Pednekar <ashupednekar49@gmail.com>
2023-09-12 01:05:31 +0530

    feat: added shorthand flags for follow and conatainre in app logs (#15400)
    * feat: added shorthand flags for follow and conatainre in app logs
    
    Signed-off-by: ashu <ashupednekar49@gmail.com>
    
    * doc: re-generated cli docs
    
    Signed-off-by: ashu <ashupednekar49@gmail.com>
    
    ---------
    
    Signed-off-by: ashu <ashupednekar49@gmail.com>
    Co-authored-by: jannfis <jann@mistrust.net>

--- cmd/argocd/commands/app.go
+++ cmd/argocd/commands/app.go
@@ -420,12 +420,12 @@ func NewApplicationLogsCommand(clientOpts *argocdclient.ClientOptions) *cobra.Co
 	command.Flags().StringVar(&kind, "kind", "", "Resource kind")
 	command.Flags().StringVar(&namespace, "namespace", "", "Resource namespace")
 	command.Flags().StringVar(&resourceName, "name", "", "Resource name")
-	command.Flags().BoolVar(&follow, "follow", false, "Specify if the logs should be streamed")
+	command.Flags().BoolVarP(&follow, "follow", "f", false, "Specify if the logs should be streamed")
 	command.Flags().Int64Var(&tail, "tail", 0, "The number of lines from the end of the logs to show")
 	command.Flags().Int64Var(&sinceSeconds, "since-seconds", 0, "A relative time in seconds before the current time from which to show logs")
 	command.Flags().StringVar(&untilTime, "until-time", "", "Show logs until this time")
 	command.Flags().StringVar(&filter, "filter", "", "Show logs contain this string")
-	command.Flags().StringVar(&container, "container", "", "Optional container name")
+	command.Flags().StringVarP(&container, "container", "c", "", "Optional container name")
 	command.Flags().BoolVarP(&previous, "previous", "p", false, "Specify if the previously terminated container logs should be returned")
 
 	return command
