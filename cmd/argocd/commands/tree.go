4e8d6448fa64e9335fd863801665de090d811f3c
schakrad <58915923+schakrad@users.noreply.github.com>
2023-09-11 21:09:35 -0700

    feat: tree option in output flag of app resources command for treeview
    Signed-off-by: schakrad <58915923+schakrad@users.noreply.github.com>

---
+++ cmd/argocd/commands/tree.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+)
+
+const (
+	firstElemPrefix = `├─`
+	lastElemPrefix  = `└─`
+	indent          = "  "
+	pipe            = `│ `
+)
+
+func treeViewAppResourcesNotOrphaned(prefix string, objs map[string]v1alpha1.ResourceNode, obj map[string][]string, parent v1alpha1.ResourceNode, w *tabwriter.Writer) {
+	if len(parent.ParentRefs) == 0 {
+		_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", parent.Group, parent.Kind, parent.Namespace, parent.Name, "No")
+	}
+	chs := obj[parent.UID]
+	for i, child := range chs {
+		var p string
+		switch i {
+		case len(chs) - 1:
+			p = prefix + lastElemPrefix
+		default:
+			p = prefix + firstElemPrefix
+		}
+		treeViewAppResourcesNotOrphaned(p, objs, obj, objs[child], w)
+	}
+}
+
+func treeViewAppResourcesOrphaned(prefix string, objs map[string]v1alpha1.ResourceNode, obj map[string][]string, parent v1alpha1.ResourceNode, w *tabwriter.Writer) {
+	_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", parent.Group, parent.Kind, parent.Namespace, parent.Name, "Yes")
+	chs := obj[parent.UID]
+	for i, child := range chs {
+		var p string
+		switch i {
+		case len(chs) - 1:
+			p = prefix + lastElemPrefix
+		default:
+			p = prefix + firstElemPrefix
+		}
+		treeViewAppResourcesOrphaned(p, objs, obj, objs[child], w)
+	}
+}
+
+func printPrefix(p string) string {
+	if strings.HasSuffix(p, firstElemPrefix) {
+		p = strings.Replace(p, firstElemPrefix, pipe, strings.Count(p, firstElemPrefix)-1)
+	} else {
+		p = strings.ReplaceAll(p, firstElemPrefix, pipe)
+	}
+
+	if strings.HasSuffix(p, lastElemPrefix) {
+		p = strings.Replace(p, lastElemPrefix, strings.Repeat(" ", len([]rune(lastElemPrefix))), strings.Count(p, lastElemPrefix)-1)
+	} else {
+		p = strings.ReplaceAll(p, lastElemPrefix, strings.Repeat(" ", len([]rune(lastElemPrefix))))
+	}
+	return p
+}
