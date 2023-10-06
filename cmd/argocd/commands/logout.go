747fb9113e74cd959f0e5cec97a7767cd5ed2d22
Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
2023-10-06 09:19:22 +0800

    feat(cli): Add example to `--help` output 1 (#15782)
    * re-sign commit with DCO
    
    Signed-off-by: Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
    
    * Update doc file
    
    Signed-off-by: Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
    
    ---------
    
    Signed-off-by: Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
    Signed-off-by: Gaurang Kudale <gaurang.kudale02@gmail.com>

--- cmd/argocd/commands/logout.go
+++ cmd/argocd/commands/logout.go
@@ -37,6 +37,10 @@ func NewLogoutCommand(globalClientOpts *argocdclient.ClientOptions) *cobra.Comma
 		argocd logout my-argocd-context
 		`),
 		Long:  "Log out from Argo CD",
+		Example: `# To log out of argocd
+$ argocd logout
+# This can be helpful for security reasons or when you want to switch between different Argo CD contexts or accounts.
+`,
 		Run: func(c *cobra.Command, args []string) {
 			if len(args) == 0 {
 				c.HelpFunc()(c, args)
