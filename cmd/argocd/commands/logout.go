84988aab1aef1e9720f043142219a9bcb876b7c5
Gaurang Kudale <gaurang.kudale02@gmail.com>
2023-10-06 11:01:44 +0530

    removing duplicate field name
    Signed-off-by: Gaurang Kudale <gaurang.kudale02@gmail.com>

--- cmd/argocd/commands/logout.go
+++ cmd/argocd/commands/logout.go
@@ -12,8 +12,9 @@ import (
 	"github.com/argoproj/argo-cd/v2/util/localconfig"
 	"github.com/argoproj/argo-cd/v2/util/templates"
 )
+
 var (
-		logoutExample = templates.Examples(`
+	logoutExample = templates.Examples(`
 		# Basic Logout
 		argocd logout
 
@@ -27,20 +28,14 @@ var (
 		argocd logout my-argocd-context 
 		`)
 )
+
 // NewLogoutCommand returns a new instance of `argocd logout` command
 func NewLogoutCommand(globalClientOpts *argocdclient.ClientOptions) *cobra.Command {
 	var command = &cobra.Command{
 		Use:   "logout CONTEXT",
 		Short: "Log out from Argo CD",
-		Example: templates.Examples(`
-		# Basic Logout from a Specific Context
-		argocd logout my-argocd-context
-		`),
-		Long:  "Log out from Argo CD",
-		Example: `# To log out of argocd
-$ argocd logout
-# This can be helpful for security reasons or when you want to switch between different Argo CD contexts or accounts.
-`,
+		Example: logoutExample,
+		Long: "Log out from Argo CD",
 		Run: func(c *cobra.Command, args []string) {
 			if len(args) == 0 {
 				c.HelpFunc()(c, args)
