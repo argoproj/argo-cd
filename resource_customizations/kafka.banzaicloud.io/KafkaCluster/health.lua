c0363e66fa9855b795f4e963b52490621fc876ab
Jordan Moore <1930631+OneCricketeer@users.noreply.github.com>
2023-10-13 21:02:54 +0000

    Update health.lua
    Signed-off-by: Jordan Moore <1930631+OneCricketeer@users.noreply.github.com>

--- resource_customizations/kafka.banzaicloud.io/KafkaCluster/health.lua
+++ resource_customizations/kafka.banzaicloud.io/KafkaCluster/health.lua
@@ -1,6 +1,7 @@
 local health_status = {}
-if obj.status ~= nil then
-  local brokersState = obj.status.brokersState
+local objectStatus = obj.status
+if objectStatus ~= nil then
+  local brokersState = objectStatus.brokersState
   if brokersState ~= nil then
     local counter = 0
     local brokerReady = 0
@@ -18,31 +19,31 @@ if obj.status ~= nil then
         end
       end
     end
-    if counter == brokerReady then
-      local statusState = obj.status.state
-      local cruiseControlTopicStatus = obj.status.cruiseControlTopicStatus
-      if cruiseControlTopicStatus == "CruiseControlTopicReady" and statusState == "ClusterRunning" then
-        health_status.message = "Kafka Brokers, CruiseControl and cluster are in Healthy State."
-        health_status.status = "Healthy"
-        return health_status
-      end
-      if cruiseControlTopicStatus == "CruiseControlTopicNotReady" or cruiseControlTopicStatus == nil then
-        if statusState == "ClusterReconciling" then
-          health_status.message = "Kafka Cluster is Reconciling."
-          health_status.status = "Progressing"
-          return health_status
-        end
-        if statusState == "ClusterRollingUpgrading" then
-          health_status.message = "Kafka Cluster is Rolling Upgrading."
-          health_status.status = "Progressing"
-          return health_status
-        end
-      end
-    else
+    if counter != brokerReady then
       health_status.message = "Broker Config is out of Sync or CruiseControlState is not Ready"
       health_status.status = "Degraded"
       return health_status
+
+    local statusState = objectStatus.state
+    local cruiseControlTopicStatus = objectStatus.cruiseControlTopicStatus
+    if cruiseControlTopicStatus == "CruiseControlTopicReady" and statusState == "ClusterRunning" then
+      health_status.message = "Kafka Brokers, CruiseControl and cluster are in Healthy State."
+      health_status.status = "Healthy"
+      return health_status
     end
+    if cruiseControlTopicStatus == "CruiseControlTopicNotReady" or cruiseControlTopicStatus == nil then
+      if statusState == "ClusterReconciling" then
+        health_status.message = "Kafka Cluster is Reconciling."
+        health_status.status = "Progressing"
+        return health_status
+      end
+      if statusState == "ClusterRollingUpgrading" then
+        health_status.message = "Kafka Cluster is Rolling Upgrading."
+        health_status.status = "Progressing"
+        return health_status
+      end
+    end
+
   end
 end
 health_status.status = "Progressing"
