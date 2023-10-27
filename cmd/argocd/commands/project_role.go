819fea7d8bd9d4796d9f8bf7e8ccf61572ce7353
Ratan Gulati <ratangulati.dev@gmail.com>
2023-10-28 04:37:30 +0530

    feat: Add examples to --help output for remaining "create PROJECT ROLE-NAME" (#15983)
    * feat: Add examples to --help output for remaining create PROJECT ROLE-NAME
    
    Signed-off-by: Ratan Gulati <ratangulati.dev@gmail.com>
    
    * feat: Add examples to --help output for remaining create PROJECT ROLE-NAME
    
    Signed-off-by: Ratan Gulati <ratangulati.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Ratan Gulati <ratangulati.dev@gmail.com>
    Signed-off-by: Kevin Lyda <kevin@lyda.ie>

--- cmd/argocd/commands/project_role.go
+++ cmd/argocd/commands/project_role.go
@@ -189,6 +189,11 @@ func NewProjectRoleCreateCommand(clientOpts *argocdclient.ClientOptions) *cobra.
 	var command = &cobra.Command{
 		Use:   "create PROJECT ROLE-NAME",
 		Short: "Create a project role",
+		Example: templates.Examples(`  
+  # Create a project role in the "my-project" project with the name "my-role".
+  argocd proj role create my-project my-role --description "My project role description"
+  		`),
+
 		Run: func(c *cobra.Command, args []string) {
 			ctx := c.Context()
 
