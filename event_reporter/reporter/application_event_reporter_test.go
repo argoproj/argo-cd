e2cc015297feb05f181f5acfbf1fa43b4b90284c
pashakostohrys <pavel@codefresh.io>
2023-11-28 12:20:09 +0200

    Revert "Revert "feat: support new reporting component (#265)""
    This reverts commit 3514011024ec9623bc85946bf609a500e4a338f0.

---
+++ event_reporter/reporter/application_event_reporter_test.go
@@ -0,0 +1,461 @@
+package reporter
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/argoproj/argo-cd/v2/event_reporter/metrics"
+	"github.com/argoproj/argo-cd/v2/util/io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/ghodss/yaml"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"google.golang.org/grpc"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
+	appsv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	fakeapps "github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned/fake"
+	appinformer "github.com/argoproj/argo-cd/v2/pkg/client/informers/externalversions"
+	applisters "github.com/argoproj/argo-cd/v2/pkg/client/listers/application/v1alpha1"
+
+	servercache "github.com/argoproj/argo-cd/v2/server/cache"
+	cacheutil "github.com/argoproj/argo-cd/v2/util/cache"
+	appstatecache "github.com/argoproj/argo-cd/v2/util/cache/appstate"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/argoproj/argo-cd/v2/common"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/events"
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	repoApiclient "github.com/argoproj/argo-cd/v2/reposerver/apiclient"
+	"github.com/argoproj/argo-cd/v2/util/argo"
+)
+
+const (
+	testNamespace = "default"
+)
+
+func TestGetResourceEventPayload(t *testing.T) {
+	t.Run("Deleting timestamp is empty", func(t *testing.T) {
+
+		app := v1alpha1.Application{}
+		rs := v1alpha1.ResourceStatus{}
+
+		man := "{ \"key\" : \"manifest\" }"
+
+		actualState := application.ApplicationResourceResponse{
+			Manifest: &man,
+		}
+		desiredState := repoApiclient.Manifest{
+			CompiledManifest: "{ \"key\" : \"manifest\" }",
+		}
+		appTree := v1alpha1.ApplicationTree{}
+		revisionMetadata := v1alpha1.RevisionMetadata{
+			Author:  "demo usert",
+			Date:    metav1.Time{},
+			Message: "some message",
+		}
+
+		event, err := getResourceEventPayload(&app, &rs, &actualState, &desiredState, &appTree, true, "", nil, &revisionMetadata, nil, common.LabelKeyAppInstance, argo.TrackingMethodLabel, &repoApiclient.ApplicationVersions{})
+		assert.NoError(t, err)
+
+		var eventPayload events.EventPayload
+
+		err = json.Unmarshal(event.Payload, &eventPayload)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "{ \"key\" : \"manifest\" }", eventPayload.Source.DesiredManifest)
+		assert.Equal(t, "{ \"key\" : \"manifest\" }", eventPayload.Source.ActualManifest)
+	})
+
+	t.Run("Deleting timestamp is empty", func(t *testing.T) {
+
+		app := v1alpha1.Application{
+			ObjectMeta: metav1.ObjectMeta{
+				DeletionTimestamp: &metav1.Time{},
+			},
+			Status: v1alpha1.ApplicationStatus{},
+		}
+		rs := v1alpha1.ResourceStatus{}
+		man := "{ \"key\" : \"manifest\" }"
+		actualState := application.ApplicationResourceResponse{
+			Manifest: &man,
+		}
+		desiredState := repoApiclient.Manifest{
+			CompiledManifest: "{ \"key\" : \"manifest\" }",
+		}
+		appTree := v1alpha1.ApplicationTree{}
+		revisionMetadata := v1alpha1.RevisionMetadata{
+			Author:  "demo usert",
+			Date:    metav1.Time{},
+			Message: "some message",
+		}
+
+		event, err := getResourceEventPayload(&app, &rs, &actualState, &desiredState, &appTree, true, "", nil, &revisionMetadata, nil, common.LabelKeyAppInstance, argo.TrackingMethodLabel, &repoApiclient.ApplicationVersions{})
+		assert.NoError(t, err)
+
+		var eventPayload events.EventPayload
+
+		err = json.Unmarshal(event.Payload, &eventPayload)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "", eventPayload.Source.DesiredManifest)
+		assert.Equal(t, "", eventPayload.Source.ActualManifest)
+	})
+}
+
+func TestGetApplicationLatestRevision(t *testing.T) {
+	appRevision := "a-revision"
+	history1Revision := "history-revision-1"
+	history2Revision := "history-revision-2"
+
+	t.Run("resource revision should be taken from sync.revision", func(t *testing.T) {
+		noStatusHistoryAppMock := v1alpha1.Application{
+			Status: v1alpha1.ApplicationStatus{
+				Sync: v1alpha1.SyncStatus{
+					Revision: appRevision,
+				},
+			},
+		}
+
+		revisionResult := getApplicationLatestRevision(&noStatusHistoryAppMock)
+		assert.Equal(t, revisionResult, appRevision)
+
+		emptyStatusHistoryAppMock := v1alpha1.Application{
+			Status: v1alpha1.ApplicationStatus{
+				Sync: v1alpha1.SyncStatus{
+					Revision: appRevision,
+				},
+				History: []v1alpha1.RevisionHistory{},
+			},
+		}
+
+		revision2Result := getApplicationLatestRevision(&emptyStatusHistoryAppMock)
+		assert.Equal(t, revision2Result, appRevision)
+	})
+
+	t.Run("resource revision should be taken from latest history.revision", func(t *testing.T) {
+		appMock := v1alpha1.Application{
+			Status: v1alpha1.ApplicationStatus{
+				Sync: v1alpha1.SyncStatus{
+					Revision: appRevision,
+				},
+				History: []v1alpha1.RevisionHistory{
+					v1alpha1.RevisionHistory{
+						Revision: history1Revision,
+					},
+					v1alpha1.RevisionHistory{
+						Revision: history2Revision,
+					},
+				},
+			},
+		}
+
+		revisionResult := getApplicationLatestRevision(&appMock)
+		assert.Equal(t, revisionResult, history2Revision)
+	})
+}
+
+func TestGetLatestAppHistoryId(t *testing.T) {
+	history1Id := int64(1)
+	history2Id := int64(2)
+
+	t.Run("resource revision should be 0", func(t *testing.T) {
+		noStatusHistoryAppMock := v1alpha1.Application{}
+
+		idResult := getLatestAppHistoryId(&noStatusHistoryAppMock)
+		assert.Equal(t, idResult, int64(0))
+
+		emptyStatusHistoryAppMock := v1alpha1.Application{
+			Status: v1alpha1.ApplicationStatus{
+				History: []v1alpha1.RevisionHistory{},
+			},
+		}
+
+		id2Result := getLatestAppHistoryId(&emptyStatusHistoryAppMock)
+		assert.Equal(t, id2Result, int64(0))
+	})
+
+	t.Run("resource revision should be taken from latest history.Id", func(t *testing.T) {
+		appMock := v1alpha1.Application{
+			Status: v1alpha1.ApplicationStatus{
+				History: []v1alpha1.RevisionHistory{
+					v1alpha1.RevisionHistory{
+						ID: history1Id,
+					},
+					v1alpha1.RevisionHistory{
+						ID: history2Id,
+					},
+				},
+			},
+		}
+
+		revisionResult := getLatestAppHistoryId(&appMock)
+		assert.Equal(t, revisionResult, history2Id)
+	})
+}
+
+func newAppLister(objects ...runtime.Object) applisters.ApplicationLister {
+	fakeAppsClientset := fakeapps.NewSimpleClientset(objects...)
+	factory := appinformer.NewSharedInformerFactoryWithOptions(fakeAppsClientset, 0, appinformer.WithNamespace(""), appinformer.WithTweakListOptions(func(options *metav1.ListOptions) {}))
+	appsInformer := factory.Argoproj().V1alpha1().Applications()
+	for _, obj := range objects {
+		switch obj.(type) {
+		case *appsv1.Application:
+			_ = appsInformer.Informer().GetStore().Add(obj)
+		}
+	}
+	appLister := appsInformer.Lister()
+	return appLister
+}
+
+type MockcodefreshClient interface {
+	Send(ctx context.Context, appName string, event *events.Event) error
+}
+
+type MockCodefreshConfig struct {
+	BaseURL   string
+	AuthToken string
+}
+
+type MockCodefreshClient struct {
+	cfConfig   *MockCodefreshConfig
+	httpClient *http.Client
+}
+
+func (cc *MockCodefreshClient) Send(ctx context.Context, appName string, event *events.Event) error {
+	return nil
+}
+
+func fakeReporter() *applicationEventReporter {
+	guestbookApp := &appsv1.Application{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Application",
+			APIVersion: "argoproj.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "guestbook",
+			Namespace: testNamespace,
+		},
+		Spec: appsv1.ApplicationSpec{
+			Project: "default",
+			Source: &appsv1.ApplicationSource{
+				RepoURL:        "https://test",
+				TargetRevision: "HEAD",
+				Helm: &appsv1.ApplicationSourceHelm{
+					ValueFiles: []string{"values.yaml"},
+				},
+			},
+		},
+		Status: appsv1.ApplicationStatus{
+			History: appsv1.RevisionHistories{
+				{
+					Revision: "abcdef123567",
+					Source: appsv1.ApplicationSource{
+						RepoURL:        "https://test",
+						TargetRevision: "HEAD",
+						Helm: &appsv1.ApplicationSourceHelm{
+							ValueFiles: []string{"values-old.yaml"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	appLister := newAppLister(guestbookApp)
+
+	cache := servercache.NewCache(
+		appstatecache.NewCache(
+			cacheutil.NewCache(cacheutil.NewInMemoryCache(1*time.Hour)),
+			1*time.Minute,
+		),
+		1*time.Minute,
+		1*time.Minute,
+		1*time.Minute,
+	)
+
+	cfClient := &MockCodefreshClient{
+		cfConfig: &MockCodefreshConfig{
+			BaseURL:   "",
+			AuthToken: "",
+		},
+		httpClient: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
+
+	metricsServ := metrics.NewMetricsServer("", 8099)
+	closer, cdClient, _ := apiclient.NewClientOrDie(&apiclient.ClientOptions{
+		ServerAddr: "site.com",
+	}).NewApplicationClient()
+	defer io.Close(closer)
+
+	return &applicationEventReporter{
+		cache,
+		cfClient,
+		appLister,
+		cdClient,
+		metricsServ,
+	}
+}
+
+func TestShouldSendEvent(t *testing.T) {
+	eventReporter := fakeReporter()
+	t.Run("should send because cache is missing", func(t *testing.T) {
+		app := &v1alpha1.Application{}
+		rs := v1alpha1.ResourceStatus{}
+
+		res := eventReporter.shouldSendResourceEvent(app, rs)
+		assert.True(t, res)
+	})
+
+	t.Run("should not send - same entities", func(t *testing.T) {
+		app := &v1alpha1.Application{}
+		rs := v1alpha1.ResourceStatus{}
+
+		_ = eventReporter.cache.SetLastResourceEvent(app, rs, time.Minute, "")
+
+		res := eventReporter.shouldSendResourceEvent(app, rs)
+		assert.False(t, res)
+	})
+
+	t.Run("should send - different entities", func(t *testing.T) {
+		app := &v1alpha1.Application{}
+		rs := v1alpha1.ResourceStatus{}
+
+		_ = eventReporter.cache.SetLastResourceEvent(app, rs, time.Minute, "")
+
+		rs.Status = v1alpha1.SyncStatusCodeOutOfSync
+
+		res := eventReporter.shouldSendResourceEvent(app, rs)
+		assert.True(t, res)
+	})
+
+}
+
+type MockEventing_StartEventSourceServer struct {
+	grpc.ServerStream
+}
+
+var result func(*events.Event) error
+
+func (m *MockEventing_StartEventSourceServer) Send(event *events.Event) error {
+	return result(event)
+}
+
+func TestStreamApplicationEvent(t *testing.T) {
+	eventReporter := fakeReporter()
+	t.Run("root application", func(t *testing.T) {
+		app := &v1alpha1.Application{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "argoproj.io/v1alpha1",
+				Kind:       "Application",
+			},
+		}
+
+		result = func(event *events.Event) error {
+			var payload events.EventPayload
+			_ = json.Unmarshal(event.Payload, &payload)
+
+			var actualApp v1alpha1.Application
+			_ = json.Unmarshal([]byte(payload.Source.ActualManifest), &actualApp)
+			assert.Equal(t, *app, actualApp)
+			return nil
+		}
+		_ = eventReporter.StreamApplicationEvents(context.Background(), app, "", false, common.LabelKeyAppInstance, argo.TrackingMethodLabel)
+	})
+
+}
+
+func TestGetResourceEventPayloadWithoutRevision(t *testing.T) {
+	app := v1alpha1.Application{}
+	rs := v1alpha1.ResourceStatus{}
+
+	mf := "{ \"key\" : \"manifest\" }"
+
+	actualState := application.ApplicationResourceResponse{
+		Manifest: &mf,
+	}
+	desiredState := repoApiclient.Manifest{
+		CompiledManifest: "{ \"key\" : \"manifest\" }",
+	}
+	appTree := v1alpha1.ApplicationTree{}
+
+	_, err := getResourceEventPayload(&app, &rs, &actualState, &desiredState, &appTree, true, "", nil, nil, nil, common.LabelKeyAppInstance, argo.TrackingMethodLabel, &repoApiclient.ApplicationVersions{})
+	assert.NoError(t, err)
+
+}
+
+func StrToUnstructured(jsonStr string) *unstructured.Unstructured {
+	obj := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(jsonStr), &obj)
+	if err != nil {
+		panic(err)
+	}
+	return &unstructured.Unstructured{Object: obj}
+}
+
+func TestAddCommitDetailsToLabels(t *testing.T) {
+	revisionMetadata := v1alpha1.RevisionMetadata{
+		Author:  "demo usert",
+		Date:    metav1.Time{},
+		Message: "some message",
+	}
+
+	t.Run("set labels when lable object missing", func(t *testing.T) {
+		resource := StrToUnstructured(`
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: helm-guestbook
+    namespace: default
+    resourceVersion: "123"
+    uid: "4"
+  spec:
+    selector:
+      app: guestbook
+    type: LoadBalancer
+  status:
+    loadBalancer:
+      ingress:
+      - hostname: localhost`,
+		)
+
+		result := addCommitDetailsToLabels(resource, &revisionMetadata)
+		labels := result.GetLabels()
+		assert.Equal(t, revisionMetadata.Author, labels["app.meta.commit-author"])
+		assert.Equal(t, revisionMetadata.Message, labels["app.meta.commit-message"])
+	})
+
+	t.Run("set labels when labels present", func(t *testing.T) {
+		resource := StrToUnstructured(`
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: helm-guestbook
+    namespace: default
+    labels:
+      link: http://my-grafana.com/pre-generated-link
+  spec:
+    selector:
+      app: guestbook
+    type: LoadBalancer
+  status:
+    loadBalancer:
+      ingress:
+      - hostname: localhost`,
+		)
+
+		result := addCommitDetailsToLabels(resource, &revisionMetadata)
+		labels := result.GetLabels()
+		assert.Equal(t, revisionMetadata.Author, labels["app.meta.commit-author"])
+		assert.Equal(t, revisionMetadata.Message, labels["app.meta.commit-message"])
+		assert.Equal(t, "http://my-grafana.com/pre-generated-link", result.GetLabels()["link"])
+	})
+}
---
+++ event_reporter/reporter/broadcaster.go
@@ -0,0 +1,140 @@
+package reporter
+
+import (
+	argocommon "github.com/argoproj/argo-cd/v2/common"
+	"github.com/argoproj/argo-cd/v2/event_reporter/sharding"
+	"github.com/argoproj/argo-cd/v2/util/env"
+	"math"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/watch"
+
+	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+)
+
+type subscriber struct {
+	ch      chan *appv1.ApplicationWatchEvent
+	filters []func(*appv1.ApplicationWatchEvent) bool
+}
+
+func (s *subscriber) matches(event *appv1.ApplicationWatchEvent) bool {
+	for i := range s.filters {
+		if !s.filters[i](event) {
+			return false
+		}
+	}
+	return true
+}
+
+// Broadcaster is an interface for broadcasting application informer watch events to multiple subscribers.
+type Broadcaster interface {
+	Subscribe(ch chan *appv1.ApplicationWatchEvent, filters ...func(event *appv1.ApplicationWatchEvent) bool) func()
+	OnAdd(interface{})
+	OnUpdate(interface{}, interface{})
+	OnDelete(interface{})
+}
+
+type broadcasterHandler struct {
+	lock           sync.Mutex
+	subscribers    []*subscriber
+	filter         sharding.ApplicationFilterFunction
+	featureManager *FeatureManager
+}
+
+func NewBroadcaster(featureManager *FeatureManager) Broadcaster {
+	// todo: pass real value here
+	filter := getApplicationFilter("")
+	return &broadcasterHandler{
+		filter:         filter,
+		featureManager: featureManager,
+	}
+}
+
+func (b *broadcasterHandler) notify(event *appv1.ApplicationWatchEvent) {
+	// Make a local copy of b.subscribers, then send channel events outside the lock,
+	// to avoid data race on b.subscribers changes
+	subscribers := []*subscriber{}
+	b.lock.Lock()
+	subscribers = append(subscribers, b.subscribers...)
+	b.lock.Unlock()
+
+	if !b.featureManager.ShouldReporterRun() {
+		log.Infof("filtering application '%s', event reporting is turned off and old one is in use", event.Application.Name)
+		return
+	}
+
+	if b.filter != nil {
+		result, expectedShard := b.filter(&event.Application)
+		if !result {
+			log.Infof("filtering application '%s', wrong shard, should be %d", event.Application.Name, expectedShard)
+			return
+		}
+	}
+
+	for _, s := range subscribers {
+		if s.matches(event) {
+			select {
+			case s.ch <- event:
+				log.Infof("adding application '%s' to channel", event.Application.Name)
+			default:
+				// drop event if cannot send right away
+				log.WithField("application", event.Application.Name).Warn("unable to send event notification")
+			}
+		}
+	}
+}
+
+// Subscribe forward application informer watch events to the provided channel.
+// The watch events are dropped if no receives are reading events from the channel so the channel must have
+// buffer if dropping events is not acceptable.
+func (b *broadcasterHandler) Subscribe(ch chan *appv1.ApplicationWatchEvent, filters ...func(event *appv1.ApplicationWatchEvent) bool) func() {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	subscriber := &subscriber{ch, filters}
+	b.subscribers = append(b.subscribers, subscriber)
+	return func() {
+		b.lock.Lock()
+		defer b.lock.Unlock()
+		for i := range b.subscribers {
+			if b.subscribers[i] == subscriber {
+				b.subscribers = append(b.subscribers[:i], b.subscribers[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+func (b *broadcasterHandler) OnAdd(obj interface{}) {
+	if app, ok := obj.(*appv1.Application); ok {
+		b.notify(&appv1.ApplicationWatchEvent{Application: *app, Type: watch.Added})
+	}
+}
+
+func (b *broadcasterHandler) OnUpdate(_, newObj interface{}) {
+	if app, ok := newObj.(*appv1.Application); ok {
+		b.notify(&appv1.ApplicationWatchEvent{Application: *app, Type: watch.Modified})
+	}
+}
+
+func (b *broadcasterHandler) OnDelete(obj interface{}) {
+	if app, ok := obj.(*appv1.Application); ok {
+		b.notify(&appv1.ApplicationWatchEvent{Application: *app, Type: watch.Deleted})
+	}
+}
+
+func getApplicationFilter(shardingAlgorithm string) sharding.ApplicationFilterFunction {
+	shardingSvc := sharding.NewSharding()
+	replicas := env.ParseNumFromEnv(argocommon.EnvEventReporterReplicas, 0, 0, math.MaxInt32)
+	var applicationFilter func(app *appv1.Application) (bool, int)
+	if replicas > 1 {
+		shard := sharding.GetShardNumber()
+		log.Infof("Processing applications from shard %d", shard)
+		log.Infof("Using filter function:  %s", shardingAlgorithm)
+		distributionFunction := shardingSvc.GetDistributionFunction(shardingAlgorithm)
+		applicationFilter = shardingSvc.GetApplicationFilter(distributionFunction, shard)
+	} else {
+		log.Info("Processing all application shards")
+	}
+	return applicationFilter
+}
