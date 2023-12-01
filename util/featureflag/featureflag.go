c2cad51043b389c95fbd56e298d621fef823ebfb
Leonardo Luz Almeida <leonardo_almeida@intuit.com>
2023-12-01 14:54:21 -0500

    add godocs
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- util/featureflag/featureflag.go
+++ util/featureflag/featureflag.go
@@ -1,12 +1,14 @@
 package featureflag
 
+// featureFlags This is the main struct where all featureFlags must be
+// defined as public attributes following the ExampleFeature pattern.
 type featureFlags struct {
 	ExampleFeature *featureFlag
 }
 
 // New will instantiate a new featureFlags object with all default values.
 // Every new feature flag must be initialized in this function or the test
-// will fail.
+// will fail. Description also needs to be provided or the test will fail.
 func New() featureFlags {
 	return featureFlags{
 		ExampleFeature: &featureFlag{
@@ -21,6 +23,7 @@ type featureFlag struct {
 	enabled     bool
 }
 
+// IsEnabled returns true if the feature flag is enabled.
 func (ff *featureFlag) IsEnabled() bool {
 	if ff == nil {
 		return false
@@ -28,14 +31,17 @@ func (ff *featureFlag) IsEnabled() bool {
 	return ff.enabled
 }
 
+// Enable will enable this feature flag.
 func (ff *featureFlag) Enable() {
 	ff.enabled = true
 }
 
+// Disable will disable this feature flag.
 func (ff *featureFlag) Disable() {
 	ff.enabled = false
 }
 
+// Description returns the description of this feature flag.
 func (ff *featureFlag) Description() string {
 	if ff == nil {
 		return ""
