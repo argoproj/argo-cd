3d4c1213080ed1b1164231cfc078acba64bad3de
Alexy Mantha <alexy@mantha.dev>
2023-12-18 15:38:53 -0500

    feat(ui): add status panel extensions (#15780)
    * add extension
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * rename to status panel
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * wip
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * cleanup
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * add docs
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * add key
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * fix copy/paste
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * wip
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * flyout
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * cleanup
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * lint
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * document flyout
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * rename
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    * linting
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    
    ---------
    
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>
    Signed-off-by: Kevin Lyda <kevin@lyda.ie>

--- ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
+++ ui/src/app/applications/components/application-status-panel/application-status-panel.tsx
@@ -16,6 +16,7 @@ interface Props {
     showDiff?: () => any;
     showOperation?: () => any;
     showConditions?: () => any;
+    showExtension?: (id: string) => any;
     showMetadataInfo?: (revision: string) => any;
 }
 
@@ -45,7 +46,7 @@ const sectionHeader = (info: SectionInfo, hasMultipleSources: boolean, onClick?:
     );
 };
 
-export const ApplicationStatusPanel = ({application, showDiff, showOperation, showConditions, showMetadataInfo}: Props) => {
+export const ApplicationStatusPanel = ({application, showDiff, showOperation, showConditions, showExtension, showMetadataInfo}: Props) => {
     const today = new Date();
 
     let daysSinceLastSynchronized = 0;
@@ -62,6 +63,8 @@ export const ApplicationStatusPanel = ({application, showDiff, showOperation, sh
     if (application.metadata.deletionTimestamp && !appOperationState) {
         showOperation = null;
     }
+
+    const statusExtensions = services.extensions.getStatusPanelExtensions();
 
     const infos = cntByCategory.get('info');
     const warnings = cntByCategory.get('warning');
@@ -203,6 +206,7 @@ export const ApplicationStatusPanel = ({application, showDiff, showOperation, sh
                     </React.Fragment>
                 )}
             </DataLoader>
+            {statusExtensions && statusExtensions.map(ext => <ext.component key={ext.title} application={application} openFlyout={() => showExtension && showExtension(ext.id)} />)}
         </div>
     );
 };
