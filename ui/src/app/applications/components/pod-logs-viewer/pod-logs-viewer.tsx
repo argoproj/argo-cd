97235a8ae02f1fad0c15e697f65bc4686d89718f
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2024-01-08 14:42:36 +0100

    UI pod-logs-viewer shows true for the first timestamp [NOK] #16744
    Signed-off-by: 4TT1L4 <2914096+4TT1L4@users.noreply.github.com>

--- ui/src/app/applications/components/pod-logs-viewer/pod-logs-viewer.tsx
+++ ui/src/app/applications/components/pod-logs-viewer/pod-logs-viewer.tsx
@@ -143,7 +143,7 @@ export const PodsLogsViewer = (props: PodLogsProps) => {
         // show the pod name if there are multiple pods, pad with spaces to align
         (viewPodNames ? (lineNum === 0 || logs[lineNum - 1].podName !== log.podName ? podColor(podName) + log.podName + reset : ' '.repeat(log.podName.length)) + ' ' : '') +
         // show the timestamp if requested, pad with spaces to align
-        (viewTimestamps ? (lineNum === 0 || (logs[lineNum - 1].timeStamp !== log.timeStamp ? log.timeStampStr : '').padEnd(30)) + ' ' : '') +
+        (viewTimestamps ? (((lineNum === 0 || logs[lineNum - 1].timeStamp !== log.timeStamp) ? (log.timeStampStr) : '').padEnd(31)) : '') +
         // show the log content, highlight the filter text
         log.content?.replace(highlight, (substring: string) => whiteOnYellow + substring + reset);
     const logsContent = (width: number, height: number, isWrapped: boolean) => (
