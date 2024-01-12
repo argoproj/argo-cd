883ccf501d8bbba9b366a8e912b3a5abc29e7292
Adrian Aneci <aneci@adobe.com>
2024-01-12 13:20:59 +0200

    Fix sync windows matches
    Signed-off-by: Adrian Aneci <aneci@adobe.com>

--- ui/src/app/settings/components/project-sync-windows-edit-panel/project-sync-windows-edit-panel.tsx
+++ ui/src/app/settings/components/project-sync-windows-edit-panel/project-sync-windows-edit-panel.tsx
@@ -67,6 +67,10 @@ export const ProjectSyncWindowsEditPanel = (props: ProjectSyncWindowsEditPanelPr
                             <div className='argo-form-row'>
                                 <FormField formApi={api} label='Enable manual sync' field='window.manualSync' component={CheckboxField} />
                             </div>
+                            {/*<div className='argo-form-row'>*/}
+                            {/*    <FormField formApi={api} label='Use AND operator while selecting the apps that match the configured selectors(applications, namespaces ad clusters)' */}
+                            {/*               field='window.andOperator' component={CheckboxField}/>*/}
+                            {/*</div>*/}
                         </div>
                         <div className='white-box'>
                             <ProjectSyncWindowApplicationsEdit projName={api.values.projName} window={api.values.window} formApi={api} />
