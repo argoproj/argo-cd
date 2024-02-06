25d69f81039c8e724fa54a0a905a79f6281cacfc
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2024-02-06 10:27:05 -0500

    fix(ui): Change path to "root" when path is root directory (#14949)
    * change path to display root
    
    Signed-off-by: Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
    
    * Fix inequality typo
    
    Signed-off-by: Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
    
    * Fix lint issues
    
    Signed-off-by: Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
    
    ---------
    
    Signed-off-by: Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
    Signed-off-by: Kevin Lyda <kevin@lyda.ie>

--- ui/src/app/applications/components/application-summary/application-summary.tsx
+++ ui/src/app/applications/components/application-summary/application-summary.tsx
@@ -37,6 +37,16 @@ function swap(array: any[], a: number, b: number) {
     return array;
 }
 
+function processPath(path: string) {
+    if (path !== null && path !== undefined) {
+        if (path === '.') {
+            return '(root)';
+        }
+        return path;
+    }
+    return '';
+}
+
 export interface ApplicationSummaryProps {
     app: models.Application;
     updateApp: (app: models.Application, query: {validate?: boolean}) => Promise<any>;
@@ -239,7 +249,7 @@ export const ApplicationSummary = (props: ApplicationSummaryProps) => {
                       title: 'PATH',
                       view: (
                           <Revision repoUrl={source.repoURL} revision={source.targetRevision || 'HEAD'} path={source.path} isForPath={true}>
-                              {source.path ?? ''}
+                              {processPath(source.path)}
                           </Revision>
                       ),
                       edit: (formApi: FormApi) =>
