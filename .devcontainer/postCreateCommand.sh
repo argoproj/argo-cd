cd43d239e0a34645246dc973961331750f1d505b
Carlos Santana <csantana23@gmail.com>
2024-02-23 09:53:24 -0500

    add goreman
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,25 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # install ubuntu packages
 sudo apt-get update
-sudo apt-get install -y gnupg2 bash-completion
+sudo apt-get install -y redis-server gnupg2 bash-completion
 
 # install tools
 curl -sLS https://get.arkade.dev | sudo sh
 arkade get kind
 arkade get kubectl
-arkade get helm
-arkade get gh
 echo "export PATH=\$PATH:/home/vscode/.arkade/bin" >> $HOME/.bashrc
-
-# install goreman for local development
-go install github.com/mattn/goreman@latest
 
 # Make sure go path is owned by vscode
 sudo chown -R vscode:vscode /home/vscode/go
+
+# install goreman for local development
+cat <<EOT >> $HOME/.bashrc
+if ! command -v goreman; then
+    echo "Installing goreman"
+    go install github.com/mattn/goreman@latest
+fi
+EOT
 
 # setup autocomplete for kubectl and alias k
 echo "source <(kubectl completion bash)" >> $HOME/.bashrc
 echo "alias k=kubectl" >> $HOME/.bashrc
 echo "complete -F __start_kubectl k" >> $HOME/.bashrc
-
