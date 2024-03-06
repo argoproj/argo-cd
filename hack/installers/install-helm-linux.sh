cb3181f5958e7513406055e443c705f477ee81b4
lets-call-n-walk <cw404@hotmail.com>
2024-03-06 11:26:14 -0500

    fix helm installation to work with mac
    Signed-off-by: lets-call-n-walk <cw404@hotmail.com>

--- hack/installers/install-helm-linux.sh
+++ hack/installers/install-helm-linux.sh
@@ -3,10 +3,10 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-export TARGET_FILE=helm-v${helm3_version}-linux-${ARCHITECTURE}.tar.gz
+export TARGET_FILE=helm-v${helm3_version}-${INSTALL_OS}-${ARCHITECTURE}.tar.gz
 
-[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o $DOWNLOADS/${TARGET_FILE} https://get.helm.sh/helm-v${helm3_version}-linux-$ARCHITECTURE.tar.gz
+[ -e $DOWNLOADS/${TARGET_FILE} ] || curl -sLf --retry 3 -o $DOWNLOADS/${TARGET_FILE} https://get.helm.sh/helm-v${helm3_version}-$INSTALL_OS-$ARCHITECTURE.tar.gz
 $(dirname $0)/compare-chksum.sh
 mkdir -p /tmp/helm && tar -C /tmp/helm -xf $DOWNLOADS/${TARGET_FILE}
-sudo install -m 0755 /tmp/helm/linux-$ARCHITECTURE/helm $BIN/helm
+sudo install -m 0755 /tmp/helm/$INSTALL_OS-$ARCHITECTURE/helm $BIN/helm
 helm version --client
