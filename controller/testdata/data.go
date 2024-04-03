6f9ae9d4f768980c46048b129de6f9b55d41a270
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2024-04-03 15:56:59 -0700

    fix: fix calculating patch for respect ignore diff feature (#17693)
    * test: unit test for respectIgnoreDifferences bug
    
    Signed-off-by: Jesse Suen <jesse@akuity.io>
    
    * test: simplify unit test
    
    Signed-off-by: Jesse Suen <jesse@akuity.io>
    
    * fix: fix calculating patch for respect ignore diff feature
    
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    
    ---------
    
    Signed-off-by: Jesse Suen <jesse@akuity.io>
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>
    Co-authored-by: Jesse Suen <jesse@akuity.io>

--- controller/testdata/data.go
+++ controller/testdata/data.go
@@ -11,4 +11,22 @@ var (
 
 	//go:embed target-deployment-new-entries.yaml
 	TargetDeploymentNewEntries string
+
+	//go:embed live-httpproxy.yaml
+	LiveHTTPProxy string
+
+	//go:embed target-httpproxy.yaml
+	TargetHTTPProxy string
+
+	//go:embed live-deployment-env-vars.yaml
+	LiveDeploymentEnvVarsYaml string
+
+	//go:embed target-deployment-env-vars.yaml
+	TargetDeploymentEnvVarsYaml string
+
+	//go:embed minimal-image-replicas-deployment.yaml
+	MinimalImageReplicaDeploymentYaml string
+
+	//go:embed additional-image-replicas-deployment.yaml
+	AdditionalImageReplicaDeploymentYaml string
 )
