168c608b22cbd10f37542f5f71fc64f81a1814ff
ishitasequeira <ishiseq29@gmail.com>
2024-04-18 22:00:47 -0400

    fix codegen
    Signed-off-by: ishitasequeira <ishiseq29@gmail.com>

--- pkg/apis/application/v1alpha1/openapi_generated.go
+++ pkg/apis/application/v1alpha1/openapi_generated.go
@@ -9,6 +9,7 @@ package v1alpha1
 
 import (
 	common "k8s.io/kube-openapi/pkg/common"
+	spec "k8s.io/kube-openapi/pkg/validation/spec"
 )
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
