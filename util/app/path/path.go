0bd7f909e79bd0cddf8575daf0b40cf6680cbf8c
Andrew Jeffree <andrew@jeffree.id.au>
2024-05-02 20:20:45 +1000

    feat(paths): Add support for glob patterns
    as per https://github.com/argoproj/argo-cd/issues/17825
    
    Signed-off-by: Andrew Jeffree <andrew@jeffree.id.au>

--- util/app/path/path.go
+++ util/app/path/path.go
@@ -131,13 +131,11 @@ func AppFilesHaveChanged(refreshPaths []string, changedFiles []string) bool {
 		f = ensureAbsPath(f)
 		for _, item := range refreshPaths {
 			item = ensureAbsPath(item)
-			changed := false
 			if f == item {
-				changed = true
+				return true
 			} else if _, err := security.EnforceToCurrentRoot(item, f); err == nil {
-				changed = true
-			}
-			if changed {
+				return true
+			} else if matched, err := filepath.Match(item, f); err == nil && matched {
 				return true
 			}
 		}
