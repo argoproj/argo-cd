f52bfae97d9ee3c6613f42540d072c90ad564f67
Siddhesh Ghadi <sghadi1203@gmail.com>
2024-05-14 20:22:31 +0530

    Add option to hide annotations on secrets
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- util/settings/settings.go
+++ util/settings/settings.go
@@ -13,6 +13,7 @@ import (
 	"net/url"
 	"path"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -515,6 +516,8 @@ const (
 	RespectRBAC            = "resource.respectRBAC"
 	RespectRBACValueStrict = "strict"
 	RespectRBACValueNormal = "normal"
+	// hideSecretAnnotations is the key to configure annotations to hide in secret resource
+	hideSecretAnnotations = "hide.secret.annotations"
 )
 
 var sourceTypeToEnableGenerationKey = map[v1alpha1.ApplicationSourceType]string{
@@ -2256,4 +2259,24 @@ func (mgr *SettingsManager) GetExcludeEventLabelKeys() []string {
 		}
 	}
 	return labelKeys
+}
+
+func (mgr *SettingsManager) GetHideSecretAnnotations() []string {
+	annotsKeys := []string{}
+	argoCDCM, err := mgr.getConfigMap()
+	if err != nil {
+		log.Error(fmt.Errorf("failed getting configmap: %v", err))
+		return annotsKeys
+	}
+	if value, ok := argoCDCM.Data[hideSecretAnnotations]; ok {
+		annots := strings.Split(value, "\n")
+		for _, a := range annots {
+			a = regexp.MustCompile(`^\s*-`).ReplaceAllString(a, "")
+			a := strings.TrimSpace(a)
+			if a != "" {
+				annotsKeys = append(annotsKeys, a)
+			}
+		}
+	}
+	return annotsKeys
 }
