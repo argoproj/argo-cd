f120800a08ab0c085f69030a9b82a1d4bf33925a
Zack Robinson <robinsoz@arcesium.com>
2024-05-17 11:53:20 -0400

    Move regex to global variable and add out of bounds check
    Signed-off-by: Zack Robinson <robinsoz@arcesium.com>

--- util/argo/resource_tracking.go
+++ util/argo/resource_tracking.go
@@ -1,6 +1,7 @@
 package argo
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 	"strings"
@@ -22,6 +23,7 @@ const (
 
 var WrongResourceTrackingFormat = fmt.Errorf("wrong resource tracking format, should be <application-name>:<group>/<kind>:<namespace>/<name>")
 var LabelMaxLength = 63
+var OkEndPattern = regexp.MustCompile("[a-zA-Z0-9]$")
 
 // ResourceTracking defines methods which allow setup and retrieve tracking information to resource
 type ResourceTracking interface {
@@ -158,9 +160,10 @@ func (rt *resourceTracking) SetAppInstance(un *unstructured.Unstructured, key, v
 			val = val[:LabelMaxLength]
 			// Prevent errors if the truncated name ends in a special character.
 			// See https://github.com/argoproj/argo-cd/issues/18237.
-			okEndPattern := regexp.MustCompile("[a-zA-Z0-9]$")
-
-			for !okEndPattern.MatchString(val) {
+			for !OkEndPattern.MatchString(val) {
+				if len(val) <= 1 {
+					return errors.New("failed to set app instance label: unable to truncate label to not end with a special character")
+				}
 				val = val[:len(val)-1]
 			}
 		}
