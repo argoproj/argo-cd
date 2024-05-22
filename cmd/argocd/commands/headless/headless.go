9614a6d05612820d56ce8b5238bc170003a51c4b
HuLaLa <34408166+HuLaLaGa@users.noreply.github.com>
2024-05-22 11:26:24 +0800

    Update headless.go
    Signed-off-by: HuLaLa <34408166+HuLaLaGa@users.noreply.github.com>

--- cmd/argocd/commands/headless/headless.go
+++ cmd/argocd/commands/headless/headless.go
@@ -253,6 +253,7 @@ func MaybeStartLocalServer(ctx context.Context, clientOpts *apiclient.ClientOpti
 		ListenHost:           *address,
 		RepoClientset:        &forwardRepoClientset{namespace: namespace, context: ctxStr, repoServerName: clientOpts.RepoServerName, kubeClientset: kubeClientset},
 		EnableProxyExtension: false,
+		ApplicationNamespaces: []string{"*"},
 	})
 	srv.Init(ctx)
 
