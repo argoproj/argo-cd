49a8b31457436d0318c92388cea7c80ff2e52f9e
Jorge Turrado <jorge.turrado@scrm.lidl>
2024-06-10 19:27:02 +0200

    add a comment
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- server/application/application.go
+++ server/application/application.go
@@ -1547,6 +1547,12 @@ func (s *Server) RevisionMetadata(ctx context.Context, q *application.RevisionMe
 		// using the specific revisionId
 		for _, h := range a.Status.History {
 			if h.ID == versionId {
+				// The iteration values are assigned to the respective iteration variables as in an assignment statement.
+				// The iteration variables may be declared by the “range” clause using a form of short variable declaration (:=).
+				// In this case their types are set to the types of the respective iteration values and their scope is the block of the "for" statement;
+				// they are re-used in each iteration. If the iteration variables are declared outside the "for" statement,
+				// after execution their values will be those of the last iteration.
+				// https://golang.org/ref/spec#For_statements
 				h := h
 				if isRevisionMultiSource {
 					source = &h.Sources[*q.SourceIndex]
