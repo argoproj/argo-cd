e041bf469c8d993ce1ed7c82442959aef69a8de4
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-06-13 17:15:52 +0200

    Update app_management_ns_test.go
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- test/e2e/app_management_ns_test.go
+++ test/e2e/app_management_ns_test.go
@@ -1419,7 +1419,7 @@ func TestNamespacedSelfManagedApps(t *testing.T) {
 					reconciledAt = &metav1.Time{}
 				}
 				if lastReconciledAt != nil && !lastReconciledAt.Equal(reconciledAt) {
-					reconciledCount++
+					reconciledCount = reconciledCount + 1
 				}
 				lastReconciledAt = reconciledAt
 			}
