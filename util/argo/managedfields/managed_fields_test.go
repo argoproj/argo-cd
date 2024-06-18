f8de4aee8f413b76a578f8d0190557c9b28694c8
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-06-18 20:34:06 +0200

    Update managed_fields_test.go
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- util/argo/managedfields/managed_fields_test.go
+++ util/argo/managedfields/managed_fields_test.go
@@ -38,14 +38,16 @@ func TestNormalize(t *testing.T) {
 		liveReplicas, ok, err := unstructured.NestedFloat64(liveResult.Object, "spec", "replicas")
 		assert.False(t, ok)
 		require.NoError(t, err)
-		assert.InEpsilon(t, desiredReplicas, liveReplicas, 0.0001)
+		assert.Zero(t, desiredReplicas)
+		assert.Zero(t, liveReplicas)
 		liveRevisionHistory, ok, err := unstructured.NestedFloat64(liveResult.Object, "spec", "revisionHistoryLimit")
 		assert.False(t, ok)
 		require.NoError(t, err)
 		desiredRevisionHistory, ok, err := unstructured.NestedFloat64(desiredResult.Object, "spec", "revisionHistoryLimit")
 		assert.False(t, ok)
 		require.NoError(t, err)
-		assert.InEpsilon(t, desiredRevisionHistory, liveRevisionHistory, 0.0001)
+		assert.Zero(t, desiredRevisionHistory)
+		assert.Zero(t, liveRevisionHistory)
 	})
 	t.Run("will keep conflicting fields if not from trusted manager", func(t *testing.T) {
 		// given
