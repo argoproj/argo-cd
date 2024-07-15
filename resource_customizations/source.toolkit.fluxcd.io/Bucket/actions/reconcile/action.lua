80724ca91883d0ee0f27b15a0c103fd57b121c78
Adrian Berger <adrian.berger@bedag.ch>
2024-07-15 08:27:25 +0200

    chore: Add upgrade docs, change annotation text
    Signed-off-by: Adrian Berger <adrian.berger@bedag.ch>

--- resource_customizations/source.toolkit.fluxcd.io/Bucket/actions/reconcile/action.lua
+++ resource_customizations/source.toolkit.fluxcd.io/Bucket/actions/reconcile/action.lua
@@ -2,6 +2,6 @@ local os = require("os")
 if obj.metadata.annotations == nil then
     obj.metadata.annotations = {}
 end
-obj.metadata.annotations["reconcile.fluxcd.io/requestedAt"] = "By ArgoCD at: " .. os.date("!%Y-%m-%dT%X")
+obj.metadata.annotations["reconcile.fluxcd.io/requestedAt"] = "By Argo CD at: " .. os.date("!%Y-%m-%dT%X")
 
 return obj
