3a41c2e987652a7ebad944c725f7a14bd3d02f4f
Rachel Sheikh <sheikhrachel97@gmail.com>
2024-07-15 12:09:49 -0700

    fix(cli): add optional password setting for headless redis client (#19035) (#19039)
    * chore: add optional password setting for headless redis client
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * fix: remove import cycle
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * fix: add shared SetOptionalRedisPasswordFromKubeConfig method
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * fix: export redis consts
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * test: add test cases for SetOptionalRedisPasswordFromKubeConfig()
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * chore: go mod tidy
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * fix: use require instead of assert
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    
    * fix: Update common/common.go
    
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Signed-off-by: Rachel Sheikh <sheikhrachel97@gmail.com>
    
    ---------
    
    Signed-off-by: Rachel Sheikh <rsheikh@squareup.com>
    Signed-off-by: Rachel Sheikh <sheikhrachel97@gmail.com>
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- cmd/argocd/commands/admin/cluster.go
+++ cmd/argocd/commands/admin/cluster.go
@@ -106,14 +106,9 @@ func loadClusters(ctx context.Context, kubeClient *kubernetes.Clientset, appClie
 		}
 
 		redisOptions := &redis.Options{Addr: fmt.Sprintf("localhost:%d", port)}
-
-		secret, err := kubeClient.CoreV1().Secrets(namespace).Get(context.Background(), defaulRedisInitialPasswordSecretName, v1.GetOptions{})
-		if err == nil {
-			if _, ok := secret.Data[defaultResisInitialPasswordKey]; ok {
-				redisOptions.Password = string(secret.Data[defaultResisInitialPasswordKey])
-			}
+		if err = common.SetOptionalRedisPasswordFromKubeConfig(ctx, kubeClient, namespace, redisOptions); err != nil {
+			log.Warnf("Failed to fetch & set redis password for namespace %s: %v", namespace, err)
 		}
-
 		client := redis.NewClient(redisOptions)
 		compressionType, err := cacheutil.CompressionTypeFromString(redisCompressionStr)
 		if err != nil {
