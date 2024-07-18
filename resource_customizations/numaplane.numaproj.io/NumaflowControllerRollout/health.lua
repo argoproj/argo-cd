ea725a9c7cad5bbfecdaa1b93cd59353633f8dc8
Dillen Padhiar <38965141+dpadhiar@users.noreply.github.com>
2024-07-18 12:11:07 -0700

    fix: Numaplane CRD health checks - bug fixes (#19099)
    * fix: Numaplane CRD health checks - phase check for Healthy
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>
    
    * fix: return diff message when phase is Failed
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>
    
    * fix: NumaflowControllerRollout test
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>
    
    * fix: return statements added
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>
    
    ---------
    
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- resource_customizations/numaplane.numaproj.io/NumaflowControllerRollout/health.lua
+++ resource_customizations/numaplane.numaproj.io/NumaflowControllerRollout/health.lua
@@ -12,12 +12,16 @@ if obj.status ~= nil then
 end
 
 if obj.metadata.generation == obj.status.observedGeneration then
-  if healthyCondition.status == "True" and (obj.metadata.generation == healthyCondition.observedGeneration) then
-    hs.status = "Healthy"
-    hs.message = healthyCondition.message
-    return hs
-  elseif (healthyCondition.status == "False" and (obj.metadata.generation == healthyCondition.observedGeneration)) or obj.status.phase == "Failed" then
+  if (healthyCondition ~= {} and healthyCondition.status == "False" and (obj.metadata.generation == healthyCondition.observedGeneration)) or obj.status.phase == "Failed" then
     hs.status = "Degraded"
+    if obj.status.phase == "Failed" then
+      hs.message = obj.status.message
+    else
+      hs.message = healthyCondition.message
+    end
+    return hs
+  elseif healthyCondition ~= {} and healthyCondition.status == "True" and (obj.metadata.generation == healthyCondition.observedGeneration) and obj.status.phase == "Deployed" then
+    hs.status = "Healthy"
     hs.message = healthyCondition.message
     return hs
   end
