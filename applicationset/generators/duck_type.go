1f2f2546f119d00d0ad2a0db0ffec7f1f42b5c00
thisishwan2 <feel000617@gmail.com>
2024-07-25 02:57:04 +0900

    chore: improve error logs
    Signed-off-by: thisishwan2 <feel000617@gmail.com>

--- applicationset/generators/duck_type.go
+++ applicationset/generators/duck_type.go
@@ -125,7 +125,7 @@ func (g *DuckTypeGenerator) GenerateParams(appSetGenerator *argoprojiov1alpha1.A
 	duckResources, err := g.dynClient.Resource(duckGVR).Namespace(g.namespace).List(g.ctx, listOptions)
 	if err != nil {
 		log.WithField("GVK", duckGVR).Warning("resources were not found")
-		return nil, err
+		return nil, fmt.Errorf("no dynamic resources found: %w", err)
 	}
 
 	if len(duckResources.Items) == 0 {
