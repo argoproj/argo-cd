51831c15d5e6828b77897e7ecd10df01ab3e6650
Cheng Fang <cfang@redhat.com>
2024-07-25 17:05:27 -0400

    fix: 'argocd admin settings rbac can' command should honor server.rbac.log.enforce.enable param in argocd-cm
    Signed-off-by: Cheng Fang <cfang@redhat.com>

--- cmd/argocd/commands/admin/settings.go
+++ cmd/argocd/commands/admin/settings.go
@@ -159,7 +159,7 @@ func NewSettingsCommand() *cobra.Command {
 
 	command.AddCommand(NewValidateSettingsCommand(&opts))
 	command.AddCommand(NewResourceOverridesCommand(&opts))
-	command.AddCommand(NewRBACCommand())
+	command.AddCommand(NewRBACCommand(&opts))
 
 	opts.clientConfig = cli.AddKubectlFlagsToCmd(command)
 	command.PersistentFlags().StringVar(&opts.argocdCMPath, "argocd-cm-path", "", "Path to local argocd-cm.yaml file")
