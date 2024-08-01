9ca1c81da2c378105eb03830b9586f8320902a7a
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2024-08-01 11:16:08 -0400

    remove unnecessary fmt call
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- reposerver/askpass/server.go
+++ reposerver/askpass/server.go
@@ -95,7 +95,7 @@ func (s *server) getCreds(id string) (*Creds, bool) {
 // Environ returns the environment variables that should be set when invoking git.
 func (s *server) Environ(id string) []string {
 	return []string{
-		fmt.Sprintf("GIT_ASKPASS=%s", "argocd"),
+		"GIT_ASKPASS=argocd",
 		fmt.Sprintf("%s=%s", ASKPASS_NONCE_ENV, id),
 		"GIT_TERMINAL_PROMPT=0",
 		"ARGOCD_BINARY_NAME=argocd-git-ask-pass",
