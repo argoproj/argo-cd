fdbaaabbd6d2007c85ad2f95924f647407e6eab8
mirageoasis <kimhw0820@naver.com>
2024-08-04 17:34:52 +0900

    test: added test for util.go
    Signed-off-by: mirageoasis <kimhw0820@naver.com>

---
+++ commitserver/commit/util_test.go
@@ -0,0 +1,39 @@
+package commit
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestMakeSecureTempDir(t *testing.T) {
+	tempDir, cleanup, err := makeSecureTempDir()	
+	
+	if err != nil {
+		t.Fatalf("Failed to create secure temporary directory: %v", err)
+	}
+	defer cleanup()
+
+	// Verify that the temporary directory exists
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		t.Errorf("Temporary directory does not exist: %s", tempDir)
+	}
+
+	// Verify that the temporary directory is a directory
+	fileInfo, err := os.Stat(tempDir)
+	if err != nil {
+		t.Fatalf("Failed to get file info for temporary directory: %v", err)
+	}
+	if !fileInfo.IsDir() {
+		t.Errorf("Temporary directory is not a directory: %s", tempDir)
+	}
+
+	// Verify that the temporary directory is empty
+	files, err := ioutil.ReadDir(tempDir)
+	if err != nil {
+		t.Fatalf("Failed to read directory: %v", err)
+	}
+	if len(files) != 0 {
+		t.Errorf("Temporary directory is not empty: %s", tempDir)
+	}
+}
