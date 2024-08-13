588b251acc71e37f47aaef597b185298f630c681
Arthur Vardevanyan <arthur@arthurvardevanyan.com>
2024-08-13 08:46:25 -0400

    feat(sourceNamespace): Regex Support (#19016) (#19017)
    * feat(sourceNamespace): Regex Support
    
    Signed-off-by: Arthur <arthur@arthurvardevanyan.com>
    
    * feat(sourceNamespace): Separate exactMatch into patternMatch
    
    Signed-off-by: Arthur <arthur@arthurvardevanyan.com>
    
    ---------
    
    Signed-off-by: Arthur <arthur@arthurvardevanyan.com>

--- util/glob/list.go
+++ util/glob/list.go
@@ -1,10 +1,30 @@
 package glob
 
-// MatchStringInList will return true if item is contained in list. If
-// exactMatch is set to false, list may contain globs to be matched.
-func MatchStringInList(list []string, item string, exactMatch bool) bool {
+import (
+	"strings"
+
+	"github.com/argoproj/argo-cd/v2/util/regex"
+)
+
+const (
+	EXACT  = "exact"
+	GLOB   = "glob"
+	REGEXP = "regexp"
+)
+
+// MatchStringInList will return true if item is contained in list.
+// patternMatch; can be set to  exact, glob, regexp.
+// If patternMatch; is set to exact, the item must be an exact match.
+// If patternMatch; is set to glob, the item must match a glob pattern.
+// If patternMatch; is set to regexp, the item must match a regular expression or glob.
+func MatchStringInList(list []string, item string, patternMatch string) bool {
 	for _, ll := range list {
-		if item == ll || (!exactMatch && Match(ll, item)) {
+		// If string is wrapped in "/", assume it is a regular expression.
+		if patternMatch == REGEXP && strings.HasPrefix(ll, "/") && strings.HasSuffix(ll, "/") && regex.Match(ll[1:len(ll)-1], item) {
+			return true
+		} else if (patternMatch == REGEXP || patternMatch == GLOB) && Match(ll, item) {
+			return true
+		} else if patternMatch == EXACT && item == ll {
 			return true
 		}
 	}
