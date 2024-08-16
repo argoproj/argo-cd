401cbb9d4197970c8c955acff5ac751a5dc74b56
ChichiCaleb <chichiuchenna@gmail.com>
2024-08-16 05:10:54 -0700

    update devcontainer
    Signed-off-by: ChichiCaleb <chichiuchenna@gmail.com>

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 # this runs as part of pre-build
-
-
 echo "on-create start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 
-# clone repos
-git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
-git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
-
-# restore the repos
-dotnet restore /workspaces/webvalidate/src/webvalidate.sln
-dotnet restore /workspaces/imdb-app/src/imdb.csproj
-
 export REPO_BASE=$PWD
 export PATH="$PATH:$REPO_BASE/cli"
-
 
 mkdir -p "$HOME/.ssh"
 
@@ -36,15 +25,7 @@ docker network connect kind kind-registry
 # update the base docker images
 docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
 docker pull mcr.microsoft.com/dotnet/sdk:6.0
-docker pull ghcr.io/cse-labs/webv-red:latest
 
-# echo "downloading kic CLI"
-# cd cli || exit
-# tag="0.4.3"
-# wget -O kic.tar.gz "https://github.com/retaildevcrews/akdc/releases/download/$tag/kic-$tag-linux-amd64.tar.gz"
-# tar -xvzf kic.tar.gz
-# rm kic.tar.gz
-# cd "$OLDPWD" || exit
 
 echo "generating completions"
 kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
@@ -57,7 +38,6 @@ apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
   kubeadmConfigPatches:
-  
   - |
     kind: InitConfiguration
     nodeRegistration:
@@ -70,23 +50,12 @@ nodes:
   - containerPort: 443
     hostPort: 443
     protocol: TCP
-
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5500"]
     endpoint = ["http://kind-registry:5000"]
 EOF
 
-
-echo "building IMDb"
-kic build imdb
-
-echo "building WebValidate"
-sed -i "s/RUN dotnet test//g" /workspaces/webvalidate/Dockerfile
-kic build webv
-
-echo "deploying to Kind cluster"
-kic cluster deploy
 
 # only run apt upgrade on pre-build
 if [ "$CODESPACE_NAME" = "null" ]
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,11 +5,6 @@
 echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
-# update the repos
-git -C /workspaces/imdb-app pull
-git -C /workspaces/webvalidate pull
-
-
 sudo apt-get install curl -y
 sudo apt-get install make -y
 
@@ -33,7 +28,6 @@ installgo() {
   # Delete the downloaded tar file
   rm go1.22.2.linux-amd64.tar.gz
 }
-
 
 # Run the installgo function
 installgo
