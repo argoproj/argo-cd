138d03f17c1bd353b1abb7077481e265ed683a12
David Wu <155603967+david-wu-octopus@users.noreply.github.com>
2024-08-22 21:24:47 +1000

    Add `NewConfigurationCommand()`
    Signed-off-by: David Wu <155603967+david-wu-octopus@users.noreply.github.com>

---
+++ cmd/argocd/commands/configuration.go
@@ -0,0 +1,42 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strconv"
+
+	argocdclient "github.com/argoproj/argo-cd/v2/pkg/apiclient"
+	"github.com/argoproj/argo-cd/v2/util/errors"
+	"github.com/argoproj/argo-cd/v2/util/localconfig"
+)
+
+// NewConfigurationCommand returns a new instance of an `argocd configuration` command
+func NewConfigurationCommand(globalClientOpts *argocdclient.ClientOptions) *cobra.Command {
+	var (
+		promptsEnabled bool
+	)
+	command := &cobra.Command{
+		Use:   "configuration",
+		Short: "Manage local configuration",
+		Example: `# Enable optional interactive prompts
+argocd configuration --prompts-enabled
+argocd configuration --prompts-enabled=true
+
+# Disable optional interactive prompts
+argocd configuration --prompts-enabled=false`,
+		Run: func(c *cobra.Command, args []string) {
+			localCfg, err := localconfig.ReadLocalConfig(globalClientOpts.ConfigPath)
+			errors.CheckError(err)
+
+			localCfg.PromptsEnabled = promptsEnabled
+
+			err = localconfig.WriteLocalConfig(*localCfg, globalClientOpts.ConfigPath)
+			errors.CheckError(err)
+
+			fmt.Println("Successfully updated the following configuration settings:")
+			fmt.Printf("prompts-enabled: %v", strconv.FormatBool(localCfg.PromptsEnabled))
+		},
+	}
+	command.Flags().BoolVar(&promptsEnabled, "prompts-enabled", false, "Enable (or disable) optional interactive prompts")
+	return command
+}
