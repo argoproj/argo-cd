25f47706d0660b336438ee7ac5c86fcb22f64b8f
Blake Pettersson <blake.pettersson@gmail.com>
2024-08-22 13:35:26 +0200

    chore: updates from master
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -137,7 +137,7 @@ func NewService(metricsServer *metrics.MetricsServer, cache *cache.Cache, initCo
 		newGitClient:              git.NewClientExt,
 		resourceTracking:          resourceTracking,
 		newOciClient: func(repoURL string, creds oci.Creds, proxy string, noProxy string, opts ...oci.ClientOpts) (oci.Client, error) {
-			return oci.NewClient(repoURL, creds, proxy, opts...)
+			return oci.NewClient(repoURL, creds, proxy, noProxy, opts...)
 		},
 		newHelmClient: func(repoURL string, creds helm.Creds, enableOci bool, proxy string, noProxy string, opts ...helm.ClientOpts) helm.Client {
 			return helm.NewClientWithLock(repoURL, creds, sync.NewKeyLock(), enableOci, proxy, noProxy, opts...)
@@ -2486,7 +2486,7 @@ func (s *Service) newClientResolveRevision(repo *v1alpha1.Repository, revision s
 }
 
 func (s *Service) newOciClientResolveRevision(ctx context.Context, repo *v1alpha1.Repository, revision string, noRevisionCache bool) (oci.Client, string, error) {
-	ociClient, err := s.newOciClient(repo.Repo, repo.GetOciCreds(), repo.Proxy, oci.WithIndexCache(s.cache), oci.WithChartPaths(s.chartPaths))
+	ociClient, err := s.newOciClient(repo.Repo, repo.GetOciCreds(), repo.Proxy, repo.NoProxy, oci.WithIndexCache(s.cache), oci.WithChartPaths(s.chartPaths))
 	if err != nil {
 		return nil, "", err
 	}
@@ -2642,7 +2642,7 @@ func (s *Service) TestRepository(ctx context.Context, q *apiclient.TestRepositor
 			return git.TestRepo(repo.Repo, repo.GetGitCreds(s.gitCredsStore), repo.IsInsecure(), repo.IsLFSEnabled(), repo.Proxy, repo.NoProxy)
 		},
 		"oci": func() error {
-			client, err := oci.NewClient(repo.Repo, repo.GetOciCreds(), repo.Proxy)
+			client, err := oci.NewClient(repo.Repo, repo.GetOciCreds(), repo.Proxy, repo.NoProxy)
 			if err != nil {
 				return err
 			}
