5452c6e5c5133fea104cf45e6a892e4dd69ee2e5
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-09-21 13:28:15 +0900

    refactor : exclude from search if name is an empty value (including trim)
    Co-authored-by: Suraj yadav <harrypotter1108@gmail.com>
    Signed-off-by: Eunji <129590633+bianbbc87@users.noreply.github.com>

--- ui/src/app/settings/components/repos-list/repos-list.tsx
+++ ui/src/app/settings/components/repos-list/repos-list.tsx
@@ -1066,7 +1066,10 @@ export class ReposList extends React.Component<
 >>>>>>> 0a23f1847 (refactor : delete sort dropdown and fix eslint)
     private filteredName(repos: models.Repository[], name: string) {
         const trimmedName = name.trim();
-        const newRepos = repos.filter(repo => !repo.name || repo.name.toLowerCase().includes(trimmedName.toLowerCase()));
+          if (trimmedName === ''){
+            return repos;
+        }
+        const newRepos = repos.filter(repo => repo.name && repo.name.toLowerCase().includes(trimmedName.toLowerCase()));
         return newRepos;
     }
 
