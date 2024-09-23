028baf2e5190306660f17a251974b7cc93776691
Cayde6 <tjdfkr2421@gmail.com>
2024-09-23 12:02:17 +0900

    feat: option to disable writing k8s events(#18205) (#18441)
    * feat: option to disable writing k8s events
    
    optioned to write logs for k8s events.
    Each is passed as an environment variable and defaults to true,
    disabling it requires explicitly setting the option to false.
    
    Signed-off-by: Jack-R-lantern <tjdfkr2421@gmail.com>
    
    * feat: option to disable writing k8s events
    
    fix unit test
    - application_test
    - applicationset_test
    - project_test
    - appcontroller_tes
    - audit_logger_test
    
    Signed-off-by: Jack-R-lantern <tjdfkr2421@gmail.com>
    
    * rebase
    
    Signed-off-by: Jack-R-lantern <tjdfkr2421@gmail.com>
    
    ---------
    
    Signed-off-by: Jack-R-lantern <tjdfkr2421@gmail.com>

--- controller/appcontroller_test.go
+++ controller/appcontroller_test.go
@@ -45,11 +45,14 @@ import (
 	"github.com/argoproj/argo-cd/v2/reposerver/apiclient"
 	mockrepoclient "github.com/argoproj/argo-cd/v2/reposerver/apiclient/mocks"
 	"github.com/argoproj/argo-cd/v2/test"
+	"github.com/argoproj/argo-cd/v2/util/argo"
 	"github.com/argoproj/argo-cd/v2/util/argo/normalizers"
 	cacheutil "github.com/argoproj/argo-cd/v2/util/cache"
 	appstatecache "github.com/argoproj/argo-cd/v2/util/cache/appstate"
 	"github.com/argoproj/argo-cd/v2/util/settings"
 )
+
+var testEnableEventList []string = argo.DefaultEnableEventList()
 
 type namespacedResource struct {
 	v1alpha1.ResourceNode
@@ -170,6 +173,7 @@ func newFakeController(data *fakeData, repoErr error) *ApplicationController {
 		false,
 		false,
 		normalizers.IgnoreNormalizerOpts{},
+		testEnableEventList,
 	)
 	db := &dbmocks.ArgoDB{}
 	db.On("GetApplicationControllerReplicas").Return(1)
