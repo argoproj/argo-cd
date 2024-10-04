5ddb49d193eddf9741c3d4a344208b304f6904d0
Paul Larsen <pnvlarsen@gmail.com>
2024-10-04 20:29:20 +0200

    add IsConstraint method
    Signed-off-by: Paul Larsen <pnvlarsen@gmail.com>

--- util/versions/version.go
+++ util/versions/version.go
@@ -6,3 +6,8 @@ func IsVersion(text string) bool {
 	_, err := semver.NewVersion(text)
 	return err == nil
 }
+
+func IsConstraint(text string) bool {
+	_, err := semver.NewConstraint(text)
+	return !IsVersion(text) && err == nil
+}
