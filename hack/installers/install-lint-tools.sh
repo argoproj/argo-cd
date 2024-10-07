95f650a3f477d7f6219634d23f14b0d2d6e5ad6f
David Coppa <dcoppa@cocus.com>
2024-10-07 11:33:21 +0200

    Fix GOLANGCI_LINT_VERSION
    Signed-off-by: David Coppa <dcoppa@cocus.com>

--- hack/installers/install-lint-tools.sh
+++ hack/installers/install-lint-tools.sh
@@ -2,6 +2,6 @@
 set -eux -o pipefail
 
 # renovate: datasource=go packageName=github.com/golangci/golangci-lint
-GOLANGCI_LINT_VERSION=1.61.0
+GOLANGCI_LINT_VERSION=v1.61.0
 
 GO111MODULE=on go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}"
