951f749406db7aa0df95f4cbf9887fad71974f68
Dillen Padhiar <38965141+dpadhiar@users.noreply.github.com>
2024-10-09 14:32:14 -0700

    fix: remove hardcoded conditions from Numaflow CRD health checks (#20316)
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- resource_customizations/numaflow.numaproj.io/InterStepBufferService/health.lua
+++ resource_customizations/numaflow.numaproj.io/InterStepBufferService/health.lua
@@ -1,27 +1,28 @@
 local hs = {}
-local healthyCondition = {}
+local healthy = {}
 
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "ChildrenResourcesHealthy" then
-        healthyCondition = condition
+      if condition.status == "False" then
+        healthy.status = "False"
+        healthy.message = condition.message
       end
     end
   end
 
   if obj.metadata.generation == obj.status.observedGeneration then
-    if (healthyCondition ~= {} and healthyCondition.status == "False") or obj.status.phase == "Failed" then
+    if (healthy ~= {} and healthy.status == "False") or obj.status.phase == "Failed" then
       hs.status = "Degraded"
       if obj.status.phase == "Failed" then
         hs.message = obj.status.message
       else
-        hs.message = healthyCondition.message
+        hs.message = healthy.message
       end
       return hs
-    elseif (healthyCondition ~= {} and healthyCondition.status == "True") and obj.status.phase == "Running" then
+    elseif obj.status.phase == "Running" then
       hs.status = "Healthy"
-      hs.message = healthyCondition.message
+      hs.message = "InterStepBufferService is healthy"
       return hs
     end
   end   
--- resource_customizations/numaflow.numaproj.io/Vertex/health.lua
+++ resource_customizations/numaflow.numaproj.io/Vertex/health.lua
@@ -1,27 +1,28 @@
 local hs = {}
-local podsHealth = {}
+local healthy = {}
 
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "PodsHealthy" then
-        podsHealth = condition
+      if condition.status == "False" then
+        healthy.status = "False"
+        healthy.message = condition.message
       end
     end
   end
 
   if obj.metadata.generation == obj.status.observedGeneration then
-    if (podsHealth ~= {} and podsHealth.status == "False") or obj.status.phase == "Failed" then
+    if (healthy ~= {} and healthy.status == "False") or obj.status.phase == "Failed" then
       hs.status = "Degraded"
       if obj.status.phase == "Failed" then
         hs.message = obj.status.message
       else
-        hs.message = podsHealth.message
+        hs.message = healthy.message
       end
       return hs
-    elseif (podsHealth ~= {} and podsHealth.status == "True") and obj.status.phase == "Running" then
+    elseif obj.status.phase == "Running" then
       hs.status = "Healthy"
-      hs.message = podsHealth.message
+      hs.message = "Vertex is healthy"
       return hs
     end
   end
