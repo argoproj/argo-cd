8d3becc5cf7891c5812affe77db295112fd4a9ed
David Wu <155603967+david-wu-octopus@users.noreply.github.com>
2024-10-11 14:40:11 +1100

    Add test case for `(p *Prompt).Confirm()`
    Signed-off-by: David Wu <155603967+david-wu-octopus@users.noreply.github.com>

--- cmd/argocd/commands/utils/prompt_test.go
+++ cmd/argocd/commands/utils/prompt_test.go
@@ -39,3 +39,14 @@ func TestNewPrompt_PromptsEnabled_Unspecified(t *testing.T) {
 
 	assert.False(t, prompt.enabled)
 }
+
+func TestConfirm_PromptsEnabled_False(t *testing.T) {
+	clientOpts := apiclient.ClientOptions{
+		PromptsEnabled: false,
+	}
+
+	prompt, err := NewPrompt(&clientOpts)
+	require.NoError(t, err)
+
+	assert.True(t, prompt.Confirm("Are you sure you want to run this command? (y/n) "))
+}
