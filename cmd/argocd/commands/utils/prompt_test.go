fe592d8da1ed671d01bc76cd571628b2d19e1ff9
David Wu <155603967+david-wu-octopus@users.noreply.github.com>
2024-10-11 14:36:48 +1100

    Update existing `prompt_test.go` test cases
    Signed-off-by: David Wu <155603967+david-wu-octopus@users.noreply.github.com>

--- cmd/argocd/commands/utils/prompt_test.go
+++ cmd/argocd/commands/utils/prompt_test.go
@@ -4,36 +4,14 @@ import (
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"os"
 	"testing"
 )
 
 const testConfigFilePath = "../testdata/local.config"
 
 func TestNewPrompt_PromptsEnabled_True(t *testing.T) {
-	testConfig := `contexts:
-- name: localhost:8080
-  server: localhost:8080
-  user: localhost:8080
-current-context: localhost:8080
-servers:
-- plain-text: true
-  server: localhost:8080
-users:
-- auth-token: vErrYS3c3tReFRe$hToken
-  name: localhost:8080
-prompts-enabled: true`
-
-	err := os.WriteFile(testConfigFilePath, []byte(testConfig), os.ModePerm)
-	require.NoError(t, err)
-
-	defer os.Remove(testConfigFilePath)
-
-	err = os.Chmod(testConfigFilePath, 0o600)
-	require.NoError(t, err, "Could not change the file permission to 0600 %v", err)
-
 	clientOpts := apiclient.ClientOptions{
-		ConfigPath: testConfigFilePath,
+		PromptsEnabled: true,
 	}
 
 	prompt, err := NewPrompt(&clientOpts)
@@ -43,29 +21,8 @@ prompts-enabled: true`
 }
 
 func TestNewPrompt_PromptsEnabled_False(t *testing.T) {
-	testConfig := `contexts:
-- name: localhost:8080
-  server: localhost:8080
-  user: localhost:8080
-current-context: localhost:8080
-servers:
-- plain-text: true
-  server: localhost:8080
-users:
-- auth-token: vErrYS3c3tReFRe$hToken
-  name: localhost:8080
-prompts-enabled: false`
-
-	err := os.WriteFile(testConfigFilePath, []byte(testConfig), os.ModePerm)
-	require.NoError(t, err)
-
-	defer os.Remove(testConfigFilePath)
-
-	err = os.Chmod(testConfigFilePath, 0o600)
-	require.NoError(t, err, "Could not change the file permission to 0600 %v", err)
-
 	clientOpts := apiclient.ClientOptions{
-		ConfigPath: testConfigFilePath,
+		PromptsEnabled: false,
 	}
 
 	prompt, err := NewPrompt(&clientOpts)
@@ -75,29 +32,7 @@ prompts-enabled: false`
 }
 
 func TestNewPrompt_PromptsEnabled_Unspecified(t *testing.T) {
-	testConfig := `contexts:
-- name: localhost:8080
-  server: localhost:8080
-  user: localhost:8080
-current-context: localhost:8080
-servers:
-- plain-text: true
-  server: localhost:8080
-users:
-- auth-token: vErrYS3c3tReFRe$hToken
-  name: localhost:8080`
-
-	err := os.WriteFile(testConfigFilePath, []byte(testConfig), os.ModePerm)
-	require.NoError(t, err)
-
-	defer os.Remove(testConfigFilePath)
-
-	err = os.Chmod(testConfigFilePath, 0o600)
-	require.NoError(t, err, "Could not change the file permission to 0600 %v", err)
-
-	clientOpts := apiclient.ClientOptions{
-		ConfigPath: testConfigFilePath,
-	}
+	clientOpts := apiclient.ClientOptions{}
 
 	prompt, err := NewPrompt(&clientOpts)
 	require.NoError(t, err)
