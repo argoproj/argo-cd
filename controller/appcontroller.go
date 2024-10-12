703a1ea483d9076344b69e6bf6624361226ccc0a
Kunho Lee <gunho1020@gmail.com>
2024-10-13 02:32:46 +0900

    fix: check err before use schedule and duration (#20043)
    * fix: check err before use schedule and duration
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>
    
    * test: add tests for invalid schedule and duration
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>
    
    * feat: change to return error when sync window is invalid
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>
    
    * fix: use assert.Error or assert.NoError
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>
    
    * fix: use require instead of assert
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>
    
    ---------
    
    Signed-off-by: daengdaengLee <gunho1020@gmail.com>

--- controller/appcontroller.go
+++ controller/appcontroller.go
@@ -1609,7 +1609,8 @@ func (ctrl *ApplicationController) processAppRefreshQueueItem() (processNext boo
 		app.Status.Summary = tree.GetSummary(app)
 	}
 
-	if project.Spec.SyncWindows.Matches(app).CanSync(false) {
+	canSync, _ := project.Spec.SyncWindows.Matches(app).CanSync(false)
+	if canSync {
 		syncErrCond, opMS := ctrl.autoSync(app, compareResult.syncStatus, compareResult.resources)
 		setOpMs = opMS
 		if syncErrCond != nil {
