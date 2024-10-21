e4e29d9e4b9e7c6a12bf974ea04032a02dafb98a
Mostafa Negim <mostafa.negim@snapp.cab>
2024-10-21 13:39:16 +0330

    Fix: check for app.Name if is empty, and pick the args[0] if so #20474
    Signed-off-by: Mostafa Negim <mostafa.negim@snapp.cab>
    Signed-off-by: Mostafa Negim <mostafa.negim@gmail.com>

--- cmd/util/app.go
+++ cmd/util/app.go
@@ -605,6 +605,10 @@ func constructAppsFromFileUrl(fileURL, appName string, labels, annotations, args
 		return nil, err
 	}
 	for _, app := range apps {
+		// if app.Name is empty, it should be picked from the command line
+		if len(args) == 1 && app.Name == "" {
+			app.Name = args[0]
+		}
 		if len(args) == 1 && args[0] != app.Name {
 			return nil, fmt.Errorf("app name '%s' does not match app spec metadata.name '%s'", args[0], app.Name)
 		}
