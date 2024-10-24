c8bf282dd9eca00f738bd7e8458f62e4a089a5f2
solidcellamoon <solidcella@gmail.com>
2024-10-24 17:40:00 +0900

    Aligned the indentation in custom_actions_test.go
    Signed-off-by: solidcellamoon <solidcella@gmail.com>

--- util/lua/custom_actions_test.go
+++ util/lua/custom_actions_test.go
@@ -83,23 +83,23 @@ func (t testNormalizer) Normalize(un *unstructured.Unstructured) error {
 			return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
 		}
 	case "MachineDeployment", "KubeadmControlPlane":
-			err := unstructured.SetNestedStringMap(un.Object, map[string]string{"cluster.x-k8s.io/restartedAt": "0001-01-01T00:00:00Z"}, "metadata", "annotations")
-			if err != nil {
-				return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
-			}
-			err = unstructured.SetNestedField(un.Object, nil, "spec", "rolloutAfter")
-			if err != nil {
-				return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
-			}
-			err = unstructured.SetNestedField(un.Object, nil, "metadata", "generation")
-			if err != nil {
-				return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
-			}
-			err = unstructured.SetNestedField(un.Object, nil, "status", "observedGeneration")
-			if err != nil {
-				return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
-			}
+		err := unstructured.SetNestedStringMap(un.Object, map[string]string{"cluster.x-k8s.io/restartedAt": "0001-01-01T00:00:00Z"}, "metadata", "annotations")
+		if err != nil {
+			return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
 		}
+		err = unstructured.SetNestedField(un.Object, nil, "spec", "rolloutAfter")
+		if err != nil {
+			return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
+		}
+		err = unstructured.SetNestedField(un.Object, nil, "metadata", "generation")
+		if err != nil {
+			return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
+		}
+		err = unstructured.SetNestedField(un.Object, nil, "status", "observedGeneration")
+		if err != nil {
+			return fmt.Errorf("failed to normalize %s: %w", un.GetKind(), err)
+		}
+	}
 	return nil
 }
 
