3ca3a5945275f767d04e81a7215c97ef28936f43
Blake Pettersson <blake.pettersson@gmail.com>
2024-10-25 11:09:33 +0200

    Update util/versions/version.go
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- util/versions/version.go
+++ util/versions/version.go
@@ -6,8 +6,3 @@ func IsVersion(text string) bool {
 	_, err := semver.NewVersion(text)
 	return err == nil
 }
-
-func IsConstraint(text string) bool {
-	_, err := semver.NewConstraint(text)
-	return !IsVersion(text) && err == nil
-}
