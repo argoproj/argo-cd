e9b6ebe80b3bc36de8e63a98698baff03ccbf190
pashakostohrys <pavel@codefresh.io>
2024-10-26 00:42:47 +0300

    fix imports
    Signed-off-by: pashakostohrys <pavel@codefresh.io>

--- cmd/argocd/commands/gpg.go
+++ cmd/argocd/commands/gpg.go
@@ -8,9 +8,8 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/utils"
-
 	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/headless"
+	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/utils"
 	argocdclient "github.com/argoproj/argo-cd/v2/pkg/apiclient"
 	gpgkeypkg "github.com/argoproj/argo-cd/v2/pkg/apiclient/gpgkey"
 	appsv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
