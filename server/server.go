47832d783d6da90bb48d252567ad2f81221043b2
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2024-11-01 13:20:39 -0700

    Update server/server.go
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- server/server.go
+++ server/server.go
@@ -562,7 +562,10 @@ func (a *ArgoCDServer) Run(ctx context.Context, listeners *Listeners) {
 		// If not matched, we assume that its TLS.
 		tlsl := tcpm.Match(cmux.Any())
 		tlsConfig := tls.Config{
-			// Prefer http/1.1 for HTTPS and HTTP2 for grpc
+			// Advertise that we support both http/1.1 and http2 for application level communication.
+			// By putting http/1.1 first, we ensure that HTTPS clients will use http/1.1, which is the only
+			// protocol our server supports for HTTPS clients. By including h2 in the list, we ensure that
+			// gRPC clients know we support http2 for their communication.
 			NextProtos: []string{"http/1.1", "h2"},
 		}
 		tlsConfig.GetCertificate = func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
