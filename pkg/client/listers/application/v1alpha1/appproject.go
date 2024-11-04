438e01bc4fb74f2c6f665ec2be1751075a36eb09
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2024-11-03 22:31:04 -0500

    fix(codegen): use kube_codegen.sh deepcopy and client gen correctly (#20644)

--- pkg/client/listers/application/v1alpha1/appproject.go
+++ pkg/client/listers/application/v1alpha1/appproject.go
@@ -4,8 +4,8 @@ package v1alpha1
 
 import (
 	v1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/listers"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -22,25 +22,17 @@ type AppProjectLister interface {
 
 // appProjectLister implements the AppProjectLister interface.
 type appProjectLister struct {
-	indexer cache.Indexer
+	listers.ResourceIndexer[*v1alpha1.AppProject]
 }
 
 // NewAppProjectLister returns a new AppProjectLister.
 func NewAppProjectLister(indexer cache.Indexer) AppProjectLister {
-	return &appProjectLister{indexer: indexer}
-}
-
-// List lists all AppProjects in the indexer.
-func (s *appProjectLister) List(selector labels.Selector) (ret []*v1alpha1.AppProject, err error) {
-	err = cache.ListAll(s.indexer, selector, func(m interface{}) {
-		ret = append(ret, m.(*v1alpha1.AppProject))
-	})
-	return ret, err
+	return &appProjectLister{listers.New[*v1alpha1.AppProject](indexer, v1alpha1.Resource("appproject"))}
 }
 
 // AppProjects returns an object that can list and get AppProjects.
 func (s *appProjectLister) AppProjects(namespace string) AppProjectNamespaceLister {
-	return appProjectNamespaceLister{indexer: s.indexer, namespace: namespace}
+	return appProjectNamespaceLister{listers.NewNamespaced[*v1alpha1.AppProject](s.ResourceIndexer, namespace)}
 }
 
 // AppProjectNamespaceLister helps list and get AppProjects.
@@ -58,26 +50,5 @@ type AppProjectNamespaceLister interface {
 // appProjectNamespaceLister implements the AppProjectNamespaceLister
 // interface.
 type appProjectNamespaceLister struct {
-	indexer   cache.Indexer
-	namespace string
-}
-
-// List lists all AppProjects in the indexer for a given namespace.
-func (s appProjectNamespaceLister) List(selector labels.Selector) (ret []*v1alpha1.AppProject, err error) {
-	err = cache.ListAllByNamespace(s.indexer, s.namespace, selector, func(m interface{}) {
-		ret = append(ret, m.(*v1alpha1.AppProject))
-	})
-	return ret, err
-}
-
-// Get retrieves the AppProject from the indexer for a given namespace and name.
-func (s appProjectNamespaceLister) Get(name string) (*v1alpha1.AppProject, error) {
-	obj, exists, err := s.indexer.GetByKey(s.namespace + "/" + name)
-	if err != nil {
-		return nil, err
-	}
-	if !exists {
-		return nil, errors.NewNotFound(v1alpha1.Resource("appproject"), name)
-	}
-	return obj.(*v1alpha1.AppProject), nil
+	listers.ResourceIndexer[*v1alpha1.AppProject]
 }
