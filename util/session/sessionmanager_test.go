5d5312e59d8f06cbd975692a30c00073a5a8a350
Atif Ali <atali@redhat.com>
2024-11-08 21:53:34 -0500

    updated sessionmanager_test.go
    Signed-off-by: Atif Ali <atali@redhat.com>

--- util/session/sessionmanager_test.go
+++ util/session/sessionmanager_test.go
@@ -226,13 +226,8 @@ type tokenVerifierMock struct {
 	err    error
 }
 
-<<<<<<< HEAD
-func (tm *tokenVerifierMock) VerifyToken(_ string) (jwt.Claims, string, error) {
-	if tm.claims == nil {
-=======
 func (tm *tokenVerifierMock) VerifyToken(token string) (jwt.Claims, string, error) {
 	if tm.err != nil {
->>>>>>> 7bd5b1485 (updated getUser tests and fixed some failing tests)
 		return nil, "", tm.err
 	}
 	mapClaims := jwt.MapClaims{
@@ -353,27 +348,34 @@ func TestSessionManager_WithAuthMiddleware(t *testing.T) {
 
 var loggedOutContext = context.Background()
 
-//nolint:staticcheck
-var loggedInContext = context.WithValue(context.Background(), "claims", &jwt.MapClaims{"iss": "qux", "sub": "foo", "email": "bar", "groups": []string{"baz"}})
+// nolint:staticcheck
+var loggedInContext = context.WithValue(context.Background(), "claims", &jwt.MapClaims{"iss": "qux", "sub": "foo", "email": "bar", "groups": []string{"baz"}, "federated_claims": map[string]interface{}{"user_id": "foo"}})
+
+// for testing without federated claims
+var loggedInContextNoFederated = context.WithValue(context.Background(), "claims", &jwt.MapClaims{"iss": "qux", "sub": "foo", "email": "bar", "groups": []string{"baz"}})
 
 func TestIss(t *testing.T) {
 	assert.Empty(t, Iss(loggedOutContext))
 	assert.Equal(t, "qux", Iss(loggedInContext))
+	assert.Equal(t, "foo", Sub(loggedInContextNoFederated)) // Without federated claims, falls back to sub
 }
 
 func TestLoggedIn(t *testing.T) {
 	assert.False(t, LoggedIn(loggedOutContext))
 	assert.True(t, LoggedIn(loggedInContext))
+	assert.Equal(t, "foo", Username(loggedInContextNoFederated))
 }
 
 func TestUsername(t *testing.T) {
 	assert.Empty(t, Username(loggedOutContext))
-	assert.Equal(t, "bar", Username(loggedInContext))
+	assert.Equal(t, "foo", Username(loggedInContext))
+	assert.Equal(t, "foo", Username(loggedInContextNoFederated))
 }
 
 func TestSub(t *testing.T) {
 	assert.Empty(t, Sub(loggedOutContext))
 	assert.Equal(t, "foo", Sub(loggedInContext))
+	assert.Equal(t, "foo", Username(loggedInContextNoFederated))
 }
 
 func TestGroups(t *testing.T) {
