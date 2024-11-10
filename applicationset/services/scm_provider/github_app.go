af0b9217f0409bf171fee5ec9f09dd6e06a0b8ff
adam_buran <adam.buran@airbnb.com>
2024-11-10 10:43:46 -0800

    updates
    Signed-off-by: adam_buran <adam.buran@airbnb.com>

--- applicationset/services/scm_provider/github_app.go
+++ applicationset/services/scm_provider/github_app.go
@@ -5,10 +5,10 @@ import (
 	"github.com/argoproj/argo-cd/v2/applicationset/services/internal/github_app"
 )
 
-func NewGithubAppProviderFor(g github_app_auth.Authentication, organization string, url string, allBranches bool) (*GithubProvider, error) {
+func NewGithubAppProviderFor(g github_app_auth.Authentication, organization string, url string, allBranches bool, excludeArchivedRepos bool) (*GithubProvider, error) {
 	client, err := github_app.Client(g, url)
 	if err != nil {
 		return nil, err
 	}
-	return &GithubProvider{client: client, organization: organization, allBranches: allBranches}, nil
+	return &GithubProvider{client: client, organization: organization, allBranches: allBranches, excludeArchivedRepos: excludeArchivedRepos}, nil
 }
