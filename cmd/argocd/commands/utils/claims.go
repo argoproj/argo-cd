59055e3bf08dc0681bdd51625b3cfc2eeacdc501
Atif Ali <atali@redhat.com>
2024-11-18 10:05:27 -0500

    refactor claims to use const and dockerfile proper version
    Signed-off-by: Atif Ali <atali@redhat.com>

--- cmd/argocd/commands/utils/claims.go
+++ cmd/argocd/commands/utils/claims.go
@@ -4,15 +4,21 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+const (
+	federatedClaimsKey = "federated_claims"
+	userIDKey          = "user_id"
+	subKey             = "sub"
+)
+
 // GetUserIdentifier returns a consistent user identifier, checking federated_claims.user_id when Dex is in use
 func GetUserIdentifier(claims jwt.MapClaims) string {
-	if federatedClaims, ok := claims["federated_claims"].(map[string]interface{}); ok {
-		if userID, exists := federatedClaims["user_id"].(string); exists && userID != "" {
+	if federatedClaims, ok := claims[federatedClaimsKey].(map[string]interface{}); ok {
+		if userID, exists := federatedClaims[userIDKey].(string); exists && userID != "" {
 			return userID
 		}
 	}
 	// Fallback to sub
-	if sub, ok := claims["sub"].(string); ok && sub != "" {
+	if sub, ok := claims[subKey].(string); ok && sub != "" {
 		return sub
 	}
 	return ""
