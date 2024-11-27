44c437eab8fb6cbf56de81c58b0bab95a43f26ac
linghaoSu <linghao.su@daocloud.io>
2024-11-27 18:34:06 +0800

    fix(ui): extract common code form terminate sync and sync panel
    Signed-off-by: linghaoSu <linghao.su@daocloud.io>

--- ui/src/app/applications/components/applications-terminate-sync-panel/applications-terminate-sync-panel.tsx
+++ ui/src/app/applications/components/applications-terminate-sync-panel/applications-terminate-sync-panel.tsx
@@ -1,118 +1,47 @@
-import {ErrorNotification, NotificationType, SlidingPanel} from 'argo-ui';
+import {ErrorNotification, NotificationType} from 'argo-ui';
 import * as React from 'react';
-import {Form, FormApi} from 'react-form';
-import {ProgressPopup, Spinner} from '../../../shared/components';
-import {Consumer, ContextApis} from '../../../shared/context';
+import {ContextApis} from '../../../shared/context';
 import * as models from '../../../shared/models';
 import {services} from '../../../shared/services';
 import {ApplicationSelector} from '../../../shared/components';
-import {confirmSyncingAppOfApps} from '../utils';
+import {ApplicationsOperationPanel} from '../applications-operation-panel/applications-operation-panel';
+import {FormFunctionProps} from 'react-form';
 
 interface Progress {
     percentage: number;
     title: string;
 }
+interface OperationHandlerContext {
+    setProgress: (progress: Progress) => void;
+    ctx: ContextApis;
+}
 
 export const ApplicationsTerminateSyncPanel = ({show, apps, hide}: {show: boolean; apps: models.Application[]; hide: () => void}) => {
-    const [form, setForm] = React.useState<FormApi>(null);
-    const [progress, setProgress] = React.useState<Progress>(null);
-    const getSelectedApps = (params: any) => apps.filter((_, i) => params['app/' + i]);
-    const [isPending, setPending] = React.useState(false);
-    const syncHandler = (currentForm: FormApi, ctx: ContextApis, applications: models.Application[]) => {
-        const formValues = currentForm.getFormState().values;
-        const replaceChecked = formValues.syncOptions?.includes('Replace=true');
-        const selectedApps = [];
-        const selectedAppOfApps: models.Application[] = [];
-        let containAppOfApps = false;
+    const handleSubmit = async (selectedApps: models.Application[], _: any, {ctx, setProgress}: OperationHandlerContext) => {
+        for (let i = 0; i < selectedApps.length; i++) {
+            const app = selectedApps[i];
+            try {
+                setProgress({
+                    percentage: i / selectedApps.length,
+                    title: `Terminating sync for ${app.metadata.name} (${i + 1}/${selectedApps.length})`
+                });
 
-        for (const key in formValues) {
-            if (key.startsWith('app/') && formValues[key]) {
-                selectedApps.push(applications[parseInt(key.slice(key.lastIndexOf('/') + 1), 10)]);
+                await services.applications.terminateOperation(app.metadata.name, app.metadata.namespace);
+            } catch (e) {
+                ctx.notifications.show({
+                    content: <ErrorNotification title={`Unable to terminate sync ${app.metadata.name}`} e={e} />,
+                    type: NotificationType.Error
+                });
             }
         }
-
-        selectedApps.forEach(app => {
-            if (app.isAppOfAppsPattern) {
-                containAppOfApps = true;
-                selectedAppOfApps.push(app);
-            }
-        });
-
-        if (replaceChecked && containAppOfApps) {
-            confirmSyncingAppOfApps(selectedAppOfApps, ctx, currentForm).then(confirmed => {
-                setPending(confirmed ? true : false);
-            });
-        } else {
-            currentForm.submitForm(null);
-        }
+        setProgress({percentage: 100, title: 'All operations terminated'});
     };
 
+    const syncingApps = apps.filter(app => app.status.operationState?.phase === 'Running' && app.status.operationState.operation.sync !== undefined);
+
     return (
-        <Consumer>
-            {ctx => (
-                <SlidingPanel
-                    isMiddle={true}
-                    isShown={show}
-                    onClose={() => hide()}
-                    header={
-                        <div>
-                            <button className='argo-button argo-button--base' disabled={isPending} onClick={() => syncHandler(form, ctx, apps)}>
-                                <Spinner show={isPending} style={{marginRight: '5px'}} />
-                                Terminate
-                            </button>{' '}
-                            <button onClick={() => hide()} className='argo-button argo-button--base-o'>
-                                Cancel
-                            </button>
-                        </div>
-                    }>
-                    <Form
-                        defaultValues={{syncFlags: []}}
-                        onSubmit={async (params: any) => {
-                            setPending(true);
-                            const selectedApps = getSelectedApps(params);
-
-                            if (selectedApps.length === 0) {
-                                ctx.notifications.show({content: `No apps selected`, type: NotificationType.Error});
-                                setPending(false);
-                                return;
-                            }
-
-                            setProgress({percentage: 0, title: 'Starting...'});
-                            let i = 0;
-                            for (const app of selectedApps) {
-                                await services.applications
-                                    .terminateOperation(app.metadata.name, app.metadata.namespace)
-                                    .catch(e => {
-                                        ctx.notifications.show({
-                                            content: <ErrorNotification title={`Unable to terminate sync ${app.metadata.name}`} e={e} />,
-                                            type: NotificationType.Error
-                                        });
-                                    })
-                                    .finally(() => {
-                                        setPending(false);
-                                    });
-                                i++;
-                                setProgress({
-                                    percentage: i / selectedApps.length,
-                                    title: `${i} of ${selectedApps.length} apps now terminating sync`
-                                });
-                            }
-                            setProgress({percentage: 100, title: 'Complete'});
-                        }}
-                        getApi={setForm}>
-                        {formApi => (
-                            <React.Fragment>
-                                <div className='argo-form-row' style={{marginTop: 0}}>
-                                    <h4>Terminate Sync app(s)</h4>
-                                    {progress !== null && <ProgressPopup onClose={() => setProgress(null)} percentage={progress.percentage} title={progress.title} />}
-
-                                    <ApplicationSelector terminate apps={apps} formApi={formApi} />
-                                </div>
-                            </React.Fragment>
-                        )}
-                    </Form>
-                </SlidingPanel>
-            )}
-        </Consumer>
+        <ApplicationsOperationPanel show={show} apps={apps} hide={hide} title='Terminate Sync app(s)' buttonTitle='Terminate' onSubmit={handleSubmit}>
+            {(formApi: FormFunctionProps) => <ApplicationSelector apps={syncingApps} formApi={formApi} filterOptions={{showOutOfSync: false, showAll: true, showNone: true}} />}
+        </ApplicationsOperationPanel>
     );
 };
