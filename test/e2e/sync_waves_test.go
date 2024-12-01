0b58c3f2b94f9e6ddd2d768860daab5320d3a434
Andrii Korotkov <andrii.korotkov@verkada.com>
2024-12-01 15:38:39 -0800

    Fix imports linter error (try 2)
    Signed-off-by: Andrii Korotkov <andrii.korotkov@verkada.com>

--- test/e2e/sync_waves_test.go
+++ test/e2e/sync_waves_test.go
@@ -6,8 +6,8 @@ import (
 	. "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	. "github.com/argoproj/argo-cd/v2/test/e2e/fixture"
 	. "github.com/argoproj/argo-cd/v2/test/e2e/fixture/app"
-
 	"github.com/argoproj/argo-cd/v2/util/errors"
+
 	"github.com/argoproj/gitops-engine/pkg/health"
 	. "github.com/argoproj/gitops-engine/pkg/sync/common"
 
