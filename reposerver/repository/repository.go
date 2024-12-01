4d4dc17ce843f4b6a5093d373da04e8f822ace18
toyamagu2021 <toyamagu2021@gmail.com>
2024-12-02 01:53:42 +0900

    fix
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -2844,6 +2844,8 @@ func (s *Service) UpdateRevisionForPaths(_ context.Context, request *apiclient.U
 	defer s.metricsServer.DecPendingRepoRequest(repo.Repo)
 
 	// Check if the synced revision is present in the repo. If not, checkout the revision
+	// Note that fetching with an explicit version can cause repo bloat [https://github.com/argoproj/argo-cd/issues/8845],
+	// so we should take lock (theoretically not necessary) and checkout.
 	if !gitClient.IsRevisionPresent(syncedRevision) {
 		closer, err := s.repoLock.Lock(gitClient.Root(), syncedRevision, true, func() (goio.Closer, error) {
 			return s.checkoutRevision(gitClient, syncedRevision, false)
