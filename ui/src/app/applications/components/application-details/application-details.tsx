b5932d6cc6c0893375704231c1ad002ddb93ba61
Blake Pettersson <blake.pettersson@gmail.com>
2024-12-12 11:29:37 +0100

    chore: more cr fixes
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- ui/src/app/applications/components/application-details/application-details.tsx
+++ ui/src/app/applications/components/application-details/application-details.tsx
@@ -32,6 +32,7 @@ import {useSidebarTarget} from '../../../sidebar/sidebar';
 import './application-details.scss';
 import {TopBarActionMenuExt, AppViewExtension, StatusPanelExtension} from '../../../shared/services/extensions-service';
 import {ApplicationHydrateOperationState} from '../application-hydrate-operation-state/application-hydrate-operation-state';
+import {isValidURL} from "../../../shared/utils";
 
 interface ApplicationDetailsState {
     page: number;
@@ -260,8 +261,8 @@ export class ApplicationDetails extends React.Component<RouteComponentProps<{app
                                     <div className='row white-box__details-row'>
                                         <div className='columns small-3'>Image URL:</div>
                                         <div className='columns small-9'>
-                                            {aSource.chart}&nbsp;
-                                            {m.imageUrl && (
+                                            {aSource.repoURL}&nbsp;
+                                            {isValidURL(m.imageUrl) && (
                                                 <a
                                                     title={m.imageUrl}
                                                     onClick={e => {
