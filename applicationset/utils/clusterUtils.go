710d36301a3425761cb5c80214891ef2e39499dc
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2024-12-15 21:33:03 -0500

    chore(appset): use DB instead of kube client for cluster validation
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- applicationset/utils/clusterUtils.go
+++ applicationset/utils/clusterUtils.go
@@ -20,19 +20,6 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-// The contents of this file are from
-// github.com/argoproj/argo-cd/util/db/cluster.go
-//
-// The main difference is that ListClusters(...) calls the kubeclient directly,
-// via `g.clientset.CoreV1().Secrets`, rather than using the `db.listClusterSecrets()``
-// which appears to have a race condition on when it is called.
-//
-// I was reminded of this issue that I opened, which might be related:
-// https://github.com/argoproj/argo-cd/issues/4755
-//
-// I hope to upstream this change in some form, so that we do not need to worry about
-// Argo CD changing the logic on us.
-
 var (
 	localCluster = appv1.Cluster{
 		Name:            "in-cluster",
@@ -41,71 +28,6 @@ var (
 	}
 	initLocalCluster sync.Once
 )
-
-const (
-	ArgoCDSecretTypeLabel   = "argocd.argoproj.io/secret-type"
-	ArgoCDSecretTypeCluster = "cluster"
-)
-
-// ValidateDestination checks:
-// if we used destination name we infer the server url
-// if we used both name and server then we return an invalid spec error
-func ValidateDestination(ctx context.Context, dest *appv1.ApplicationDestination, clientset kubernetes.Interface, argoCDNamespace string) error {
-	if dest.IsServerInferred() && dest.IsNameInferred() {
-		return fmt.Errorf("application destination can't have both name and server inferred: %s %s", dest.Name, dest.Server)
-	}
-	if dest.Name != "" {
-		if dest.Server == "" {
-			server, err := getDestinationBy(ctx, dest.Name, clientset, argoCDNamespace, true)
-			if err != nil {
-				return fmt.Errorf("unable to find destination server: %w", err)
-			}
-			if server == "" {
-				return fmt.Errorf("application references destination cluster %s which does not exist", dest.Name)
-			}
-			dest.SetInferredServer(server)
-		} else if !dest.IsServerInferred() && !dest.IsNameInferred() {
-			return fmt.Errorf("application destination can't have both name and server defined: %s %s", dest.Name, dest.Server)
-		}
-	} else if dest.Server != "" {
-		if dest.Name == "" {
-			serverName, err := getDestinationBy(ctx, dest.Server, clientset, argoCDNamespace, false)
-			if err != nil {
-				return fmt.Errorf("unable to find destination server: %w", err)
-			}
-			if serverName == "" {
-				return fmt.Errorf("application references destination cluster %s which does not exist", dest.Server)
-			}
-			dest.SetInferredName(serverName)
-		}
-	}
-	return nil
-}
-
-func getDestinationBy(ctx context.Context, cluster string, clientset kubernetes.Interface, argoCDNamespace string, byName bool) (string, error) {
-	// settingsMgr := settings.NewSettingsManager(context.TODO(), clientset, namespace)
-	// argoDB := db.NewDB(namespace, settingsMgr, clientset)
-	// clusterList, err := argoDB.ListClusters(ctx)
-	clusterList, err := ListClusters(ctx, clientset, argoCDNamespace)
-	if err != nil {
-		return "", err
-	}
-	var servers []string
-	for _, c := range clusterList.Items {
-		if byName && c.Name == cluster {
-			servers = append(servers, c.Server)
-		}
-		if !byName && c.Server == cluster {
-			servers = append(servers, c.Name)
-		}
-	}
-	if len(servers) > 1 {
-		return "", fmt.Errorf("there are %d clusters with the same name: %v", len(servers), servers)
-	} else if len(servers) == 0 {
-		return "", fmt.Errorf("there are no clusters with this name: %s", cluster)
-	}
-	return servers[0], nil
-}
 
 func ListClusters(ctx context.Context, clientset kubernetes.Interface, namespace string) (*appv1.ClusterList, error) {
 	clusterSecretsList, err := clientset.CoreV1().Secrets(namespace).List(ctx,
