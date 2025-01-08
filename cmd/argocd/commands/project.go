thevenkatesh/6633

feat: add support for project allowed node labels to the cli
Signed-off-by: Marcus Söderberg <msoderb@gmail.com>

cmd/argocd/commands/project.go
@@ -14,6 +14,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/yaml"
 
 	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/headless"
@@ -78,6 +79,8 @@ func NewProjectCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
 	command.AddCommand(NewProjectWindowsCommand(clientOpts))
 	command.AddCommand(NewProjectAddOrphanedIgnoreCommand(clientOpts))
 	command.AddCommand(NewProjectRemoveOrphanedIgnoreCommand(clientOpts))
+	command.AddCommand(NewProjectAdddNodeLabelCommand(clientOpts))
+	command.AddCommand(NewProjectRemoveNodeLabelCommand(clientOpts))
 	return command
 }
 
@@ -995,6 +998,91 @@ func NewProjectEditCommand(clientOpts *argocdclient.ClientOptions) *cobra.Comman
 				}
 				return nil
 			})
+		},
+	}
+	return command
+}
+
+// NewProjectAddNodeLabelCommand returns a new instance of an `argocd proj add-node-label` command
+func NewProjectAdddNodeLabelCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "add-node-label PROJECT LABEL",
+		Short: "Add allowed node label",
+		Example: templates.Examples(`
+			# Add an allowed node label to the project with name PROJECT
+			argocd proj add-node-label PROJECT LABEL
+		`),
+		Run: func(c *cobra.Command, args []string) {
+			ctx := c.Context()
+
+			if len(args) != 2 {
+				c.HelpFunc()(c, args)
+				os.Exit(1)
+			}
+			projName := args[0]
+			label := args[1]
+			conn, projIf := headless.NewClientOrDie(clientOpts, c).NewProjectClientOrDie()
+			defer argoio.Close(conn)
+
+			proj, err := projIf.Get(ctx, &projectpkg.ProjectQuery{Name: projName})
+			errors.CheckError(err)
+
+			if errs := validation.IsQualifiedName(label); len(errs) != 0 {
+				fmt.Println(strings.Join(errs, "; "))
+			}
+
+			for _, item := range proj.Spec.AllowedNodeLabels {
+				if item == label {
+					fmt.Printf("Node label '%s' already allowed in project\n", item)
+					return
+				}
+			}
+			proj.Spec.AllowedNodeLabels = append(proj.Spec.AllowedNodeLabels, label)
+			_, err = projIf.Update(ctx, &projectpkg.ProjectUpdateRequest{Project: proj})
+			errors.CheckError(err)
+		},
+	}
+	return command
+}
+
+// NewProjectRemoveNodeLabelCommand returns a new instance of an `argocd proj remove-node-label` command
+func NewProjectRemoveNodeLabelCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "remove-node-label PROJECT LABEL",
+		Short: "Remove allowed node label",
+		Example: templates.Examples(`
+			# Remove allowed node label LABEL from project PROJECT
+			argocd proj remove-node-label PROJECT LABEL
+		`),
+		Run: func(c *cobra.Command, args []string) {
+			ctx := c.Context()
+
+			if len(args) != 2 {
+				c.HelpFunc()(c, args)
+				os.Exit(1)
+			}
+			projName := args[0]
+			label := args[1]
+			conn, projIf := headless.NewClientOrDie(clientOpts, c).NewProjectClientOrDie()
+			defer argoio.Close(conn)
+
+			proj, err := projIf.Get(ctx, &projectpkg.ProjectQuery{Name: projName})
+			errors.CheckError(err)
+
+			index := -1
+			for i, item := range proj.Spec.AllowedNodeLabels {
+				if item == label {
+					index = i
+					break
+				}
+			}
+			if index == -1 {
+				fmt.Printf("Node label '%s' does not exist in project\n", label)
+			} else {
+				proj.Spec.AllowedNodeLabels = append(proj.Spec.AllowedNodeLabels[:index], proj.Spec.AllowedNodeLabels[index+1:]...)
+				_, err = projIf.Update(ctx, &projectpkg.ProjectUpdateRequest{Project: proj})
+				errors.CheckError(err)
+			}
 		},
 	}
 	return command

argoproj/argo-cd

cmd/argocd/commands/project.go
@@ -15,6 +15,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/yaml"
 
 	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/headless"
@@ -84,6 +85,8 @@ func NewProjectCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
 	command.AddCommand(NewProjectRemoveSourceNamespace(clientOpts))
 	command.AddCommand(NewProjectAddDestinationServiceAccountCommand(clientOpts))
 	command.AddCommand(NewProjectRemoveDestinationServiceAccountCommand(clientOpts))
+	command.AddCommand(NewProjectAddNodeLabelCommand(clientOpts))
+	command.AddCommand(NewProjectRemoveNodeLabelCommand(clientOpts))
 	return command
 }
 
@@ -1214,5 +1217,90 @@ func NewProjectRemoveDestinationServiceAccountCommand(clientOpts *argocdclient.C
 		},
 	}
 
+	return command
+}
+
+// NewProjectAddNodeLabelCommand returns a new instance of an `argocd proj add-node-label` command
+func NewProjectAddNodeLabelCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "add-node-label PROJECT LABEL",
+		Short: "Add allowed node label",
+		Example: templates.Examples(`
+			# Add an allowed node label to the project with name PROJECT
+			argocd proj add-node-label PROJECT LABEL
+		`),
+		Run: func(c *cobra.Command, args []string) {
+			ctx := c.Context()
+
+			if len(args) != 2 {
+				c.HelpFunc()(c, args)
+				os.Exit(1)
+			}
+			projName := args[0]
+			label := args[1]
+			conn, projIf := headless.NewClientOrDie(clientOpts, c).NewProjectClientOrDie()
+			defer argoio.Close(conn)
+
+			proj, err := projIf.Get(ctx, &projectpkg.ProjectQuery{Name: projName})
+			errors.CheckError(err)
+
+			if errs := validation.IsQualifiedName(label); len(errs) != 0 {
+				fmt.Println(strings.Join(errs, "; "))
+			}
+
+			for _, item := range proj.Spec.AllowedNodeLabels {
+				if item == label {
+					fmt.Printf("Node label '%s' already allowed in project\n", item)
+					return
+				}
+			}
+			proj.Spec.AllowedNodeLabels = append(proj.Spec.AllowedNodeLabels, label)
+			_, err = projIf.Update(ctx, &projectpkg.ProjectUpdateRequest{Project: proj})
+			errors.CheckError(err)
+		},
+	}
+	return command
+}
+
+// NewProjectRemoveNodeLabelCommand returns a new instance of an `argocd proj remove-node-label` command
+func NewProjectRemoveNodeLabelCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "remove-node-label PROJECT LABEL",
+		Short: "Remove allowed node label",
+		Example: templates.Examples(`
+			# Remove allowed node label LABEL from project PROJECT
+			argocd proj remove-node-label PROJECT LABEL
+		`),
+		Run: func(c *cobra.Command, args []string) {
+			ctx := c.Context()
+
+			if len(args) != 2 {
+				c.HelpFunc()(c, args)
+				os.Exit(1)
+			}
+			projName := args[0]
+			label := args[1]
+			conn, projIf := headless.NewClientOrDie(clientOpts, c).NewProjectClientOrDie()
+			defer argoio.Close(conn)
+
+			proj, err := projIf.Get(ctx, &projectpkg.ProjectQuery{Name: projName})
+			errors.CheckError(err)
+
+			index := -1
+			for i, item := range proj.Spec.AllowedNodeLabels {
+				if item == label {
+					index = i
+					break
+				}
+			}
+			if index == -1 {
+				fmt.Printf("Node label '%s' does not exist in project\n", label)
+			} else {
+				proj.Spec.AllowedNodeLabels = append(proj.Spec.AllowedNodeLabels[:index], proj.Spec.AllowedNodeLabels[index+1:]...)
+				_, err = projIf.Update(ctx, &projectpkg.ProjectUpdateRequest{Project: proj})
+				errors.CheckError(err)
+			}
+		},
+	}
 	return command
 }