8a8cff277f6dcf0b518affccb172e7a98f434c9f
Regina Voloshin <regina.voloshin@codefresh.io>
2025-01-21 18:11:15 +0000

    fix: resolve the failing e2e appset tests for ksonnet applications (#21580)
    Signed-off-by: reggie-k <regina.voloshin@codefresh.io>

--- test/e2e/applicationset_test.go
+++ test/e2e/applicationset_test.go
@@ -1146,7 +1146,6 @@ func TestSimpleGitDirectoryGenerator(t *testing.T) {
 	expectedApps := []argov1alpha1.Application{
 		generateExpectedApp("kustomize-guestbook"),
 		generateExpectedApp("helm-guestbook"),
-		generateExpectedApp("ksonnet-guestbook"),
 	}
 
 	var expectedAppsNewNamespace []argov1alpha1.Application
@@ -1256,7 +1255,6 @@ func TestSimpleGitDirectoryGeneratorGoTemplate(t *testing.T) {
 	expectedApps := []argov1alpha1.Application{
 		generateExpectedApp("kustomize-guestbook"),
 		generateExpectedApp("helm-guestbook"),
-		generateExpectedApp("ksonnet-guestbook"),
 	}
 
 	var expectedAppsNewNamespace []argov1alpha1.Application
