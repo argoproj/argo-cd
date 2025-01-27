4ca5da720bb4c6daf7a3a4bb7891faff2523dad5
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2025-01-27 14:05:06 -0500

    Resolve comments
    Signed-off-by: Jagpreet Singh Tamber <jagpreetstamber@gmail.com>

--- util/oidc/oidc_test.go
+++ util/oidc/oidc_test.go
@@ -139,9 +139,7 @@ requestedScopes: ["oidc"]`, oidcTestServer.URL),
 
 		app.HandleLogin(w, req)
 
-		if !strings.Contains(w.Body.String(), "certificate signed by unknown authority") && !strings.Contains(w.Body.String(), "certificate is not trusted") {
-			t.Fatal("did not receive expected certificate verification failure error")
-		}
+		assert.Contains(t, w.Body.String(), "certificate signed by unknown authority")
 
 		cdSettings.OIDCTLSInsecureSkipVerify = true
 
