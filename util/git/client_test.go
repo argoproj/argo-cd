9d6625dddc0bb5185ae4a494c1ace385a5a66f45
Atif Ali <atali@redhat.com>
2025-02-26 08:31:09 -0500

    go mod vendor
    Signed-off-by: Atif Ali <atali@redhat.com>

--- util/git/client_test.go
+++ util/git/client_test.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport"
-	//"github.com/go-git/go-git/v5/plumbing"
 	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
