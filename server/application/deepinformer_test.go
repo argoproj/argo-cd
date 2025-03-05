18f6bc465f7f2f37c111af3d6adb270f215cbcd9
rumstead <37445536+rumstead@users.noreply.github.com>
2025-03-05 15:12:19 -0500

    feat(server): make deep copies of objects returned by informers
    Signed-off-by: rumstead <37445536+rumstead@users.noreply.github.com>

--- server/application/deepinformer_test.go
+++ server/application/deepinformer_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
