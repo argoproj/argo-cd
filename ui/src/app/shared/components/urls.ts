450a9d65cddef22eccb802a2074510a636350e5a
Atif Ali <atali@redhat.com>
2025-03-06 17:05:39 -0500

    resolve linter issue
    Signed-off-by: Atif Ali <atali@redhat.com>

--- ui/src/app/shared/components/urls.ts
+++ ui/src/app/shared/components/urls.ts
@@ -12,11 +12,10 @@ function protocol(proto: string): string {
 }
 
 export function repoUrl(url: string): string {
-        if (/^https?:\/\//.test(url)) {
-            // return url as is without .git suffix and username@ part
-            return url.replace(/\.git$/, '')
-                     .replace(/\/\/(.*?@)/, '//');
-        }       
+    if (/^https?:\/\//.test(url)) {
+        // return url as is without .git suffix and username@ part
+        return url.replace(/\.git$/, '').replace(/\/\/(.*?@)/, '//');
+    }
     try {
         const parsed = GitUrlParse(url);
 
