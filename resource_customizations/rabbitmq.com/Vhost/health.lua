3da201204fadbcd6ba0d1cc6272fe305bfe287f6
Francis Provencher <fprovencher@drw.com>
2025-03-10 14:07:40 -0400

    Removing unneeded nil checking of condition
    Signed-off-by: Francis Provencher <fprovencher@drw.com>

--- resource_customizations/rabbitmq.com/Vhost/health.lua
+++ resource_customizations/rabbitmq.com/Vhost/health.lua
@@ -3,21 +3,17 @@ if obj.status ~= nil then
   if obj.status.conditions ~= nil then
 
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type ~= nil then
-        if condition.type == "Ready" then
-          if condition.status ~= nil and condition.reason ~= nil then
-            if condition.status == "True" and condition.reason == "SuccessfulCreateOrUpdate" then
-              hs.status = "Healthy"
-              hs.message = "RabbitMQ vhost ready"
-              return hs
-            end 
+      if condition.type == "Ready" then
+        if condition.status == "True" and condition.reason == "SuccessfulCreateOrUpdate" then
+          hs.status = "Healthy"
+          hs.message = "RabbitMQ vhost ready"
+          return hs
+        end 
 
-            if condition.status == "False" and condition.reason == "FailedCreateOrUpdate" then
-              hs.status = "Degraded"
-              hs.message = "RabbitMQ vhost failed to be created or updated"
-              return hs
-            end
-          end
+        if condition.status == "False" and condition.reason == "FailedCreateOrUpdate" then
+          hs.status = "Degraded"
+          hs.message = "RabbitMQ vhost failed to be created or updated"
+          return hs
         end
       end
     end
