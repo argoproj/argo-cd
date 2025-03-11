a9582ac826d295e02b09f98dea3a961ff28b4ef4
crabique <crabique@users.noreply.github.com>
2025-03-11 23:34:12 +0200

    disable ESO refresh/push actions when it would do nothing
    Signed-off-by: crabique <crabique@users.noreply.github.com>

--- resource_customizations/external-secrets.io/PushSecret/actions/discovery.lua
+++ resource_customizations/external-secrets.io/PushSecret/actions/discovery.lua
@@ -1,3 +1,9 @@
-actions = {}
-actions["push"] = {["disabled"] = false}
+local actions = {}
+
+local push_disabled = false
+if obj.spec.refreshInterval == "0s" then
+  push_disabled = true
+end
+
+actions["push"] = {["disabled"] = push_disabled}
 return actions
