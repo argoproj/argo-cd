8ba04d7ced97922623c8c86ab43c0493b6c6a59d
nitishfy <justnitish06@gmail.com>
2025-03-18 16:44:00 +0530

    export command test case
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- cmd/argocd/commands/admin/backup_test.go
+++ cmd/argocd/commands/admin/backup_test.go
@@ -1,6 +1,11 @@
 package admin
 
 import (
+	"bufio"
+	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/argoproj/gitops-engine/pkg/utils/kube"
@@ -11,6 +16,32 @@ import (
 
 	"github.com/argoproj/argo-cd/v3/common"
 )
+
+func createTestWriter(t *testing.T, out string) (io.Writer, func()) {
+	t.Helper()
+
+	var writer io.Writer
+	var cleanup func()
+
+	if out == "-" {
+		writer = os.Stdout
+		cleanup = func() {}
+	} else {
+		f, err := os.Create(out)
+		require.NoError(t, err)
+
+		bw := bufio.NewWriter(f)
+		writer = bw
+
+		cleanup = func() {
+			require.NoError(t, bw.Flush())
+			require.NoError(t, f.Close())
+			_ = os.Remove(out)
+		}
+	}
+
+	return writer, cleanup
+}
 
 func newBackupObject(trackingValue string, trackingLabel bool, trackingAnnotation bool) *unstructured.Unstructured {
 	cm := corev1.ConfigMap{
@@ -33,6 +64,236 @@ func newBackupObject(trackingValue string, trackingLabel bool, trackingAnnotatio
 		})
 	}
 	return kube.MustToUnstructured(&cm)
+}
+
+func newConfigmapObject() *unstructured.Unstructured {
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      common.ArgoCDConfigMapName,
+			Namespace: "argocd",
+			Labels: map[string]string{
+				"app.kubernetes.io/part-of": "argocd",
+			},
+		},
+	}
+
+	return kube.MustToUnstructured(&cm)
+}
+
+func newSecretsObject() *unstructured.Unstructured {
+	secret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      common.ArgoCDSecretName,
+			Namespace: "default",
+			Labels: map[string]string{
+				"app.kubernetes.io/part-of": "argocd",
+			},
+		},
+		Data: map[string][]byte{
+			"admin.password":   nil,
+			"server.secretkey": nil,
+		},
+	}
+
+	return kube.MustToUnstructured(&secret)
+}
+
+func newAppProject() *unstructured.Unstructured {
+	appProject := v1alpha1.AppProject{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "default",
+			Namespace: "argocd",
+		},
+		Spec: v1alpha1.AppProjectSpec{
+			Destinations: []v1alpha1.ApplicationDestination{
+				{
+					Namespace: "*",
+					Server:    "*",
+				},
+			},
+			ClusterResourceWhitelist: []metav1.GroupKind{
+				{
+					Group: "*",
+					Kind:  "*",
+				},
+			},
+			SourceRepos: []string{"*"},
+		},
+	}
+
+	return kube.MustToUnstructured(&appProject)
+}
+
+func newApplication() *unstructured.Unstructured {
+	app := v1alpha1.Application{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.ApplicationSpec{
+			Source:  &v1alpha1.ApplicationSource{},
+			Project: "default",
+			Destination: v1alpha1.ApplicationDestination{
+				Server:    v1alpha1.KubernetesInternalAPIServerAddr,
+				Namespace: "default",
+			},
+		},
+	}
+
+	return kube.MustToUnstructured(&app)
+}
+
+func newApplicationSet() *unstructured.Unstructured {
+	appSet := v1alpha1.ApplicationSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-appset",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.ApplicationSetSpec{
+			Generators: []v1alpha1.ApplicationSetGenerator{
+				{
+					Git: &v1alpha1.GitGenerator{
+						RepoURL: "https://github.com/org/repo",
+					},
+				},
+			},
+		},
+	}
+
+	return kube.MustToUnstructured(&appSet)
+}
+
+// Test_exportResources tests for the resources exported when using the `argocd admin export` command
+func Test_exportResources(t *testing.T) {
+	tests := []struct {
+		name                string
+		object              *unstructured.Unstructured
+		out                 string
+		expectedFileContent string
+	}{
+		{
+			name:   "ConfigMap should be in the exported manifest",
+			object: newConfigmapObject(),
+			out:    "test.yaml",
+			expectedFileContent: `apiVersion: ""
+kind: ""
+metadata:
+  labels:
+    app.kubernetes.io/part-of: argocd
+  name: argocd-cm
+---
+`,
+		},
+		{
+			name:   "Secret should be in the exported manifest",
+			object: newSecretsObject(),
+			out:    "test.yaml",
+			expectedFileContent: `apiVersion: ""
+data:
+  admin.password: null
+  server.secretkey: null
+kind: ""
+metadata:
+  labels:
+    app.kubernetes.io/part-of: argocd
+  name: argocd-secret
+  namespace: default
+---
+`,
+		},
+		{
+			name:   "App Project should be in the exported manifest",
+			object: newAppProject(),
+			out:    "test.yaml",
+			expectedFileContent: `apiVersion: ""
+kind: ""
+metadata:
+  name: default
+spec:
+  clusterResourceWhitelist:
+  - group: '*'
+    kind: '*'
+  destinations:
+  - namespace: '*'
+    server: '*'
+  sourceRepos:
+  - '*'
+status: {}
+---
+`,
+		},
+		{
+			name:   "Application should be in the exported manifest",
+			object: newApplication(),
+			out:    "test.yaml",
+			expectedFileContent: `apiVersion: ""
+kind: ""
+metadata:
+  name: test
+  namespace: default
+spec:
+  destination:
+    namespace: default
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    repoURL: ""
+status:
+  health: {}
+  sourceHydrator: {}
+  summary: {}
+  sync:
+    comparedTo:
+      destination: {}
+      source:
+        repoURL: ""
+    status: ""
+---
+`,
+		},
+		{
+			name:   "ApplicationSet should be in the exported manifest",
+			object: newApplicationSet(),
+			out:    "test.yaml",
+			expectedFileContent: `apiVersion: ""
+kind: ""
+metadata:
+  name: test-appset
+  namespace: default
+spec:
+  generators:
+  - git:
+      repoURL: https://github.com/org/repo
+      revision: ""
+      template:
+        metadata: {}
+        spec:
+          destination: {}
+          project: ""
+  template:
+    metadata: {}
+    spec:
+      destination: {}
+      project: ""
+status: {}
+---
+`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w, cleanup := createTestWriter(t, tt.out)
+			export(w, *tt.object, ArgoCDNamespace)
+			if bw, ok := w.(*bufio.Writer); ok {
+				require.NoError(t, bw.Flush())
+			}
+			actualContent, err := os.ReadFile(tt.out)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expectedFileContent, string(actualContent))
+			cleanup()
+		})
+	}
 }
 
 func Test_updateTracking(t *testing.T) {
