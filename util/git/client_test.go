6a5895e438fa846590180df7b3f8fe7adde26c32
Atif Ali <56743004+aali309@users.noreply.github.com>
2025-03-18 11:46:19 -0400

    fix: handle annotated git tags correctly in repo server cache (#21771)
    Signed-off-by: Atif Ali <atali@redhat.com>

--- util/git/client_test.go
+++ util/git/client_test.go
@@ -13,6 +13,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/assert"
@@ -133,6 +134,19 @@ func Test_IsAnnotatedTag(t *testing.T) {
 	// We moved on, so tag doesn't point to HEAD anymore
 	atag = client.IsAnnotatedTag("HEAD")
 	assert.False(t, atag)
+}
+
+func Test_resolveTagReference(t *testing.T) {
+	// Setup
+	commitHash := plumbing.NewHash("0123456789abcdef0123456789abcdef01234567")
+	tagRef := plumbing.NewReferenceFromStrings("refs/tags/v1.0.0", "sometaghash")
+
+	// Test single function
+	resolvedRef := plumbing.NewHashReference(tagRef.Name(), commitHash)
+
+	// Verify
+	assert.Equal(t, commitHash, resolvedRef.Hash())
+	assert.Equal(t, tagRef.Name(), resolvedRef.Name())
 }
 
 func Test_ChangedFiles(t *testing.T) {
