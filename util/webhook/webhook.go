686609ed00d38a8180c186fd2a6d5d2b114e2467
Alexy Mantha <alexy.mantha@goto.com>
2025-03-25 15:29:17 -0400

    add hydration flag
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- util/webhook/webhook.go
+++ util/webhook/webhook.go
@@ -313,7 +313,7 @@ func (a *ArgoCDWebhookHandler) HandleEvent(payload any) {
 					if path.AppFilesHaveChanged(refreshPaths, changedFiles) {
 						namespacedAppInterface := a.appClientset.ArgoprojV1alpha1().Applications(app.ObjectMeta.Namespace)
 						log.Infof("webhook trigger refresh app to hydrate '%s'", app.ObjectMeta.Name)
-						_, err = argo.RefreshApp(namespacedAppInterface, app.ObjectMeta.Name, v1alpha1.RefreshTypeNormal)
+						_, err = argo.RefreshApp(namespacedAppInterface, app.ObjectMeta.Name, v1alpha1.RefreshTypeNormal, true)
 						if err != nil {
 							log.Warnf("Failed to hydrate app '%s' for controller reprocessing: %v", app.ObjectMeta.Name, err)
 							continue
