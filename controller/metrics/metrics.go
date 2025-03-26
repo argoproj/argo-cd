f81167648e9d164128bd4c198a835129c780d96b
Javier Solana <javiersolanahuertas@gmail.com>
2025-03-27 00:31:12 +0100

    chore: add "dry_run" label to sync metrics (#22010)
    Co-authored-by: Javier Solana <javier.solana@cabify.com>
    Co-authored-by: Pasha Kostohrys <pasha.kostohrys@gmail.com>
    Signed-off-by: Hapshanko <adamgaming391100@gmail.com>

--- controller/metrics/metrics.go
+++ controller/metrics/metrics.go
@@ -72,7 +72,7 @@ var (
 			Name: "argocd_app_sync_total",
 			Help: "Number of application syncs.",
 		},
-		append(descAppDefaultLabels, "dest_server", "phase"),
+		append(descAppDefaultLabels, "dest_server", "phase", "dry_run"),
 	)
 
 	k8sRequestCounter = prometheus.NewCounterVec(
@@ -80,7 +80,7 @@ var (
 			Name: "argocd_app_k8s_request_total",
 			Help: "Number of kubernetes requests executed during application reconciliation.",
 		},
-		append(descAppDefaultLabels, "server", "response_code", "verb", "resource_kind", "resource_namespace"),
+		append(descAppDefaultLabels, "server", "response_code", "verb", "resource_kind", "resource_namespace", "dry_run"),
 	)
 
 	kubectlExecCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
@@ -239,7 +239,8 @@ func (m *MetricsServer) IncSync(app *argoappv1.Application, state *argoappv1.Ope
 	if !state.Phase.Completed() {
 		return
 	}
-	m.syncCounter.WithLabelValues(app.Namespace, app.Name, app.Spec.GetProject(), app.Spec.Destination.Server, string(state.Phase)).Inc()
+	isDryRun := app.Operation != nil && app.Operation.DryRun()
+	m.syncCounter.WithLabelValues(app.Namespace, app.Name, app.Spec.GetProject(), app.Spec.Destination.Server, string(state.Phase), strconv.FormatBool(isDryRun)).Inc()
 }
 
 func (m *MetricsServer) IncKubectlExec(command string) {
@@ -266,14 +267,16 @@ func (m *MetricsServer) IncClusterEventsCount(server, group, kind string) {
 // IncKubernetesRequest increments the kubernetes requests counter for an application
 func (m *MetricsServer) IncKubernetesRequest(app *argoappv1.Application, server, statusCode, verb, resourceKind, resourceNamespace string) {
 	var namespace, name, project string
+	isDryRun := false
 	if app != nil {
 		namespace = app.Namespace
 		name = app.Name
 		project = app.Spec.GetProject()
+		isDryRun = app.Operation != nil && app.Operation.DryRun()
 	}
 	m.k8sRequestCounter.WithLabelValues(
 		namespace, name, project, server, statusCode,
-		verb, resourceKind, resourceNamespace,
+		verb, resourceKind, resourceNamespace, strconv.FormatBool(isDryRun),
 	).Inc()
 }
 
