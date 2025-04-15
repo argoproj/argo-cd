793e31f0eaf9663c07e8909514f8bbb78ff719d8
rumstead <37445536+rumstead@users.noreply.github.com>
2025-04-15 14:07:34 +0000

    fix(appset): generated app errors should use the default requeue (#21887) (#21936)
    Signed-off-by: rumstead <37445536+rumstead@users.noreply.github.com>
    Co-authored-by: Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
    Co-authored-by: Blake Pettersson <blake.pettersson@gmail.com>

--- applicationset/controllers/applicationset_controller_test.go
+++ applicationset/controllers/applicationset_controller_test.go
@@ -1884,7 +1884,7 @@ func TestRequeueGeneratorFails(t *testing.T) {
 	}
 
 	res, err := r.Reconcile(context.Background(), req)
-	require.Error(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, ReconcileRequeueOnValidationError, res.RequeueAfter)
 }
 
