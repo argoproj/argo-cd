1b9c4b0f0afb38e8aaeccb7b487dfd59a9d2b913
heshamelsherif97 <heshamelsherif97@gmail.com>
2025-04-17 17:19:08 +0200

    fix(resource_customizations): update externalsecret health check to accomadate for OnChange policy
    Signed-off-by: heshamelsherif97 <heshamelsherif97@gmail.com>

--- resource_customizations/external-secrets.io/ExternalSecret/health.lua
+++ resource_customizations/external-secrets.io/ExternalSecret/health.lua
@@ -1,16 +1,23 @@
+function checkStatus(condition)
+  local hs = {}
+  if condition.status == "False" then
+    hs.status = "Degraded"
+    hs.message = condition.message
+    return hs
+  end
+  if condition.status == "True" then
+    hs.status = "Healthy"
+    hs.message = condition.message
+    return hs
+  end
+end
+
 local hs = {}
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "False" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "Ready" and condition.status == "True" then
-        hs.status = "Healthy"
-        hs.message = condition.message
-        return hs
+      if condition.type == "Ready" and (obj.spec.refreshPolicy ~= "OnChange" or condition.lastTransitionTime >= obj.status.refreshTime) then
+        return checkStatus(condition)
       end
     end
   end
