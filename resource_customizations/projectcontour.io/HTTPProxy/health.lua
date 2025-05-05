9eb0f75d76827ddb3e802660950fb3e533da738d
Greg Barasch <gregbarasch@gmail.com>
2025-05-05 18:59:32 -0400

    Small test rework
    Signed-off-by: Greg Barasch <gregbarasch@gmail.com>

--- resource_customizations/projectcontour.io/HTTPProxy/health.lua
+++ resource_customizations/projectcontour.io/HTTPProxy/health.lua
@@ -1,19 +1,19 @@
 -- Status reporting information detailed here
 -- https://projectcontour.io/docs/main/config/fundamentals/#status-reporting
-hs = {}
+hs = {
+  status = "Progressing",
+  message = "Waiting for status",
+}
 
 if obj.status ~= nil then
   if obj.status.currentStatus ~= nil then
     if obj.status.currentStatus == "valid" then
       hs.status = "Healthy"
-    else
+    elseif obj.status.currentStatus ~= "NotReconciled" then
       hs.status = "Degraded"
     end
     hs.message = obj.status.description
-    return hs
   end
 end
 
-hs.status = "Progressing"
-hs.message = "Waiting for status"
 return hs
