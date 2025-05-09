d845d2816d1e776d0867993c366766ee99239a89
Omer Azmon <omer_azmon@intuit.com>
2025-05-09 16:28:57 -0700

    fix lint issues

--- ui/src/app/types/entities-decode.d.ts
+++ ui/src/app/types/entities-decode.d.ts
@@ -3,8 +3,8 @@ declare module 'entities/decode' {
     export default decode;
 
     // If EntityDecoder is used as a type by parse5
-    export type EntityDecoder = any; 
+    export type EntityDecoder = any;
 
     // If it's also used as a value (e.g., a class constructor), you might need this too:
     // export const EntityDecoder: { new (...args: any[]): any; /* other static props */ };
-} 
+}
