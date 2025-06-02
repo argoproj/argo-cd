83163a6f9d02a66f803472b4f92f2b5d47ef2864
Solomon Kibret <solomon.kibret@gmail.com>
2025-06-02 15:31:55 -0400

    fixes

--- resource_customizations/projectcontour.io/HTTPProxy/health.lua
+++ resource_customizations/projectcontour.io/HTTPProxy/health.lua
@@ -8,17 +8,15 @@ hs = {
 if obj.status == nil then
   hs.status = "Unknown"
   hs.message = "Cluster Status is unknown"
-  if obj.status.currentStatus == "valid" then
-    hs.status = "Healthy"
-    hs.message = obj.status.description
-  elseif obj.status.currentStatus == "invalid" then
-    hs.status = "Degraded"
-    hs.message = obj.status.description
-  elseif obj.status.currentStatus == "error" then 
-    hs.status = "Degraded"
-    hs.message = "Error detected: " .. (obj.status.description or "No details")
-  end
-  
+elseif obj.status.currentStatus == "valid" then
+  hs.status = "Healthy"
+  hs.message = obj.status.description
+elseif obj.status.currentStatus == "invalid" then
+  hs.status = "Degraded"
+  hs.message = obj.status.description
+elseif obj.status.currentStatus == "orphaned" or obj.status.currentStatus == "NotReconciled" then 
+  hs.status = "Degraded"
+  hs.message = "Error detected: " .. (obj.status.description or "No details")
 end
 
 return hs
