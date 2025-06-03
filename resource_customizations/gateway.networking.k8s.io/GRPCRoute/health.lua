703d54b26371a6c89fb5b8d7c6f71d942bc0473d
Solomon Kibret <solomon.kibret@gmail.com>
2025-06-03 15:16:25 -0400

    grpc additions

--- resource_customizations/gateway.networking.k8s.io/GRPCRoute/health.lua
+++ resource_customizations/gateway.networking.k8s.io/GRPCRoute/health.lua
@@ -9,13 +9,15 @@ if obj.status and obj.status.parents then
     for _, parent in ipairs(obj.status.parents) do
         if parent.conditions then
             for _, cond in ipairs(parent.conditions) do
-                print("Condition type:", cond.type, "status:", cond.status, "message:", cond.message)
+                -- print("Condition type:", cond.type, "status:", cond.status, "message:", cond.message)
                 if cond.type == "Accepted" and cond.status == "True" then
                     hs.status = "Healthy"
                     hs.message = cond.message
+                    return hs
                 elseif cond.type == "Accepted" and cond.status == "False" then
                     hs.status = "Degraded"
                     hs.message = cond.message
+                    return hs
                 end
             end
         end
