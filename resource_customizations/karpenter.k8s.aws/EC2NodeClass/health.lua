368156cc1e3fe098a40cb7d8d7068e8a04cfbb08
김병준 <kingbj0429@gmail.com>
2025-06-05 00:29:17 +0900

    chore: fix conditions
    Signed-off-by: 김병준 <kingbj0429@gmail.com>

--- resource_customizations/karpenter.k8s.aws/EC2NodeClass/health.lua
+++ resource_customizations/karpenter.k8s.aws/EC2NodeClass/health.lua
@@ -1,32 +1,33 @@
 local hs = {}
-if obj.status ~= nil then
-  if obj.status.conditions ~= nil then
-    for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "False" then
+if obj.status ~= nil and obj.status.conditions ~= nil then
+  for i, condition in ipairs(obj.status.conditions) do
+    if condition.type == "Ready" then
+      if condition.status == "False" then
         hs.status = "Degraded"
         hs.message = condition.message
         return hs
-      end
-      if condition.type == "SubnetsReady" and condition.status == "False" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "SecurityGroupsReady" and condition.status == "False" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "ValidationSucceeded" and condition.status == "False" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "Ready" and condition.status == "True" then
+      elseif condition.status == "True" then
         hs.status = "Healthy"
         hs.message = "EC2NodeClass is ready"
         return hs
       end
+    end
+  end
+  for i, condition in ipairs(obj.status.conditions) do
+    if condition.type == "SubnetsReady" and condition.status == "False" then
+      hs.status = "Degraded"
+      hs.message = condition.message
+      return hs
+    end
+    if condition.type == "SecurityGroupsReady" and condition.status == "False" then
+      hs.status = "Degraded"
+      hs.message = condition.message
+      return hs
+    end
+    if condition.type == "ValidationSucceeded" and condition.status == "False" then
+      hs.status = "Degraded"
+      hs.message = condition.message
+      return hs
     end
   end
 end
--- resource_customizations/karpenter.sh/NodePool/health.lua
+++ resource_customizations/karpenter.sh/NodePool/health.lua
@@ -1,22 +1,23 @@
 local hs = {}
-if obj.status ~= nil then
-  if obj.status.conditions ~= nil then
-    for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "False" then
+if obj.status ~= nil and obj.status.conditions ~= nil then
+  for i, condition in ipairs(obj.status.conditions) do
+    if condition.type == "Ready" then
+      if condition.status == "False" then
         hs.status = "Degraded"
         hs.message = condition.message
         return hs
-      end
-      if condition.type == "NodeClassReady" and condition.status == "False" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "Ready" and condition.status == "True" then
+      elseif condition.status == "True" then
         hs.status = "Healthy"
         hs.message = condition.message
         return hs
       end
+    end
+  end
+  for i, condition in ipairs(obj.status.conditions) do
+    if condition.type == "NodeClassReady" and condition.status == "False" then
+      hs.status = "Degraded"
+      hs.message = condition.message
+      return hs
     end
   end
 end
