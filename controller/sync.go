c83e142406b37edce31d359c1762310ed110e2ef
Peter Jiang <peterjiang823@gmail.com>
2025-06-09 16:05:33 -0700

    point to gitops fork
    Signed-off-by: Peter Jiang <peterjiang823@gmail.com>

--- controller/sync.go
+++ controller/sync.go
@@ -383,7 +383,7 @@ func (m *appStateManager) SyncAppState(app *v1alpha1.Application, state *v1alpha
 		sync.WithServerSideApply(syncOp.SyncOptions.HasOption(common.SyncOptionServerSideApply)),
 		sync.WithServerSideApplyManager(cdcommon.ArgoCDSSAManager),
 		sync.WithClientSideApplyMigration(
-			!syncOp.SyncOptions.HasOption(syncOp.SyncOptions.HasOption(common.SyncOptionDisableClientSideApplyMigration)),
+			!syncOp.SyncOptions.HasOption(common.SyncOptionDisableClientSideApplyMigration),
 			clientSideApplyManager,
 		),
 		sync.WithPruneConfirmed(app.IsDeletionConfirmed(state.StartedAt.Time)),
