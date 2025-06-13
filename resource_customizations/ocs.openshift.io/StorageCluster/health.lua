7dbbb0f53e072ea39db331f477377e3aee8dd789
Jennifer Weir <contact@jenniferpweir.com>
2025-06-13 17:29:13 -0400

    fix(health-check): StorageCluster.ocs.openshift.io
    Signed-off-by: Jennifer Weir <contact@jenniferpweir.com>

--- resource_customizations/ocs.openshift.io/StorageCluster/health.lua
+++ resource_customizations/ocs.openshift.io/StorageCluster/health.lua
@@ -1,28 +1,33 @@
-local hs = {}
+local hs_degraded = {}
+local hs_progressing = {}
+local hs_healthy = {}
+local isDegraded = false
+local isProgressing = false
+local isHealthy = false
 if obj.status ~= nil then
     if obj.status.conditions ~= nil then
         for _, condition in pairs(obj.status.conditions) do
-            if condition.type == "Degraded" and condition.status == "True" then 
+            if condition.type == "Degraded" and condition.status == "True" then
+                isDegraded = true
                 hs.status = "Degraded"
                 hs.message = condition.message
-                return hs
-            elseif condition.type == "Progressing" and condition.status == "False" then
+            elseif condition.type == "Progressing" and condition.status == "True" then
+                isProgressing = true
                 hs.status = "Progressing"
                 hs.message = condition.message
-                return hs
-            elseif condition.type == "Upgradeable" and condition.status == "True" then
-                hs.status = "Healthy"
-                hs.message = condition.message
-                return hs
             elseif condition.type == "Available" and condition.status == "True" then
+                isHealthy = true
                 hs.status = "Healthy"
                 hs.message = condition.message
-                return hs
             end
         end
     end
 end
-
-hs.status = "Progressing"
-hs.message = "StorageCluster is still being initialized or is in an unknown state."
-return hs
+if isDegraded then
+    return hs_degraded
+elseif isProgressing then
+    return hs_progressing
+elseif isHealthy then
+    return hs_healthy
+end
+return { status = "Unknown", message = "StorageCluster is in an unknown state." }
