07d81c15e19abf9d546cae38e2137566ff630d11
Jennifer Weir <contact@jenniferpweir.com>
2025-06-14 10:50:11 -0400

    fix(health-check): edit struct hs variable naming
    Signed-off-by: Jennifer Weir <contact@jenniferpweir.com>

--- resource_customizations/ocs.openshift.io/StorageCluster/health.lua
+++ resource_customizations/ocs.openshift.io/StorageCluster/health.lua
@@ -1,33 +1,31 @@
 local hs_degraded = {}
 local hs_progressing = {}
 local hs_healthy = {}
-local isDegraded = false
-local isProgressing = false
-local isHealthy = false
-if obj.status ~= nil then
-    if obj.status.conditions ~= nil then
-        for _, condition in pairs(obj.status.conditions) do
-            if condition.type == "Degraded" and condition.status == "True" then
-                isDegraded = true
-                hs.status = "Degraded"
-                hs.message = condition.message
-            elseif condition.type == "Progressing" and condition.status == "True" then
-                isProgressing = true
-                hs.status = "Progressing"
-                hs.message = condition.message
-            elseif condition.type == "Available" and condition.status == "True" then
-                isHealthy = true
-                hs.status = "Healthy"
-                hs.message = condition.message
-            end
+local is_degraded = false
+local is_progressing = false
+local is_healthy = false
+if obj ~= nil and obj.status ~= nil and obj.status.conditions ~= nil then
+    for _, condition in pairs(obj.status.conditions) do
+        if condition.type == "Degraded" and condition.status == "True" then
+            is_degraded = true
+            hs_degraded.status = "Degraded"
+            hs_degraded.message = condition.message
+        elseif condition.type == "Progressing" and condition.status == "True" then
+            is_progressing = true
+            hs_progressing.status = "Progressing"
+            hs_progressing.message = condition.message
+        elseif condition.type == "Available" and condition.status == "True" then
+            is_healthy = true
+            hs_healthy.status = "Healthy"
+            hs_healthy.message = condition.message
         end
     end
 end
-if isDegraded then
+if is_degraded then
     return hs_degraded
-elseif isProgressing then
+elseif is_progressing then
     return hs_progressing
-elseif isHealthy then
+elseif is_healthy then
     return hs_healthy
 end
 return { status = "Unknown", message = "StorageCluster is in an unknown state." }
