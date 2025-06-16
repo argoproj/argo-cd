30c325d952d6a23686ba24039a893a0c49a91256
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-06-16 16:29:45 -0400

    chore(refactor): simplify maps util (#23431)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- applicationset/utils/map_test.go
+++ applicationset/utils/map_test.go
@@ -15,28 +15,28 @@ func TestCombineStringMaps(t *testing.T) {
 		name        string
 		left        map[string]any
 		right       map[string]any
-		expected    map[string]string
+		expected    map[string]any
 		expectedErr error
 	}{
 		{
 			name:        "combines the maps",
 			left:        map[string]any{"foo": "bar"},
 			right:       map[string]any{"a": "b"},
-			expected:    map[string]string{"a": "b", "foo": "bar"},
+			expected:    map[string]any{"a": "b", "foo": "bar"},
 			expectedErr: nil,
 		},
 		{
 			name:        "fails if keys are the same but value isn't",
 			left:        map[string]any{"foo": "bar", "a": "fail"},
 			right:       map[string]any{"a": "b", "c": "d"},
-			expected:    map[string]string{"a": "b", "foo": "bar"},
+			expected:    map[string]any{"a": "b", "foo": "bar"},
 			expectedErr: errors.New("found duplicate key a with different value, a: fail ,b: b"),
 		},
 		{
 			name:        "pass if keys & values are the same",
 			left:        map[string]any{"foo": "bar", "a": "b"},
 			right:       map[string]any{"a": "b", "c": "d"},
-			expected:    map[string]string{"a": "b", "c": "d", "foo": "bar"},
+			expected:    map[string]any{"a": "b", "c": "d", "foo": "bar"},
 			expectedErr: nil,
 		},
 	}
