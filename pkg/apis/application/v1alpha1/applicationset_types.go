02e76105dda7ead4e44bd32b2eb9c3d0dc101526
Kanika Rana <krana@redhat.com>
2025-06-23 14:13:25 -0400

    move deletionOrder above comment
    Signed-off-by: Kanika Rana <krana@redhat.com>

--- pkg/apis/application/v1alpha1/applicationset_types.go
+++ pkg/apis/application/v1alpha1/applicationset_types.go
@@ -83,10 +83,10 @@ type ApplicationPreservedFields struct {
 
 // ApplicationSetStrategy configures how generated Applications are updated in sequence.
 type ApplicationSetStrategy struct {
-	Type        string                         `json:"type,omitempty" protobuf:"bytes,1,opt,name=type"`
-	RollingSync *ApplicationSetRolloutStrategy `json:"rollingSync,omitempty" protobuf:"bytes,2,opt,name=rollingSync"`
+	Type          string                         `json:"type,omitempty" protobuf:"bytes,1,opt,name=type"`
+	RollingSync   *ApplicationSetRolloutStrategy `json:"rollingSync,omitempty" protobuf:"bytes,2,opt,name=rollingSync"`
+	DeletionOrder string                         `json:"deletionOrder,omitempty" protobuf:"bytes,3,opt,name=deletionOrder"`
 	// RollingUpdate *ApplicationSetRolloutStrategy `json:"rollingUpdate,omitempty" protobuf:"bytes,3,opt,name=rollingUpdate"`
-	DeletionOrder string `json:"deletionOrder,omitempty" protobuf:"bytes,3,opt,name=deletionOrder"`
 }
 type ApplicationSetRolloutStrategy struct {
 	Steps []ApplicationSetRolloutStep `json:"steps,omitempty" protobuf:"bytes,1,opt,name=steps"`
