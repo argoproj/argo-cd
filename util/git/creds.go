e8bd0a9e691c8ad02b324d9cbb37f5d39236d435
Mike Bordon <mikebordon@gmail.com>
2025-07-01 17:59:59 -0500

    Send Azure DevOps token via git extra headers
    Signed-off-by: Mike Bordon <mikebordon@gmail.com>

--- util/git/creds.go
+++ util/git/creds.go
@@ -686,13 +686,11 @@ func (c GoogleCloudCreds) getAccessToken() (string, error) {
 var _ Creds = AzureWorkloadIdentityCreds{}
 
 type AzureWorkloadIdentityCreds struct {
-	store         CredsStore
 	tokenProvider workloadidentity.TokenProvider
 }
 
-func NewAzureWorkloadIdentityCreds(store CredsStore, tokenProvider workloadidentity.TokenProvider) AzureWorkloadIdentityCreds {
+func NewAzureWorkloadIdentityCreds(tokenProvider workloadidentity.TokenProvider) AzureWorkloadIdentityCreds {
 	return AzureWorkloadIdentityCreds{
-		store:         store,
 		tokenProvider: tokenProvider,
 	}
 }
@@ -708,13 +706,10 @@ func (creds AzureWorkloadIdentityCreds) Environ() (io.Closer, []string, error) {
 	if err != nil {
 		return NopCloser{}, nil, err
 	}
-	nonce := creds.store.Add("", token)
-	env := creds.store.Environ(nonce)
 
-	return utilio.NewCloser(func() error {
-		creds.store.Remove(nonce)
-		return nil
-	}), env, nil
+	var env []string
+	env = append(env, fmt.Sprintf("%s=Authorization: Bearer %s", bearerAuthHeaderEnv, token))
+	return NopCloser{}, env, nil
 }
 
 func (creds AzureWorkloadIdentityCreds) getAccessToken(scope string) (string, error) {
