90e5e3a40eaada4687773a358631fbf762c986c2
Regina Voloshin <regina.voloshin@codefresh.io>
2025-07-28 00:31:59 +0300

    added common disable sync otpion (#749)
    Signed-off-by: reggie-k <regina.voloshin@codefresh.io>

--- pkg/sync/common/types.go
+++ pkg/sync/common/types.go
@@ -28,6 +28,8 @@ const (
 	SyncOptionPruneLast = "PruneLast=true"
 	// Sync option that enables use of replace or create command instead of apply
 	SyncOptionReplace = "Replace=true"
+	// Sync option that disables use of replace or create command instead of apply
+	SyncOptionDisableReplace = "Replace=false"
 	// Sync option that enables use of --force flag, delete and re-create
 	SyncOptionForce = "Force=true"
 	// Sync option that enables use of --server-side flag instead of client-side
@@ -38,6 +40,8 @@ const (
 	SyncOptionDisableDeletion = "Delete=false"
 	// Sync option that sync only out of sync resources
 	SyncOptionApplyOutOfSyncOnly = "ApplyOutOfSyncOnly=true"
+	// Sync option that disables sync only out of sync resources
+	SyncOptionDisableApplyOutOfSyncOnly = "ApplyOutOfSyncOnly=false"
 	// Sync option that requires confirmation before deleting the resource
 	SyncOptionDeleteRequireConfirm = "Delete=confirm"
 	// Sync option that requires confirmation before deleting the resource
