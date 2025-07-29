020528c81ca5ac49fdbcb821d9663d868a33c25d
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2025-07-29 13:31:16 -0700

    sanitize ExecProviderConfig from cluster
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- pkg/apis/application/v1alpha1/types.go
+++ pkg/apis/application/v1alpha1/types.go
@@ -2235,14 +2235,6 @@ type Cluster struct {
 }
 
 func (c *Cluster) Sanitized() *Cluster {
-	var execProviderConfig *ExecProviderConfig
-	if c.Config.ExecProviderConfig != nil {
-		execProviderConfig = &ExecProviderConfig{
-			Command:     c.Config.ExecProviderConfig.Command,
-			APIVersion:  c.Config.ExecProviderConfig.APIVersion,
-			InstallHint: c.Config.ExecProviderConfig.InstallHint,
-		}
-	}
 	return &Cluster{
 		ID:                 c.ID,
 		Server:             c.Server,
@@ -2258,7 +2250,6 @@ func (c *Cluster) Sanitized() *Cluster {
 		Info:               c.Info,
 		RefreshRequestedAt: c.RefreshRequestedAt,
 		Config: ClusterConfig{
-			ExecProviderConfig: execProviderConfig,
 			AWSAuthConfig:      c.Config.AWSAuthConfig,
 			ProxyUrl:           c.Config.ProxyUrl,
 			DisableCompression: c.Config.DisableCompression,
