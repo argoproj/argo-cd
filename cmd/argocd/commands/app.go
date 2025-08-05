93a5dc40ef78dd0fb6d1902be7b2505b83ada1aa
Papapetrou Patroklos <1743100+ppapapetrou76@users.noreply.github.com>
2025-08-05 12:27:18 +0300

    fix: improves message for diff-exit-code configuration option (#24039)
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>
    Signed-off-by: Mangaal <angommeeteimangaal@gmail.com>

--- cmd/argocd/commands/app.go
+++ cmd/argocd/commands/app.go
@@ -1407,7 +1407,7 @@ func NewApplicationDiffCommand(clientOpts *argocdclient.ClientOptions) *cobra.Co
 	command.Flags().BoolVar(&refresh, "refresh", false, "Refresh application data when retrieving")
 	command.Flags().BoolVar(&hardRefresh, "hard-refresh", false, "Refresh application data as well as target manifests cache")
 	command.Flags().BoolVar(&exitCode, "exit-code", true, "Return non-zero exit code when there is a diff. May also return non-zero exit code if there is an error.")
-	command.Flags().IntVar(&diffExitCode, "diff-exit-code", 1, "Return specified exit code when there is a diff. Typical error code is 20.")
+	command.Flags().IntVar(&diffExitCode, "diff-exit-code", 1, "Return specified exit code when there is a diff. Typical error code is 20 but use another exit code if you want to differentiate from the generic exit code (20) returned by all CLI commands.")
 	command.Flags().StringVar(&local, "local", "", "Compare live app to a local manifests")
 	command.Flags().StringVar(&revision, "revision", "", "Compare live app to a particular revision")
 	command.Flags().StringVar(&localRepoRoot, "local-repo-root", "/", "Path to the repository root. Used together with --local allows setting the repository root")
