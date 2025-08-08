af724a6d23416dcc346efa05167920a1cad1cf7b
Yohan Belval <ybelval@genetec.com>
2025-08-08 11:27:37 -0400

    feat(azure): allow setting azure cloud (gov, china) for authentication
    Signed-off-by: Yohan Belval <ybelval@genetec.com>

--- util/workloadidentity/workloadidentity_cgo.go
+++ util/workloadidentity/workloadidentity_cgo.go
@@ -6,16 +6,38 @@ import (
 	"context"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	azcloud "github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type WorkloadIdentityTokenProvider struct {
 	tokenCredential azcore.TokenCredential
 }
 
-func NewWorkloadIdentityTokenProvider() TokenProvider {
-	cred, err := azidentity.NewDefaultAzureCredential(&azidentity.DefaultAzureCredentialOptions{})
+func NewWorkloadIdentityTokenProvider(azureCloud string) TokenProvider {
+	cloud := azcloud.AzurePublic
+	switch azureCloud {
+	case "AzureChina":
+		log.Info("Using Azure China cloud for Workload Identity")
+		cloud = azcloud.AzureChina
+	case "AzureGovernment":
+		log.Info("Using Azure Government cloud for Workload Identity")
+		cloud = azcloud.AzureGovernment
+	}
+
+	if azureCloud != "" && azureCloud != "AzurePublic" && azureCloud != "AzureChina" && azureCloud != "AzureGovernment" {
+		log.Warnf("Could not parse Azure cloud '%s'. Possible values are: AzurePublic, AzureChina, AzureGovernment", azureCloud)
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(
+		&azidentity.DefaultAzureCredentialOptions{
+			ClientOptions: policy.ClientOptions{
+				Cloud: cloud,
+			},
+		})
 	initError = err
 	return WorkloadIdentityTokenProvider{tokenCredential: cred}
 }
