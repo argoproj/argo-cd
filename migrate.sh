5806df1ba9a4e6295f89963304209fd28ddc4807
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-08-14 12:58:23 +0300

    fix migrate script
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 mkdir gitops-engine
 find . -type d \( -path ./gitops-engine -o -path ./.git -o -path ./.github -o -path ./.gitignore \) -prune -o -type f \( -path ./.gitignore -o -path ./.golangci.yaml -o -path ./codecov.yml -o -path ./sonar-project.properties -o -path . \) -prune -o -name '*' -maxdepth 1 -exec echo {} \;
