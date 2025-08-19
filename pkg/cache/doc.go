822d0daef7092f296c14f2cd3b9457f736de5862
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-08-19 14:54:29 +0300

    initial migrate commit

--- pkg/cache/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Package cache implements lightweight Kubernetes cluster caching that stores only resource references and ownership
-references. In addition to references cache might be configured to store custom metadata and whole body of selected
-resources.
-
-The library uses Kubernetes watch API to maintain cache up to date. This approach reduces number of Kubernetes
-API requests and provides instant access to the required Kubernetes resources.
-*/
-package cache
--- pkg/sync/hook/helm/hook.go
+++
@@ -1,9 +0,0 @@
-package helm
-
-import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-
-func IsHook(obj *unstructured.Unstructured) bool {
-	value, ok := obj.GetAnnotations()["helm.sh/hook"]
-	// Helm use the same annotation to identify CRD as hooks, but they are not.
-	return ok && value != "crd-install"
-}
