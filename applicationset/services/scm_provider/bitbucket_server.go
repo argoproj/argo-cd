9449a32f5f1ce2546159ace7fe6dcde9011280af
portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>
2025-08-20 13:36:31 +0200

    Use a clone of the defaultTransport in the httpClient of BitBucket Servers scm / pullrequest generator. This must close idle connections by default, to solve #24209
    Signed-off-by: portly-halicore-76 <170707699+portly-halicore-76@users.noreply.github.com>

--- applicationset/services/scm_provider/bitbucket_server.go
+++ applicationset/services/scm_provider/bitbucket_server.go
@@ -51,9 +51,8 @@ func NewBitbucketServerProviderNoAuth(ctx context.Context, url, projectKey strin
 func newBitbucketServerProvider(ctx context.Context, bitbucketConfig *bitbucketv1.Configuration, projectKey string, allBranches bool, scmRootCAPath string, insecure bool, caCerts []byte) (*BitbucketServerProvider, error) {
 	bitbucketConfig.BasePath = utils.NormalizeBitbucketBasePath(bitbucketConfig.BasePath)
 	tlsConfig := utils.GetTlsConfig(scmRootCAPath, insecure, caCerts)
-	bitbucketConfig.HTTPClient = &http.Client{Transport: &http.Transport{
-		TLSClientConfig: tlsConfig,
-	}}
+	bitbucketConfig.HTTPClient = &http.Client{Transport: http.DefaultTransport.(*http.Transport).Clone()}
+	bitbucketConfig.HTTPClient.Transport.(*http.Transport).TLSClientConfig = tlsConfig
 	bitbucketClient := bitbucketv1.NewAPIClient(ctx, bitbucketConfig)
 
 	return &BitbucketServerProvider{
