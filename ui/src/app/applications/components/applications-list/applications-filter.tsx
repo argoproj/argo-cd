dfaf618e6743ebb9ca0719527cee9e1546dc2e46
downfa11 <downfa11@naver.com>
2025-08-22 17:18:34 +0900

    fix(ui): Correctly display auto-sync status based on `enabled` field
    Signed-off-by: downfa11 <downfa11@naver.com>

--- ui/src/app/applications/components/applications-list/applications-filter.tsx
+++ ui/src/app/applications/components/applications-list/applications-filter.tsx
@@ -26,7 +26,7 @@ export interface FilteredApp extends Application {
 }
 
 function getAutoSyncStatus(syncPolicy?: SyncPolicy) {
-    if (!syncPolicy || !syncPolicy.automated) {
+    if (!syncPolicy || !syncPolicy.automated || syncPolicy.automated.enabled === false) {
         return 'Disabled';
     }
     return 'Enabled';
