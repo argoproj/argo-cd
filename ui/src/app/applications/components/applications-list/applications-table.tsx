c4ae2b57a697d77e185246d60c24d32f465ca9ce
Surajyadav <harrypotter1108@gmail.com>
2025-08-25 17:25:22 +0530

    bolder-font
    Signed-off-by: Surajyadav <harrypotter1108@gmail.com>

--- ui/src/app/applications/components/applications-list/applications-table.tsx
+++ ui/src/app/applications/components/applications-list/applications-table.tsx
@@ -84,13 +84,8 @@ export const ApplicationsTable = (props: {
                                                             <ApplicationURLs urls={app.status.summary.externalURLs} />
                                                         </div>
                                                     </div>
-                                                    <div className='show-for-xxlarge columns small-4'>Project:</div>
-                                                    <div className='columns small-12 xxlarge-6'>{app.spec.project}</div>
-                                                </div>
-                                                <div className='row'>
-                                                    <div className=' columns small-2' />
-                                                    <div className='show-for-xxlarge columns small-4'>Name:</div>
-                                                    <div className='columns small-12 xxlarge-6'>
+                                                    <div className='show-for-xxlrage columns small-4'>Name:</div>
+                                                    <div className='colums small-12  xxlarge-6'>
                                                         <Tooltip
                                                             content={
                                                                 <>
@@ -101,9 +96,14 @@ export const ApplicationsTable = (props: {
                                                                     </Moment>
                                                                 </>
                                                             }>
-                                                            <span>{app.metadata.name}</span>
+                                                            <span className='application-name'>{app.metadata.name}</span>
                                                         </Tooltip>
                                                     </div>
+                                                </div>
+                                                <div className='row'>
+                                                    <div className=' columns small-2' />
+                                                    <div className='show-for-xxlarge columns small-4'>Project:</div>
+                                                    <div className='columns small-12 xxlarge-6'>{app.spec.project}</div>
                                                 </div>
                                             </div>
 
