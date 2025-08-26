0ec322ebe990037c7a2edaa3416dbd945d817737
nitishfy <justnitish06@gmail.com>
2025-08-26 12:05:51 +0530

    ignore old renovate.json
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- .github/configs/renovate-config.js
+++ .github/configs/renovate-config.js
@@ -3,5 +3,14 @@ module.exports = {
     gitAuthor: 'renovate[bot] <renovate[bot]@users.noreply.github.com>',
     autodiscover: false,
     allowPostUpgradeCommandTemplating: true,
-    allowedPostUpgradeCommands: [".*"],
+    allowedPostUpgradeCommands: ["make mockgen"],
+    // Presets from the old renovate.json
+    extends: [
+        "github>argoproj/argo-cd//renovate-presets/commons.json5",
+        "github>argoproj/argo-cd//renovate-presets/custom-managers/shell.json5",
+        "github>argoproj/argo-cd//renovate-presets/custom-managers/yaml.json5",
+        "github>argoproj/argo-cd//renovate-presets/fix/disable-all-updates.json5",
+        "github>argoproj/argo-cd//renovate-presets/devtool.json5",
+        "github>argoproj/argo-cd//renovate-presets/docs.json5"
+    ]
 }
