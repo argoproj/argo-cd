06125ceb9941556ee3a4598a9124cbbea809968a
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-08-27 11:24:06 -0400

    adjust template and test (#1)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- util/hydrator/template_test.go
+++ util/hydrator/template_test.go
@@ -9,101 +9,105 @@ import (
 	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
 )
 
-var commitMessageTemplate = `{{- if .metadata }}
-        {{- if .metadata.repoURL }}
-            repoURL: {{ .metadata.repoURL }}
-        {{- end }}
-        
-        {{- if .metadata.drySha }}
-            drySha: {{ .metadata.drySha }}
-        {{- end }}
+var commitMessageTemplate = `{{ .metadata.subject }}
+{{- if .metadata.body }}
 
-        {{- if .metadata.author }}
-            Co-authored-by: {{ .metadata.author }}
-        {{- end }}
-
-        {{- if .metadata.subject }}
-            subject: {{ .metadata.subject }}
-        {{- end }}
-
-        {{- if .metadata.body }}
-            body: {{ .metadata.body }}
-        {{- end }}
-        {{- if .metadata.references }}
-            References:
-            {{- range $reference := .metadata.references }}
-                {{- if kindIs "map" $reference.commit }}
-                    Commit:
-                    {{- range $key, $value := $reference.commit }}
-                        {{- if eq $key "author" }}
-                            Co-authored-by: {{ $value }}
-                        {{- end }}
-                    {{- end }}
-                {{- end }}
-            {{- end }}
-        {{- end }}
-    {{- end }} 
+{{ .metadata.body }}
+{{- end }}
+{{ range $ref := .metadata.references }}
+{{- if and $ref.commit $ref.commit.author }}
+Co-authored-by: {{ $ref.commit.author }}
+{{- end }}
+{{- end }}
+{{- if .metadata.author }}
+Co-authored-by: {{ .metadata.author }}
+{{- end }}
 `
 
 func TestRender(t *testing.T) {
-	type args struct {
-		tmpl string
-		data HydratorCommitMetadata
-	}
-
-	references := make([]v1alpha1.RevisionReference, 0)
-	revReference := v1alpha1.RevisionReference{
-		Commit: &v1alpha1.CommitMetadata{
-			Author:  "testAuthor",
-			Subject: "test",
-			RepoURL: "https://github.com/test/argocd-example-apps",
-			SHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97c",
-		},
-	}
-	references = append(references, revReference)
-
-	commitMetadata := HydratorCommitMetadata{
-		RepoURL:    "https://github.com/test/argocd-example-apps",
-		DrySHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97d",
-		Author:     "test test@test.com",
-		Date:       metav1.Now().String(),
-		References: references,
-		Body:       "testBody",
-		Subject:    "testSubject",
-	}
 	tests := []struct {
 		name    string
-		args    args
+		metadata HydratorCommitMetadata
 		want    string
 		wantErr bool
 	}{
 		{
-			name: "test render",
-			args: args{
-				tmpl: commitMessageTemplate,
-				data: commitMetadata,
+			name: "author and multiple references",
+			metadata: HydratorCommitMetadata{
+				RepoURL:    "https://github.com/test/argocd-example-apps",
+				DrySHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97d",
+				Author:     "test <test@test.com>",
+				Date:       metav1.Now().String(),
+				References: []v1alpha1.RevisionReference{
+					{
+						Commit: &v1alpha1.CommitMetadata{
+							Author:  "ref test <ref-test@test.com>",
+							Subject: "test",
+							RepoURL: "https://github.com/test/argocd-example-apps",
+							SHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97c",
+						},
+					},
+					{
+						Commit: &v1alpha1.CommitMetadata{
+							Author:  "ref test 2 <ref-test-2@test.com>",
+							Subject: "test 2",
+							RepoURL: "https://github.com/test/argocd-example-apps",
+							SHA:     "abc12345678912345678912345678912345678912",
+						},
+					},
+				},
+				Body:       "testBody",
+				Subject:    "testSubject",
 			},
+			want:    `testSubject
+
+testBody
+
+Co-authored-by: ref test <ref-test@test.com>
+Co-authored-by: ref test 2 <ref-test-2@test.com>
+Co-authored-by: test <test@test.com>
+`,
 		},
 		{
-			name: "test render with empty template",
-			args: args{
-				data: commitMetadata,
+			name: "no references",
+			metadata: HydratorCommitMetadata{
+				RepoURL:    "https://github.com/test/argocd-example-apps",
+				DrySHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97d",
+				Author:     "test <test@test.com>",
+				Date:       metav1.Now().String(),
+				Body:       "testBody",
+				Subject:    "testSubject",
 			},
+			want: `testSubject
+
+testBody
+
+Co-authored-by: test <test@test.com>
+`,
+		},
+		{
+			name: "no body",
+			metadata: HydratorCommitMetadata{
+				RepoURL:    "https://github.com/test/argocd-example-apps",
+				DrySHA:     "3ff41cc5247197a6caf50216c4c76cc29d78a97d",
+				Author:     "test <test@test.com>",
+				Date:       metav1.Now().String(),
+				Subject:    "testSubject",
+			},
+			want: `testSubject
+
+Co-authored-by: test <test@test.com>
+`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Render(tt.args.tmpl, tt.args.data)
+			got, err := Render(commitMessageTemplate, tt.metadata)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if tt.args.tmpl != "" {
-				assert.NotEmpty(t, got)
-				assert.Contains(t, got, "Commit")
-			} else {
-				assert.Empty(t, got)
-			}
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
