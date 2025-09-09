87901724f4dd00b2eec2c42c0a7d2f0b8002dbfb
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-09-09 15:40:37 +0300

    fix linter issue
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- hack/migrate-gitops-engine/merge.sh
+++ hack/migrate-gitops-engine/merge.sh
@@ -6,21 +6,21 @@ echo "Please provide the following information:"
 echo
 
 # Prompt for GitHub handle
-read -p "Enter your GitHub handle: " github_handle
+read -r -p "Enter your GitHub handle: " github_handle
 
 # Validate that GitHub handle is not empty
 while [[ -z "$github_handle" ]]; do
     echo "GitHub handle cannot be empty."
-    read -p "Enter your GitHub handle: " github_handle
+    read -r -p "Enter your GitHub handle: " github_handle
 done
 
 # Prompt for branch name
-read -p "Enter the branch name: " branch_name
+read -r -p "Enter the branch name: " branch_name
 
 # Validate that branch name is not empty
 while [[ -z "$branch_name" ]]; do
     echo "Branch name cannot be empty."
-    read -p "Enter the branch name: " branch_name
+    read -r -p "Enter the branch name: " branch_name
 done
 
 echo "Merging migrated gitops-engine branch $branch_name from $github_handle/gitops-engine.git"
