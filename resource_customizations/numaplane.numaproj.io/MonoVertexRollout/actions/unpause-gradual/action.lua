2784b16d639700be39788eda5c49027e4f86eb61
Dillen Padhiar <dillen_padhiar@intuit.com>
2025-09-15 10:15:32 -0700

    feat: remove change to make replicas null for monovertex
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- resource_customizations/numaplane.numaproj.io/MonoVertexRollout/actions/unpause-gradual/action.lua
+++ resource_customizations/numaplane.numaproj.io/MonoVertexRollout/actions/unpause-gradual/action.lua
@@ -1,5 +1,4 @@
 obj.spec.monoVertex.spec.lifecycle.desiredPhase = "Running"
-obj.spec.monoVertex.spec.replicas = null
 -- set strategy.fastResume to false
 if obj.spec.strategy == nil then
     obj.spec.strategy = {}
