0df39f7e2c30b50bdc679f6653b18967e745ca7d
Talia Wong <blowfishpro@users.noreply.github.com>
2025-09-25 18:04:42 -0700

    feat(GitGenerator): use paramPrefix to namespace file content
    If the git generator's `paramPrefix` is set, then content included from files will be namespaced under it, allowing ApplicationSets to reference it without having to deal with potential conflicts from path params, values, or other generators in the case of mmatrix/merge generators.
    
    As in the previous commit there is some duplicate test coverage based on the existing test structure.
    
    Some edge cases are not (yet) handled, in particular the behavior when the specified `paramPrefix` creates conflicts with the path params, either because `paramPrefix` is set to `path` or because `paramPrefix` is set to the same value as `pathParamPrefix`.  In the non go template case it will merge them, but in the go template case the file content will be overwritten with the path parameters.  Note that there already exists a similar edge case when the file content contains the key `path` or a key with the sam
    e name as the value of `pathParamPrefix`.  The desired behavior doesn't seem obvious to me, there are a couple of options:
    (1) Check for these conflicts and disallow them
    (2) Attempt to deeply merge any overlapping namespaces (how do we handle attempting to merge with something that is not a mapping?).
    
    Signed-off-by: Talia Wong <blowfishpro@users.noreply.github.com>

--- applicationset/generators/git_test.go
+++ applicationset/generators/git_test.go
@@ -28,6 +28,7 @@ foo:
 		useGoTemplate     bool
 		goTemplateOptions []string
 		pathParamPrefix   string
+		paramPrefix       string
 	}
 	tests := []struct {
 		name    string
@@ -109,6 +110,28 @@ foo:
 			},
 		},
 		{
+			name: "params are prefixed",
+			args: args{
+				filePath:      "path/dir/file_name.yaml",
+				fileContent:   defaultContent,
+				values:        map[string]string{},
+				useGoTemplate: false,
+				paramPrefix:   "myRepo",
+			},
+			want: []map[string]any{
+				{
+					"myRepo.foo.bar":          "baz",
+					"path":                    "path/dir",
+					"path.basename":           "dir",
+					"path.filename":           "file_name.yaml",
+					"path.basenameNormalized": "dir",
+					"path.filenameNormalized": "file-name.yaml",
+					"path[0]":                 "path",
+					"path[1]":                 "dir",
+				},
+			},
+		},
+		{
 			name: "file parameters are added to params with go template",
 			args: args{
 				filePath:      "path/dir/file_name.yaml",
@@ -165,10 +188,40 @@ foo:
 				},
 			},
 		},
+		{
+			name: "parameters are prefixed with go template",
+			args: args{
+				filePath:      "path/dir/file_name.yaml",
+				fileContent:   defaultContent,
+				values:        map[string]string{},
+				useGoTemplate: true,
+				paramPrefix:   "myRepo",
+			},
+			want: []map[string]any{
+				{
+					"myRepo": map[string]any{
+						"foo": map[string]any{
+							"bar": "baz",
+						},
+					},
+					"path": map[string]any{
+						"path":               "path/dir",
+						"basename":           "dir",
+						"filename":           "file_name.yaml",
+						"basenameNormalized": "dir",
+						"filenameNormalized": "file-name.yaml",
+						"segments": []string{
+							"path",
+							"dir",
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			params, err := (*GitGenerator)(nil).generateParamsFromGitFile(tt.args.filePath, tt.args.fileContent, tt.args.values, tt.args.useGoTemplate, tt.args.goTemplateOptions, tt.args.pathParamPrefix)
+			params, err := (*GitGenerator)(nil).generateParamsFromGitFile(tt.args.filePath, tt.args.fileContent, tt.args.values, tt.args.useGoTemplate, tt.args.goTemplateOptions, tt.args.pathParamPrefix, tt.args.paramPrefix)
 			if tt.wantErr {
 				assert.Error(t, err, "GitGenerator.generateParamsFromGitFile()")
 			} else {
@@ -679,6 +732,7 @@ func TestGitGenerateParamsFromFiles(t *testing.T) {
 		repoPathsError  error
 		values          map[string]string
 		pathParamPrefix string
+		paramPrexix     string
 		expected        []map[string]any
 		expectedError   error
 	}{
@@ -1054,6 +1108,67 @@ cluster:
 			},
 			expectedError: nil,
 		},
+		{
+			name:  "parameter prefix",
+			files: []v1alpha1.GitFileGeneratorItem{{Path: "**/config.json"}},
+			repoFileContents: map[string][]byte{
+				"cluster-config/production/config.json": []byte(`{
+   "cluster": {
+       "owner": "john.doe@example.com",
+       "name": "production",
+       "address": "https://kubernetes.default.svc"
+   },
+   "key1": "val1",
+   "key2": {
+       "key2_1": "val2_1",
+       "key2_2": {
+           "key2_2_1": "val2_2_1"
+       }
+   },
+   "key3": 123
+}`),
+				"cluster-config/staging/config.json": []byte(`{
+   "cluster": {
+       "owner": "foo.bar@example.com",
+       "name": "staging",
+       "address": "https://kubernetes.default.svc"
+   }
+}`),
+			},
+			repoPathsError: nil,
+			paramPrexix:    "myRepo",
+			expected: []map[string]any{
+				{
+					"myRepo.cluster.owner":        "john.doe@example.com",
+					"myRepo.cluster.name":         "production",
+					"myRepo.cluster.address":      "https://kubernetes.default.svc",
+					"myRepo.key1":                 "val1",
+					"myRepo.key2.key2_1":          "val2_1",
+					"myRepo.key2.key2_2.key2_2_1": "val2_2_1",
+					"myRepo.key3":                 "123",
+					"path":                        "cluster-config/production",
+					"path.basename":               "production",
+					"path[0]":                     "cluster-config",
+					"path[1]":                     "production",
+					"path.basenameNormalized":     "production",
+					"path.filename":               "config.json",
+					"path.filenameNormalized":     "config.json",
+				},
+				{
+					"myRepo.cluster.owner":    "foo.bar@example.com",
+					"myRepo.cluster.name":     "staging",
+					"myRepo.cluster.address":  "https://kubernetes.default.svc",
+					"path":                    "cluster-config/staging",
+					"path.basename":           "staging",
+					"path[0]":                 "cluster-config",
+					"path[1]":                 "staging",
+					"path.basenameNormalized": "staging",
+					"path.filename":           "config.json",
+					"path.filenameNormalized": "config.json",
+				},
+			},
+			expectedError: nil,
+		},
 	}
 
 	for _, testCase := range cases {
@@ -1079,6 +1194,7 @@ cluster:
 							Files:           testCaseCopy.files,
 							Values:          testCaseCopy.values,
 							PathParamPrefix: testCaseCopy.pathParamPrefix,
+							ParamPrefix:     testCaseCopy.paramPrexix,
 						},
 					}},
 				},
@@ -2039,6 +2155,7 @@ func TestGitGenerateParamsFromFilesGoTemplate(t *testing.T) {
 		// if repoPathsError is non-nil, the call to GetPaths(...) will return this error value
 		repoPathsError  error
 		pathParamPrefix string
+		paramPrefix     string
 		expected        []map[string]any
 		expectedError   error
 	}{
@@ -2416,6 +2533,87 @@ cluster:
 			},
 			expectedError: nil,
 		},
+		{
+			name:  "param prefix",
+			files: []v1alpha1.GitFileGeneratorItem{{Path: "**/config.json"}},
+			repoFileContents: map[string][]byte{
+				"cluster-config/production/config.json": []byte(`{
+   "cluster": {
+       "owner": "john.doe@example.com",
+       "name": "production",
+       "address": "https://kubernetes.default.svc"
+   },
+   "key1": "val1",
+   "key2": {
+       "key2_1": "val2_1",
+       "key2_2": {
+           "key2_2_1": "val2_2_1"
+       }
+   },
+   "key3": 123
+}`),
+				"cluster-config/staging/config.json": []byte(`{
+   "cluster": {
+       "owner": "foo.bar@example.com",
+       "name": "staging",
+       "address": "https://kubernetes.default.svc"
+   }
+}`),
+			},
+			repoPathsError: nil,
+			paramPrefix:    "myRepo",
+			expected: []map[string]any{
+				{
+					"myRepo": map[string]any{
+						"cluster": map[string]any{
+							"owner":   "john.doe@example.com",
+							"name":    "production",
+							"address": "https://kubernetes.default.svc",
+						},
+						"key1": "val1",
+						"key2": map[string]any{
+							"key2_1": "val2_1",
+							"key2_2": map[string]any{
+								"key2_2_1": "val2_2_1",
+							},
+						},
+						"key3": float64(123),
+					},
+					"path": map[string]any{
+						"path":               "cluster-config/production",
+						"basename":           "production",
+						"filename":           "config.json",
+						"basenameNormalized": "production",
+						"filenameNormalized": "config.json",
+						"segments": []string{
+							"cluster-config",
+							"production",
+						},
+					},
+				},
+				{
+					"myRepo": map[string]any{
+						"cluster": map[string]any{
+							"owner":   "foo.bar@example.com",
+							"name":    "staging",
+							"address": "https://kubernetes.default.svc",
+						},
+					},
+					"path": map[string]any{
+						"path":               "cluster-config/staging",
+						"basename":           "staging",
+						"filename":           "config.json",
+						"basenameNormalized": "staging",
+						"filenameNormalized": "config.json",
+						"segments": []string{
+							"cluster-config",
+							"staging",
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
 	}
 
 	for _, testCase := range cases {
@@ -2441,6 +2639,7 @@ cluster:
 							Revision:        "Revision",
 							Files:           testCaseCopy.files,
 							PathParamPrefix: testCaseCopy.pathParamPrefix,
+							ParamPrefix:     testCaseCopy.paramPrefix,
 						},
 					}},
 				},
