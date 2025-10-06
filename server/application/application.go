1eada1ea4b810eb35f27488487f9057396161bad
Rianov Viacheslav <55545103+vr009@users.noreply.github.com>
2025-10-06 22:11:51 +0300

    fix(server): ensure resource health status is inferred on application retrieval (#24832) (#24851)
    Signed-off-by: Viacheslav Rianov <rianovviacheslav@gmail.com>

--- server/application/application.go
+++ server/application/application.go
@@ -777,9 +777,8 @@ func (s *Server) Get(ctx context.Context, q *application.ApplicationQuery) (*v1a
 		return nil, err
 	}
 
-	s.inferResourcesStatusHealth(a)
-
 	if q.Refresh == nil {
+		s.inferResourcesStatusHealth(a)
 		return a, nil
 	}
 
@@ -862,7 +861,9 @@ func (s *Server) Get(ctx context.Context, q *application.ApplicationQuery) (*v1a
 					annotations = make(map[string]string)
 				}
 				if _, ok := annotations[v1alpha1.AnnotationKeyRefresh]; !ok {
-					return event.Application.DeepCopy(), nil
+					refreshedApp := event.Application.DeepCopy()
+					s.inferResourcesStatusHealth(refreshedApp)
+					return refreshedApp, nil
 				}
 			}
 		}
