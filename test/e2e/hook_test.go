5fae1b8e9b4887ebbad7fb5f4c66a108be0357f0
Pedro Ribeiro <pedro.ribeiro@cross-join.com>
2025-10-08 12:01:12 +0100

    fix(test): remove unused import
    Signed-off-by: Pedro Ribeiro <pedro.ribeiro@cross-join.com>

--- test/e2e/hook_test.go
+++ test/e2e/hook_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
