bdccc297e4b6936604ac6693cbc8279ca4392c74
Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@ledger.fr>
2025-10-10 16:32:31 +0200

    chore: add utils to get sync option value
    This will facilitate adding logic on sync options that can be defined
    both at the app and resource level and the resource level should
    override the app level option.
    
    Also fix GetAnnotationCSVs to preserve the order returned (as map in
    golang doesn't preserve the insertion order) to make dedup works in
    GetOptionValue.
    
    Signed-off-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@ledger.fr>

--- gitops-engine/pkg/sync/resource/annotations_test.go
+++ gitops-engine/pkg/sync/resource/annotations_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/utils/ptr"
 
 	testingutils "github.com/argoproj/argo-cd/gitops-engine/pkg/utils/testing"
 )
@@ -32,6 +33,38 @@ func TestHasAnnotationOption(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.ElementsMatch(t, tt.wantVals, GetAnnotationCSVs(tt.args.obj, tt.args.key))
 			assert.Equal(t, tt.want, HasAnnotationOption(tt.args.obj, tt.args.key, tt.args.val))
+		})
+	}
+}
+
+func TestGetAnnotationOptionValue(t *testing.T) {
+	type args struct {
+		obj *unstructured.Unstructured
+		key string
+		val string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *string
+	}{
+		{"Nil", args{testingutils.NewPod(), "foo", "bar"}, nil},
+		{"Empty", args{example(""), "foo", "bar"}, nil},
+		{"Standalone", args{example("bar"), "foo", "bar"}, nil},
+		{"Single", args{example("bar=baz"), "foo", "bar"}, ptr.To("baz")},
+		{"DeDup", args{example("bar=baz1,bar=baz2"), "foo", "bar"}, ptr.To("baz1")},
+		{"Double", args{example("bar=qux,baz=quux"), "foo", "baz"}, ptr.To("quux")},
+		{"Spaces", args{example("bar=baz "), "foo", "bar"}, ptr.To("baz")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetAnnotationOptionValue(tt.args.obj, tt.args.key, tt.args.val)
+			if tt.want == nil {
+				assert.Nil(t, got)
+			} else {
+				assert.NotNil(t, got)
+				assert.Equal(t, *tt.want, *got)
+			}
 		})
 	}
 }
