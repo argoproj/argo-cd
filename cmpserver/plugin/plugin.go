293b04e7bc3235170d7d00cb5711b1f5bc1fa982
titaneric <chenyihuang001@gmail.com>
2025-10-22 00:55:32 +0800

    migrate cmp plugin to `argocdexec` plugin
    Signed-off-by: titaneric <chenyihuang001@gmail.com>

--- cmpserver/plugin/plugin.go
+++ cmpserver/plugin/plugin.go
@@ -1,10 +1,7 @@
 package plugin
 
 import (
-	"bytes"
 	"context"
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -62,16 +59,6 @@ func (s *Service) Init(workDir string) error {
 	return nil
 }
 
-const execIDLen = 5
-
-func randExecID() (string, error) {
-	execIDBytes := make([]byte, execIDLen/2+1) // we need one extra letter to discard
-	if _, err := rand.Read(execIDBytes); err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(execIDBytes)[0:execIDLen], nil
-}
-
 func runCommand(ctx context.Context, command Command, path string, env []string) (string, error) {
 	if len(command.Command) == 0 {
 		return "", errors.New("Command is empty")
@@ -81,87 +68,13 @@ func runCommand(ctx context.Context, command Command, path string, env []string)
 	cmd.Env = env
 	cmd.Dir = path
 
-	execId, err := randExecID()
-	if err != nil {
-		return "", err
-	}
-	logCtx := log.WithFields(log.Fields{"execID": execId})
-
-	argsToLog := argoexec.GetCommandArgsToLog(cmd)
-	logCtx.WithFields(log.Fields{"dir": cmd.Dir}).Info(argsToLog)
-
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	// Make sure the command is killed immediately on timeout. https://stackoverflow.com/a/38133948/684776
-	cmd.SysProcAttr = newSysProcAttr(true)
-
-	start := time.Now()
-	err = cmd.Start()
-	if err != nil {
-		return "", err
+	// Use the argoexec package to run the command with proper ARGOCD_EXEC_TIMEOUT support
+	opts := argoexec.ExecRunOpts{
+		// CaptureStderr is set to true to include stderr in the output for better error reporting
+		CaptureStderr: true,
 	}
 
-	go func() {
-		<-ctx.Done()
-		// Kill by group ID to make sure child processes are killed. The - tells `kill` that it's a group ID.
-		// Since we didn't set Pgid in SysProcAttr, the group ID is the same as the process ID. https://pkg.go.dev/syscall#SysProcAttr
-
-		// Sending a TERM signal first to allow any potential cleanup if needed, and then sending a KILL signal
-		_ = sysCallTerm(-cmd.Process.Pid)
-
-		// modify cleanup timeout to allow process to cleanup
-		cleanupTimeout := 5 * time.Second
-		time.Sleep(cleanupTimeout)
-
-		_ = sysCallKill(-cmd.Process.Pid)
-	}()
-
-	err = cmd.Wait()
-
-	duration := time.Since(start)
-	output := stdout.String()
-
-	logCtx.WithFields(log.Fields{"duration": duration}).Debug(output)
-
-	if err != nil {
-		err := newCmdError(argsToLog, errors.New(err.Error()), strings.TrimSpace(stderr.String()))
-		logCtx.Error(err.Error())
-		return strings.TrimSuffix(output, "\n"), err
-	}
-
-	logCtx = logCtx.WithFields(log.Fields{
-		"stderr":  stderr.String(),
-		"command": command,
-	})
-	if output == "" {
-		logCtx.Warn("Plugin command returned zero output")
-	} else {
-		// Log stderr even on successful commands to help develop plugins
-		logCtx.Info("Plugin command successful")
-	}
-
-	return strings.TrimSuffix(output, "\n"), nil
-}
-
-type CmdError struct {
-	Args   string
-	Stderr string
-	Cause  error
-}
-
-func (ce *CmdError) Error() string {
-	res := fmt.Sprintf("`%v` failed %v", ce.Args, ce.Cause)
-	if ce.Stderr != "" {
-		res = fmt.Sprintf("%s: %s", res, ce.Stderr)
-	}
-	return res
-}
-
-func newCmdError(args string, cause error, stderr string) *CmdError {
-	return &CmdError{Args: args, Stderr: stderr, Cause: cause}
+	return argoexec.RunWithExecRunOpts(cmd, opts)
 }
 
 // Environ returns a list of environment variables in name=value format from a list of variables
