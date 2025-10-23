5682480c14239f42dea6ed2b29cefa32a895b069
nitishfy <justnitish06@gmail.com>
2025-10-23 17:38:44 +0530

    add func
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- gitops-engine/pkg/health/health.go
+++ gitops-engine/pkg/health/health.go
@@ -14,19 +14,19 @@ import (
 type HealthStatusCode string
 
 const (
-	// Indicates that health assessment failed and actual health status is unknown
+	// HealthStatusUnknown Indicates that health assessment failed and actual health status is unknown
 	HealthStatusUnknown HealthStatusCode = "Unknown"
-	// Progressing health status means that resource is not healthy but still have a chance to reach healthy state
+	// HealthStatusProgressing Progressing health status means that resource is not healthy but still have a chance to reach healthy state
 	HealthStatusProgressing HealthStatusCode = "Progressing"
-	// Resource is 100% healthy
+	// HealthStatusHealthy Resource is 100% healthy
 	HealthStatusHealthy HealthStatusCode = "Healthy"
-	// Assigned to resources that are suspended or paused. The typical example is a
+	// HealthStatusSuspended is assigned to resources that are suspended or paused. The typical example is a
 	// [suspended](https://kubernetes.io/docs/tasks/job/automated-tasks-with-cron-jobs/#suspend) CronJob.
 	HealthStatusSuspended HealthStatusCode = "Suspended"
-	// Degrade status is used if resource status indicates failure or resource could not reach healthy state
+	// HealthStatusDegraded  status is used if resource status indicates failure or resource could not reach healthy state
 	// within some timeout.
 	HealthStatusDegraded HealthStatusCode = "Degraded"
-	// Indicates that resource is missing in the cluster.
+	// HealthStatusMissing Indicates that resource is missing in the cluster.
 	HealthStatusMissing HealthStatusCode = "Missing"
 )
 
@@ -35,10 +35,35 @@ type HealthOverride interface {
 	GetResourceHealth(obj *unstructured.Unstructured) (*HealthStatus, error)
 }
 
-// Holds health assessment results
+// HealthStatus holds health assessment results
 type HealthStatus struct {
 	Status  HealthStatusCode `json:"status,omitempty"`
 	Message string           `json:"message,omitempty"`
+}
+
+// healthOrder is a list of health codes in order of most healthy to least healthy
+var healthOrder = []HealthStatusCode{
+	HealthStatusHealthy,
+	HealthStatusSuspended,
+	HealthStatusProgressing,
+	HealthStatusMissing,
+	HealthStatusDegraded,
+	HealthStatusUnknown,
+}
+
+// IsWorse returns whether the new health status code is a worse condition than the current
+func IsWorse(current, new HealthStatusCode) bool {
+	currentIndex := 0
+	newIndex := 0
+	for i, code := range healthOrder {
+		if current == code {
+			currentIndex = i
+		}
+		if new == code {
+			newIndex = i
+		}
+	}
+	return newIndex > currentIndex
 }
 
 // GetResourceHealth returns the health of a k8s resource
