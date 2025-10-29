0a3bf8051381dcf4159eba6b2baa28f36e0367ab
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2025-10-29 18:11:53 +0100

    test(controller): expand test coverage for namespace change detection
    Add comprehensive tests for the controller's ability to detect and
    handle namespace changes during cluster cache synchronization.
    
    Signed-off-by: Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>

--- controller/appcontroller.go
+++ controller/appcontroller.go
@@ -144,9 +144,7 @@ type ApplicationController struct {
 
 	// dynamicClusterDistributionEnabled if disabled deploymentInformer is never initialized
 	dynamicClusterDistributionEnabled bool
-	// enableIncrementalNamespaceSync enables incremental namespace sync in cluster cache
-	enableIncrementalNamespaceSync bool
-	deploymentInformer             informerv1.DeploymentInformer
+	deploymentInformer                informerv1.DeploymentInformer
 
 	hydrator *hydrator.Hydrator
 }
@@ -218,7 +216,6 @@ func NewApplicationController(
 		projByNameCache:                   sync.Map{},
 		applicationNamespaces:             applicationNamespaces,
 		dynamicClusterDistributionEnabled: dynamicClusterDistributionEnabled,
-		enableIncrementalNamespaceSync:    enableIncrementalNamespaceSync,
 		ignoreNormalizerOpts:              ignoreNormalizerOpts,
 		metricsClusterLabels:              metricsClusterLabels,
 	}
