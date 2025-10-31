244ead6be4dd60e79d951d89290b068e40f3d64d
Peter Jiang <35584807+pjiang-dev@users.noreply.github.com>
2025-10-31 10:41:06 -0700

    Update server/application/application.go
    Co-authored-by: Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
    Signed-off-by: Peter Jiang <35584807+pjiang-dev@users.noreply.github.com>

--- server/application/application.go
+++ server/application/application.go
@@ -1771,7 +1771,7 @@ func (s *Server) ManagedResources(ctx context.Context, q *application.ResourcesQ
 		return s.cache.GetAppManagedResources(a.InstanceName(s.ns), &items)
 	})
 	if err != nil {
-		log.Warnf("error getting cached app managed resources, Cache unavailable for %s/%s: %v. Returning empty managed resources.", a.Namespace, a.Name, err)
+		log.Warnf("error getting cached app managed resources for %s/%s, cache unavailable, returning empty managed resources: %v", a.Namespace, a.Name, err)
 		return &application.ManagedResourcesResponse{Items: []*v1alpha1.ResourceDiff{}}, nil
 	}
 	res := &application.ManagedResourcesResponse{}
