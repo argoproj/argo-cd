f52a4857c099af50e19e373aaab71f96f7e8b65f
Rick Brouwer <rickbrouwer@gmail.com>
2025-11-17 18:37:41 +0100

    add paused-replicas
    Signed-off-by: Rick Brouwer <rickbrouwer@gmail.com>

--- resource_customizations/keda.sh/ScaledObject/actions/discovery.lua
+++ resource_customizations/keda.sh/ScaledObject/actions/discovery.lua
@@ -1,11 +1,27 @@
 local actions = {}
 
 local paused = false
+local hasPausedReplicas = false
+local currentPausedReplicas = nil
+
 if obj.metadata and obj.metadata.annotations then
     paused = obj.metadata.annotations["autoscaling.keda.sh/paused"] == "true"
+    currentPausedReplicas = obj.metadata.annotations["autoscaling.keda.sh/paused-replicas"]
+    hasPausedReplicas = currentPausedReplicas ~= nil
 end
 
-actions["pause"] = {["disabled"] = paused}
-actions["resume"] = {["disabled"] = not paused}
+local isPaused = paused or hasPausedReplicas
+
+actions["pause"] = {["disabled"] = isPaused}
+actions["paused-replicas"] = {
+    ["disabled"] = paused,
+    ["params"] = {
+        {
+            ["name"] = "replicas",
+            ["default"] = currentPausedReplicas or "0"
+        }
+    },
+}
+actions["resume"] = {["disabled"] = not isPaused}
 
 return actions
