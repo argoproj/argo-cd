7aace0330faa8b44376172f3d81f7368dd02d01a
Jonathan Winters <wintersjonathan0@gmail.com>
2025-11-17 09:59:50 -0800

    initial upgrade changes
    Signed-off-by: Jonathan Winters <wintersjonathan0@gmail.com>

--- ui/src/app/index.tsx
+++ ui/src/app/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import * as Moment from 'moment';
 import {App} from './app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
 
 const mdl = module as any;
 if (mdl.hot) {
     mdl.hot.accept('./app.tsx', () => {
         const UpdatedApp = require('./app.tsx').App;
-        ReactDOM.render(<UpdatedApp />, document.getElementById('app'));
+        root.render(<UpdatedApp />);
     });
 }
 
