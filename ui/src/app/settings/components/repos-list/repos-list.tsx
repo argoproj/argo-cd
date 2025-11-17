e9900ac401bf9333b822df2398d1c09d96c765ad
Allan Yung <allan.yung@bbdsoftware.com>
2025-11-17 09:10:45 +0000

    add Azure Service Principal support to repository access queries
    Signed-off-by: Allan Yung <allan.yung@bbdsoftware.com>

--- ui/src/app/settings/components/repos-list/repos-list.tsx
+++ ui/src/app/settings/components/repos-list/repos-list.tsx
@@ -345,9 +345,9 @@ export const ReposList = ({match, location}: RouteComponentProps) => {
         return url.replace('https://', '').replace('oci://', '');
     };
 
-    // only connections of git type which is not via GitHub App are updatable
+    // only connections of git type which are not via GitHub App or Azure Service Principal are updatable
     const isRepoUpdatable = (repo: models.Repository) => {
-        return isHTTPOrHTTPSUrl(repo.repo) && repo.type === 'git' && !repo.githubAppId;
+        return isHTTPOrHTTPSUrl(repo.repo) && repo.type === 'git' && !repo.githubAppId && !repo.azureServicePrincipalClientId;
     };
 
     // Forces a reload of configured repositories, circumventing the cache
