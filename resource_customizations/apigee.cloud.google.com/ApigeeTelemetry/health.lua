eeea9003d5f6b83b86fa5b612fcfcd173d68ffae
Fabien <ewhat@pm.me>
2025-11-30 17:26:44 +0100

    Add health checks for Apigee CRDs: ApigeeDatastore, ApigeeDeployment, ApigeeEnvironment, ApigeeOrganization, ApigeeRedis, ApigeeTelemetry for group apigee.cloud.google.com

---
+++ resource_customizations/apigee.cloud.google.com/ApigeeTelemetry/health.lua
@@ -0,0 +1,13 @@
+health_status = {}
+if obj.status ~= nil then
+  if obj.status.state ~= nil then
+    if obj.status.state == "running" then
+      health_status.status = "Healthy"
+      health_status.message = "Install of Apigee Telemetry is done"
+      return health_status
+    end
+  end
+end
+health_status.status = "Progressing"
+health_status.message = "An install plan for an Apigee Telemetry is pending installation"
+return health_status
