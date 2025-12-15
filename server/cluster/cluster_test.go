f29e5b4eba605bf19c6b7ade9279d37005a1c566
hai.yue <hai.yue@ingka.com>
2025-12-15 13:11:42 +0800

    adjust test cluster cases
    Signed-off-by: hai.yue <hai.yue@ingka.com>

--- server/cluster/cluster_test.go
+++ server/cluster/cluster_test.go
@@ -140,19 +140,19 @@ func TestUpdateCluster_RejectInvalidParams(t *testing.T) {
 	}{
 		{
 			name:    "allowed cluster URL in body, disallowed cluster URL in query",
-			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "allowed-unscoped", Server: "https://127.0.0.1", Project: "", ClusterResources: true}, Id: &cluster.ClusterID{Type: "", Value: "https://127.0.0.2"}, UpdatedFields: []string{"clusterResources", "project"}},
+			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "", Server: "https://127.0.0.1", Project: "", ClusterResources: true}, Id: &cluster.ClusterID{Type: "", Value: "https://127.0.0.2"}, UpdatedFields: []string{"clusterResources", "project"}},
 		},
 		{
 			name:    "allowed cluster URL in body, disallowed cluster name in query",
-			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "disallowed-unscoped", Server: "https://127.0.0.1", Project: "", ClusterResources: true}, Id: &cluster.ClusterID{Type: "name", Value: "disallowed-unscoped"}, UpdatedFields: []string{"clusterResources", "project"}},
+			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "", Server: "https://127.0.0.1", Project: "", ClusterResources: true}, Id: &cluster.ClusterID{Type: "name", Value: "disallowed-unscoped"}, UpdatedFields: []string{"clusterResources", "project"}},
 		},
 		{
 			name:    "allowed cluster URL in body, disallowed cluster name in query, changing unscoped to scoped",
-			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "allowed-scoped", Server: "https://127.0.0.1", Project: "allowed-project", ClusterResources: true}, Id: &cluster.ClusterID{Type: "", Value: "https://127.0.0.2"}, UpdatedFields: []string{"clusterResources", "project"}},
+			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "", Server: "https://127.0.0.1", Project: "allowed-project", ClusterResources: true}, Id: &cluster.ClusterID{Type: "", Value: "https://127.0.0.2"}, UpdatedFields: []string{"clusterResources", "project"}},
 		},
 		{
 			name:    "allowed cluster URL in body, disallowed cluster URL in query, changing unscoped to scoped",
-			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "disallowed-unscoped"}, UpdatedFields: []string{"clusterResources", "project"}},
+			request: cluster.ClusterUpdateRequest{Cluster: &appv1.Cluster{Name: "", Server: "https://127.0.0.1", Project: "allowed-project", ClusterResources: true}, Id: &cluster.ClusterID{Type: "name", Value: "disallowed-unscoped"}, UpdatedFields: []string{"clusterResources", "project"}},
 		},
 	}
 
@@ -188,7 +188,7 @@ func TestUpdateCluster_RejectInvalidParams(t *testing.T) {
 	db.EXPECT().UpdateCluster(mock.Anything, mock.Anything).RunAndReturn(
 		func(_ context.Context, c *appv1.Cluster) (*appv1.Cluster, error) {
 			for _, cluster := range clusters {
-				if c.Server == cluster.Server {
+				if c.Server == cluster.Server && c.Name == cluster.Name {
 					return c, nil
 				}
 			}
@@ -317,7 +317,7 @@ func TestGetCluster_UrlEncodedNameClustersWithSameServer(t *testing.T) {
 
 	db.EXPECT().ListClusters(mock.Anything).Return(&mockClusterList, nil)
 	db.EXPECT().GetCluster(mock.Anything, mock.Anything).Return(&mockCluster, nil)
-	db.EXPECT().GetClusterByServerAndName(mock.Anything, mock.Anything, mock.Anything).Return(&mockCluster, nil)
+	db.EXPECT().GetClusterByServerAndName(mock.Anything, mock.Anything, mock.Anything).Return(&mockCluster3rd, nil)
 
 	server := NewServer(db, newNoopEnforcer(), newServerInMemoryCache(), &kubetest.MockKubectlCmd{})
 
@@ -337,6 +337,7 @@ func TestGetCluster_UrlNameEscapedTypeFailToGetCluster(t *testing.T) {
 	db := &dbmocks.ArgoDB{}
 
 	db.EXPECT().GetCluster(mock.Anything, mock.Anything).Return(nil, errors.New("failed to get clusters by server"))
+	db.EXPECT().GetClusterByServerAndName(mock.Anything, mock.Anything, mock.Anything).Return(nil, errors.New("failed to get cluster by server and name"))
 
 	server := NewServer(db, newNoopEnforcer(), newServerInMemoryCache(), &kubetest.MockKubectlCmd{})
 
