ed80183842cec8f420f1bdf57a72ac461a897dd8
Oliver Gondža <ogondza@gmail.com>
2025-12-15 13:43:03 +0100

    feat(source-integrity): Fix tests
    Signed-off-by: Oliver Gondža <ogondza@gmail.com>

--- controller/state_test.go
+++ controller/state_test.go
@@ -1419,7 +1419,6 @@ func TestUseDiffCache(t *testing.T) {
 				Server:     "",
 				Revision:   revision,
 				SourceType: "Kustomize",
-				// TODO irrelevant for the test? SourceIntegrityResult: &v1alpha1.SourceIntegrityCheckResult{Git: []string{}}, // Valid
 			},
 		}
 	}
