96afbe3973651c0fddf0df350a6cafac41a330ca
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2026-01-05 14:08:23 -0500

    revert back to tmp mounting to validate CI
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- test/fixture/testrepos/start-git.sh
+++ test/fixture/testrepos/start-git.sh
@@ -5,10 +5,12 @@
 # The host directory is mounted to /tmp/argo-e2e inside the container
 ARGOCD_E2E_DIR="${ARGOCD_E2E_DIR:-/tmp/argo-e2e}"
 
+#  -v "$ARGOCD_E2E_DIR":/tmp/argo-e2e \
+
 docker run --name e2e-git --rm -i \
     -p 2222:2222 -p 9080:9080 -p 9443:9443 -p 9444:9444 -p 9445:9445 \
     -w /go/src/github.com/argoproj/argo-cd \
+    -v /tmp:/tmp \
     -v "$(pwd)":/go/src/github.com/argoproj/argo-cd \
-    -v "$ARGOCD_E2E_DIR":/tmp/argo-e2e \
     docker.io/argoproj/argo-cd-ci-builder:v1.0.0 \
     bash -c "goreman -f ./test/fixture/testrepos/Procfile start"
