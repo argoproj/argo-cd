751054b696b0de293aaf0271a7ec5a26139e6fbf
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2026-01-05 18:38:04 -0500

    CI without tmp mount
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- test/fixture/testrepos/start-git.sh
+++ test/fixture/testrepos/start-git.sh
@@ -8,7 +8,6 @@ ARGOCD_E2E_DIR="${ARGOCD_E2E_DIR:-/tmp/argo-e2e}"
 docker run --name e2e-git --rm -i \
     -p 2222:2222 -p 9080:9080 -p 9443:9443 -p 9444:9444 -p 9445:9445 \
     -w /go/src/github.com/argoproj/argo-cd \
-    -v /tmp:/tmp \
     -v "$ARGOCD_E2E_DIR":/tmp/argo-e2e \
     -v "$(pwd)":/go/src/github.com/argoproj/argo-cd \
     docker.io/argoproj/argo-cd-ci-builder:v1.0.0 \
