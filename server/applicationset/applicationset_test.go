2f9148422f5fef87ff420eedb4b8c64cdeea437c
Jean-Damien HATZENBUHLER <jean-damien.hatzenbuhler@contentsquare.com>
2026-01-06 18:14:18 +0100

    feat: add support for httpcaching of github requests
    Signed-off-by: Jean-Damien HATZENBUHLER <jean-damien.hatzenbuhler@contentsquare.com>

--- server/applicationset/applicationset_test.go
+++ server/applicationset/applicationset_test.go
@@ -212,6 +212,8 @@ func newTestAppSetServerWithEnforcerConfigure(t *testing.T, f func(*rbac.Enforce
 		[]string{},
 		true,
 		true,
+		false,
+		100,
 		testEnableEventList,
 		clusterInformer,
 	)
