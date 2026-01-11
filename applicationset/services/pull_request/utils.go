1e763d532a38ab0fc5b4babc4b4cbe2795c13c2d
Marko Mirosavljev <mirosavljevm023@gmail.com>
2026-01-11 19:00:12 +0100

    fix appset e2e mock and add log to pr filtering func
    Signed-off-by: Marko Mirosavljev <mirosavljevm023@gmail.com>

--- applicationset/services/pull_request/utils.go
+++ applicationset/services/pull_request/utils.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	argoprojiov1alpha1 "github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
+	log "github.com/sirupsen/logrus"
 )
 
 func compileFilters(filters []argoprojiov1alpha1.PullRequestGeneratorFilter) ([]*Filter, error) {
@@ -61,10 +62,10 @@ func matchFilter(pullRequest *PullRequest, filter *Filter) bool {
 	if filter.TitleMatch != nil && !filter.TitleMatch.MatchString(pullRequest.Title) {
 		return false
 	}
-	if filter.CreatedWithin != nil && pullRequest.CreatedAt.Before(time.Now().Add(-*filter.CreatedWithin)) {
+	if filter.CreatedWithin != nil && pullRequest.CreatedAt.Before(time.Now().UTC().Add(-*filter.CreatedWithin)) {
 		return false
 	}
-	if filter.UpdatedWithin != nil && pullRequest.UpdatedAt.Before(time.Now().Add(-*filter.UpdatedWithin)) {
+	if filter.UpdatedWithin != nil && pullRequest.UpdatedAt.Before(time.Now().UTC().Add(-*filter.UpdatedWithin)) {
 		return false
 	}
 
@@ -88,12 +89,20 @@ func ListPullRequests(ctx context.Context, provider PullRequestService, filters 
 
 	filteredPullRequests := make([]*PullRequest, 0, len(pullRequests))
 	for _, pullRequest := range pullRequests {
+		matches := false
 		for _, filter := range compiledFilters {
-			matches := matchFilter(pullRequest, filter)
+			matches = matchFilter(pullRequest, filter)
 			if matches {
 				filteredPullRequests = append(filteredPullRequests, pullRequest)
 				break
 			}
+		}
+
+		if !matches {
+			log.WithFields(log.Fields{
+				"pr":              pullRequest,
+				"applied_filters": compiledFilters,
+			}).Debug("pull request filtered out")
 		}
 	}
 
