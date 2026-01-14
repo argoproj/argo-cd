eb2f41445ceda7cecac1a2a669852c5924389ed1
Elton de Boer <elton@playgroundtech.io>
2026-01-14 16:19:30 +0100

    chore: update theme default to auto This is a small addition switching the default theme to auto instead of light mode, to match the users system default.
    Signed-off-by: Elton de Boer <elton@playgroundtech.io>

--- ui/src/app/shared/services/view-preferences-service.ts
+++ ui/src/app/shared/services/view-preferences-service.ts
@@ -199,7 +199,7 @@ const DEFAULT_PREFERENCES: ViewPreferences = {
     hideBannerContent: '',
     hideSidebar: false,
     position: '',
-    theme: 'light'
+    theme: 'auto'
 };
 
 export class ViewPreferencesService {
