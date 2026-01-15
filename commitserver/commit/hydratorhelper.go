2e638831a696ebf7cbbe15d52c9734d22c23eb2d
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2026-01-15 17:00:11 -0500

    chore(hydrator): improve error message (#25737)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- commitserver/commit/hydratorhelper.go
+++ commitserver/commit/hydratorhelper.go
@@ -247,9 +247,5 @@ func AddNote(gitClient git.Client, drySha, commitSha string) error {
 	if err != nil {
 		return fmt.Errorf("failed to marshal commit note: %w", err)
 	}
-	err = gitClient.AddAndPushNote(commitSha, NoteNamespace, string(jsonBytes))
-	if err != nil {
-		return fmt.Errorf("failed to add commit note: %w", err)
-	}
-	return nil
+	return gitClient.AddAndPushNote(commitSha, NoteNamespace, string(jsonBytes)) // nolint:wrapcheck // wrapping the error wouldn't add any information
 }
