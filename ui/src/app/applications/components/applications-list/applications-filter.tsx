6584054de73b7a5d20099c4009cc99d8ba15653c
choejwoo <jaewoo45@gmail.com>
2026-01-15 22:02:31 +0900

    please lint
    Signed-off-by: choejwoo <jaewoo45@gmail.com>

--- ui/src/app/applications/components/applications-list/applications-filter.tsx
+++ ui/src/app/applications/components/applications-list/applications-filter.tsx
@@ -360,12 +360,7 @@ export const ApplicationsFilter = (props: AppFilterProps) => {
     };
 
     return (
-        <FiltersGroup
-            title='Application filters'
-            content={props.children}
-            appliedFilter={appliedFilter}
-            onClearFilter={onClearFilter}
-            collapsed={props.collapsed}>
+        <FiltersGroup title='Application filters' content={props.children} appliedFilter={appliedFilter} onClearFilter={onClearFilter} collapsed={props.collapsed}>
             <FavoriteFilter {...props} />
             <SyncFilter {...props} />
             <HealthFilter {...props} />
