5829c3a6ae8bef5f8b6cb610f466d2c6600e4fcc
nitishfy <justnitish06@gmail.com>
2026-02-06 19:59:50 +0530

    change helm3 version
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- hack/tool-versions.sh
+++ hack/tool-versions.sh
@@ -12,7 +12,7 @@
 # add-kustomize-checksums.sh to help download checksums.
 ###############################################################################
 
-helm3_version=3.19.4
+helm3_version=3.19.2
 kustomize5_version=5.8.0
 protoc_version=29.3
 oras_version=1.2.0
