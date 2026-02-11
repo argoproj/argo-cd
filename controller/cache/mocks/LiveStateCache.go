092f655c9a734b54fe835ecc3a132803d91e38bd
Leonardo Luz Almeida <leonardo_almeida@intuit.com>
2026-02-11 17:34:55 -0500

    fix codegen
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- controller/cache/mocks/LiveStateCache.go
+++ controller/cache/mocks/LiveStateCache.go
@@ -7,10 +7,10 @@ package mocks
 import (
 	"context"
 
-	cache0 "github.com/argoproj/argo-cd/v3/controller/cache"
-	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/cache"
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/utils/kube"
+	cache0 "github.com/argoproj/argo-cd/v3/controller/cache"
+	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
 	mock "github.com/stretchr/testify/mock"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
