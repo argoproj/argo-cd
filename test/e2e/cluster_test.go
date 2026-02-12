bb54dbbb1188a631c1a151d80b40bd5cc4a5f2f5
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2026-02-12 22:16:41 +0100

    fix: use fork module path for gitops-engine imports
    CI resolves github.com/argoproj/gitops-engine to upstream v0.7.3
    which is incompatible with Go 1.26 kubectl APIs. Use the fork
    path github.com/argoproj/argo-cd/gitops-engine everywhere.
    
    Signed-off-by: Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/argoproj/gitops-engine/pkg/health"
-	. "github.com/argoproj/gitops-engine/pkg/sync/common"
+	"github.com/argoproj/argo-cd/gitops-engine/pkg/health"
+	. "github.com/argoproj/argo-cd/gitops-engine/pkg/sync/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
