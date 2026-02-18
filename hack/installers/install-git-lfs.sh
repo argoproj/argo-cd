ba8ce824dd4b7225cd9198db77ff3986ae16fc89
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2026-02-18 11:33:21 +0200

    address pr comments
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- hack/installers/install-git-lfs.sh
+++ hack/installers/install-git-lfs.sh
@@ -7,6 +7,6 @@ export TARGET_FILE=git-lfs-${INSTALL_OS}-${ARCHITECTURE}-v${git_lfs_version}.tar
 
 [ -e "$DOWNLOADS/${TARGET_FILE}" ] || curl -sLf --retry 3 -o "$DOWNLOADS/${TARGET_FILE}" "https://github.com/git-lfs/git-lfs/releases/download/v${git_lfs_version}/${TARGET_FILE}"
 "$(dirname "$0")"/compare-chksum.sh
-mkdir -p /tmp/git-lfs && tar -C /tmp/git-lfs --strip-components=1 -xf "$DOWNLOADS/${TARGET_FILE}"
+mkdir -p /tmp/git-lfs && tar -C /tmp/git-lfs --strip-components=1 -xzf "$DOWNLOADS/${TARGET_FILE}"
 sudo install -m 0755 "/tmp/git-lfs/git-lfs" "$BIN/git-lfs"
 git-lfs version
