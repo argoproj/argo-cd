9f0a86136ce3c2c983c6e773aeeb8642e533d73e
chansuke <moonset20@gmail.com>
2026-02-20 18:40:01 +0900

    use `protojson.Unmarshal` to eliminate the intermediate `map[string]interface{}` step
    Signed-off-by: chansuke <moonset20@gmail.com>

--- server/events/events.go
+++ server/events/events.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/structpb"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -24,14 +25,9 @@ func EventListToStruct(eventList *corev1.EventList) (*structpb.Struct, error) {
 		return nil, fmt.Errorf("failed to marshal EventList to JSON: %w", err)
 	}
 
-	var data map[string]any
-	if err := json.Unmarshal(jsonBytes, &data); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal EventList JSON: %w", err)
-	}
-
-	result, err := structpb.NewStruct(data)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create protobuf Struct: %w", err)
+	result := &structpb.Struct{}
+	if err := protojson.Unmarshal(jsonBytes, result); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal into protobuf Struct: %w", err)
 	}
 
 	return result, nil
