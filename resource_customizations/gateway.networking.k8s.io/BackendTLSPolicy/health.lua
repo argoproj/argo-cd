3c3394c5ad72a3dfe206631c8fb38eb17137f89e
Norwin Schnyder <norwin.schnyder+github@gmail.com>
2026-04-17 12:10:24 +0200

    feat(health): check observedGeneration for BackendTLSPolicy.gateway.networking.k8s.io
    Signed-off-by: Norwin Schnyder <norwin.schnyder+github@gmail.com>

--- resource_customizations/gateway.networking.k8s.io/BackendTLSPolicy/health.lua
+++ resource_customizations/gateway.networking.k8s.io/BackendTLSPolicy/health.lua
@@ -4,26 +4,37 @@ hs.status = "Progressing"
 hs.message = "Waiting for BackendTLSPolicy status"
 
 if obj.status ~= nil and obj.status.ancestors ~= nil then
-  for i, ancestor in ipairs(obj.status.ancestors) do
-    if ancestor.conditions ~= nil then
-      for j, condition in ipairs(ancestor.conditions) do
-        if condition.type == "Accepted" then
-          if condition.status ~= "True" then
-            hs.status = "Degraded"
-            hs.message = "Ancestor " .. (ancestor.ancestorRef.name or "") .. ": " .. condition.message
-            return hs
-          else
-            hs.status = "Healthy"
-            hs.message = "BackendTLSPolicy is healthy"
+  if obj.metadata.generation ~= nil then
+    for i, ancestor in ipairs(obj.status.ancestors) do
+      for _, condition in ipairs(ancestor.conditions) do
+        if condition.observedGeneration ~= nil then
+          if condition.observedGeneration ~= obj.metadata.generation then
+              hs.message = "Waiting for Ancestor " .. (ancestor.ancestorRef.name or "") .. " to update BackendTLSPolicy status"
+             return hs
           end
         end
+      end
+    end
+  end
 
-        if condition.type == "ResolvedRefs" then
-          if condition.status ~= "True" then
-            hs.status = "Degraded"
-            hs.message = "Ancestor " .. (ancestor.ancestorRef.name or "") .. ": " .. condition.message
-            return hs
-          end
+  for i, ancestor in ipairs(obj.status.ancestors) do
+    for j, condition in ipairs(ancestor.conditions) do
+      if condition.type == "Accepted" then
+        if condition.status ~= "True" then
+          hs.status = "Degraded"
+          hs.message = "Ancestor " .. (ancestor.ancestorRef.name or "") .. ": " .. condition.message
+          return hs
+        else
+          hs.status = "Healthy"
+          hs.message = "BackendTLSPolicy is healthy"
+        end
+      end
+
+      if condition.type == "ResolvedRefs" then
+        if condition.status ~= "True" then
+          hs.status = "Degraded"
+          hs.message = "Ancestor " .. (ancestor.ancestorRef.name or "") .. ": " .. condition.message
+          return hs
         end
       end
     end
