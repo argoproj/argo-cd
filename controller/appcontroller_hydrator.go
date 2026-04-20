8595ed664094abf74bfcfba8135e6b02fc7a8ab5
liketosweep <liketosweep@gmail.com>
2026-04-20 21:11:30 +0000

    fix: add RollbackApp delegation to ApplicationController
    Signed-off-by: liketosweep <liketosweep@gmail.com>

---
+++ controller/appcontroller_hydrator.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+    "context"
+    appv1 "github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
+)
+
+func (ctrl *ApplicationController) RollbackApp(ctx context.Context, app *appv1.Application, hydratedRevision string) error {
+    return ctrl.hydrator.RollbackApp(ctx, app, hydratedRevision)
+}
