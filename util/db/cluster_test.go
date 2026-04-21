7712e9197604aa1601c03f664a2113b70bff20d8
anandf <anjoseph@redhat.com>
2026-04-21 18:25:36 +0530

    fixed failing unit test
    Signed-off-by: anandf <anjoseph@redhat.com>

--- util/db/cluster_test.go
+++ util/db/cluster_test.go
@@ -597,7 +597,8 @@ func TestGetClusterServersByName_IsInClusterEnabledLazyLoad(t *testing.T) {
 		{
 			name:        "in-cluster name calls IsInClusterEnabled()",
 			clusterName: "in-cluster",
-			wantErr:     true,
+			wantErr:     false,
+			wantServers: []string{"https://kubernetes.default.svc"},
 		},
 	}
 
