975ac078da30924c6dd847dc068a6acc90b37aeb
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2026-04-27 15:35:01 +0300

    makes agentVersion in ts structure optional
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- ui/src/app/shared/models.ts
+++ ui/src/app/shared/models.ts
@@ -709,7 +709,7 @@ export interface Cluster {
     info?: {
         applicationsCount: number;
         serverVersion: string;
-        agentVersion: string;
+        agentVersion?: string;
         connectionState: ConnectionState;
         cacheInfo: ClusterCacheInfo;
     };
