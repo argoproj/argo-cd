4f1d86a075f33f8d8f5ee37676ef8052f3bcc38f
AvivGuiser <avivguiser@gmail.com>
2026-04-30 22:55:46 +0300

    remove copy paste leftovers
    Signed-off-by: AvivGuiser <avivguiser@gmail.com>

--- resource_customizations/psmdb.percona.com/PerconaServerMongoDB/actions/discovery.lua
+++ resource_customizations/psmdb.percona.com/PerconaServerMongoDB/actions/discovery.lua
@@ -6,12 +6,3 @@ end
 actions["pause"] = {["disabled"] = is_paused}
 actions["unpause"] = {["disabled"] = not is_paused}
 return actions
-        definitions:
-        - name: pause
-          action.lua: |
-            obj.spec.pause = true
-            return obj
-        - name: unpause
-          action.lua: |
-            obj.spec.pause = false
-            return obj
