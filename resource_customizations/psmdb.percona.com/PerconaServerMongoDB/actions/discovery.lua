59c041e3491cef549a6ef13e9a6b50403e19641b
AvivGuiser <avivguiser@gmail.com>
2026-04-30 23:00:43 +0300

    add icons
    Signed-off-by: AvivGuiser <avivguiser@gmail.com>

--- resource_customizations/psmdb.percona.com/PerconaServerMongoDB/actions/discovery.lua
+++ resource_customizations/psmdb.percona.com/PerconaServerMongoDB/actions/discovery.lua
@@ -3,6 +3,12 @@ local is_paused = false
 if obj.spec.pause == true then
   is_paused = true
 end
-actions["pause"] = {["disabled"] = is_paused}
-actions["unpause"] = {["disabled"] = not is_paused}
+actions["pause"] = {
+    ["disabled"] = is_paused
+    ["iconClass"] = "fa fa-fw fa-pause-circle"
+}
+actions["unpause"] = {
+    ["disabled"] = not is_paused
+    ["iconClass"] = "fa fa-fw fa-play-circle"
+}
 return actions
