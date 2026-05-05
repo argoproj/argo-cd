34537bf0aa1e1aa209ea77c0ccf80bcd4492e555
Marco Groß (Marco Gross) <gross@b1-systems.de>
2026-05-05 13:33:28 +0200

    Add comments to action files in order to clarify function
    Signed-off-by: Marco Groß (Marco Gross) <gross@b1-systems.de>

--- resource_customizations/k8s.mariadb.com/MariaDB/actions/resume/action.lua
+++ resource_customizations/k8s.mariadb.com/MariaDB/actions/resume/action.lua
@@ -1,3 +1,6 @@
+-- We are setting the suspend flag to false in order to enable
+-- the operator
+-- see: https://github.com/mariadb-operator/mariadb-operator/blob/main/docs/suspend.md
 if obj.spec.suspend ~= nil and obj.spec.suspend then
 	obj.spec.suspend = false
 end
--- resource_customizations/k8s.mariadb.com/MariaDB/actions/suspend/action.lua
+++ resource_customizations/k8s.mariadb.com/MariaDB/actions/suspend/action.lua
@@ -1,3 +1,6 @@
+-- We are setting the suspend flag to true in order to disable
+-- the operator
+-- see: https://github.com/mariadb-operator/mariadb-operator/blob/main/docs/suspend.md
 if obj.spec ~= nil then
 	obj.spec.suspend = true
 end
