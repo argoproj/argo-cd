a8304418efe38be37cb4830f6336e71e6e31ea58
Marco Groß (Marco Gross) <gross@b1-systems.de>
2026-05-26 09:53:52 +0200

    Refactor health check
    Now, we use status.conditions as a base for our health check.
    
    Signed-off-by: Marco Groß (Marco Gross) <gross@b1-systems.de>

--- resource_customizations/cassandra.datastax.com/CassandraDatacenter/health.lua
+++ resource_customizations/cassandra.datastax.com/CassandraDatacenter/health.lua
@@ -1,28 +1,48 @@
 local hs = {}
+hs.status = "Unknown"
 
-local amountOfNodes = 0
-if obj.status ~= nil and obj.status.nodeStatuses ~= nil then
-	for _, _ in pairs(obj.status.nodeStatuses) do
-		amountOfNodes = amountOfNodes + 1
-	end
-else
-	hs.status = "Degraded"
-	hs.message = "No nodeStatus in object's status"
+if obj.status == nil or obj.status.conditions == nil then
+	hs.message = "No conditions found"
 	return hs
 end
 
-if obj.spec ~= nil and obj.spec.size ~= nil then
-	if obj.spec.size == amountOfNodes then
-		hs.status = "Healthy"
-		return hs
-	else
-		hs.status = "Degraded"
-		hs.message = "Size in spec and number of nodes in status differ"
+local newestTimestamp = 0
+local newestIndex = 0
+for idx, condition in pairs(obj.status.conditions) do
+	local timestamp = condition.lastTransitionTime
+	local format_string = "(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)Z"
+	local year, month, day, hour, min, sec =
+		string.match(timestamp, format_string)
+
+	local time = os.time({
+		day = day,
+		month = month,
+		year = year,
+		hour = hour,
+		min = min,
+		sec = sec
+	})
+
+	if os.difftime(time, newestTimestamp) > 0 then
+		newestTimestamp = time
+		newestIndex = idx
 	end
-else
-	hs.status = "Degraded"
-	hs.message = "No size in object's spec"
+end
+
+local lastCondition = obj.status.conditions[newestIndex]
+if lastCondition.type == 'Healthy' then
+	hs.status = "Healthy"
 	return hs
 end
+
+if lastCondition.status == 'True' then
+	hs.status = "Progressing"
+end
+
+if lastCondition.status == 'False' then
+	hs.status = "Degraded"
+end
+
+hs.message = "Last transition type was " .. lastCondition.type
 
 return hs
