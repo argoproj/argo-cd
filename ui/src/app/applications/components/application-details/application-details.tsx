argoproj/argo-cd

commit 8a7ab41f6450c3910ef5cec8ddcda50645073e3a

fix(ui): format parent node info with Argo styles (#28385)
Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

ui/src/app/applications/components/application-details/application-details.tsx
@@ -25,7 +25,7 @@ import {isApp} from '../utils';
 import {ResourceDetails} from '../resource-details/resource-details';
 import {AppSetResourceDetails} from '../resource-details/appset-resource-details';
 import * as AppUtils from '../utils';
-import {ApplicationResourceList} from './application-resource-list';
+import {ApplicationResourceList, ApplicationResourceParentRef} from './application-resource-list';
 import {Filters, FiltersProps} from './application-resource-filter';
 import {getAppDefaultSource, getAppCurrentVersion, urlPattern} from '../utils';
 import {ChartDetails, OCIMetadata} from '../../../shared/models';
@@ -1170,6 +1170,9 @@ Are you sure you want to disable auto-sync and rollback application '${props.mat
                                         {isApplication && (
                                             <SlidingPanel isShown={groupedResources.length > 0} onClose={() => closeGroupedNodesPanel()}>
                                                 <div className='application-details__sliding-panel-pagination-wrap'>
+                                                    {(pref.view === 'tree' || pref.view === 'network') && (
+                                                        <ApplicationResourceParentRef resources={state.groupedResources} tree={tree} />
+                                                    )}
                                                     <Paginate
                                                         page={state.slidingPanelPage}
                                                         data={groupedResources}

ui/src/app/applications/components/application-details/application-details.scss
@@ -411,28 +411,3 @@ $header: 120px;
         }
     }
 }
-
-.resource-parent-node-info-title {
-    flex-direction: column;
-    color: $argo-color-gray-6;
-
-    &__label {
-        display: flex;
-        margin-bottom: 0.25em;
-        flex-shrink: 1;
-        div:first-child {
-            margin-right: 10px;
-            width: 60px;
-            text-align: right;
-        }
-        div:last-child {
-            font-weight: 500;
-            width: 100%;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            text-align: left;
-
-        }
-    }
-}

ui/src/app/applications/components/application-details/application-resource-list.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import * as models from '../../../shared/models';
 import {ResourceIcon} from '../resource-icon';
 import {ResourceLabel} from '../resource-label';
-import {ActionMenuButton} from '../../../shared/components';
+import {ActionMenuButton, EditablePanel} from '../../../shared/components';
 import {
     ComparisonStatusIcon,
     HealthStatusIcon,
@@ -32,6 +32,44 @@ export interface ApplicationResourceListProps {
     nodeMenu?: (node: models.ResourceNode) => React.ReactNode;
     tree?: models.ApplicationTree;
 }
+
+export interface ApplicationResourceParentRefProps {
+    resources: models.ResourceStatus[];
+    tree?: models.ApplicationTree;
+}
+
+export const ApplicationResourceParentRef = (props: ApplicationResourceParentRefProps) => {
+    const nodeByKey = new Map<string, models.ResourceNode>();
+    props.tree?.nodes?.forEach(res => nodeByKey.set(nodeKey(res), res));
+
+    const firstParentNode = props.resources.length > 0 && (nodeByKey.get(nodeKey(props.resources[0])) as ResourceNode)?.parentRefs?.[0];
+    const isSameParent = firstParentNode && props.resources?.every(x => (nodeByKey.get(nodeKey(x)) as ResourceNode)?.parentRefs?.every(p => isSameNode(p, firstParentNode)));
+
+    if (!isSameParent) {
+        return null;
+    }
+
+    return (
+        <EditablePanel
+            title='PARENT NODE'
+            values={firstParentNode}
+            items={[
+                {
+                    title: 'KIND',
+                    view: firstParentNode.kind
+                },
+                {
+                    title: 'NAME',
+                    view: firstParentNode.name
+                },
+                {
+                    title: 'NAMESPACE',
+                    view: firstParentNode.namespace
+                }
+            ]}
+        />
+    );
+};
 
 export const ApplicationResourceList = (props: ApplicationResourceListProps) => {
     const nodeByKey = new Map<string, models.ResourceNode>();
@@ -120,37 +158,11 @@ export const ApplicationResourceList = (props: ApplicationResourceListProps) => 
         return resourcesToSort;
     }, [props.resources, sortConfig]);
 
-    const firstParentNode = props.resources.length > 0 && (nodeByKey.get(nodeKey(props.resources[0])) as ResourceNode)?.parentRefs?.[0];
-    const isSameParent = firstParentNode && props.resources?.every(x => (nodeByKey.get(nodeKey(x)) as ResourceNode)?.parentRefs?.every(p => isSameNode(p, firstParentNode)));
     const isSameKind = props.resources?.every(x => x.group === props.resources[0].group && x.kind === props.resources[0].kind);
-    const view = props.pref.view;
 
-    const ParentRefDetails = () => {
-        return isSameParent ? (
-            <div className='resource-parent-node-info-title'>
-                <div>Parent Node Info</div>
-                <div className='resource-parent-node-info-title__label'>
-                    <div>Name:</div>
-                    <div>{firstParentNode.name}</div>
-                </div>
-                <div className='resource-parent-node-info-title__label'>
-                    <div>Kind:</div>
-                    <div>{firstParentNode.kind}</div>
-                </div>
-            </div>
-        ) : (
-            <div />
-        );
-    };
     return (
         props.resources.length > 0 && (
             <div>
-                {/* Display only when the view is set to  or network */}
-                {(view === 'tree' || view === 'network') && (
-                    <div className='resource-details__header' style={{paddingTop: '20px'}}>
-                        <ParentRefDetails />
-                    </div>
-                )}
                 <div className='application-resource-list argo-table-list argo-table-list--clickable'>
                     <div className='argo-table-list__head'>
                         <div className='row'>

commit e020d5175d639e078061d84e8926bbbd2a3f9050

fix(ui): use groupedResources local variable in parent ref panel
Fixes a TypeScript error introduced in #28385 where state.groupedResources
was referenced but ApplicationDetailsState only defines groupedResourceIds.

Co-authored-by: Cursor <cursoragent@cursor.com>
Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

ui/src/app/applications/components/application-details/application-details.tsx
@@ -1170,9 +1170,7 @@ Are you sure you want to disable auto-sync and rollback application '${props.mat
                                         {isApplication && (
                                             <SlidingPanel isShown={groupedResources.length > 0} onClose={() => closeGroupedNodesPanel()}>
                                                 <div className='application-details__sliding-panel-pagination-wrap'>
-                                                    {(pref.view === 'tree' || pref.view === 'network') && (
-                                                        <ApplicationResourceParentRef resources={state.groupedResources} tree={tree} />
-                                                    )}
+                                                    {(pref.view === 'tree' || pref.view === 'network') && <ApplicationResourceParentRef resources={groupedResources} tree={tree} />}
                                                     <Paginate
                                                         page={state.slidingPanelPage}
                                                         data={groupedResources}