adc636e7400ee18c555559bf57e5fca627553dfe
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2026-06-24 16:14:51 -0400

    chore(ci): automate production binary bumps
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- .github/configs/renovate-config.js
+++ .github/configs/renovate-config.js
@@ -3,7 +3,12 @@ module.exports = {
     gitAuthor: 'renovate[bot] <renovate[bot]@users.noreply.github.com>',
     autodiscover: false,
     allowPostUpgradeCommandTemplating: true,
-    allowedPostUpgradeCommands: ["make mockgen"],
+    allowedPostUpgradeCommands: [
+        "make mockgen",
+        "hack/installers/checksums/add-helm-checksums.sh",
+        "hack/installers/checksums/add-kustomize-checksums.sh",
+        "hack/installers/checksums/add-git-lfs-checksums.sh",
+    ],
     binarySource: 'install',
     extends: [
         "github>argoproj/argo-cd//renovate-presets/commons.json5",
@@ -11,6 +16,7 @@ module.exports = {
         "github>argoproj/argo-cd//renovate-presets/custom-managers/yaml.json5",
         "github>argoproj/argo-cd//renovate-presets/fix/disable-all-updates.json5",
         "github>argoproj/argo-cd//renovate-presets/devtool.json5",
+        "github>argoproj/argo-cd//renovate-presets/production-binaries.json5",
         "github>argoproj/argo-cd//renovate-presets/docs.json5",
         "group:aws-sdk-go-v2Monorepo"
     ],
