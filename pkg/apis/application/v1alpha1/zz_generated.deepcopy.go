8da109cf6b06e827d7a410a38d4ef96e841602b2
Cecil Wöbker <me@cwoebker.com>
2026-07-09 12:29:18 +0200

    Run make codegen
    Signed-off-by: Cecil Wöbker <me@cwoebker.com>

--- pkg/apis/application/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/application/v1alpha1/zz_generated.deepcopy.go
@@ -3558,6 +3558,11 @@ func (in *PullRequestGeneratorGitea) DeepCopyInto(out *PullRequestGeneratorGitea
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
+	if in.ExcludedLabels != nil {
+		in, out := &in.ExcludedLabels, &out.ExcludedLabels
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -3581,6 +3586,11 @@ func (in *PullRequestGeneratorGithub) DeepCopyInto(out *PullRequestGeneratorGith
 	}
 	if in.Labels != nil {
 		in, out := &in.Labels, &out.Labels
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.ExcludedLabels != nil {
+		in, out := &in.ExcludedLabels, &out.ExcludedLabels
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
