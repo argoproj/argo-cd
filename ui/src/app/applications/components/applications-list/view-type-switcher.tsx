1e50ddd707dee0dac819b7f4c6090d17c31f19d8
Kartik Upadhyay <kartikupadhyay653@gmail.com>
2026-07-13 02:00:50 +0530

    feat(ui): add option to group applications by project #28605
    Signed-off-by: Kartik Upadhyay <kartikupadhyay653@gmail.com>

--- ui/src/app/applications/components/applications-list/view-type-switcher.tsx
+++ ui/src/app/applications/components/applications-list/view-type-switcher.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import * as React from 'react';
+import {Checkbox} from 'argo-ui';
 import {ContextApis} from '../../../shared/context';
 import {AppsListPreferences, AppsListViewKey, services} from '../../../shared/services';
 
@@ -12,7 +13,19 @@ export const ViewTypeSwitcher: React.FC<ViewTypeSwitcherProps> = ({pref, ctx}) =
     const {List, Summary, Tiles} = AppsListViewKey;
 
     return (
-        <div className='applications-list__view-type'>
+        <div className='applications-list__view-type' style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{marginRight: '15px', display: 'flex', alignItems: 'center'}}>
+                <Checkbox
+                    id='group-by-project-checkbox'
+                    checked={!!pref.groupByProject}
+                    onChange={val => {
+                        services.viewPreferences.updatePreferences({appList: {...pref, groupByProject: val}});
+                    }}
+                />
+                <label htmlFor='group-by-project-checkbox' style={{marginLeft: '6px', cursor: 'pointer', marginBottom: 0}}>
+                    Group by Project
+                </label>
+            </div>
             <i
                 className={classNames('fa fa-th', {selected: pref.view === Tiles}, 'menu_icon')}
                 title='Tiles'
