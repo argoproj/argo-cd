ee57a19c65cc21385dfd89e870849e5c7e9615a0
dudinea <eugene.doudine@octopus.com>
2026-07-15 17:06:14 +0300

    test(e2e): fix github's failure to show complete results of e2e test (#28727) (#28726)
    Signed-off-by: Eugene Doudine <eugene.doudine@octopus.com>

--- test/e2e/fixture/fixture.go
+++ test/e2e/fixture/fixture.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	stderrors "errors"
 	"fmt"
+	"math"
 	"os"
 	"path"
 	"path/filepath"
@@ -76,6 +77,9 @@ const (
 
 	// Account for batch events processing (set to 1ms in e2e tests)
 	WhenThenSleepInterval = 5 * time.Millisecond
+
+	// maximum length of log line
+	defaultLogLineMaxLen = 4096
 )
 
 const (
@@ -86,6 +90,7 @@ const (
 	EnvArgoCDRedisName         = "ARGOCD_E2E_REDIS_NAME"
 	EnvArgoCDRepoServerName    = "ARGOCD_E2E_REPO_SERVER_NAME"
 	EnvArgoCDAppControllerName = "ARGOCD_E2E_APPLICATION_CONTROLLER_NAME"
+	EnvLogLineMaxLen           = "ARGOCD_E2E_LOG_LINE_MAX_LEN"
 )
 
 var (
@@ -194,6 +199,8 @@ func IsLocal() bool {
 func init() {
 	// ensure we log all shell execs
 	log.SetLevel(log.DebugLevel)
+	// truncate eccessively long entries
+	log.SetFormatter(MakeTruncatingFormatter(env.ParseNumFromEnv(EnvLogLineMaxLen, defaultLogLineMaxLen, 0, math.MaxInt32)))
 	// set-up variables
 	config := getKubeConfig("", clientcmd.ConfigOverrides{})
 	AppClientset = appclientset.NewForConfigOrDie(config)
