90ed2cd60148c9ccbf3e4862e106b130f6617b07
vishnu97770 <vv162370@gmail.com>
2026-07-18 18:39:09 +0530

    test: add regression tests for cluster sharding cache updates
    Signed-off-by: vishnu97770 <vv162370@gmail.com>

--- controller/appcontroller_informer_test.go
+++ controller/appcontroller_informer_test.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 
+	"github.com/argoproj/argo-cd/v3/controller/sharding"
 	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v3/test"
 )
@@ -51,9 +52,7 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		ctrl.projByNameCache.Store("my-proj", struct{}{})
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		h.OnAdd(newProject("my-proj"), false)
-
 		assert.Contains(t, assertEnqueued(t, ctrl.projectRefreshQueue), "my-proj")
 		_, ok := ctrl.projByNameCache.Load("my-proj")
 		assert.False(t, ok, "project cache entry should have been invalidated")
@@ -63,9 +62,7 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		ctrl.projByNameCache.Store("my-proj", struct{}{})
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		h.OnUpdate(newProject("my-proj"), newProject("my-proj"))
-
 		assert.Contains(t, assertEnqueued(t, ctrl.projectRefreshQueue), "my-proj")
 		_, ok := ctrl.projByNameCache.Load("my-proj")
 		assert.False(t, ok)
@@ -75,9 +72,7 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		ctrl.projByNameCache.Store("my-proj", struct{}{})
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		h.OnDelete(newProject("my-proj"))
-
 		assert.Contains(t, assertEnqueued(t, ctrl.projectRefreshQueue), "my-proj")
 		_, ok := ctrl.projByNameCache.Load("my-proj")
 		assert.False(t, ok)
@@ -87,9 +82,7 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		ctrl.projByNameCache.Store("my-proj", struct{}{})
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		h.OnDelete(cache.DeletedFinalStateUnknown{Key: test.FakeArgoCDNamespace + "/my-proj", Obj: newProject("my-proj")})
-
 		assert.Contains(t, assertEnqueued(t, ctrl.projectRefreshQueue), "my-proj")
 		_, ok := ctrl.projByNameCache.Load("my-proj")
 		assert.False(t, ok)
@@ -99,7 +92,6 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		ctrl.projByNameCache.Store("my-proj", struct{}{})
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		assert.NotPanics(t, func() {
 			h.OnDelete(cache.DeletedFinalStateUnknown{Key: test.FakeArgoCDNamespace + "/gone", Obj: nil})
 		})
@@ -111,7 +103,6 @@ func TestAppProjectEventHandlerFuncs(t *testing.T) {
 	t.Run("unexpected object types are ignored without panicking", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.appProjectEventHandlerFuncs()
-
 		assert.NotPanics(t, func() {
 			h.OnAdd("not-a-project", false)
 			h.OnUpdate("not-a-project", "not-a-project")
@@ -125,21 +116,17 @@ func TestApplicationEventHandlerFuncs(t *testing.T) {
 	t.Run("add of a processable application requeues a refresh", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		h.OnAdd(newFakeApp(), false)
-
 		assert.Contains(t, assertEnqueued(t, ctrl.appRefreshQueue), "my-app")
 	})
 
 	t.Run("update of a processable application requeues a refresh", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		old := newFakeApp()
 		updated := newFakeApp()
 		updated.ResourceVersion = "2"
 		h.OnUpdate(old, updated)
-
 		assert.Contains(t, assertEnqueued(t, ctrl.appRefreshQueue), "my-app")
 		assertNotEnqueued(t, ctrl.appOperationQueue)
 	})
@@ -174,26 +161,21 @@ func TestApplicationEventHandlerFuncs(t *testing.T) {
 	t.Run("delete of a processable application requeues a refresh", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		h.OnDelete(newFakeApp())
-
 		assert.Contains(t, assertEnqueued(t, ctrl.appRefreshQueue), "my-app")
 	})
 
 	t.Run("delete unwraps a tombstone wrapping an application", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		app := newFakeApp()
 		h.OnDelete(cache.DeletedFinalStateUnknown{Key: app.Namespace + "/" + app.Name, Obj: app})
-
 		assert.Contains(t, assertEnqueued(t, ctrl.appRefreshQueue), "my-app")
 	})
 
 	t.Run("delete of a tombstone wrapping nil is ignored without panicking", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		assert.NotPanics(t, func() {
 			h.OnDelete(cache.DeletedFinalStateUnknown{Key: "gone", Obj: nil})
 		})
@@ -203,15 +185,61 @@ func TestApplicationEventHandlerFuncs(t *testing.T) {
 	t.Run("an application in a disallowed namespace is ignored", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
 		h := ctrl.applicationEventHandlerFuncs()
-
 		app := newFakeApp()
 		app.Namespace = "some-other-namespace"
 		h.OnAdd(app, false)
 		h.OnUpdate(app, app)
 		h.OnDelete(app)
-
 		assertNotEnqueued(t, ctrl.appRefreshQueue)
 	})
+
+	makeNonOwningSharding := func(ctrl *ApplicationController) *sharding.ClusterSharding {
+		cs := sharding.NewClusterSharding(nil, 1, 2, "legacy").(*sharding.ClusterSharding)
+		cs.Shards["https://localhost:6443"] = 0
+		ctrl.clusterSharding = cs
+		return cs
+	}
+
+	t.Run("AddFunc: non-processable application still updates cluster sharding cache", func(t *testing.T) {
+		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
+		cs := makeNonOwningSharding(ctrl)
+		h := ctrl.applicationEventHandlerFuncs()
+		app := newFakeApp()
+		_, existsBefore := cs.Apps[app.Name]
+		assert.False(t, existsBefore, "app should not be in the sharding cache before OnAdd")
+		h.OnAdd(app, false)
+		assertNotEnqueued(t, ctrl.appRefreshQueue)
+		_, existsAfter := cs.Apps[app.Name]
+		assert.True(t, existsAfter, "app must be tracked in the sharding cache even when canProcessApp is false")
+	})
+
+	t.Run("UpdateFunc: non-processable application still updates cluster sharding cache", func(t *testing.T) {
+		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
+		cs := makeNonOwningSharding(ctrl)
+		h := ctrl.applicationEventHandlerFuncs()
+		old := newFakeApp()
+		updated := newFakeApp()
+		updated.ResourceVersion = "2"
+		h.OnUpdate(old, updated)
+		assertNotEnqueued(t, ctrl.appRefreshQueue)
+		tracked, exists := cs.Apps[updated.Name]
+		require.True(t, exists, "app must be tracked in the sharding cache even when canProcessApp is false")
+		assert.Equal(t, "2", tracked.ResourceVersion, "sharding cache must hold the latest application version")
+	})
+
+	t.Run("DeleteFunc: non-processable application still updates cluster sharding cache", func(t *testing.T) {
+		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
+		cs := makeNonOwningSharding(ctrl)
+		h := ctrl.applicationEventHandlerFuncs()
+
+		app := newFakeApp()
+		cs.AddApp(app)
+		h.OnDelete(app)
+		assertNotEnqueued(t, ctrl.appRefreshQueue)
+		_, existsAfter := cs.Apps[app.Name]
+		assert.False(t, existsAfter, "deleted app must be removed from the sharding cache even when canProcessApp is false")
+	})
+
 
 	t.Run("unexpected object types are ignored without panicking", func(t *testing.T) {
 		ctrl := newFakeController(t.Context(), &fakeData{}, nil)
