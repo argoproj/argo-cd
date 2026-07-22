8820e901885569704b0edf2af3040950b038f5cf
akhil nittala <nakhil@redhat.com>
2026-07-22 10:27:33 +0530

    Configure TLS Curve Preferences for PQC
    Signed-off-by: akhil nittala <nakhil@redhat.com>

--- util/tls/tls_test.go
+++ util/tls/tls_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 	"time"
 
+	logtest "github.com/sirupsen/logrus/hooks/test"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
