85ed5c106151a454922ef1998f747b2484a51109
Jan Kučera <jankucera254@gmail.com>
2026-07-27 17:01:42 +0200

    feat: Add deprecation warning when using --signature-keys
    Add deprecation warning when using --signature-keys flag in the
    argocd admin proj generate-spec, argocd proj create and argocd
    proj set.
    
    Signed-off-by: Jan Kučera <jankucera254@gmail.com>

--- cmd/util/project_test.go
+++ cmd/util/project_test.go
@@ -3,8 +3,13 @@ package util
 import (
 	"testing"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/argoproj/argo-cd/v3/util/test"
 
 	"github.com/argoproj/argo-cd/v3/pkg/apis/application/v1alpha1"
 )
@@ -47,4 +52,24 @@ func TestProjectOpts_GetDestinationServiceAccounts(t *testing.T) {
 			},
 		}, opts.GetDestinationServiceAccounts(),
 	)
+}
+
+// TODO: Remove deprecated https://github.com/argoproj/argo-cd/issues/27695
+func TestProjectOpts_SetProjSpecOptions_SignatureKeysWarns(t *testing.T) {
+	spec := &v1alpha1.AppProjectSpec{}
+	opts := ProjectOpts{}
+
+	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	flags.StringSliceVar(&opts.SignatureKeys, "signature-keys", []string{}, "")
+	require.NoError(t, flags.Set("signature-keys", "1234ABCD1234ABCD"))
+
+	hook := test.NewLogHook(log.WarnLevel)
+	log.AddHook(hook)
+	t.Cleanup(hook.CleanupHook)
+
+	visited := SetProjSpecOptions(flags, spec, &opts)
+
+	assert.Equal(t, 1, visited)
+	assert.Equal(t, []v1alpha1.SignatureKey{{KeyID: "1234ABCD1234ABCD"}}, spec.SignatureKeys) // nolint:staticcheck
+	assert.Equal(t, []string{"Warning: SignatureKeys are deprecated. Use SourceIntegrity instead."}, hook.GetEntries())
 }
