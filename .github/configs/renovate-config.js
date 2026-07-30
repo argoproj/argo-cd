9ef63bac97f1c921646517e6832b79b0d81d3de2
Eugene Doudine <eugene.doudine@octopus.com>
2026-07-30 17:46:52 +0300

    fix renovate updates
    Signed-off-by: Eugene Doudine <eugene.doudine@octopus.com>

--- .github/configs/renovate-config.js
+++ .github/configs/renovate-config.js
@@ -11,18 +11,19 @@ module.exports = {
     ],
     binarySource: 'install',
     extends: [
-        "github>argoproj/argo-cd//renovate-presets/commons.json5",
-        "github>argoproj/argo-cd//renovate-presets/custom-managers/shell.json5",
-        "github>argoproj/argo-cd//renovate-presets/custom-managers/yaml.json5",
-        "github>argoproj/argo-cd//renovate-presets/fix/disable-all-updates.json5",
-        "github>argoproj/argo-cd//renovate-presets/devtool.json5",
-        "github>argoproj/argo-cd//renovate-presets/production-binaries.json5",
-        "github>argoproj/argo-cd//renovate-presets/dex.json5",
-        "github>argoproj/argo-cd//renovate-presets/docs.json5",
+        "github>dudinea/argo-cd//renovate-presets/commons.json5",
+        "github>dudinea/argo-cd//renovate-presets/custom-managers/shell.json5",
+        "github>dudinea/argo-cd//renovate-presets/custom-managers/yaml.json5",
+        "github>dudinea/argo-cd//renovate-presets/custom-managers/renovate-action.json5",
+        "github>dudinea/argo-cd//renovate-presets/fix/disable-all-updates.json5",
+        "github>dudinea/argo-cd//renovate-presets/devtool.json5",
+        "github>dudinea/argo-cd//renovate-presets/production-binaries.json5",
+        "github>dudinea/argo-cd//renovate-presets/dex.json5",
+        "github>dudinea/argo-cd//renovate-presets/docs.json5",
         "group:aws-sdk-go-v2Monorepo",
-        "github>argoproj/argo-cd//renovate-presets/fix/ignore-paths.json5"
+        "github>dudinea/argo-cd//renovate-presets/fix/ignore-paths.json5"
     ],
     ignoreDeps: [
-        'github.com/argoproj/argo-cd/gitops-engine/v3'
+        'github.com/dudinea/argo-cd/gitops-engine/v3'
     ]
 }
