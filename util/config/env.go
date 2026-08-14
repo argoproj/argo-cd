a9c07748c3cdc14c17188b6ffef62435fccdaae0
k8s-1 <176627561+k8s-1@users.noreply.github.com>
2026-08-14 19:33:24 +0200

    fix: allow repeated --header flags in ARGOCD_OPTS
    Signed-off-by: k8s-1 <176627561+k8s-1@users.noreply.github.com>

--- util/config/env.go
+++ util/config/env.go
@@ -11,7 +11,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var flags map[string]string
+var flags map[string][]string
 
 func init() {
 	err := LoadFlags()
@@ -21,7 +21,7 @@ func init() {
 }
 
 func LoadFlags() error {
-	flags = make(map[string]string)
+	flags = make(map[string][]string)
 
 	opts, err := shellquote.Split(os.Getenv("ARGOCD_OPTS"))
 	if err != nil {
@@ -33,27 +33,27 @@ func LoadFlags() error {
 		switch {
 		case strings.HasPrefix(opt, "--"):
 			if key != "" {
-				flags[key] = "true"
+				flags[key] = append(flags[key], "true")
 			}
 			key = strings.TrimPrefix(opt, "--")
 		case key != "":
-			flags[key] = opt
+			flags[key] = append(flags[key], opt)
 			key = ""
 		default:
 			return errors.New("ARGOCD_OPTS invalid at '" + opt + "'")
 		}
 	}
 	if key != "" {
-		flags[key] = "true"
+		flags[key] = append(flags[key], "true")
 	}
 	// pkg shellquota doesn't recognize `=` so that the opts in format `foo=bar` could not work.
 	// issue ref: https://github.com/argoproj/argo-cd/issues/6822
 	for k, v := range flags {
-		if strings.Contains(k, "=") && v == "true" {
+		if strings.Contains(k, "=") && len(v) == 1 && v[0] == "true" {
 			kv := strings.SplitN(k, "=", 2)
 			actualKey, actualValue := kv[0], kv[1]
 			if _, ok := flags[actualKey]; !ok {
-				flags[actualKey] = actualValue
+				flags[actualKey] = []string{actualValue}
 			}
 		}
 	}
@@ -62,8 +62,8 @@ func LoadFlags() error {
 
 func GetFlag(key, fallback string) string {
 	val, ok := flags[key]
-	if ok {
-		return val
+	if ok && len(val) > 0 {
+		return val[len(val)-1]
 	}
 	return fallback
 }
@@ -74,11 +74,11 @@ func GetBoolFlag(key string) bool {
 
 func GetIntFlag(key string, fallback int) int {
 	val, ok := flags[key]
-	if !ok {
+	if !ok || len(val) == 0 {
 		return fallback
 	}
 
-	v, err := strconv.Atoi(val)
+	v, err := strconv.Atoi(val[len(val)-1])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -91,14 +91,18 @@ func GetStringSliceFlag(key string, fallback []string) []string {
 		return fallback
 	}
 
-	if val == "" {
-		return []string{}
+	result := []string{}
+	for _, v := range val {
+		if v == "" {
+			continue
+		}
+		stringReader := strings.NewReader(v)
+		csvReader := csv.NewReader(stringReader)
+		parsed, err := csvReader.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, parsed...)
 	}
-	stringReader := strings.NewReader(val)
-	csvReader := csv.NewReader(stringReader)
-	v, err := csvReader.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return v
+	return result
 }
--- util/config/env_test.go
+++ util/config/env_test.go
@@ -113,6 +113,21 @@ func TestStringSliceFlagAtEnd(t *testing.T) {
 	assert.Equal(t, "Strict-Transport-Security: max-age=31536000", strings[0])
 }
 
+func TestStringSliceFlagRepeated(t *testing.T) {
+	loadOpts(t, `--header "CF-Access-Client-Id: foo" --header "CF-Access-Client-Secret: bar"`)
+	strings := GetStringSliceFlag("header", []string{})
+
+	assert.Len(t, strings, 2)
+	assert.Equal(t, "CF-Access-Client-Id: foo", strings[0])
+	assert.Equal(t, "CF-Access-Client-Secret: bar", strings[1])
+}
+
+func TestFlagRepeatedLastWins(t *testing.T) {
+	loadOpts(t, "--foo bar --foo baz")
+
+	assert.Equal(t, "baz", GetFlag("foo", ""))
+}
+
 func TestFlagAtStart(t *testing.T) {
 	loadOpts(t, "--foo bar")
 
