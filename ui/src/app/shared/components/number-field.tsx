a13cc32a0b712465d0fa3e6ba6d263efe2637b39
SHINMH <sky95012@gmail.com>
2026-08-18 21:00:08 +0900

    fix(ui): normalize cleared number fields
    Signed-off-by: SHINMH <sky95012@gmail.com>

--- ui/src/app/shared/components/number-field.tsx
+++ ui/src/app/shared/components/number-field.tsx
@@ -8,5 +8,14 @@ export const NumberField = ReactForm.FormField((props: {fieldApi: ReactForm.Fiel
         ...rest
     } = props;
 
-    return <input {...rest} className={props.className} type='number' value={getValue()} onChange={el => setValue(parseInt(el.target.value, 10))} onBlur={onBlur} />;
+    return (
+        <input
+            {...rest}
+            className={props.className}
+            type='number'
+            value={getValue() ?? ''}
+            onChange={el => setValue(el.target.value === '' ? undefined : parseInt(el.target.value, 10))}
+            onBlur={onBlur}
+        />
+    );
 });
