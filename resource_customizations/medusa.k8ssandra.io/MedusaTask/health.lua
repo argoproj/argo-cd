f71e480e4265f4a4fa29967f758dc6053992ea46
Marco Groß (Marco Gross) <gross@b1-systems.de>
2026-08-31 14:49:46 +0200

    Fix tests in regards to observedGeneration
    Signed-off-by: Marco Groß (Marco Gross) <gross@b1-systems.de>

--- resource_customizations/medusa.k8ssandra.io/MedusaTask/health.lua
+++ resource_customizations/medusa.k8ssandra.io/MedusaTask/health.lua
@@ -5,16 +5,25 @@ local hs = {}
 hs.status = "Unknown"
 
 if obj.status == nil then
+	hs.message = "No status object"
+	return hs
+end
+
+if obj.status.observedGeneration == nil then
+	hs.message = "No observedGeneration"
 	return hs
 end
 
 -- We check if we are checking the correct version of obj.status
-if obj.status.observedGeneration ~= nil and obj.status.observedGeneration ~= obj.metadata.generation then
+if obj.status.observedGeneration ~= obj.metadata.generation then
+	hs.message = "observedGeneration is not equal to generation"
+	hs.status = "Progressing"
 	return hs
 end
 
 if obj.status.finished == nil and obj.status.failed == nil then
 	hs.status = "Progressing"
+	return hs
 end
 
 if obj.status.finished ~= nil then
