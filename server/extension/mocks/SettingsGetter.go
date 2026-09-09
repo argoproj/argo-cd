dbf4757019be45525dc9af18d8c73a66b9524158
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2026-09-09 10:00:01 +0300

    updated generated files
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- server/extension/mocks/SettingsGetter.go
+++ server/extension/mocks/SettingsGetter.go
@@ -15,10 +15,19 @@ func NewSettingsGetter(t interface {
 	mock.TestingT
 	Cleanup(func())
 }) *SettingsGetter {
+	if helper, ok := t.(interface{ Helper() }); ok {
+		helper.Helper()
+	}
+
 	mock := &SettingsGetter{}
 	mock.Mock.Test(t)
 
-	t.Cleanup(func() { mock.AssertExpectations(t) })
+	t.Cleanup(func() {
+		if helper, ok := t.(interface{ Helper() }); ok {
+			helper.Helper()
+		}
+		mock.AssertExpectations(t)
+	})
 
 	return mock
 }
