90cfc884c9a60865cd504f268da874a95ccfdfbd
Jan Kučera <jankucera254@gmail.com>
2026-09-10 13:54:08 +0200

    fix(test): fix SSHCreds Environ temp files cleanup test
    Signed-off-by: Jan Kučera <jankucera254@gmail.com>

--- util/git/creds_test.go
+++ util/git/creds_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 	"slices"
 	"strings"
@@ -330,6 +331,18 @@ func Test_SSHCreds_Environ_TempFileCleanupOnInvalidProxyURL(t *testing.T) {
 	if argoioTempDir == "" {
 		argoioTempDir = os.TempDir()
 	}
+
+	// This argoioTempDir is shared between all processes, making it unreliable
+	// to count the files inside before and after the invocation of creds.Environ().
+	// Use unique temp directory inside of it to isolate the validation logic.
+	argoioTempDir = filepath.Join(argoioTempDir, "argo-cd-ssh-proxy-test-tmp-dir")
+	require.NoError(t, os.MkdirAll(argoioTempDir, 0o755))
+	t.Cleanup(func() { os.RemoveAll(argoioTempDir) })
+
+	origArgoioTempDir := argoio.TempDir
+	t.Cleanup(func() { argoio.TempDir = origArgoioTempDir })
+
+	argoio.TempDir = argoioTempDir
 
 	// countDev returns the number of files in the temporary directory
 	countFilesInDevShm := func() int {
