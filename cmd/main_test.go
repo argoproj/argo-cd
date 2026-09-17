b43b2d185dccf223e0fac8381f43d7644d31211e
Jan Kučera <jankucera254@gmail.com>
2026-09-17 12:09:40 +0200

    test(main): add test for main error handling with isArgocdCLI == false
    Signed-off-by: Jan Kučera <jankucera254@gmail.com>

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -171,6 +171,72 @@ func TestExitErrorHandlingWithPlugin(t *testing.T) {
 	}
 }
 
+func TestExitErrorHandlingNotIsArgocdCLI(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name             string
+		args             []string
+		cmdError         error
+		expectedExitCode int
+		expectedOutput   string
+	}{
+		{
+			name:             "no error",
+			cmdError:         nil,
+			expectedExitCode: 0,
+			expectedOutput:   "",
+		},
+		{
+			name:             "generic error",
+			cmdError:         errors.New("test error"),
+			expectedExitCode: 1,
+			expectedOutput:   "Error: test error\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			if os.Getenv("BE_CRASHER_NOT_IS_ARG_CLI") == "1" {
+				dummyCmd := &cobra.Command{
+					RunE: func(_ *cobra.Command, _ []string) error {
+						return test.cmdError
+					},
+				}
+
+				selectCommand = func(_ string) (*cobra.Command, bool) {
+					return dummyCmd, false
+				}
+
+				main()
+				os.Exit(0) // when here, no error - exit OK
+			}
+
+			cmd := exec.CommandContext(t.Context(), os.Args[0], "-test.run=^"+t.Name()+"$")
+			cmd.Env = append(os.Environ(), "BE_CRASHER_NOT_IS_ARG_CLI=1")
+			var stdout bytes.Buffer
+			cmd.Stdout = &stdout
+			err := cmd.Run()
+
+			if test.expectedExitCode == 0 {
+				require.NoError(t, err, "expected command to exit successfully")
+				assert.Equal(t, test.expectedOutput, stdout.String())
+				return
+			}
+
+			if e, ok := errors.AsType[*exec.ExitError](err); ok && !e.Success() {
+				assert.Equal(t, test.expectedExitCode, e.ExitCode(), "expected exit code to be %d but got %d", test.expectedExitCode, e.ExitCode())
+				assert.Equal(t, test.expectedOutput, stdout.String())
+				return
+			}
+
+			t.Fatalf("process ran with err %v, want exit status %d", err, test.expectedExitCode)
+		})
+	}
+}
+
 func getTestPluginsPath(t *testing.T) string {
 	t.Helper()
 	wd, err := os.Getwd()
